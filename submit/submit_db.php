f6d7c4101c7d58a779b19c42cfd509d1bef17b5a
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-05-28 21:50:33 +0000

    perl mkstemps gebruiken voor duidelijkere filenames. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@180 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submit_db.php
+++ submit/submit_db.php
@@ -18,28 +18,29 @@
 	$lang = strtolower(@$argv[4]);
 	$file = @$argv[5];
 
+
 	// Check 0: called correctly?
 	if(!$team)	error("No value for team.");
 	if(!$ip)	error("No value for ip.");
 	if(!$prob)	error("No value for prob.");
 	if(!$lang)	error("No value for lang.");
 	if(!$file)	error("No value for file.");
+
 	
 	// Check 1: valid parameters?
-	if(!$langext = $DB->q('MAYBEVALUE SELECT extension FROM language WHERE langid = %s',
-		$lang) ){
+	if(!$langext = $DB->q('MAYBEVALUE SELECT extension FROM language
+		                   WHERE langid = %s', $lang) ) {
 		error("Language '$lang' not found in database");
 	}
 	if(!$row = $DB->q('MAYBETUPLE SELECT * FROM team WHERE login = %s',
-		$team) ){
+		              $team) ) {
 		error("Team '$team' not found in database");
 	}
 	if($row['ipadres'] != $ip) {
 		error("Team '$team' not registered at this IP address.");
 	}
 	if(!$DB->q('MAYBETUPLE SELECT * FROM problem WHERE probid = %s
-		AND allow_submit = "1"',
-		$prob) ){
+		        AND allow_submit = "1"', $prob) ) {
 		error("Problem '$prob' not found in database or not submittable.");
 	}
 	if(!is_readable(INCOMINGDIR."/$file")) {
@@ -47,19 +48,24 @@
 	}
 	logmsg ("submit_db: input verified");
 
-	// exec tempfile to create a temporary file in the submitdir
-	// we take the basename because we reprepend SUBMITDIR in get_submission.
-	$dummy = null;
-	$tofile = basename(exec("tempfile -d SUBMITDIR -p subm_ -s _".$team."_".$prob.".$langext", $dummy, $retval));
-	if ( $retval != 0 ) error("Could not create tempfile.");
 
-	copy(INCOMINGDIR."/$file", SUBMITDIR."/$tofile");
-	
+	// Copy the submission to a (uniquely generated) file in SUBMITDIR
+	$dummy = null;
+	$tofile = exec(SYSTEM_ROOT."/runprogs/mkstemps.pl ".
+	               SUBMITDIR."/$team.$prob.XXXX .$langext", $dummy, $retval);
+	if ( $retval != 0 ) error("Could not create tempfile.");
+	$tofile = basename($tofile);
+
+	if ( ! copy(INCOMINGDIR."/$file", SUBMITDIR."/$tofile") ) {
+		error("Could not copy file to ".SUBMITDIR);
+	}
+
+	// Insert submission into the database	
 	$id = $DB->q('RETURNID INSERT INTO submission 
 		(team,probid,langid,submittime,source)
 		VALUES (%s, %s, %s, NOW(), %s)',
 		$team, $prob, $lang, $tofile);
 
-	logmsg ("submit_db: submitted $team/$prob/$lang, filename: $tofile, id: $id");
+	logmsg ("submit_db: submitted $team/$prob/$lang, file $tofile, id $id");
 
 	exit;
