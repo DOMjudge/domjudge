644d1e92517fa2c636603d031de6d8fd817dd538
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-05-30 12:09:00 +0000

    Fix veranderde paths in scripts.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@193 caf94f87-7bd6-0310-88dc-dee8202e2269

--- php/init.php
+++ php/init.php
@@ -1,7 +1,8 @@
 <?php
-// $Id: $
+// $Id$
 	
 	define('PHP_LIB_PATH', SYSTEM_ROOT.'/lib');
 	
 	require(PHP_LIB_PATH . '/use_db_jury.php');
 	require(PHP_LIB_PATH . '/lib.error.php');
+	require(PHP_LIB_PATH . '/lib.misc.php');
