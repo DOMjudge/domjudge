ef6ea14ae96d1ddb438cc2c463b34c1efeeb6f99
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-06-03 17:36:00 +0000

    copy ipv scp voor DS practicum. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@221 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submit_copy.sh
+++ submit/submit_copy.sh
@@ -10,10 +10,10 @@ team=$1
 fromfile=$2
 tofile=$3
 
-output=`scp -Bq "$team@localhost:$fromfile" "$tofile" 2>&1`
-[ $? -eq 0 -a ${#output} -eq 0 ] || exit 1
+#output=`scp -Bq "$team@localhost:$fromfile" "$tofile" 2>&1`
+#[ $? -eq 0 -a ${#output} -eq 0 ] || exit 1
 
-#cp "$fromfile" "$tofile"
+cp "$fromfile" "$tofile" || exit 1
 
 exit 0
 
