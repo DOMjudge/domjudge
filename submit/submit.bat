9272f0dc33129ff4a042b1d2a5c96606b25d5de4
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-06-09 11:54:36 +0000

    bat file voor ds-pract (kink)
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@269 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submit.bat
+++ submit/submit.bat
@@ -1,5 +1,5 @@
 @echo off
-set SUBMITSERVER=___SUBMITSERVER___
-set CONTESTANT=___ACCOUNT___
-set PASSWORD=___PASSWD___
-s:\perl\bin\perl.exe h:\bin\submit %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+rem Batch script om het submit-script onder Windows toegankelijk te maken.
+
+s:\perl\bin\perl.exe t:\nkp\submit.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
