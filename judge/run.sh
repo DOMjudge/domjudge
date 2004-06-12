ee75b2daca85ef928629367b492b52903e043757
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-06-12 12:29:24 +0000

    - filelimit en proclimit via config - rechtstreeks programma uitvoeren, want is toch een taal-onafhankelijke   statisch gelinkte executable Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@280 caf94f87-7bd6-0310-88dc-dee8202e2269

--- judge/run.sh
+++ judge/run.sh
@@ -4,7 +4,8 @@
 # Run wrapper-script for 'test_solution.sh'.
 # See that script for more info.
 
-# Usage: $0 <lang> <prog> <testin> <output> <error> <exitfile> <memlim>
+# Usage: $0 <lang> <prog> <testin> <output> <error> <exitfile>
+#           <memlimit> <filelimit> <proclimit>
 #
 # <lang>      Language of the compiled program.
 # <prog>      Executable of the program to be run.
@@ -13,27 +14,28 @@
 # <error>     File where to write error messages.
 # <exitfile>  File where to write solution exitcode.
 # <memlimit>  Maximum total memory usage in KB.
+# <filelimit> Maximum filesize in KB.
+# <proclimit> Maximum total no. processes (including this shell)
 
-LANG="$1";     shift
-PROG="$1";     shift
-TESTIN="$1";   shift
-OUTPUT="$1";   shift
-ERROR="$1";    shift
-EXITFILE="$1"; shift
-MEMLIMIT="$1"; shift
-
-# Create the program output file, so that it always exists
-echo -n >$OUTPUT
-echo -n >$EXITFILE
+LANG="$1";      shift
+PROG="$1";      shift
+TESTIN="$1";    shift
+OUTPUT="$1";    shift
+ERROR="$1";     shift
+EXITFILE="$1";  shift
+MEMLIMIT="$1";  shift
+FILELIMIT="$1"; shift
+PROCLIMIT="$1"; shift
 
 # Set some resource limits (for restricting the running solution)
-ulimit -c 0         # max. size of coredump files in KB
-ulimit -f 65536     # max. size of created files in KB
-ulimit -v $MEMLIMIT # max. total memory in KB
-ulimit -u 8         # max. processes
+ulimit -c 0           # max. size of coredump files in KB
+ulimit -v $MEMLIMIT   # max. total memory usage in KB
+ulimit -f $FILELIMIT  # max. size of created files in KB
+ulimit -u $PROCLIMIT  # max. no. processes
 
-# `source' the specific language run-script, which executes the solution
-source /run_$LANG.sh
+# Run the program while redirecting input, output and stderr
+$PROG <$TESTIN >$OUTPUT 2>$ERROR
+exitcode=$?
 
 echo -n "$exitcode" >$EXITFILE
 
