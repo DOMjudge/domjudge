ad5d8568d07756ddec7d57da2f9dedf90d5d9041
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-06-15 08:52:51 +0000

    submit config toegevoegd voor C-versie van submit(daemon). Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@303 caf94f87-7bd6-0310-88dc-dee8202e2269

--- etc/config.h
+++ etc/config.h
@@ -5,11 +5,29 @@
 #ifndef _LOCAL_CONFIG_
 #define _LOCAL_CONFIG_
 
+/* Root-paths for different parts of the system. */
+#define SYSTEM_ROOT HOME"/systeem/svn/jury"
+#define OUTPUT_ROOT HOME"/systeem/systest"
+#define INPUT_ROOT  HOME"/opgaven"
+
+/* Paths within OUTPUT_ROOT */
+#define INCOMINGDIR OUTPUT_ROOT"/incoming"
+#define SUBMITDIR   OUTPUT_ROOT"/sources"
+#define JUDGEDIR    OUTPUT_ROOT"/judging"
+#define LOGDIR      OUTPUT_ROOT"/log"
+
 /* Absolute path prefix within which chroot is allowed. */
-#define ROOT_PREFIX "/home/cies/nkp0405/"
+#define CHROOT_PREFIX JUDGEDIR
 
 /* User ID's under which programs are allowed to run.
    End list with -1, UID 0 (root) is ignored. */
 const int valid_uid[5] = {5051,65534,9013,-1};
+
+/* TCP port on which submitdaemon listens. */
+#define SUBMITPORT   9147
+#define SUBMITSERVER "square.a-eskwadraat.nl"
+
+/* Directory where submit-client puts files for sending (relative to $HOME). */
+#define USERSUBMITDIR ".submit"
 
 #endif /* _LOCAL_CONFIG_ */
