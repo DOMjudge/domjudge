3fb71461882fb7b54a4dbcd375af3e9d598c8feb
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-06-15 16:00:08 +0000

    - non-fatal warning toegevoegd - wat documentatie in commentaar Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@308 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/lib.error.h
+++ lib/lib.error.h
@@ -15,6 +15,31 @@
 
 const int exit_failure = -1;
 
+/* Argument-list and va_list versions of logging function:
+ * Logs a message to stderr and/or logfile, including date and program name,
+ * depending on the loglevel treshold values.
+ *
+ * Arguments:
+ * int loglevel    syslog loglevel of this log-message
+ * char *mesg      message, may include printf output format characters '%'
+ * ... or va_list  optional arguments for format characters
+ */
+void logmsg (int, char *, ...);
+void vlogmsg(int, char *, va_list);
+
+/* Error and warning functions:
+ * Logs an error message including error string from 'errno' and error
+ * then exits with exit_failure, warning returns.
+ *
+ * Arguments:
+ * int errnum    'errno' value to use for error string output, set 0 to skip
+ * char *mesg    message, may include printf output format characters '%'
+ * ...           optional arguments for format characters
+ */
+void error  (int, char *, ...);
+void warning(int, char *, ...);
+
+
 void vlogmsg(int msglevel, char *mesg, va_list ap)
 {
     time_t     currtime;
@@ -51,6 +76,38 @@ void logmsg(int msglevel, char *mesg, ...)
 }
 
 void error(int errnum, char *mesg, ...)
+{
+	int msglen = (mesg==NULL ? 0 : strlen(mesg));
+	char *buffer, *endptr;
+	va_list ap;
+	
+	va_start(ap, mesg);
+	
+	buffer = (char *) malloc(mesglen+256);
+
+	sprintf(buffer,"error");
+	endptr = strchr(buffer,0);
+	
+	if ( mesg!=NULL ) {
+		snprintf(endptr, sizeof(buffer)-strlen(buffer), ": %s", mesg);
+		endptr = strchr(endptr,0);
+	}		
+	if ( errnum!=0 ) {
+		snprintf(endptr, sizeof(buffer)-strlen(buffer), ": %s",strerror(errnum));
+		endptr = strchr(endptr,0);
+	}
+	if ( mesg==NULL && errnum==0 ) {
+		sprintf(endptr,": unknown error");
+	}
+
+	vlogmsg(LOG_ERR, buffer, ap);
+
+	free(buffer);
+	va_end(ap);
+	exit(exit_failure);
+}
+
+void warning(int errnum, char *mesg, ...)
 {
 	int msglen = (mesg==NULL ? 0 : strlen(mesg));
 	char *buffer, *endptr;
