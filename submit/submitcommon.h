57f6997b50704c28a51d89fdda43543a85d6e699
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-06-17 10:30:58 +0000

    - sendit kan nu met printf-alike message en argumenten aangeroepen worden - receive meer checks op fouten ("error: " in message) Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@317 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submitcommon.h
+++ submit/submitcommon.h
@@ -10,6 +10,8 @@
 
 #define SOCKETBUFFERSIZE 256
 
+#define ERRSTR "error: "
+
 /* Buffer where the last received message is stored */
 char lastmesg[SOCKETBUFFERSIZE];
 
@@ -17,9 +19,10 @@ char lastmesg[SOCKETBUFFERSIZE];
  *
  * Arguments:
  * int fd      filedescriptor of the socket
- * char *mesg  message to write  
+ * char *mesg  message to write, may include printf format characters '%'
+ * ...         optional arguments for format characters
  */
-void sendit(int, char*);
+void sendit(int, char *, ...);
 
 /* Receive a message over a socket and log it.
  * Message is put in 'lastmesg' and number of characters read is returned.
@@ -29,14 +32,17 @@ void sendit(int, char*);
  */
 int  receive(int);
 
-void sendit(int fd, char *mesg)
+void sendit(int fd, char *mesg, ...)
 {
 	char buffer[SOCKETBUFFERSIZE];
+	va_list ap;
 
-	strncpy(buffer,mesg,SOCKETBUFFERSIZE-2);
-
+	va_start(ap, mesg);
+	vsnprintf(buffer, SOCKETBUFFERSIZE-2, mesg, ap);
+	va_end(ap);
+	
 	logmsg(LOG_DEBUG, "send: %s", buffer);
-	strcat(buffer,"\015\012");
+	strcat(buffer, "\015\012");
 	
 	if ( write(fd, buffer, strlen(buffer)) == -1 ) {
 		error(errno,"writing to socket");
@@ -48,14 +54,33 @@ void sendit(int fd, char *mesg)
  */
 int receive(int fd)
 {
+	char buffer[SOCKETBUFFERSIZE];
 	int nread;
+	int i;
 	
-	if ( (nread = read(fd, lastmesg, SOCKETBUFFERSIZE)) == -1 ) {
+	if ( (nread = read(fd, buffer, SOCKETBUFFERSIZE)) == -1 ) {
 		error(errno,"reading from socket");
 	}
 
+	strcpy(lastmsg,buffer);
 	while ( nread>0 && iscntrl(lastmesg[nread-1]) ) lastmesg[--nread] = 0;
 	logmsg(LOG_DEBUG, "recv: %s", lastmesg);
+
+	if ( lastmesg[0]!='+' ) {
+		close(fd);
+		i = 0;
+		if ( lastmesg[i]=='-' ) i++;
+		if ( strncmp(&lastmesg[i],ERRSTR,strlen(ERRSTR))==0 ) {
+			i += strlen(ERRSTR);
+		}
+		error(0,&lastmesg[i]);
+	}
+
+	/* Remove the first character from the message (if '+' or '-') */
+	if ( lastmesg[0]=='+' || lastmesg[0]=='-' ) {
+		for(i=0; i<nread; i++) lastmesg[i] = lastmesg[i+1];
+		nread--;
+	}
 
 	return nread;
 }
