02ab1bf35976173be38b92f9f3887a14f2dedb84
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-06-27 12:48:16 +0000

    Open STDERR alleen als dit geen webinterface is; niet alleen performance maar dit is wat de crashes lijkt te veroorzaken. Thijs.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@343 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/lib.error.php
+++ lib/lib.error.php
@@ -5,12 +5,16 @@
  *
  * $Id$
  */
-
 if ( ! defined('SCRIPT_ID') ) {
 	define('SCRIPT_ID', basename($PHP_SELF, '.php'));
 }
 
-define('STDERR', fopen('php://stderr', 'w'));
+// is this the webinterface or commandline?
+$is_web = isset($_SERVER['REMOTE_ADDR']);
+
+if(!$is_web) {
+	define('STDERR', fopen('php://stderr', 'w'));
+}
 
 if ( defined('LOGFILE') ) {
 	define('STDLOG', fopen(LOGFILE, 'a'));
@@ -21,11 +25,11 @@ $verbose  = LOG_NOTICE;
 $loglevel = LOG_DEBUG;
 
 function logmsg($msglevel, $string) {
-	global $verbose, $loglevel;
+	global $verbose, $loglevel,$is_web;
 	$msg = "[" . date('M d H:i:s') . "] " . SCRIPT_ID . ": ". $string . "\n";
 	if ( $msglevel <= $verbose  ) {
 		// if this is the webinterface, print it to stdout, else to stderr
-		if ( isset ( $_SERVER['REMOTE_ADDR'] ) ) {
+		if ( $is_web ) {
 			echo "<fieldset class=\"error\"><legend>Error</legend>\n" . htmlspecialchars($msg) . "</fieldset>\n";
 		} else {
 			fwrite(STDERR, $msg); fflush(STDERR);
