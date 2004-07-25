78e774e38ad0293919d744e4b9a752b04edb7a04
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-07-25 08:34:07 +0000

    perl mkstemps vervangen door c-versie. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@390 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submit_db.php
+++ submit/submit_db.php
@@ -76,8 +76,8 @@
 
 	// Copy the submission to a (uniquely generated) file in SUBMITDIR
 	$dummy = null;
-	$tofile = exec(SYSTEM_ROOT."/bin/mkstemps.pl ".
-	               SUBMITDIR."/$team.$prob.XXXX .$langext", $dummy, $retval);
+	$tofile = exec(SYSTEM_ROOT."/bin/mkstemps ".
+	               SUBMITDIR."/$team.$prob.XXXXXX.$langext", $dummy, $retval);
 	if ( $retval != 0 ) error("Could not create tempfile.");
 	$tofile = basename($tofile);
 
