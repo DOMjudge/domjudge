a87977f9592a601f0dfcb8ae935dc8094ed3b971
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-08-21 14:33:56 +0000

    optie -lm weg, omdat mathlib automatisch meegelinked wordt. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@405 caf94f87-7bd6-0310-88dc-dee8202e2269

--- judge/compile_cpp.sh
+++ judge/compile_cpp.sh
@@ -8,7 +8,6 @@ DEST="$2"
 
 # -Wall:	Report all warnings
 # -O2:		Level 2 optimizations (default for speed)
-# -lm:		Link with math-library
 # -static:	Static link with all libraries
-g++ -Wall -O2 -lm -static -o $DEST $SOURCE
+g++ -Wall -O2 -static -o $DEST $SOURCE
 exit $?
