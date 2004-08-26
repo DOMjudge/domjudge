258a075eb3a988cc8326cb29c80d86a93d893ec0
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-08-26 19:15:15 +0000

    * Eerste begin clarification requests * Clock ook in teaminterface
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@413 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ www/team/clarification.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Display a clarification response
+ *
+ * $Id$
+ */
+
+require('init.php');
+$title = 'Clarification Response';
+include('../header.php');
+
+$respid = (int)$_GET['id'];
+
+// select also on teamid so we can only select our own submissions
+$row = $DB->q('MAYBETUPLE SELECT *
+	FROM clar_response
+	WHERE cid = %i AND (rcpt = %s OR rcpt IS NULL) AND respid = %i',
+	getCurCont(), $login, $respid);
+
+if(!$row) {
+	echo "Clarification Response not found for this team.\n";
+	include('../footer.php');
+	exit;
+}
+?>
+<h1>Clarification Response</h1>
+
+<p>
+<table>
+<tr><td><b>To:</b></td><td><?=htmlentities(empty($row['rcpt']) ? 'All' : $row['rcpt'])?></td></tr>
+<tr><td><b>Time:</b></td><td><?=printtime($row['submittime'])?></td></tr>
+<tr><td valign="top"><b>Response:</b></td><td class="output_text"><?=$row['body']?></td></tr>
+</table>
+</p>
+
+
+<?php
+include('../footer.php');
--- www/team/index.php
+++ www/team/index.php
@@ -7,9 +7,33 @@ require('init.php');
 $title = 'Team overview';
 require('../header.php');
 
+putClock();
+
 echo "<h1>Teampage ".htmlentities($name)."</h1>\n\n";
 
 getSubmissions('team', $login, FALSE);
+
+?>
+<p><table>
+<tr><th colspan="2">Clarifications</th></tr>
+<?php
+$data = $DB->q('TABLE SELECT * FROM clar_response WHERE cid = %i 
+	AND (rcpt = %s OR rcpt IS NULL) ORDER BY submittime DESC',
+	getCurCont(), $login );
+
+foreach($data as $row) {
+	echo "<tr><td>".
+		printtime($row['submittime'])."</td><td>".
+		"<a href=\"clarification.php?id=".
+		urlencode($row['respid'])."\">".
+		htmlentities(substr($row['body'],0,50))."...</a></td></tr>\n";
+
+}
+
+?>
+</table>
+</p>
+<?php
 
 echo "<p><a href=\"../public/\">Scoreboard</a></p>\n\n";
 
