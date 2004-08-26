a3ed76e1e353aa3269d8773659d3c7b68ede8891
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-08-26 10:32:55 +0000

    clarifications in het menutje (thijs)
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@408 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/menu.php
+++ www/jury/menu.php
@@ -4,6 +4,7 @@
 <a href="judgers.php">judgers</a>
 <a href="submissions.php">submissions</a>
 <a href="teams.php">teams</a>
+<a href="clarifications.php">clarifications</a>
 </div>
 
 <div id="clock"><?php echo strftime('%a %e %b %Y %T'); ?></div>
