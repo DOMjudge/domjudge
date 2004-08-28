c9e172b53f2d397a1ed0bb234c3fc96edcd3f67b
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-08-28 19:42:19 +0000

    compare functie versimpeld en verbeterd. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@430 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/public/index.php
+++ www/public/index.php
@@ -16,6 +16,7 @@ $cid = $contdata['cid'];
 
 <h1>Scoreboard <?=htmlentities($contdata['contestname'])?></h1>
 
+
 <table border="1">
 <?php
 $teams = $DB->q('TABLE SELECT login,name,category
@@ -24,7 +25,7 @@ $probs = $DB->q('TABLE SELECT probid,name
 	FROM problem WHERE allow_submit = 1 ORDER BY probid');
 
 echo "<tr><th>TEAM</th>";
-echo "<th>#corr</th><th>time</th>\n";
+echo "<th>#correct</th><th>time</th>\n";
 foreach($probs as $pr) {
 	echo "<th title=\"".htmlentities($pr['name'])."\">".htmlentities($pr['probid'])."</th>";
 }
@@ -44,9 +45,9 @@ foreach($teams as $team) {
 	foreach($probs as $pr) {
 
 		$result = $DB->q('SELECT result, 
-				(UNIX_TIMESTAMP(submittime)-UNIX_TIMESTAMP(c.starttime))/60 as timediff
+			(UNIX_TIMESTAMP(submittime)-UNIX_TIMESTAMP(c.starttime))/60 as timediff
 			FROM judging LEFT JOIN submission s USING(submitid)
-				LEFT OUTER JOIN contest c ON(c.cid=s.cid)
+			LEFT OUTER JOIN contest c ON(c.cid=s.cid)
 			WHERE team = %s AND probid = %s AND valid = 1 AND result IS NOT NULL AND s.cid = %i
 			ORDER BY submittime',
 			$team['login'], $pr['probid'], $cid);
@@ -90,9 +91,11 @@ foreach($teams as $team) {
 
 	}
 
-	// SCORES contains a grand total for each time; this is our sorting criterion
+	// SCORES contains the total number correct and time for each team.
+	// This is our sorting criterion and alpabetically on teamname otherwise. 
 	$SCORES[$team['login']]['num_correct'] = $grand_total_correct;
 	$SCORES[$team['login']]['total_time'] = $grand_total_time;
+	$SCORES[$team['login']]['teamname'] = $TEAMNAMES[$team['login']];
 
 }
 
@@ -138,18 +141,15 @@ echo "<div id=\"lastmod\">Last Update: " . date('j M Y H:i') . "</div>\n\n";
 require('../footer.php');
 
 // comparison function
-function cmp ($b, $a) {
-	if($a['num_correct'] > $b['num_correct']) {
-		return 1;
-	} elseif($a['num_correct'] == $b['num_correct']) {
-		if($a['total_time'] > $b['total_time']) {
-			return -1;
-		} elseif ($a['total_time'] == $b['total_time']) {
-			return 0;
-		} else {
-			return 1;
-		}
-	} else {
-		return -1;
+function cmp ($a, $b) {
+	if ( $a['num_correct'] != $b['num_correct'] ) {
+		return $a['num_correct'] > $b['num_correct'] ? -1 : 1;
 	}
+	if ( $a['total_time'] != $b['total_time'] ) {
+		return $a['total_time'] < $b['total_time'] ? -1 : 1;
+	}
+	if ( $a['teamname'] != $b['teamname'] ) {
+		return $a['teamname'] < $b['teamname'] ? -1 : 1;
+	}
+	return 0;
 }
