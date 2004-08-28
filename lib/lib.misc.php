888c012152b8ce5dbe74a724e9a56f47e3894c11
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-08-28 19:40:35 +0000

    - functienaam veranderd ivm consistente naamgeving - getCurContest hierheen verplaast als globaal beschikbare functie Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@429 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/lib.misc.php
+++ lib/lib.misc.php
@@ -2,14 +2,26 @@
 
 /* $Id$ */
 
-// helperfunction to read 50,000 bytes from a file
-function get_content($filename) {
+/**
+ * helperfunction to read 50,000 bytes from a file
+ */
+function getFileContents($filename) {
 
 	if ( ! file_exists($filename) ) return '';
 	$fh = fopen($filename,'r');
-	if ( ! $fh ) {
-		error("Could not open $filename for reading");
-	}
+	if ( ! $fh ) error("Could not open $filename for reading");
 	return fread($fh, 50000);
+}
+
+/**
+ * Will return either the current contest, or else the upcoming one
+ */
+function getCurContest() {
+	static $curcontest;
+	if(isset($curcontest)) return $curcontest;
+
+	global $DB;
+	return $curcontest = $DB->q('MAYBEVALUE SELECT cid FROM contest
+	                             ORDER BY starttime DESC LIMIT 1');
 }
 
