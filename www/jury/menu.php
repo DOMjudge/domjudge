8db3ac5a5c5c3397a31686f1d7823a625007d409
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-09-15 20:05:33 +0000

    problems ook in menu. kink
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@469 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/menu.php
+++ www/jury/menu.php
@@ -1,6 +1,7 @@
 
 <div id="menutop">
 <a href="index.php">home</a>
+<a href="problems.php">problems</a>
 <a href="judgers.php">judgers</a>
 <a href="submissions.php">submissions</a>
 <a href="teams.php">teams</a>
