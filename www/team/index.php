d3d3d0472c2d2cb49a673e5187050a0fd314a0ae
Peter van de Werken <pwerken@a-eskwadraat.nl>
2004-09-15 19:56:40 +0000

    clean up, naar eigen paginas
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@463 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/team/index.php
+++ www/team/index.php
@@ -7,38 +7,12 @@ require('init.php');
 $title = 'Team overview';
 require('../header.php');
 
+include('menu.php');
+
 putClock();
 
 echo "<h1>Teampage ".htmlentities($name)."</h1>\n\n";
 
 getSubmissions('team', $login, FALSE);
-
-?>
-<p><table>
-<tr><th colspan="2">Clarifications</th></tr>
-<?php
-$data = $DB->q('TABLE SELECT * FROM clar_response WHERE cid = %i 
-	AND (rcpt = %s OR rcpt IS NULL) ORDER BY submittime DESC',
-	getCurContest(), $login );
-
-foreach($data as $row) {
-	echo "<tr><td>".
-		printtime($row['submittime'])."</td><td>".
-		"<a href=\"clarification.php?id=".
-		urlencode($row['respid'])."\">".
-		htmlspecialchars(str_cut($row['body'],50)).
-		"</a></td></tr>\n";
-
-}
-
-?>
-</table>
-</p>
-
-<p><a href="clar_request.php">Request Clarification</a></p>
-
-<p><a href="../public/">Scoreboard</a></p>
-
-<?php
 
 require('../footer.php');
