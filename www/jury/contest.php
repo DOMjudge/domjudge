38f57e771b42d1009412b8d64729bfd233215926
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-09-15 22:40:29 +0000

    Fix contest pagina.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@484 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/contest.php
+++ www/jury/contest.php
@@ -12,45 +12,26 @@ require('menu.php');
 
 echo "<h1>Contests</h1>\n\n";
 
+$curcont = getCurContest();
 $res = $DB->q('TABLE SELECT *,
-	UNIX_TIMESTAMP(starttime) as start_u, UNIX_TIMESTAMP(endtime) as end_u
-	FROM contest ORDER BY starttime DESC');
+               UNIX_TIMESTAMP(starttime) as start_u,
+               UNIX_TIMESTAMP(endtime) as end_u
+               FROM contest ORDER BY starttime DESC');
 
-if(count($res) == 0) {
-	echo "<p><em>No contests found</em></p>";
-	include('footer.php');
-	exit;
-}
-
-$mostrecent = array_shift($res);
-
-if($mostrecent['start_u'] < time()) { 
-	if($mostrecent['end_u'] > time()) {
-		echo "<h3>Current Contest</h3>\n";
-	} else {
-		echo "<h3>Most Recent Contest</h3>\n";
-	}
-} else {
-	echo "<h3>Upcoming Contest</h3>\n";
-}
-echo "<table>\n".
-	"<tr><td>Contest:</td><td>".htmlentities($mostrecent['contestname']).
-		' [c'.(int)$mostrecent['cid']."]</td></tr>\n".
-	"<tr><td>Start:</td><td>".htmlspecialchars($mostrecent['starttime'])."</td></tr>\n".
-	"<tr><td>End:</td><td>".htmlspecialchars($mostrecent['endtime'])."</td></tr>\n".
-	"</table>\n\n";
-
-echo "<h3>Past Contests</h3>\n\n";
-
-echo "<table>\n<tr><th>Contest</th><th>Start</th><th>End</th></tr>\n";
+echo "<table>\n<tr><th>CID</th><th>starttime</th><th>endtime</th><th>name</th></tr>\n";
 foreach($res as $row) {
-	echo "<tr><td>".htmlentities($row['contestname']).
-	        ' [c'.(int)$row['cid'].
-			"]</td><td>" . htmlspecialchars($row['starttime']).
-			"</td><td>" . htmlspecialchars($row['endtime']).
-			"</td></tr>\n";
+	echo "<tr" .
+		($row['cid'] == $curcont ? ' class="highlight"':'') . ">" .
+		"<td align=\"right\">" . htmlentities($row['cid']) . "</td>" .
+		"<td title=\"" . htmlentities($row['starttime']) . "\">" .
+			printtime($row['starttime'])."</td>".
+		"<td title=\"".htmlentities($row['endtime']) . "\">" .
+			printtime($row['endtime'])."</td>".
+		"<td>" . htmlentities($row['contestname']) . "</td>" .
+		"</tr>\n";
+	
+
 }
 echo "</table>\n\n";
-
 
 require('../footer.php');
