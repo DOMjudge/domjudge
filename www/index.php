5da01d271eb132cab16815c32e6ec86297b57824
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-09-24 15:15:13 +0000

    Switchboardfile. Thijs
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@497 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ www/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Switch a user to the right site based on IP (from database)
+ * $Id$
+ */
+$MYWEBROOT = 'http://judge.nkp.nl/';
+
+require_once('../etc/config.php');
+require_once('../lib/lib.error.php');
+require_once('../lib/use_db_public.php');
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$res = $DB->q('SELECT ipaddress FROM team WHERE ipaddress = %s', $ip);
+if($res->count() > 0) {
+	$target = 'team/';
+} else {
+	$target = 'public/';
+}
+
+header('HTTP/1.1 301 Please see this page');
+header('Location: '.$MYWEBROOT.$target);
