76d15c97657b17e3dd6a9348fa3c65a4b3896754
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-10-14 21:45:23 +0000

    error reporting en ook $HOME/cygwin checken voor windows. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@566 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submit_copy.sh
+++ submit/submit_copy.sh
@@ -4,13 +4,30 @@
 # Script to copy a submission file from a team to judge account.
 # Usage: $0 <team> <fromfile> <tofile>
 
+function error()
+{
+    echo "[`date '+Bb %d %T'`] $0[$$]: $@"
+}
+
 [ $# -eq 3 ] || exit 1
 
 team=$1
 fromfile=$2
 tofile=$3
+copyhost=`hostname`
 
-output=`scp -Bq "$team@localhost:$fromfile" "$tofile" 2>&1`
-[ $? -eq 0 -a ${#output} -eq 0 ] || exit 1
+output1=`scp -Bq "$team@$copyhost:$fromfile" "$tofile" 2>&1`
+if [ $? -eq 0 -a ${#output1} -eq 0 ]; then
+	exit 0
+else
+	homedir="${fromfile%%.submit*}"
+	fromfile="${fromfile#*.submit}"
+	fromfile="${homedir}cygwin/.submit${fromfile}"
 
-exit 0
+	output2=`scp -Bq "$team@localhost:$fromfile" "$tofile" 2>&1`
+	[ $? -eq 0 -a ${#output2} -eq 0 ] && exit 0
+fi
+
+error "$output1"
+error "$output2"
+exit 1
