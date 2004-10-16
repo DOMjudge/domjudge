e26de373789975e9d80b9621e67030e1453a6a2d
Jaap Eldering <eldering@a-eskwadraat.nl>
2004-10-16 19:12:19 +0000

    host voor scp instellen via config. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@574 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submit_copy.sh
+++ submit/submit_copy.sh
@@ -4,6 +4,9 @@
 # Script to copy a submission file from a team to judge account.
 # Usage: $0 <team> <fromfile> <tofile>
 
+# Global configuration
+source `dirname $0`/../etc/config.sh
+
 function error()
 {
     echo "[`date '+Bb %d %T'`] $0[$$]: $@"
@@ -14,9 +17,8 @@ function error()
 team=$1
 fromfile=$2
 tofile=$3
-copyhost=`hostname`
 
-output1=`scp -Bq "$team@$copyhost:$fromfile" "$tofile" 2>&1`
+output1=`scp -Bq "${team}@${SCP_HOST}:${fromfile}" "$tofile" 2>&1`
 if [ $? -eq 0 -a ${#output1} -eq 0 ]; then
 	exit 0
 else
@@ -24,7 +26,7 @@ else
 	fromfile="${fromfile#*.submit}"
 	fromfile="${homedir}cygwin/.submit${fromfile}"
 
-	output2=`scp -Bq "$team@localhost:$fromfile" "$tofile" 2>&1`
+	output2=`scp -Bq "${team}@${SCP_HOST}:${fromfile}" "$tofile" 2>&1`
 	[ $? -eq 0 -a ${#output2} -eq 0 ] && exit 0
 fi
 
