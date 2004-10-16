b831fbfd676508bb8d62519df1e1abc39027b8b6
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-10-16 12:32:28 +0000

    breid checkconfig uit met wat extra sanity checks.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@570 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/checkconfig.php
+++ www/jury/checkconfig.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Do various sanity checks on the system regarding data constraints,
- * permissions and the like. At the moment this only checks the constraints
- * on the contest table.
+ * permissions and the like. At the moment this only contains some basic
+ * checks but this can be extended in the future.
  *
  * $Id$
  */
@@ -14,6 +14,24 @@ require('menu.php');
 ?>
 
 <h1>Config Checker</h1>
+
+<h2>Software</h2>
+
+<?php
+$phpver = PHP_VERSION;
+
+echo "<p>You are using DOMjudge version " . DOMJUDGE_VERSION . "<br />\n" .
+"PHP version " . $phpver;
+
+// are we using the right php version?
+if($phpver{0} < 4 || ($phpver{0} == 4 && $phpver{2} == 0 ) ) {
+	err('You need at least PHP version 4.1.0.');
+} else {
+	echo " OK";
+}
+echo "</p>\n\n";
+?>
+
 
 <h2>Contests</h2>
 
@@ -69,6 +87,49 @@ while($cdata = $res->next()) {
 	echo "</p>\n\n";
 }
 
+echo "<h2>Submissions</h2>\n\n<p>Checking submissions...<br />\n";
+
+$res = $DB->q('SELECT s.submitid,s.probid,s.cid FROM submission s LEFT OUTER JOIN problem p
+	USING(probid)
+	WHERE s.cid != p.cid');
+
+if($res->count() > 0) {
+	while($row = $res->next()) {
+		err('Submission s' .  $row['submitid'] . ' is for problem "' .
+			$row['probid'] . '" while this problem is not found (in c'.$row['cid'].')!');
+	}
+}
+
+
+echo "</p>\n\n<h2>Judgings</h2>\n\n<p>Checking judgings...<br />\n";
+
+$res = $DB->q('SELECT s.submitid as s_submitid, j.submitid as j_submitid,
+	judgingid, starttime, endtime, submittime, s.cid AS s_cid, j.cid AS j_cid
+	FROM judging j LEFT OUTER JOIN submission s USING(submitid)
+	WHERE  (j.cid != s.cid) OR
+		(j.endtime IS NOT NULL AND j.endtime < j.starttime) OR (j.starttime < s.submittime) OR
+		(s.submitid IS NULL) ');
+
+if($res->count() > 0) {
+	while($row = $res->next()) {
+		$err = 'Judging j' . $row['judgingid'] . '/s' . $row['j_submitid'] . ' ';
+		if(isset($row['endtime']) && $row['endtime'] < $row['starttime']) {
+			err($err.'ended before it started!');
+		}
+		if($row['starttime'] < $row['submittime']) {
+			err($err.'started before it was submitted!');
+		}
+		if(!isset($row['s_submitid'])) {
+			err($err .'has no corresponding submitid (in c'.$row['j_cid'] .')!');
+		}
+		if($row['s_cid'] != NULL && $row['s_cid'] != $row['j_cid']) {
+			err('Judging j'.$row['judgingid'].' is from a different contest (c' . $row['j_cid'] .
+				') than its submission s'.$row['j_submitid'] . ' (c' . $row['s_cid'] . ')!');
+		}
+	}
+}
+
+echo "</p>\n\n";
 
 echo "<p>End of config checker.</p>\n\n";
 
