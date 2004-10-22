cb882c5a5929b6c44875711bae7c149989e5440b
Peter van de Werken <pwerken@a-eskwadraat.nl>
2004-10-22 19:47:54 +0000

    clarification sturen naar eigen pagina (peter)
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@616 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ www/jury/clarification.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Send Clarification Response
+ *
+ * $Id: clarifications.php 615 2004-10-15 21:23:40Z nkp0405 $
+ */
+
+require('init.php');
+$title = 'Clarification Requests';
+require('../header.php');
+require('menu.php');
+
+/** insert new response */
+if (isset($_REQUEST['submit'])
+  && !empty($_REQUEST['response']))
+{
+	if(empty($_REQUEST['sendto'])) {
+		$respid = $DB->q('RETURNID INSERT INTO clar_response (reqid, cid, submittime, rcpt, body)
+			VALUES (NULL, %i, now(), NULL, %s)', getCurContest(), $_REQUEST['response']);
+	} else {
+		$respid = $DB->q('RETURNID INSERT INTO clar_response (reqid, cid, submittime, rcpt, body)
+			VALUES (NULL, %i, now(), %s, %s)', getCurContest(), $_REQUEST['sendto'], $_REQUEST['response']);
+	}
+
+	/** redirect back to the clarifications overview */
+	header('Location: clarifications.php');
+	exit;
+}
+
+?>
+<h1>Send Clarification Request</h1>
+
+<form action="clarification.php" method="post">
+<table>
+<tr><td>Send to:</td><td>
+<select name="sendto">
+<option value="">ALL</option>
+<?
+
+	$res = $DB->q('SELECT login, name FROM team ORDER  BY category ASC, name ASC');
+	while ($row = $res->next()) {
+		echo "<option value=\"" . htmlspecialchars($row['login']) . "\">" .
+			htmlentities($row['login'] . ": " . $row['name']) . "</option>\n";
+	}
+?>
+</select>
+</td></tr>
+<tr><td valign="top">Response:</td><td><textarea name="response" cols="80" rows="5"></textarea></td></tr>
+<tr><td>&nbsp;</td><td><input type="submit" name="submit" value="Send" /></td></tr>
+</table>
+</form>
+
+<?php
+require('../footer.php');
--- www/jury/clarifications.php
+++ www/jury/clarifications.php
@@ -6,28 +6,14 @@
  */
 
 require('init.php');
-
-/** insert new response */
-if (isset($_REQUEST['submit'])
-  && !empty($_REQUEST['response']))
-{
-	if(empty($_REQUEST['sendto'])) {
-		$respid = $DB->q('RETURNID INSERT INTO clar_response (reqid, cid, submittime, rcpt, body)
-			VALUES (NULL, %i, now(), NULL, %s)', getCurContest(), $_REQUEST['response']);
-	} else {
-		$respid = $DB->q('RETURNID INSERT INTO clar_response (reqid, cid, submittime, rcpt, body)
-			VALUES (NULL, %i, now(), %s, %s)', getCurContest(), $_REQUEST['sendto'], $_REQUEST['response']);
-	}
-
-	/** redirect back to the clarifications overview */
-	header('Location: clarifications.php');
-	exit;
-}
-
 $refresh = '15;url=clarifications.php';
 $title = 'Clarification Requests';
 require('../header.php');
 require('menu.php');
+
+?>
+<h1><a href="clarification.php">Send Clarification Response</a></h1>
+<?
 
 echo "<h1>Clarification Requests</h1>\n\n";
 
@@ -119,28 +105,4 @@ if ( $res->count() == 0 ) {
 	echo "</table>\n\n";
 }
 
-?>
-<h1>Send Response</h1>
-
-<form action="clarifications.php" method="post">
-<table>
-<tr><td>Send to:</td><td>
-<select name="sendto">
-<option value="">ALL</option>
-<?
-
-	$res = $DB->q('SELECT login, name FROM team ORDER  BY category ASC, name ASC');
-	while ($row = $res->next()) {
-		echo "<option value=\"" . htmlspecialchars($row['login']) . "\">" .
-			htmlentities($row['login'] . ": " . $row['name']) . "</option>\n";
-	}
-?>
-</select>
-</td></tr>
-<tr><td valign="top">Response:</td><td><textarea name="response" cols="80" rows="5"></textarea></td></tr>
-<tr><td>&nbsp;</td><td><input type="submit" name="submit" value="Send" /></td></tr>
-</table>
-</form>
-
-<?php
 require('../footer.php');
