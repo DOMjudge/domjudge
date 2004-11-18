13890b65af2f112792d7321d38ed2fa1c9ef630c
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2004-11-18 15:20:45 +0000

    databasestructuur in doc dir (en ook alleen de db struct) Thijs
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@635 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ doc/mysql_db_structure.sql
@@ -0,0 +1,147 @@
+-- These are the database tables needed for DOMjudge
+--
+-- You can pipe this file into the 'mysql' command to create the tables.
+--
+-- $Id$
+
+--
+-- Table structure for table 'category'
+--
+
+CREATE TABLE category (
+  catid tinyint(4) unsigned NOT NULL auto_increment,
+  name varchar(255) NOT NULL default '',
+  PRIMARY KEY  (catid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'clar_request'
+--
+
+CREATE TABLE clar_request (
+  reqid tinyint(3) unsigned NOT NULL auto_increment,
+  cid tinyint(3) unsigned NOT NULL default '0',
+  submittime datetime NOT NULL default '0000-00-00 00:00:00',
+  login varchar(15) NOT NULL default '',
+  body text NOT NULL,
+  PRIMARY KEY  (reqid)
+) TYPE=MyISAM COMMENT='Clarification Requests';
+
+--
+-- Table structure for table 'clar_response'
+--
+
+CREATE TABLE clar_response (
+  respid tinyint(3) unsigned NOT NULL auto_increment,
+  reqid tinyint(3) unsigned default NULL,
+  cid tinyint(3) unsigned NOT NULL default '0',
+  submittime datetime NOT NULL default '0000-00-00 00:00:00',
+  rcpt varchar(15) default NULL,
+  body text NOT NULL,
+  PRIMARY KEY  (respid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'contest'
+--
+
+CREATE TABLE contest (
+  cid tinyint(2) NOT NULL auto_increment,
+  starttime datetime NOT NULL default '0000-00-00 00:00:00',
+  lastscoreupdate datetime default NULL,
+  endtime datetime NOT NULL default '0000-00-00 00:00:00',
+  contestname varchar(255) NOT NULL default '',
+  PRIMARY KEY  (cid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'judger'
+--
+
+CREATE TABLE judger (
+  judgerid varchar(50) NOT NULL default '',
+  active tinyint(1) unsigned NOT NULL default '1',
+  PRIMARY KEY  (judgerid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'judging'
+--
+
+CREATE TABLE judging (
+  judgingid int(10) unsigned NOT NULL auto_increment,
+  cid tinyint(2) NOT NULL default '0',
+  submitid int(10) unsigned NOT NULL default '0',
+  starttime datetime NOT NULL default '0000-00-00 00:00:00',
+  endtime datetime default NULL,
+  judgerid varchar(50) NOT NULL default '',
+  result enum('correct','compiler-error','timelimit','run-error','wrong-answer','no-output') default NULL,
+  valid tinyint(1) unsigned NOT NULL default '1',
+  output_compile text,
+  output_run text,
+  output_diff text,
+  output_error text,
+  PRIMARY KEY  (judgingid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'language'
+--
+
+CREATE TABLE language (
+  langid varchar(8) NOT NULL default '',
+  name varchar(255) NOT NULL default '',
+  extension varchar(5) NOT NULL default '',
+  allow_submit tinyint(1) unsigned NOT NULL default '1',
+  allow_judge tinyint(1) unsigned NOT NULL default '1',
+  time_factor float NOT NULL default '0',
+  PRIMARY KEY  (langid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'problem'
+--
+
+CREATE TABLE problem (
+  probid varchar(8) NOT NULL default '',
+  cid int(10) unsigned NOT NULL default '0',
+  name varchar(255) NOT NULL default '',
+  allow_submit tinyint(1) unsigned NOT NULL default '0',
+  allow_judge tinyint(1) unsigned NOT NULL default '1',
+  testdata varchar(255) NOT NULL default '',
+  timelimit int(11) NOT NULL default '0',
+  PRIMARY KEY  (probid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'submission'
+--
+
+CREATE TABLE submission (
+  submitid int(10) unsigned NOT NULL auto_increment,
+  cid tinyint(2) NOT NULL default '0',
+  team varchar(15) NOT NULL default '',
+  probid varchar(8) NOT NULL default '',
+  langid varchar(8) NOT NULL default '',
+  submittime datetime NOT NULL default '0000-00-00 00:00:00',
+  sourcefile varchar(255) NOT NULL default '',
+  sourcecode text NOT NULL,
+  judgerid varchar(50) default NULL,
+  judgemark varchar(255) default NULL,
+  PRIMARY KEY  (submitid),
+  UNIQUE KEY judgemark (judgemark)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'team'
+--
+
+CREATE TABLE team (
+  login varchar(15) NOT NULL default '',
+  name varchar(255) NOT NULL default '',
+  category tinyint(4) unsigned NOT NULL default '0',
+  ipaddress varchar(15) default NULL,
+  PRIMARY KEY  (login),
+  UNIQUE KEY ipadres (ipaddress)
+) TYPE=MyISAM;
+
