7e2f685345adc59ed510c29db923807a7c24a0af
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-01-21 21:12:55 +0000

    made code better readable by removing excessive php-tags. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@701 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/header.php
+++ www/header.php
@@ -2,9 +2,12 @@
 /**
  * Common page header.
  * Before including this, one can set $title, $refresh and $popup.
+ *
+ * $Id$
  */
 
-echo '<?xml version="1.0" encoding="iso-8859-1" ?>'."\n";
+echo '<?xml version="1.0" encoding="iso-8859-1" ?>' . "\n";
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -12,26 +15,25 @@ echo '<?xml version="1.0" encoding="iso-8859-1" ?>'."\n";
 <head>
 	<!-- DOMjudge version <?= DOMJUDGE_VERSION ?> -->
 <?php
+
 	if( isset($refresh) ) {
-?>
-	<meta http-equiv="refresh" content="<?
-		if( isset($popup) ) {
-			echo addUrl($refresh, $popupTag);
-		} else {
-			echo $refresh;
-		}
-		?>" />
-<?php
+		echo '<meta http-equiv="refresh" content="' .
+			( isset($popup) ? addUrl($refresh, $popupTag) : $refresh ) .
+			"\" />\n";
 	}
+	echo "<title>" . $title . "</title>\n";
+
 ?>
-	<title><?= $title ?></title>
-	<link rel="stylesheet" href="style.css" type="text/css" />
-	
-	<script type="text/javascript">
-	function popUp(URL) {
-		var w = window.open(URL, 'ALERT', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=300,height=200');
-		w.focus();
-	}
-	</script>
+<link rel="stylesheet" href="style.css" type="text/css" />
+<script type="text/javascript">
+function popUp(URL) {
+	var w = window.open(URL, 'ALERT', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=300,height=200');
+	w.focus();
+}
+</script>
 </head>
-<body <?php if( isset($popup) && $popup ) { echo "onLoad=\"javascript:popUp('popup.php')\""; } ?>>
+<?php
+
+echo '<body';
+if( isset($popup) && $popup ) echo " onLoad=\"javascript:popUp('popup.php')\"";
+echo ">\n\n";
