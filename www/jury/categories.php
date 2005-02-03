34a6e227881fcd411dcfe72afc04f1ce82d632aa
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-02-03 21:24:27 +0000

    categorieen sorteren op nummer, niet naam. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@712 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/categories.php
+++ www/jury/categories.php
@@ -12,13 +12,13 @@ require('menu.php');
 
 echo "<h1>Categories</h1>\n\n";
 
-$res = $DB->q('SELECT * FROM category ORDER BY name');
+$res = $DB->q('SELECT * FROM category ORDER BY catid');
 
 echo "<table>
 <tr><th>nr</th><th>name</th></tr>\n";
 while($row = $res->next()) {
-	echo "<tr><td>".(int)$row['catid'].
-		"</td><td>".htmlentities($row['name']).
+	echo "<tr><td>" . (int)$row['catid'] .
+		"</td><td>" . htmlentities($row['name']) .
 		"</td></tr>\n";
 }
 echo "</table>\n\n";
