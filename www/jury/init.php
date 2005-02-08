778b9b60b6ebee5e5ddddb0aee4d1d3276d4eca0
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2005-02-08 11:07:35 +0000

    Nettere error als DOMjudge nog niet configured is. Thijs
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@716 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/init.php
+++ www/jury/init.php
@@ -5,9 +5,15 @@
  * $Id$
  */
 
-require_once('../../etc/config.php');
 require_once('../../lib/lib.error.php');
-require_once('../../lib/lib.misc.php');
+
+/** If config not found, exit with an error */
+if( ! @include_once('../../etc/config.php') ) {
+	error('Failed to include etc/config.php! '.
+		'Check if you configured DOMjudge through editing "etc/global.cfg" '.
+		'and then run \'make\'.');
+}
+
 require_once('../../lib/use_db_jury.php');
 require_once('../common.php');
 require_once('../print.php');
--- www/public/init.php
+++ www/public/init.php
@@ -6,8 +6,15 @@
  * $Id$
  */
 
-require_once('../../etc/config.php');
 require_once('../../lib/lib.error.php');
+
+/** If config not found, exit with an error */
+if( ! @include_once('../../etc/config.php') ) {
+	error('Failed to include etc/config.php! '.
+		'Check if you configured DOMjudge through editing "etc/global.cfg" '.
+		'and then run \'make\'.');
+}
+
 require_once('../../lib/use_db_public.php');
 require_once('../../lib/lib.misc.php');
 require_once('../common.php');
