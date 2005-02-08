663f019612081703957d77920bd172f148a3e99b
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2005-02-08 11:11:56 +0000

    getCurContest() was niet beschikbaar op www/jury/scoreboard.php. Thijs
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@717 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/init.php
+++ www/jury/init.php
@@ -15,6 +15,7 @@ if( ! @include_once('../../etc/config.php') ) {
 }
 
 require_once('../../lib/use_db_jury.php');
+require_once('../../lib/lib.misc.php');
 require_once('../common.php');
 require_once('../print.php');
 require_once('validate.php');
