eeda9d63492f0fc1851d2a355e0619d8e9e28647
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-02-23 17:04:15 +0000

    Include config of copy command in template. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@727 caf94f87-7bd6-0310-88dc-dee8202e2269

--- etc/config.template.h
+++ etc/config.template.h
@@ -14,6 +14,14 @@
 #ifndef _LOCAL_CONFIG_
 #define _LOCAL_CONFIG_
 
+/* Define the command to copy files to the user submit directory in the
+   submit client differently under Windows and Linux */
+#if defined(__CYGWIN__) || defined(__CYGWIN32__)
+#define COPY_CMD "c:/cygwin/bin/cp"
+#else
+#define COPY_CMD "cp"
+#endif
+
 /*** GLOBAL CONFIG INCLUDE START ***/
 /*** GLOBAL CONFIG INCLUDE END ***/
 
