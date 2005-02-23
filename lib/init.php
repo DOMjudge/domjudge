c6499a8a5e1af924cee86ade0926c7c2aea1bb22
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-02-23 17:09:29 +0000

    Use SYSTEM_ROOT directly. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@731 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/init.php
+++ lib/init.php
@@ -1,8 +1,6 @@
 <?php
 // $Id$
 	
-	define('PHP_LIB_PATH', SYSTEM_ROOT.'/lib');
-	
-	require(PHP_LIB_PATH . '/use_db_jury.php');
-	require(PHP_LIB_PATH . '/lib.error.php');
-	require(PHP_LIB_PATH . '/lib.misc.php');
+	require(SYSTEM_ROOT . '/lib/use_db_jury.php');
+	require(SYSTEM_ROOT . '/lib/lib.error.php');
+	require(SYSTEM_ROOT . '/lib/lib.misc.php');
