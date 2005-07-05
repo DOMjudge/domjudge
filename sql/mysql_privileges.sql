b1651ebb0a0d9777b531488cec9ce5c0b529b3cc
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-07-05 22:53:31 +0000

    - do a USE statement instead of explicit referencing of mysql db. - add a missing columns_priv for domjudge_team on problem->cid. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@791 caf94f87-7bd6-0310-88dc-dee8202e2269

--- sql/mysql_privileges.sql
+++ sql/mysql_privileges.sql
@@ -5,50 +5,53 @@
 #
 # $Id$
 
+USE mysql;
+
 # Add users and passwords
 # Change these default passwords, and change them in etc/passwords.php too!
-INSERT INTO mysql.user (Host, User, Password) VALUES ('localhost','domjudge_jury',  PASSWORD('jury'));
-INSERT INTO mysql.user (Host, User, Password) VALUES ('localhost','domjudge_team',  PASSWORD('team'));
-INSERT INTO mysql.user (Host, User, Password) VALUES ('localhost','domjudge_public',PASSWORD('public'));
+INSERT INTO user (Host, User, Password) VALUES ('localhost','domjudge_jury'  ,PASSWORD('jury'));
+INSERT INTO user (Host, User, Password) VALUES ('localhost','domjudge_team'  ,PASSWORD('team'));
+INSERT INTO user (Host, User, Password) VALUES ('localhost','domjudge_public',PASSWORD('public'));
 
 # Juryaccount can do anything to the database
-INSERT INTO mysql.db (Host, Db, User, Select_priv, Insert_priv, Update_priv, Delete_priv) VALUES ('localhost','domjudge','domjudge_jury','Y','Y','Y','Y');
+INSERT INTO db (Host, Db, User, Select_priv, Insert_priv, Update_priv, Delete_priv) VALUES ('localhost','domjudge','domjudge_jury','Y','Y','Y','Y');
 
 # Other privileges
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'name'        ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'langid'      ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'probid'      ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'name'        ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'allow_submit',NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'judgingid'   ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'submitid'    ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'result'      ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'valid'       ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'probid'      ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'cid'         ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'name'        ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'allow_submit',NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','team'        ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submittime'  ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submitid'    ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','probid'      ,NOW(),'Select');
-INSERT INTO mysql.columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','cid'         ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','cid'         ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','probid'      ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submitid'    ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submittime'  ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','team'        ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'allow_submit',NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'name'        ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'cid'         ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'probid'      ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'valid'       ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'result'      ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'submitid'    ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'judgingid'   ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'allow_submit',NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'cid'         ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'name'        ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'probid'      ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'langid'      ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'name'        ,NOW(),'select');
 
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'judging'          ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'submission'       ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'contest'          ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'team'             ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'          ,'domjudge@localhost',NOW(),'','Select');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'         ,'domjudge@localhost',NOW(),'','Select');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'category'         ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'clarification'    ,'domjudge@localhost',NOW(),'Select,Insert','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_team'  ,'scoreboard_public','domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_public','category'         ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_public','contest'          ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_public','judging'          ,'domjudge@localhost',NOW(),'','Select');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_public','problem'          ,'domjudge@localhost',NOW(),'','Select');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_public','submission'       ,'domjudge@localhost',NOW(),'','Select');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_public','team'             ,'domjudge@localhost',NOW(),'Select','');
-INSERT INTO mysql.tables_priv VALUES ('localhost','domjudge','domjudge_public','scoreboard_public','domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','scoreboard_public','domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','team'             ,'domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','submission'       ,'domjudge@localhost',NOW(),'','Select');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','problem'          ,'domjudge@localhost',NOW(),'','Select');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','judging'          ,'domjudge@localhost',NOW(),'','Select');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','contest'          ,'domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','category'         ,'domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'scoreboard_public','domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'clarification'    ,'domjudge@localhost',NOW(),'Select,Insert','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'category'         ,'domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'language'         ,'domjudge@localhost',NOW(),'','Select');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'problem'          ,'domjudge@localhost',NOW(),'','Select');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'team'             ,'domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'contest'          ,'domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'submission'       ,'domjudge@localhost',NOW(),'Select','');
+INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_team'  ,'judging'          ,'domjudge@localhost',NOW(),'Select','');
 
 FLUSH PRIVILEGES;
