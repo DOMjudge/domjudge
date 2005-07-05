390afffbf6afa67ed40d2364cff21cf28da59949
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-07-05 22:54:32 +0000

    New export with some minor structure fixes and use the USE statement. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@792 caf94f87-7bd6-0310-88dc-dee8202e2269

--- sql/mysql_db_structure.sql
+++ sql/mysql_db_structure.sql
@@ -1,28 +1,30 @@
 # These are the database tables needed for DOMjudge
 # This assumes database name 'domjudge'
 #
-# You can pipe this file into the 'mysql' command to create the tables.
+# You can pipe this file into the 'mysql' command to create the
+# database and tables.
 #
 # $Id$
 
-# First create the database (if non-existent)
-CREATE DATABASE IF NOT EXISTS domjudge;
+# Create and use the database:
+CREATE DATABASE domjudge;
+USE domjudge;
 
 #
-# Table structure for table 'category'
+# Table structure for table `category`
 #
 
-CREATE TABLE domjudge.category (
-  catid tinyint(4) unsigned NOT NULL auto_increment,
-  name varchar(255) NOT NULL default '',
-  PRIMARY KEY  (catid)
+CREATE TABLE `category` (
+  `catid` mediumint(8) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`catid`)
 ) TYPE=MyISAM;
 
 #
-# Table structure for table 'clarification'
+# Table structure for table `clarification`
 #
 
-CREATE TABLE domjudge.clarification (
+CREATE TABLE `clarification` (
   `clarid` mediumint(8) unsigned NOT NULL auto_increment,
   `cid` mediumint(8) unsigned NOT NULL default '0',
   `respid` mediumint(8) unsigned default NULL,
@@ -34,138 +36,136 @@ CREATE TABLE domjudge.clarification (
   PRIMARY KEY  (`clarid`)
 ) TYPE=MyISAM;
 
-
 #
-# Table structure for table 'contest'
+# Table structure for table `contest`
 #
 
-CREATE TABLE domjudge.contest (
-  cid tinyint(2) NOT NULL auto_increment,
-  starttime datetime NOT NULL default '0000-00-00 00:00:00',
-  lastscoreupdate datetime default NULL,
-  endtime datetime NOT NULL default '0000-00-00 00:00:00',
-  contestname varchar(255) NOT NULL default '',
-  PRIMARY KEY  (cid)
+CREATE TABLE `contest` (
+  `cid` mediumint(8) unsigned NOT NULL auto_increment,
+  `starttime` datetime NOT NULL default '0000-00-00 00:00:00',
+  `lastscoreupdate` datetime default NULL,
+  `endtime` datetime NOT NULL default '0000-00-00 00:00:00',
+  `contestname` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`cid`)
 ) TYPE=MyISAM;
 
 #
-# Table structure for table 'judger'
+# Table structure for table `judger`
 #
 
-CREATE TABLE domjudge.judger (
-  judgerid varchar(50) NOT NULL default '',
-  active tinyint(1) unsigned NOT NULL default '1',
-  PRIMARY KEY  (judgerid)
+CREATE TABLE `judger` (
+  `judgerid` varchar(50) NOT NULL default '',
+  `active` tinyint(8) unsigned NOT NULL default '1',
+  PRIMARY KEY  (`judgerid`)
 ) TYPE=MyISAM;
 
 #
-# Table structure for table 'judging'
+# Table structure for table `judging`
 #
 
-CREATE TABLE domjudge.judging (
-  judgingid int(10) unsigned NOT NULL auto_increment,
-  cid tinyint(2) NOT NULL default '0',
-  submitid int(10) unsigned NOT NULL default '0',
-  starttime datetime NOT NULL default '0000-00-00 00:00:00',
-  endtime datetime default NULL,
-  judgerid varchar(50) NOT NULL default '',
-  result enum('correct','compiler-error','timelimit','run-error','wrong-answer','no-output') default NULL,
-  valid tinyint(1) unsigned NOT NULL default '1',
-  output_compile text,
-  output_run text,
-  output_diff text,
-  output_error text,
-  PRIMARY KEY  (judgingid)
+CREATE TABLE `judging` (
+  `judgingid` mediumint(10) unsigned NOT NULL auto_increment,
+  `cid` mediumint(2) unsigned NOT NULL default '0',
+  `submitid` mediumint(10) unsigned NOT NULL default '0',
+  `starttime` datetime NOT NULL default '0000-00-00 00:00:00',
+  `endtime` datetime default NULL,
+  `judgerid` varchar(50) NOT NULL default '',
+  `result` enum('correct','compiler-error','timelimit','run-error','wrong-answer','no-output') default NULL,
+  `valid` tinyint(1) unsigned NOT NULL default '1',
+  `output_compile` text,
+  `output_run` text,
+  `output_diff` text,
+  `output_error` text,
+  PRIMARY KEY  (`judgingid`)
 ) TYPE=MyISAM;
 
 #
-# Table structure for table 'language'
+# Table structure for table `language`
 #
 
-CREATE TABLE domjudge.language (
-  langid varchar(8) NOT NULL default '',
-  name varchar(255) NOT NULL default '',
-  extension varchar(5) NOT NULL default '',
-  allow_submit tinyint(1) unsigned NOT NULL default '1',
-  allow_judge tinyint(1) unsigned NOT NULL default '1',
-  time_factor float NOT NULL default '0',
-  PRIMARY KEY  (langid)
+CREATE TABLE `language` (
+  `langid` varchar(8) NOT NULL default '',
+  `name` varchar(255) NOT NULL default '',
+  `extension` varchar(5) NOT NULL default '',
+  `allow_submit` tinyint(1) unsigned NOT NULL default '1',
+  `allow_judge` tinyint(1) unsigned NOT NULL default '1',
+  `time_factor` float NOT NULL default '0',
+  PRIMARY KEY  (`langid`)
 ) TYPE=MyISAM;
 
 #
-# Table structure for table 'problem'
+# Table structure for table `problem`
 #
 
-CREATE TABLE domjudge.problem (
-  probid varchar(8) NOT NULL default '',
-  cid int(10) unsigned NOT NULL default '0',
-  name varchar(255) NOT NULL default '',
-  allow_submit tinyint(1) unsigned NOT NULL default '0',
-  allow_judge tinyint(1) unsigned NOT NULL default '1',
-  testdata varchar(255) NOT NULL default '',
-  timelimit int(11) NOT NULL default '0',
-  PRIMARY KEY  (probid)
+CREATE TABLE `problem` (
+  `probid` varchar(8) NOT NULL default '',
+  `cid` mediumint(10) unsigned NOT NULL default '0',
+  `name` varchar(255) NOT NULL default '',
+  `allow_submit` tinyint(1) unsigned NOT NULL default '0',
+  `allow_judge` tinyint(1) unsigned NOT NULL default '1',
+  `testdata` varchar(255) NOT NULL default '',
+  `timelimit` mediumint(11) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`probid`)
 ) TYPE=MyISAM;
 
 #
 # Table structure for table `scoreboard_jury`
 #
 
-CREATE TABLE domjudge.scoreboard_jury (
-  cid mediumint(8) unsigned NOT NULL default '0',
-  team varchar(15) NOT NULL default '',
-  problem varchar(8) NOT NULL default '',
-  submissions int(3) unsigned NOT NULL default '0',
-  totaltime int(4) unsigned NOT NULL default '0',
-  penalty int(4) unsigned NOT NULL default '0',
-  is_correct tinyint(1) unsigned NOT NULL default '0',
-  PRIMARY KEY  (cid,team,problem)
+CREATE TABLE `scoreboard_jury` (
+  `cid` mediumint(8) unsigned NOT NULL default '0',
+  `team` varchar(15) NOT NULL default '',
+  `problem` varchar(8) NOT NULL default '',
+  `submissions` int(3) unsigned NOT NULL default '0',
+  `totaltime` int(4) unsigned NOT NULL default '0',
+  `penalty` int(4) unsigned NOT NULL default '0',
+  `is_correct` tinyint(1) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`cid`,`team`,`problem`)
 ) TYPE=MyISAM COMMENT='Scoreboard cache (jury version)';
 
 #
-# Table structure for table `scoreboard_jury`
+# Table structure for table `scoreboard_public`
 #
 
-CREATE TABLE domjudge.scoreboard_public (
-  cid mediumint(8) unsigned NOT NULL default '0',
-  team varchar(15) NOT NULL default '',
-  problem varchar(8) NOT NULL default '',
-  submissions int(3) unsigned NOT NULL default '0',
-  totaltime int(4) unsigned NOT NULL default '0',
-  penalty int(4) unsigned NOT NULL default '0',
-  is_correct tinyint(1) unsigned NOT NULL default '0',
-  PRIMARY KEY  (cid,team,problem)
+CREATE TABLE `scoreboard_public` (
+  `cid` mediumint(8) unsigned NOT NULL default '0',
+  `team` varchar(15) NOT NULL default '',
+  `problem` varchar(8) NOT NULL default '',
+  `submissions` int(3) unsigned NOT NULL default '0',
+  `totaltime` int(4) unsigned NOT NULL default '0',
+  `penalty` int(4) unsigned NOT NULL default '0',
+  `is_correct` tinyint(1) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`cid`,`team`,`problem`)
 ) TYPE=MyISAM COMMENT='Scoreboard cache (public/team version)';
 
 #
-# Table structure for table 'submission'
+# Table structure for table `submission`
 #
 
-CREATE TABLE domjudge.submission (
-  submitid int(10) unsigned NOT NULL auto_increment,
-  cid tinyint(2) NOT NULL default '0',
-  team varchar(15) NOT NULL default '',
-  probid varchar(8) NOT NULL default '',
-  langid varchar(8) NOT NULL default '',
-  submittime datetime NOT NULL default '0000-00-00 00:00:00',
-  sourcefile varchar(255) NOT NULL default '',
-  sourcecode text NOT NULL,
-  judgerid varchar(50) default NULL,
-  judgemark varchar(255) default NULL,
-  PRIMARY KEY  (submitid),
-  UNIQUE KEY judgemark (judgemark)
+CREATE TABLE `submission` (
+  `submitid` mediumint(10) unsigned NOT NULL auto_increment,
+  `cid` mediumint(2) NOT NULL default '0',
+  `team` varchar(15) NOT NULL default '',
+  `probid` varchar(8) NOT NULL default '',
+  `langid` varchar(8) NOT NULL default '',
+  `submittime` datetime NOT NULL default '0000-00-00 00:00:00',
+  `sourcefile` varchar(255) NOT NULL default '',
+  `sourcecode` text NOT NULL,
+  `judgerid` varchar(50) default NULL,
+  `judgemark` varchar(255) default NULL,
+  PRIMARY KEY  (`submitid`),
+  UNIQUE KEY `judgemark` (`judgemark`)
 ) TYPE=MyISAM;
 
 #
-# Table structure for table 'team'
+# Table structure for table `team`
 #
 
-CREATE TABLE domjudge.team (
-  login varchar(15) NOT NULL default '',
-  name varchar(255) NOT NULL default '',
-  category tinyint(4) unsigned NOT NULL default '0',
-  ipaddress varchar(15) default NULL,
-  PRIMARY KEY  (login),
-  UNIQUE KEY ipadres (ipaddress)
+CREATE TABLE `team` (
+  `login` varchar(15) NOT NULL default '',
+  `name` varchar(255) NOT NULL default '',
+  `category` mediumint(4) unsigned NOT NULL default '0',
+  `ipaddress` varchar(15) default NULL,
+  PRIMARY KEY  (`login`),
+  UNIQUE KEY `ipadres` (`ipaddress`)
 ) TYPE=MyISAM;
-
