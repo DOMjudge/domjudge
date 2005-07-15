34b1d79754f9c0ece264e65efeff82ff9414f65a
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-07-15 21:49:50 +0000

    Fixed popups for own clarifications. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@808 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/team/popupcheck.php
+++ www/team/popupcheck.php
@@ -14,10 +14,10 @@ $popupTag = 'stamp='.time();
 // do not throw a popup if stamp is not set
 if ( isset($_REQUEST['stamp']) ) {
 	$res = $DB->q('SELECT * FROM clarification
-		WHERE submittime >= FROM_UNIXTIME(%i)
-		AND cid = %i AND ( recipient IS NULL OR recipient = %s )',
-		$_REQUEST['stamp'], getCurContest(), $login);
+	               WHERE submittime >= FROM_UNIXTIME(%i) AND cid = %i
+	               AND ( recipient = %s OR
+	               ( sender IS NULL AND recipient IS NULL ) )',
+	              $_REQUEST['stamp'], getCurContest(), $login);
 	
 	if ( $res->count() > 0 ) $popup = true;
 }
-
