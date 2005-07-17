51ad9695606215af72ccb40439b4ef4bdb1e582c
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-07-17 11:51:28 +0000

    Correct indentation level. Jaap
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@811 caf94f87-7bd6-0310-88dc-dee8202e2269

--- submit/submit_db.php
+++ submit/submit_db.php
@@ -11,91 +11,92 @@
  *
  * $Id$
  */
-	require ('../etc/config.php');
 
-	define ('SCRIPT_ID', 'submit_db');
-	define ('LOGFILE', LOGDIR.'/submit.log');
+require ('../etc/config.php');
 
-	require (SYSTEM_ROOT . '/lib/init.php');
-	
-	// every file written has to be in 0600 filemode
-	umask(0177);
+define ('SCRIPT_ID', 'submit_db');
+define ('LOGFILE', LOGDIR.'/submit.log');
 
-	// Get commandline vars and case-normalize them
-	$argv = $_SERVER['argv'];
-	
-	$team    = strtolower(@$argv[1]);
-	$ip      = @$argv[2];
-	$prob    = strtolower(@$argv[3]);
-	$langext = strtolower(@$argv[4]);
-	$file    = @$argv[5];
+require (SYSTEM_ROOT . '/lib/init.php');
 
-	logmsg(LOG_DEBUG, "arguments: '$team' '$ip' '$prob' '$langext' '$file'");
+// every file written has to be in 0600 filemode
+umask(0177);
 
+// Get commandline vars and case-normalize them
+$argv = $_SERVER['argv'];
 
-	// Check 0: called correctly?
-	if( ! $team    ) error("No value for Team.");
-	if( ! $ip      ) error("No value for IP.");
-	if( ! $prob    ) error("No value for Problem.");
-	if( ! $langext ) error("No value for Language.");
-	if( ! $file    ) error("No value for Filename.");
+$team    = strtolower(@$argv[1]);
+$ip      = @$argv[2];
+$prob    = strtolower(@$argv[3]);
+$langext = strtolower(@$argv[4]);
+$file    = @$argv[5];
+
+logmsg(LOG_DEBUG, "arguments: '$team' '$ip' '$prob' '$langext' '$file'");
 
 
-	// Check 1: is the contest open?
-	$cid = getCurContest();
-	$cont = $DB->q('TUPLE SELECT *,
-		UNIX_TIMESTAMP(starttime) as start_u, UNIX_TIMESTAMP(endtime) as end_u
-		FROM contest WHERE cid = %i',$cid);
-	if( $cont['start_u'] > time() || $cont['end_u'] < time() ) {
-		error("The contest is closed, no submissions accepted. [c$cont[cid]]");
-	}
+// Check 0: called correctly?
+if( ! $team    ) error("No value for Team.");
+if( ! $ip      ) error("No value for IP.");
+if( ! $prob    ) error("No value for Problem.");
+if( ! $langext ) error("No value for Language.");
+if( ! $file    ) error("No value for Filename.");
 
 
-	// Check 2: valid parameters?
-	if( ! $lang = $DB->q('MAYBEVALUE SELECT langid FROM language WHERE
-		                  extension = %s AND allow_submit = 1', $langext) ) {
-		error("Language '$langext' not found in database or not submittable.");
-	}
-	if( ! $teamrow = $DB->q('MAYBETUPLE SELECT * FROM team WHERE login = %s',
-	                        $team) ) {
-		error("Team '$team' not found in database.");
-	}
-	if( $teamrow['ipaddress'] != $ip ) {
-		error("Team '$team' not registered at this IP address.");
-	}
-	if( ! $probid = $DB->q('MAYBEVALUE SELECT probid FROM problem WHERE probid = %s
-	              AND cid = %i AND allow_submit = "1"', $prob, $cid) ) {
-		error("Problem '$prob' not found in database or not submittable [c$cid].");
-	}
-	if( ! is_readable(INCOMINGDIR."/$file") ) {
-		error("File '$file' not found in incoming directory (or not readable).");
-	}
-	if( filesize(INCOMINGDIR."/$file") > SOURCESIZE*1024 ) {
-		error("Submission file is larger than ".SOURCESIZE." kB."); 
-	}
-
-	logmsg (LOG_INFO, "input verified");
+// Check 1: is the contest open?
+$cid = getCurContest();
+$cont = $DB->q('TUPLE SELECT *,
+                UNIX_TIMESTAMP(starttime) as start_u,
+                UNIX_TIMESTAMP(endtime) as end_u
+                FROM contest WHERE cid = %i',$cid);
+if( $cont['start_u'] > time() || $cont['end_u'] < time() ) {
+	error("The contest is closed, no submissions accepted. [c$cont[cid]]");
+}
 
 
-	// Copy the submission to a (uniquely generated) file in SUBMITDIR
-	$dummy = null;
-	$tofile = exec(SYSTEM_ROOT."/bin/mkstemps ".
-	               SUBMITDIR."/$team.$prob.XXXXXX.$langext", $dummy, $retval);
-	if ( $retval != 0 ) error("Could not create tempfile.");
-	$tofile = basename($tofile);
+// Check 2: valid parameters?
+if( ! $lang = $DB->q('MAYBEVALUE SELECT langid FROM language WHERE
+                      extension = %s AND allow_submit = 1', $langext) ) {
+	error("Language '$langext' not found in database or not submittable.");
+}
+if( ! $teamrow = $DB->q('MAYBETUPLE SELECT * FROM team WHERE login = %s',$team) ) {
+	error("Team '$team' not found in database.");
+}
+if( $teamrow['ipaddress'] != $ip ) {
+	error("Team '$team' not registered at this IP address.");
+}
+if( ! $probid = $DB->q('MAYBEVALUE SELECT probid FROM problem WHERE probid = %s
+                        AND cid = %i AND allow_submit = "1"', $prob, $cid) ) {
+	error("Problem '$prob' not found in database or not submittable [c$cid].");
+}
+if( ! is_readable(INCOMINGDIR."/$file") ) {
+	error("File '$file' not found in incoming directory (or not readable).");
+}
+if( filesize(INCOMINGDIR."/$file") > SOURCESIZE*1024 ) {
+	error("Submission file is larger than ".SOURCESIZE." kB."); 
+}
 
-	if ( ! copy(INCOMINGDIR."/$file", SUBMITDIR."/$tofile") ) {
-		error("Could not copy '".INCOMINGDIR."/".$file.
-		                "' to '".SUBMITDIR."/".$tofile."'");
-	}
+logmsg (LOG_INFO, "input verified");
 
-	// Insert submission into the database
-	$id = $DB->q('RETURNID INSERT INTO submission
-	              (cid,team,probid,langid,submittime,sourcefile,sourcecode)
-	              VALUES (%i, %s, %s, %s, NOW(), %s, %s)',
-	             $cid, $teamrow['login'], $probid, $lang, $tofile,
-	             getFileContents(SUBMITDIR."/".$tofile));
 
-	logmsg (LOG_NOTICE, "submitted $team/$prob/$lang, file $tofile, id s$id/c$cid");
+// Copy the submission to a (uniquely generated) file in SUBMITDIR
+$dummy = null;
+$tofile = exec(SYSTEM_ROOT."/bin/mkstemps ".
+			   SUBMITDIR."/$team.$prob.XXXXXX.$langext", $dummy, $retval);
+if ( $retval != 0 ) error("Could not create tempfile.");
+$tofile = basename($tofile);
 
-	exit;
+if ( ! copy(INCOMINGDIR."/$file", SUBMITDIR."/$tofile") ) {
+	error("Could not copy '".INCOMINGDIR."/".$file.
+	      "' to '".SUBMITDIR."/".$tofile."'");
+}
+
+// Insert submission into the database
+$id = $DB->q('RETURNID INSERT INTO submission
+              (cid,team,probid,langid,submittime,sourcefile,sourcecode)
+              VALUES (%i, %s, %s, %s, NOW(), %s, %s)',
+             $cid, $teamrow['login'], $probid, $lang, $tofile,
+             getFileContents(SUBMITDIR."/".$tofile));
+
+logmsg (LOG_NOTICE, "submitted $team/$prob/$lang, file $tofile, id s$id/c$cid");
+
+exit;
