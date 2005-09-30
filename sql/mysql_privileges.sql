ba909a290db78595eae50bff8d51ab7a997f1cec
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2005-09-30 14:14:33 +0000

    fix case bug in privileges sql
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@856 caf94f87-7bd6-0310-88dc-dee8202e2269

--- sql/mysql_privileges.sql
+++ sql/mysql_privileges.sql
@@ -22,25 +22,25 @@ INSERT INTO user (Host, User, Password) VALUES ('localhost','domjudge_public',PA
 INSERT INTO db (Host, Db, User, Select_priv, Insert_priv, Update_priv, Delete_priv) VALUES ('localhost','domjudge','domjudge_jury','Y','Y','Y','Y');
 
 -- Other privileges
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','cid'         ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','probid'      ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submitid'    ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submittime'  ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','team'        ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'allow_submit',NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'name'        ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'cid'         ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'probid'      ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'valid'       ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'result'      ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'submitid'    ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'judgingid'   ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'allow_submit',NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'cid'         ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'name'        ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'probid'      ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'langid'      ,NOW(),'select');
-INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'name'        ,NOW(),'select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','cid'         ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','probid'      ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submitid'    ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','submittime'  ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','submission','team'        ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'allow_submit',NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'name'        ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'cid'         ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','problem'   ,'probid'      ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'valid'       ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'result'      ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'submitid'    ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_public','judging'   ,'judgingid'   ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'allow_submit',NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'cid'         ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'name'        ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'problem'   ,'probid'      ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'langid'      ,NOW(),'Select');
+INSERT INTO columns_priv VALUES ('localhost','domjudge','domjudge_team'  ,'language'  ,'name'        ,NOW(),'Select');
 
 INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','scoreboard_public','domjudge@localhost',NOW(),'Select','');
 INSERT INTO tables_priv  VALUES ('localhost','domjudge','domjudge_public','team'             ,'domjudge@localhost',NOW(),'Select','');
