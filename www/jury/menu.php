18a4adea3a311f0904b6b5e355af6f60838db60e
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2005-10-01 13:31:45 +0000

    Reorder menubuttons
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@861 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/menu.php
+++ www/jury/menu.php
@@ -3,9 +3,9 @@
 <a href="index.php">home</a>
 <a href="problems.php">problems</a>
 <a href="judgers.php">judgers</a>
-<a href="submissions.php">submissions</a>
 <a href="teams.php">teams</a>
 <a href="clarifications.php">clarifications</a>
+<a href="submissions.php">submissions</a>
 <a href="scoreboard.php">scoreboard</a>
 </div>
 
