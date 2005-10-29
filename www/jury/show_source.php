c0d6953078cda36d43048dc50a0b2d8448c74c30
Jaap Eldering <eldering@a-eskwadraat.nl>
2005-10-29 22:56:22 +0000

    Added diff to previous submission in source display.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@892 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/show_source.php
+++ www/jury/show_source.php
@@ -12,12 +12,43 @@ require('menu.php');
 
 $id = (int)$_GET['id'];
 
-$source = $DB->q('TUPLE SELECT sourcefile,sourcecode FROM submission WHERE submitid = %i',$id);
+$source = $DB->q('TUPLE SELECT * FROM submission
+                  WHERE submitid = %i',$id);
 
-echo "<h2 class=\"filename\">".htmlspecialchars($source['sourcefile'])."</h2>\n\n";
+$oldsource = $DB->q('MAYBETUPLE SELECT * FROM submission
+                     WHERE team = %s AND probid = %s AND langid = %s AND
+                     submittime < %s ORDER BY submittime DESC LIMIT 1',
+					$source['team'],$source['probid'],$source['langid'],
+					$source['submittime']);
+
+if ( $oldsource ) {
+	echo '<p><a href="#diff">Goto diff to previous submission</a></p>';
+}
+
+echo '<h2 class="filename"><a name="source"></a>Submission ' .
+	"<a href=\"submission.php?id=$id\">s$id</a> source: " .
+	"<a href=\"show_source.php?id=$id\">" .
+	htmlspecialchars($source['sourcefile']) . "</a></h2>\n\n";
 
 echo '<pre class="output_text">' .
-	htmlspecialchars($source['sourcecode']) .
-	"</pre>\n\n";
+	htmlspecialchars($source['sourcecode']) . "</pre>\n\n";
+
+if ( $oldsource ) {
+	
+	$oldid = $oldsource['submitid'];
+	
+	$oldfile = SUBMITDIR.'/'.$oldsource['sourcefile'];
+	$newfile = SUBMITDIR.'/'.$source['sourcefile'];
+	
+	$difftext = `diff -bBt -u2 $oldfile $newfile 2>&1`;
+	
+	echo '<h2 class="filename"><a name="diff"></a>Diff to submission ' .
+		"<a href=\"submission.php?id=$oldid\">s$oldid</a> source: " .
+		"<a href=\"show_source.php?id=$oldid\">" .
+		htmlspecialchars($oldsource['sourcefile']) . "</a></h2>\n\n";
+
+	echo '<pre class="output_text">' .
+		htmlspecialchars($difftext) . "</pre>\n\n";
+}
 
 require('../footer.php');
