f06c7a77fac298acda1aab0868b737578772e66f
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2005-11-29 15:37:45 +0000

    tweak categories table
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@910 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/categories.php
+++ www/jury/categories.php
@@ -18,11 +18,12 @@ $res = $DB->q('SELECT * FROM team_category ORDER BY sortorder,categoryid');
 if( $res->count() == 0 ) {
 	echo "<p><em>No categories defined</em></p>\n\n";
 } else {
-	echo "<table>
-	<tr><th>sort</th><th>cat id</th><th>name</th></tr>\n";
+	echo "<table>\n".
+		"<tr><th>ID</th><th>sort</th><th>name</th></tr>\n";
 	while($row = $res->next()) {
-		echo "<tr><td>" . (int)$row['sortorder'] .
-			"</td><td>" . (int)$row['categoryid'] .
+		echo "<tr class=\"category". (int)$row['categoryid'] .
+			"\"><td>" . (int)$row['categoryid'] .
+			"</td><td>" . (int)$row['sortorder'] .
 			"</td><td>" . htmlentities($row['name']) .
 			"</td></tr>\n";
 	}
