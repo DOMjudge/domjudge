bccd8bd5bef107553aa09cd4ee88adff4483691a
Jaap Eldering <eldering@a-eskwadraat.nl>
2006-04-17 15:32:16 +0000

    Added alternative compare script that uses external program to check results.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@934 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ judge/check_diff.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# $Id$
+
+# Simple program for compare_program.sh script: this just runs a diff
+# comparison between the program and expected output. Should give same
+# results as the default compare.sh script.
+#
+# Additional options to diff can be passed here, like:
+#  -i    Ignore case differences.
+#  -b    Ignore changes in the amount of white space.
+#  -B    Ignore changes whose lines are all blank.
+# and see 'man diff' for more options.
+
+TESTIN="$1"
+PROGRAM="$2"
+TESTOUT="$3"
+
+diff "$PROGRAM" "$TESTOUT"
+EXITCODE=$?
+
+# EXITCODE = 1 indicates differences, others errors:
+[ $EXITCODE -gt 1 ] && exit 1
+
+exit 0
---
+++ judge/compare_program.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# $Id$
+
+# Compare wrapper-script for 'test_solution.sh'.
+# See that script for syntax and more info.
+#
+# This script is written to comply with the ICPC Validator Interface Standard
+# as described in http://www.ecs.csus.edu/pc2/doc/valistandard.html.
+
+# Usage: $0 <testdata.in> <program.out> <testdata.out> <result.xml> <diff.out>
+#
+# <testdata.in>   File containing testdata input.
+# <program.out>   File containing the program output.
+# <testdata.out>  File containing the correct output.
+# <result.xml>    File containing an XML document describing the result.
+# <diff.out>      File to write program/correct output differences to (optional).
+#
+#
+# Exits successfully except when an internal error occurs. Submitted
+# program output is considered correct when diff.out is empty (and
+# specified).
+#
+# This script calls another (configurable) program to check the
+# results. Calling syntax:
+#    $CHECK_PROGRAM <testdata.in> <program.out> <testdata.out>
+#
+# The $CHECK_PROGRAM should return the contents of <diff.out> to
+# standard output. It must exit with exitcode zero to indicate
+# successful checking.
+
+# Check program, specify with absolute path or use `dirname $0`:
+CHECK_PROGRAM="`dirname $0`/check_diff.sh"
+
+TESTIN="$1"
+PROGRAM="$2"
+TESTOUT="$3"
+RESULT="$4"
+DIFFOUT="$5"
+
+function writeresult()
+{
+    ( cat <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE result [
+  <!ELEMENT result (#PCDATA)>
+  <!ATTLIST result outcome CDATA #REQUIRED>
+]>
+<result outcome="$1">$1</result>
+EOF
+    ) > "$RESULT"
+}
+
+if [ ! -x "$CHECK_PROGRAM" ]; then
+	echo "Error: '$CHECK_PROGRAM' not found or executable." >&2
+	writeresult "Internal error"
+	exit 1
+fi
+
+# Run the program:
+"$CHECK_PROGRAM" "$TESTIN" "$PROGRAM" "$TESTOUT" > "$DIFFOUT"
+EXITCODE=$?
+
+# Exit with failure, when non-zero exitcode found:
+if [ $EXITCODE -ne 0 ]; then
+	echo "Error: '$CHECK_PROGRAM' exited with exitcode $EXITCODE." >&2
+	writeresult "Internal error"
+	exit 1
+fi
+
+# Check result and write result file:
+if [ -s "$DIFFOUT" ]; then
+	writeresult "Accepted"
+else
+	writeresult "Wrong answer"
+fi
+
+exit 0
