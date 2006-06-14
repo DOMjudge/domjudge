ce21779119807ccc4a1e819a92bf6fb0b5d5cea8
Jaap Eldering <eldering@a-eskwadraat.nl>
2006-06-14 21:54:25 +0000

    Java memsize test file.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@990 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ test-sources/test-memsize.java
@@ -0,0 +1,20 @@
+/* $Id$
+ *
+ * This should fail with RUN-ERROR due to running out of memory, which
+ * is restricted. Check the amount available, because the java binary
+ * might consume a lot of the total memory available.
+ */
+
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+		int i;
+		int[][] ar = new int[10240][];
+		
+		for(i=0; 1==1; i++) {
+			ar[i] = new int[1024*1024/4];
+			System.out.print("allocated " + (i+1) + " MB of memory\n");
+		}
+    }
+}
