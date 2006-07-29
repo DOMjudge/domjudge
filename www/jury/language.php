fa0a34f254e4a2a6ef49bb120e819d1b86986574
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2006-07-29 18:28:25 +0000

    make all buttons part of one form, and handle this form. reduces code and HTML output and circumvents line breaks.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1025 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/language.php
+++ www/jury/language.php
@@ -14,20 +14,22 @@ $title = 'Language '.htmlspecialchars(@$id);
 
 if ( ! $id ) error("Missing or invalid language id");
 
-if ( isset($_POST['cmd']) && $_POST['cmd'] == 'rejudge' ) {
-	rejudge('submission.langid',$id);
-	header('Location: '.getBaseURI().'jury/'.$pagename.'?id='.urlencode($id));
-	exit;
-}
+if ( !empty($_POST['cmd']) ) {
+	if ( isset($_POST['cmd']['rejudge']) ) {
+		rejudge('submission.langid',$id);
+		header('Location: '.getBaseURI().'jury/'.$pagename.'?id='.urlencode($id));
+		exit;
+	}
 
-if ( isset($_POST['cmd']) && $_POST['cmd'] == 'toggle_submit' ) {
-	$DB->q('UPDATE language SET allow_submit = %i WHERE langid = %s',
-		   $_POST['val'], $id);
-}
+	if ( isset($_POST['cmd']['toggle_submit']) ) {
+		$DB->q('UPDATE language SET allow_submit = %i WHERE langid = %s',
+			   $_POST['val']['toggle_submit'], $id);
+	}
 
-if ( isset($_POST['cmd']) && $_POST['cmd'] == 'toggle_judge' ) {
-	$DB->q('UPDATE language SET allow_judge = %i WHERE langid = %s',
-		   $_POST['val'], $id);
+	if ( isset($_POST['cmd']['toggle_judge']) ) {
+		$DB->q('UPDATE language SET allow_judge = %i WHERE langid = %s',
+			   $_POST['val']['toggle_judge'], $id);
+	}
 }
 
 
@@ -39,42 +41,28 @@ echo "<h1>Language ".htmlspecialchars($id)."</h1>\n\n";
 $data = $DB->q('TUPLE SELECT * FROM language WHERE langid = %s', $id);
 
 ?>
+
+<form action="<?=$pagename?>" method="post">
+<input type="hidden" name="id" value="<?=$id?>" />
+<input type="hidden" name="val[toggle_judge]" value="<?=!$data['allow_judge']?>" />
+<input type="hidden" name="val[toggle_submit]" value="<?=!$data['allow_submit']?>" />
 <table>
 <tr><td>ID:          </td><td><?=htmlspecialchars($data['langid'])?></td></tr>
 <tr><td>Name:        </td><td><?=htmlentities($data['name'])?></td></tr>
 <tr><td>Extension:   </td><td class="filename">.<?=htmlspecialchars($data['extension'])?></td></tr>
 <tr><td>Allow submit:</td><td><?=printyn($data['allow_submit'])?>
-<?php
-$val = ! $data['allow_submit'];
-$str = $val ? 'Allow' : 'Disallow';
-?>
-<form action="<?=$pagename?>" method="post">
-<input type="hidden" name="id" value="<?=$id?>" />
-<input type="hidden" name="cmd" value="toggle_submit" />
-<input type="hidden" name="val" value="<?=$val?>" />
-<input type="submit" value="toggle"
- onclick="return confirm('<?=$str?> submissions for this language?')" />
-</form></td></tr>
+ <input type="submit" name="cmd[toggle_submit]" value="toggle"
+ onclick="return confirm('<?= $data['allow_submit'] ? 'Disallow' : 'Allow' ?> submissions for this language?')" />
+</td></tr>
 <tr><td>Allow judge: </td><td><?=printyn($data['allow_judge'])?>
-<?php
-$val = ! $data['allow_judge'];
-$str = $val ? 'Allow' : 'Disallow';
-?>
-<form action="<?=$pagename?>" method="post">
-<input type="hidden" name="id" value="<?=$id?>" />
-<input type="hidden" name="cmd" value="toggle_judge" />
-<input type="hidden" name="val" value="<?=$val?>" />
-<input type="submit" value="toggle"
- onclick="return confirm('<?=$str?> judging for this language?')" />
-</form></td></tr>
-<tr><td>Timefactor:  </td><td><?=htmlspecialchars($data['time_factor'])?></td></tr>
+ <input type="submit" name="cmd[toggle_judge]" value="toggle"
+ onclick="return confirm('<?= $data['allow_judge'] ? 'Disallow' : 'Allow'?> judging for this language?')" />
+</td></tr>
+<tr><td>Time factor:  </td><td><?=htmlspecialchars($data['time_factor'])?></td></tr>
 </table>
 
-<form action="<?=$pagename?>" method="post">
 <p>
-<input type="hidden" name="id" value="<?=$id?>" />
-<input type="hidden" name="cmd" value="rejudge" />
-<input type="submit" value="REJUDGE ALL for language <?=htmlentities($data['name'])?>"
+<input type="submit" name="cmd[rejudge]" value="REJUDGE ALL for language <?=htmlentities($data['name'])?>"
  onclick="return confirm('Rejudge all submissions for this language?')" />
 </p>
 </form>
--- www/jury/problem.php
+++ www/jury/problem.php
@@ -15,20 +15,22 @@ $title = 'Problem '.htmlspecialchars(@$id);
 
 if ( ! $id ) error("Missing or invalid problem id");
 
-if ( isset($_POST['cmd']) && $_POST['cmd'] == 'rejudge' ) {
-	rejudge('submission.probid',$id);
-	header('Location: '.getBaseURI().'jury/'.$pagename.'?id='.urlencode($id));
-	exit;
-}
+if ( !empty($_POST['cmd']) ) {
+	if ( isset($_POST['cmd']['rejudge']) ) {
+		rejudge('submission.probid',$id);
+		header('Location: '.getBaseURI().'jury/'.$pagename.'?id='.urlencode($id));
+		exit;
+	}
 
-if ( isset($_POST['cmd']) && $_POST['cmd'] == 'toggle_submit' ) {
-	$DB->q('UPDATE problem SET allow_submit = %i WHERE probid = %s',
-		   $_POST['val'], $id);
-}
+	if ( isset($_POST['cmd']['toggle_submit']) ) {
+		$DB->q('UPDATE problem SET allow_submit = %i WHERE probid = %s',
+			   $_POST['val']['toggle_submit'], $id);
+	}
 
-if ( isset($_POST['cmd']) && $_POST['cmd'] == 'toggle_judge' ) {
-	$DB->q('UPDATE problem SET allow_judge = %i WHERE probid = %s',
-		   $_POST['val'], $id);
+	if ( isset($_POST['cmd']['toggle_judge']) ) {
+		$DB->q('UPDATE problem SET allow_judge = %i WHERE probid = %s',
+			   $_POST['val']['toggle_judge'], $id);
+	}
 }
 
 require('../header.php');
@@ -39,44 +41,31 @@ echo "<h1>Problem ".htmlspecialchars($id)."</h1>\n\n";
 $data = $DB->q('TUPLE SELECT * FROM problem NATURAL JOIN contest WHERE probid = %s', $id);
 
 ?>
+
+<form action="<?=$pagename?>" method="post">
+<input type="hidden" name="id" value="<?=$id?>" />
+<input type="hidden" name="val[toggle_judge]" value="<?=!$data['allow_judge']?>" />
+<input type="hidden" name="val[toggle_submit]" value="<?=!$data['allow_submit']?>" />
+
 <table>
 <tr><td>ID:          </td><td><?=htmlspecialchars($data['probid'])?></td></tr>
 <tr><td>Name:        </td><td><?=htmlentities($data['name'])?></td></tr>
 <tr><td>Contest:     </td><td><?=htmlspecialchars($data['cid']).' - '.
                                  htmlentities($data['contestname'])?></td></tr>
-<tr><td>Allow submit:</td><td><?=printyn($data['allow_submit'])?>
-<?php
-$val = ! $data['allow_submit'];
-$str = $val ? 'Allow' : 'Disallow';
-?>
-<form action="<?=$pagename?>" method="post">
-<input type="hidden" name="id" value="<?=$id?>" />
-<input type="hidden" name="cmd" value="toggle_submit" />
-<input type="hidden" name="val" value="<?=$val?>" />
-<input type="submit" value="toggle"
- onclick="return confirm('<?=$str?> submissions for this problem?')" />
-</form></td></tr>
+<tr><td>Allow submit:</td><td class="nobreak"><?=printyn($data['allow_submit'])?>
+ <input type="submit" name="cmd[toggle_submit]" value="toggle"
+ onclick="return confirm('<?= $data['allow_submit'] ? 'Disallow' : 'Allow' ?> submissions for this problem?')" />
+</td></tr>
 <tr><td>Allow judge: </td><td><?=printyn($data['allow_judge'])?>
-<?php
-$val = ! $data['allow_judge'];
-$str = $val ? 'Allow' : 'Disallow';
-?>
-<form action="<?=$pagename?>" method="post">
-<input type="hidden" name="id" value="<?=$id?>" />
-<input type="hidden" name="cmd" value="toggle_judge" />
-<input type="hidden" name="val" value="<?=$val?>" />
-<input type="submit" value="toggle"
- onclick="return confirm('<?=$str?> judging for this problem?')" />
-</form></td></tr>
+ <input type="submit" name="cmd[toggle_judge]" value="toggle"
+ onclick="return confirm('<?= $data['allow_judge'] ? 'Disallow' : 'Allow'?> judging for this problem?')" />
+</td></tr>
 <tr><td>Testdata:    </td><td class="filename"><?=htmlspecialchars($data['testdata'])?></td></tr>
 <tr><td>Timelimit:   </td><td><?=(int)$data['timelimit']?></td></tr>
 </table>
 
-<form action="<?=$pagename?>" method="post">
 <p>
-<input type="hidden" name="id" value="<?=$id?>" />
-<input type="hidden" name="cmd" value="rejudge" />
-<input type="submit" value="REJUDGE ALL for problem <?=$id?>"
+<input type="submit" name="cmd[rejudge]" value="REJUDGE ALL for problem <?=$id?>"
  onclick="return confirm('Rejudge all submissions for this problem?')" />
 </p>
 </form>
