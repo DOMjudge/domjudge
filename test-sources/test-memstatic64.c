ff39365e8526028104e9956c70a6245a950b8207
Jaap Eldering <eldering@a-eskwadraat.nl>
2006-09-10 18:34:11 +0000

    Change expected answer for high memory limits.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1038 caf94f87-7bd6-0310-88dc-dee8202e2269

--- test-sources/test-memstatic64.c
+++ test-sources/test-memstatic64.c
@@ -1,7 +1,8 @@
 /* $Id$
  *
- * This should fail with RUN-ERROR due to running out of memory, which
- * is restricted.
+ * This should fail in the default configuration with RUN-ERROR due to 
+ * running out of memory, which is restricted. When the memory limit is 
+ * set higher, it will give WRONG ANSWER.
  */
 
 #include <stdio.h>
