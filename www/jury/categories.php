33c206cac01e9b7c83795536e7baffdc9b04f7ea
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2006-10-30 16:29:45 +0000

    add number of teams in categories table (like affiliations)
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1136 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/categories.php
+++ www/jury/categories.php
@@ -13,18 +13,22 @@ require('menu.php');
 
 echo "<h1>Categories</h1>\n\n";
 
-$res = $DB->q('SELECT * FROM team_category ORDER BY sortorder,categoryid');
+$res = $DB->q('SELECT team_category.*,count(login) as numteams
+	FROM team_category LEFT JOIN team USING(categoryid)
+	GROUP BY team.categoryid
+	ORDER BY sortorder,categoryid');
 
 if( $res->count() == 0 ) {
 	echo "<p><em>No categories defined</em></p>\n\n";
 } else {
 	echo "<table class=\"list\">\n" .
-		"<tr><th>ID</th><th>sort</th><th>name</th></tr>\n";
+		"<tr><th>ID</th><th>sort</th><th>name</th><th>#teams</th></tr>\n";
 	while($row = $res->next()) {
 		echo "<tr class=\"category". (int)$row['categoryid'] .
 			"\"><td>" . (int)$row['categoryid'] .
 			"</td><td>" . (int)$row['sortorder'] .
 			"</td><td>" . htmlentities($row['name']) .
+			"</td><td align=\"right\">" . (int)$row['numteams'] .
 			"</td></tr>\n";
 	}
 	echo "</table>\n\n";
