0272748b6aa3d2b12944cd3548d9ca7db12c694d
Jaap Eldering <eldering@a-eskwadraat.nl>
2006-11-23 22:48:30 +0000

    Add renamed script to run command on all judgehosts.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1164 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ bin/runssh_judgehosts.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php -q
+<?php
+/**
+ * Program to run a specific command on all judgehosts using ssh.
+ * 
+ * Usage: $0 <program>
+ *
+ * $Id$
+ */
+	require ('../etc/config.php');
+
+	define ('SCRIPT_ID', 'runssh_judgehosts');
+	define ('LOGFILE', LOGDIR.'/check.log');
+
+	require (SYSTEM_ROOT . '/lib/init.php');
+
+	$argv = $_SERVER['argv'];
+	
+	$program = @$argv[1];
+
+	if ( ! $program ) error("No program specified");
+
+	logmsg(LOG_DEBUG, "running program '$program'");
+
+	$judgehosts = $DB->q('COLUMN SELECT hostname FROM judgehost');
+
+	foreach($judgehosts as $host) {
+		logmsg(LOG_DEBUG, "running on judgehost '$host'");
+		system("ssh $host $program",$exitcode);
+		if ( $exitcode != 0 ) {
+			logmsg(LOG_NOTICE, "on '$host': exitcode $exitcode");
+		}
+	}
+
+	logmsg(LOG_NOTICE, "finished");
+
+	exit;
