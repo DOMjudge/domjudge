32bf7e122669648695c0f0f707a4b526ebc2f4b3
Jaap Eldering <eldering@a-eskwadraat.nl>
2006-12-10 18:52:04 +0000

    Make DBNAME configurable in SQL scripts and Makefile.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1202 caf94f87-7bd6-0310-88dc-dee8202e2269

--- sql/mysql_create.sql
+++ sql/mysql_create.sql
@@ -1,7 +1,7 @@
 -- Create script for the DOMjudge MySQL tables.
 --
 -- You can pipe this file into the 'mysql' command to create domjudge
--- database and users. This assumes database name 'domjudge'.
+-- database and users.
 --
 -- THIS FILE SHOULD ALWAYS BE NON-READABLE!
 -- (because of database-login usernames/passwords)
@@ -9,7 +9,7 @@
 -- $Id: mysql_create.sql 1170 2006-11-26 14:52:33Z eldering $
 
 -- Create the domjudge database:
-CREATE DATABASE IF NOT EXISTS domjudge;
+CREATE DATABASE IF NOT EXISTS DOMJUDGE_DBNAME;
 
 -- Add users and passwords
 -- These passwords are initialised automatically when running
--- sql/mysql_remove.sql
+++ sql/mysql_remove.sql
@@ -1,14 +1,13 @@
 -- Remove script for the DOMjudge MySQL tables.
 --
 -- You can pipe this file into the 'mysql' command to remove the
--- domjudge database and the users associated with it. This assumes
--- database name 'domjudge'. USE WITH CARE!!!
+-- domjudge database and the users associated with it. USE WITH CARE!!!
 --
 -- $Id: mysql_remove.sql 1170 2006-11-26 14:52:33Z eldering $
 
 
 -- Remove the whole 'domjudge' database:
-DROP DATABASE IF EXISTS domjudge;
+DROP DATABASE IF EXISTS DOMJUDGE_DBNAME;
 
 -- Remove domjudge users:
 USE mysql;
