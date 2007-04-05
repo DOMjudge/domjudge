fe7423f3fbad910f56fa67648ae7655b9fe60c88
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2007-04-05 12:11:09 +0000

    - source balloon data from scoreboard_jury so balloon   notifications are not necessarily dependent on the scoreboard   being frozen or not (if you don't want to keep sending out balloons,   stop the balloon daemon) - list all the balloons a team is supposed to have, so this can   be checked by the balloon runner when delivering a new one.   (#1682759)
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1283 caf94f87-7bd6-0310-88dc-dee8202e2269

--- bin/balloons.php
+++ bin/balloons.php
@@ -19,14 +19,22 @@ $verbose = LOG_INFO;
 
 $waittime = 5;
 
-function notification_text($team, $problem) {
-	return
+function notification_text($team, $problem, $probs_solved, $probs_data) {
+	$ret = 
 		"Notification of a problem solved:\n".
 		"\n".
 		(empty($team['room']) ? "" : "Room:    ".$team['room']."\n" ) .
 		"Team:    ".$team['login'].": ".$team['name']."\n".
-		"Problem: ".$problem['probid'].": ".$problem['name'].
-		(empty($problem['color']) ? "" : " (color: ".$problem['color'].")" ) . "\n";
+		"Problem: ".$problem.": ".$probs_data[$problem]['name'].
+		(empty($probs_data[$problem]['color']) ? "" : " (colour: ".$probs_data[$problem]['color'].")" ) . "\n\n" .
+		"Current balloon status:\n";
+
+	foreach($probs_solved as $probid) {
+		$ret .= " - " . $probid .": " . $probs_data[$probid]['name'] .
+			(empty($probs_data[$probid]['color']) ? "" : " (colour: ".$probs_data[$probid]['color'].")" )."\n";
+	}
+
+	return $ret;
 }
 
 $cid = getCurContest();
@@ -46,9 +54,8 @@ while ( TRUE ) {
 	}
 	
 	do {
-		$res = $DB->q('SELECT s.*,t.name as teamname,t.room,p.name as probname,p.color
-		               FROM scoreboard_public s
-		               LEFT JOIN problem p USING(probid)
+		$res = $DB->q('SELECT s.*,t.name as teamname,t.room
+		               FROM scoreboard_jury s
 		               LEFT JOIN team t ON (t.login = s.team)
 		               WHERE s.cid = %i AND s.is_correct = 1 AND s.balloon = 0',
 		               $cid);
@@ -57,14 +64,17 @@ while ( TRUE ) {
 			$team = array ('name'   => $row['teamname'],
 			               'room'   => $row['room'],
 				       'login'  => $row['team']);
-			$prob = array ('name'   => $row['probname'],
-			               'probid' => $row['probid'],
-			               'color'  => $row['color']);
 
 			logmsg(LOG_DEBUG,"New problem solved: ".$row['probid'].
 				   " by team ".$row['team']);
-			
+
 			if ( defined('BALLOON_CMD') && BALLOON_CMD ) {
+			
+				$probs_solved = $DB->q('COLUMN SELECT probid FROM scoreboard_jury
+					WHERE cid = %i AND team = %s AND is_correct = 1',
+					$cid, $row['team']);
+				$probs_data = $DB->q('KEYTABLE SELECT probid AS ARRAYKEY,name,color FROM problem
+					WHERE cid = %i', $cid);
 				
 				logmsg(LOG_INFO,"Sending notification: team '".
 					   $row['team']."', problem '".$row['probid']."'.");
@@ -74,13 +84,13 @@ while ( TRUE ) {
 				$handle = popen(BALLOON_CMD, 'w');
 				if ( ! $handle ) error("Could not run command '".BALLOON_CMD."'");
 				
-				fwrite($handle,notification_text($team,$prob));
+				fwrite($handle,notification_text($team,$row['probid'],$probs_solved, $probs_data));
 				if ( ($exitcode = pclose($handle))!=0 ) {
 					warning("Notification command exited with exitcode $exitcode");
 				}
 			}
 			
-			$DB->q('UPDATE scoreboard_public SET balloon=1
+			$DB->q('UPDATE scoreboard_jury SET balloon=1
 			        WHERE cid = %i AND team = %s AND probid = %s',
 				   $row['cid'], $row['team'], $row['probid']);
 		}
