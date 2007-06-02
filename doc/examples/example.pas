c6001929da83205e671da2397e4ce1c441d17380
Jaap Eldering <eldering@a-eskwadraat.nl>
2007-06-02 09:22:23 +0000

    Small updates to the team documentation.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1416 caf94f87-7bd6-0310-88dc-dee8202e2269

--- doc/examples/example.pas
+++ doc/examples/example.pas
@@ -1,15 +1,15 @@
-program voorbeeld(input, output);
+program example(input, output);
 
 var
-   ntests, test : integer;
-   name			: string[100];
+	ntests, test : integer;
+	name         : string[100];
 
 begin
-   readln(ntests);
+	readln(ntests);
 
-   for test := 1 to ntests do
-   begin
-	  readln(name);
-	  writeln('Hello ',name,'!');
-   end;
+	for test := 1 to ntests do
+	begin
+		readln(name);
+		writeln('Hello ',name,'!');
+	end;
 end.
