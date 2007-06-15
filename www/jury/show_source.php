cca282b26da50213d828ee925563d750dcea1718
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2007-06-15 12:32:35 +0000

    implement source highlighting if PEAR Text::Highlighter class is available
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1472 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/show_source.php
+++ www/jury/show_source.php
@@ -21,7 +21,7 @@ $oldsource = $DB->q('MAYBETUPLE SELECT * FROM submission
                     $source['submittime']);
 
 if ( $oldsource ) {
-	echo '<p><a href="#diff">Goto diff to previous submission</a></p>';
+	echo "<p><a href=\"#diff\">Go to diff to previous submission</a></p>\n\n";
 }
 
 echo '<h2 class="filename"><a name="source"></a>Submission ' .
@@ -29,23 +29,54 @@ echo '<h2 class="filename"><a name="source"></a>Submission ' .
 	"<a href=\"show_source.php?id=$id\">" .
 	htmlspecialchars($source['sourcefile']) . "</a></h2>\n\n";
 
-$sourcelines = explode("\n", $source['sourcecode']);
-echo '<pre class="output_text">';
-$i = 1;
-$lnlen = strlen(count($sourcelines));
-foreach ($sourcelines as $line ) {
-	echo "<span class=\"lineno\">" . str_pad($i, $lnlen, ' ', STR_PAD_LEFT) .
-		"</span>  " . htmlspecialchars($line) . "\n";
-	$i++;
+// Use PEAR Text::Highlighter class if available
+if ( @include('Text/Highlighter.php' ) ) {
+	switch (strtolower($source['langid'])) {
+		case 'c':
+		case 'cpp':
+			$lang = 'cpp';
+			break;
+		case 'java';
+		case 'perl':
+		case 'ruby':
+		case 'php':
+		case 'python':
+			$lang = $source['langid'];
+	}
+	if ( isset($lang) ) {
+		include('Text/Highlighter/Renderer/Html.php');
+		$renderer = new Text_Highlighter_Renderer_Html(array("numbers" => HL_NUMBERS_TABLE, "tabsize" => 4));
+		$hl =& Text_Highlighter::factory(($source['langid'] == 'c'?'cpp':$source['langid']));
+	}
 }
-echo "</pre>\n\n";
 
+if ( isset($hl) ) {
+	// We managed to set up the highligher
+	$hl->setRenderer($renderer);
+	echo $hl->highlight($source['sourcecode']);
+} else {
+	// else display it ourselves
+	$sourcelines = explode("\n", $source['sourcecode']);
+	echo '<pre class="output_text">';
+	$i = 1;
+	$lnlen = strlen(count($sourcelines));
+	foreach ($sourcelines as $line ) {
+		echo "<span class=\"lineno\">" . str_pad($i, $lnlen, ' ', STR_PAD_LEFT) .
+			"</span>  " . htmlspecialchars($line) . "\n";
+		$i++;
+	}
+	echo "</pre>\n\n";
+}
+
+
+// show diff to old source
 if ( $oldsource ) {
 	
+	$oldfile = SUBMITDIR.'/'.$oldsource['sourcefile'];
+	$newfile = SUBMITDIR.'/'.$source['sourcefile'];
 	$oldid = (int)$oldsource['submitid'];
 
 	// Try different ways of diffing, in order of preference.
-
 	if ( function_exists('xdiff_string_diff') ) {
 		// The PECL xdiff PHP-extension.
 		
@@ -54,9 +85,6 @@ if ( $oldsource ) {
 		
 	} elseif ( is_readable($oldfile) && is_readable($newfile) ) {
 		// A direct diff on the sources in the SUBMITDIR.
-
-		$oldfile = SUBMITDIR.'/'.$oldsource['sourcefile'];
-		$newfile = SUBMITDIR.'/'.$source['sourcefile'];
 
 		$difftext = `diff -bBt -U 2 $oldfile $newfile 2>&1`;
 
