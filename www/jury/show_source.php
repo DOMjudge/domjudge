04dddf4e4671e65772dae3232278b230ffd96141
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2007-06-15 10:52:30 +0000

    add line numbers in source display, add some comments
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1471 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/show_source.php
+++ www/jury/show_source.php
@@ -29,27 +29,43 @@ echo '<h2 class="filename"><a name="source"></a>Submission ' .
 	"<a href=\"show_source.php?id=$id\">" .
 	htmlspecialchars($source['sourcefile']) . "</a></h2>\n\n";
 
-echo '<pre class="output_text">' .
-	htmlspecialchars($source['sourcecode']) . "</pre>\n\n";
+$sourcelines = explode("\n", $source['sourcecode']);
+echo '<pre class="output_text">';
+$i = 1;
+$lnlen = strlen(count($sourcelines));
+foreach ($sourcelines as $line ) {
+	echo "<span class=\"lineno\">" . str_pad($i, $lnlen, ' ', STR_PAD_LEFT) .
+		"</span>  " . htmlspecialchars($line) . "\n";
+	$i++;
+}
+echo "</pre>\n\n";
 
 if ( $oldsource ) {
 	
 	$oldid = (int)$oldsource['submitid'];
 
-	$oldfile = SUBMITDIR.'/'.$oldsource['sourcefile'];
-	$newfile = SUBMITDIR.'/'.$source['sourcefile'];
-	
+	// Try different ways of diffing, in order of preference.
+
 	if ( function_exists('xdiff_string_diff') ) {
+		// The PECL xdiff PHP-extension.
 		
 		$difftext = xdiff_string_diff($oldsource['sourcecode'],
 									  $source['sourcecode'],2);
 		
 	} elseif ( is_readable($oldfile) && is_readable($newfile) ) {
+		// A direct diff on the sources in the SUBMITDIR.
+
+		$oldfile = SUBMITDIR.'/'.$oldsource['sourcefile'];
+		$newfile = SUBMITDIR.'/'.$source['sourcefile'];
 
 		$difftext = `diff -bBt -U 2 $oldfile $newfile 2>&1`;
 
 	} else {
-		
+		// Write sources to tempfile and diff those.
+		// This does not work with safe_mode, which is a known
+		// limitation of tempnam. In safe_mode, this currently
+		// creates temp files that we cannot access nor remove...
+
 		if ( ! ($oldfile = tempnam("/tmp","source-old-s$oldid-")) ||
 			 ! ($newfile = tempnam("/tmp","source-new-s$id-"   )) ||
 			 ! ($oldhandle = fopen($oldfile,'w')) ||
