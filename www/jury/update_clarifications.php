ad511dd91775c1a46dec2403f2489dc4ea77a480
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2007-07-16 09:53:46 +0000

    simplify update_clarifications code/queries and add a comment
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1554 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/update_clarifications.php
+++ www/jury/update_clarifications.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Supply information for AJAX RPC calls (update the number
+ * of new clarifications in the menu line).
+ *
+ * $Id$
+ */
 
 require('init.php');
 
@@ -7,7 +13,6 @@ header("Expires: Wed, 10 Feb 1971 05:00:00 GMT");
 header("Content-type: text/plain");
 
 $cid = getCurContest();
-$cnew = $DB->q('VALUE SELECT COUNT(*) FROM clarification
-                WHERE sender IS NOT NULL AND cid = %i AND answered = 0
-                ORDER BY submittime DESC', $cid);
-echo $cnew;
+echo (int) $DB->q('VALUE SELECT COUNT(*) FROM clarification
+                   WHERE sender IS NOT NULL AND cid = %i AND answered = 0',
+                   $cid);
--- www/team/update_clarifications.php
+++ www/team/update_clarifications.php
@@ -1,14 +1,15 @@
 <?php
-
+/**
+ * Supply information for AJAX RPC calls (update the number
+ * of new clarifications in the menu line).
+ *
+ * $Id$
+ */
 require('init.php');
 
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Wed, 10 Feb 1971 05:00:00 GMT");
 header("Content-type: text/plain");
 
-$res = $DB->q('KEYTABLE SELECT type AS ARRAYKEY, COUNT(*) AS count FROM team_unread
-               WHERE teamid = %s GROUP BY type', $login);
-if ( isset($res['clarification']) ) {
-	echo (int)$res['clarification']['count'];
-} else
-	echo 0;
+echo (int) $DB->q('VALUE SELECT COUNT(*) FROM team_unread
+                   WHERE type="clarification" AND teamid = %s', $login);
