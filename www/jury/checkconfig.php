28444cfad0d97e82b2bb6417c140febe3e21a852
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2007-07-23 21:19:52 +0000

    also check for optional Text_Highlighter class
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1595 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/checkconfig.php
+++ www/jury/checkconfig.php
@@ -58,6 +58,12 @@ if( !function_exists('version_compare') || version_compare( '4.3.2',PHP_VERSION,
 }
 echo " <a href=\"?phpinfo\">(phpinfo)</a></p>\n\n";
 
+@include('Text/Highlighter.php');
+echo "<p>Optional PEAR Text_Highlighter class is ";
+echo class_exists('Text_Highlighter') ? "available" : "not available.\n" .
+	"Install it in PHP's include path to get better source syntax highlighting";
+echo ".</p>\n\n";
+
 ?>
 
 <h2>Authentication</h2>
