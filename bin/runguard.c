4acbb2f1667f428c0696a1e9ead086fc731b0c60
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2007-07-31 13:33:49 +0000

    Change runguard license to GPL 2 or higher. The current code is a different implementation of the original idea from timeout.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1605 caf94f87-7bd6-0310-88dc-dee8202e2269

--- bin/runguard.c
+++ bin/runguard.c
@@ -1,14 +1,25 @@
 /*
    runguard -- run command with optional restrictions: time, root, user
-   Copyright (C) 2004 Jaap Eldering (eldering@a-eskwadraat.nl).
+   Copyright (C) 2004-2007 Jaap Eldering (eldering@a-eskwadraat.nl).
 
-   Based on timeout - written by Wietse Venema
+   Based on an idea from the timeout program, written by Wietse Venema
+   as part of The Coroner's Toolkit.
    
    $Id$
 
-   This program is licensed under the terms of the IBM PUBLIC LICENSE,
-   see 'timeout.license'. The IBM PUBLIC LICENSE must be distributed
-   with this software.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
    
  */
 
