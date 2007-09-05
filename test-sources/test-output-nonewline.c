8f2d7bd13f4a96e68bacaac867d587cdc9b8a043
Jaap Eldering <eldering@a-eskwadraat.nl>
2007-09-05 19:37:13 +0000

    - Revert test target dependency on testfiles as it gives problems with shell IFS. - Add Id tag to test-output-nonewline.c.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1695 caf94f87-7bd6-0310-88dc-dee8202e2269

--- test-sources/test-output-nonewline.c
+++ test-sources/test-output-nonewline.c
@@ -1,4 +1,4 @@
-/* $Id: test-slow-output.c 884 2005-10-10 17:57:31Z eldering $
+/* $Id$
  *
  * Writes one line of output without a trailing newline. This should
  * give WRONG-ANSWER and the diff output should show the line.
