48d5417257fb02dcbd98cd29957bff4ae5c983ef
Peter van de Werken <pwerken@a-eskwadraat.nl>
2007-10-06 14:35:19 +0000

    correctly freeze scoreboard
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/branches/2.0SVN@1725 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/lib.misc.php
+++ lib/lib.misc.php
@@ -125,13 +125,13 @@ function calcScoreRow($cid, $team, $prob) {
 	if ( $correct_p == 0 ) $penalty_p = 0;
 
 	// insert or update the values in the jury scores table
-	$DB->q('REPLACE INTO scoreboard_jury
+	$DB->q('REPLACE INTO scoreboard_public
 	        (cid, teamid, probid, submissions, totaltime, penalty, is_correct, balloon)
 	        VALUES (%i,%s,%s,%i,%i,%i,%i,%i)',
 	       $cid, $team, $prob, $submitted_p, $time_p, $penalty_p, $correct_p, $balloon);
 
 	// insert or update the values in the public/team scores table
-	$DB->q('REPLACE INTO scoreboard_public
+	$DB->q('REPLACE INTO scoreboard_jury
 	        (cid, teamid, probid, submissions, totaltime, penalty, is_correct, balloon)
 	        VALUES (%i,%s,%s,%i,%i,%i,%i,%i)',
 	       $cid, $team, $prob, $submitted_j, $time_j, $penalty_j, $correct_j, $balloon);
