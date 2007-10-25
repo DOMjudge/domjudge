f7a692d0f5481681dc944d80b6b9b7edce73cc9a
Jaap Eldering <eldering@a-eskwadraat.nl>
2007-10-25 15:23:42 +0000

    Apply r1831 from trunk: revert incorrect fix of visibility.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/branches/nwerc@1838 caf94f87-7bd6-0310-88dc-dee8202e2269

--- sql/mysql_db_examples.sql
+++ sql/mysql_db_examples.sql
@@ -75,9 +75,9 @@ INSERT INTO `team_affiliation` (`affilid`, `name`, `country`, `comments`) VALUES
 -- Dumping data for table `team_category`
 -- 
 
-INSERT INTO `team_category` (`categoryid`, `name`, `sortorder`, `color`, `visible`) VALUES (1, 'Participants', 1, NULL, 1);
+INSERT INTO `team_category` (`categoryid`, `name`, `sortorder`, `color`, `visible`) VALUES (1, 'Participants', 0, NULL, 1);
 INSERT INTO `team_category` (`categoryid`, `name`, `sortorder`, `color`, `visible`) VALUES (2, 'Observers', 1, "#ffcc33", 1);
-INSERT INTO `team_category` (`categoryid`, `name`, `sortorder`, `color`, `visible`) VALUES (3, 'Organisation', 0, "#ff99cc", 0);
+INSERT INTO `team_category` (`categoryid`, `name`, `sortorder`, `color`, `visible`) VALUES (3, 'Organisation', 1, "#ff99cc", 0);
 
 
 -- 
