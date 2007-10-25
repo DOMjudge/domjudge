3c152e62a6ea793e4b89ed0d2332b73bb1d1a529
Jaap Eldering <eldering@a-eskwadraat.nl>
2007-10-25 12:43:15 +0000

    Fix merge of invisible teams to include r1763,1784,1821 from trunk.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/branches/nwerc@1837 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/scoreboard.php
+++ www/scoreboard.php
@@ -122,21 +122,19 @@ function putScoreBoard($myteamid = null, $isjury = FALSE, $static = FALSE) {
 	// the SCORES table contains the totals for each team which we will
 	// use for determining the ranking. Initialise them here
 	foreach ($teams as $login => $team ) {
-		if ($isjury || !$team['invisible']) {
-			$SCORES[$login]['num_correct'] = 0;
-			$SCORES[$login]['total_time']  = 0;
-			$SCORES[$login]['last_solved'] = 0;
-			$SCORES[$login]['teamname']    = $team['name'];
-			$SCORES[$login]['categoryid']  = $team['categoryid'];
-			$SCORES[$login]['sortorder']   = $team['sortorder'];
-		}
+		$SCORES[$login]['num_correct'] = 0;
+		$SCORES[$login]['total_time']  = 0;
+		$SCORES[$login]['last_solved'] = 0;
+		$SCORES[$login]['teamname']    = $team['name'];
+		$SCORES[$login]['categoryid']  = $team['categoryid'];
+		$SCORES[$login]['sortorder']   = $team['sortorder'];
 	}
 
 	// loop all info the scoreboard cache and put it in our own datastructure
 	while ( $srow = $scoredata->next() ) {
 	
-		// skip this row if we're don't know or care about the team or problem is not known by us
-		if ( ! array_key_exists ( $srow['teamid'], $SCORES ) ||
+		// skip this row if the team or problem is not known by us
+		if ( ! array_key_exists ( $srow['teamid'], $teams ) ||
 		     ! array_key_exists ( $srow['probid'], $probs ) ) continue;
 	
 		// fill our matrix with the scores from the database,
@@ -330,12 +328,10 @@ function putScoreBoard($myteamid = null, $isjury = FALSE, $static = FALSE) {
 			jurylink('team_categories.php','Legend',$isjury) .
 			"</th></tr></thead>\n<tbody>\n";
 		while ( $cat = $categs->next() ) {
-			if ($isjury || !$cat['invisible']) {
-				echo '<tr' . (!empty($cat['color']) ? ' style="background: ' .
-				              $cat['color'] . ';"' : '') . '>' .
-					'<td align="center" class="scoretn">' .
-					jurylink(null,htmlspecialchars($cat['name']),$isjury) .	"</td></tr>\n";
-			}
+			echo '<tr' . (!empty($cat['color']) ? ' style="background: ' .
+				          $cat['color'] . ';"' : '') . '>' .
+				'<td align="center" class="scoretn">' .
+				jurylink(null,htmlspecialchars($cat['name']),$isjury) .	"</td></tr>\n";
 		}
 		echo "</tbody>\n</table>\n\n";
 	}
