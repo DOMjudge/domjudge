d320b7067ae3a8c57c14aeaa7293152f861afd94
Jaap Eldering <eldering@a-eskwadraat.nl>
2007-11-25 12:18:18 +0000

    Added test source generating binary output.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@1909 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ test-sources/test-binary-output.c
@@ -0,0 +1,23 @@
+/* $Id$
+ *
+ * This program test binary output and should give WRONG-ANSWER, but
+ * display diff correctly.
+ */
+
+#include <stdio.h>
+
+#define min(a,b) ((a)<(b) ? (a) : (b))
+
+int main()
+{
+	int c;
+
+	printf("testing all characters...");
+	for(c=0; c<256; c++) {
+		if ( c%10==0 ) printf("\ntesting characters %d-%d...\n",c,min(c+9,255));
+		printf("%c",c);
+	}
+	printf("\ndone.\n");
+	
+	return 0;
+}
