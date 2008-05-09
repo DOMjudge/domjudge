988ddcec46b96492a13970a1f6dc59998bf776d1
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2008-05-09 10:51:55 +0000

    add id's so that labels work, and set maxlength to real max length of a passwd
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2049 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/team/validate.php
+++ www/team/validate.php
@@ -28,8 +28,8 @@ Please supply team credentials below, or contact a staff member for assistance.
 
 <form action="checkpasswd.php" method="post">
 <table>
-<tr><td><label for="login">Login:</label></td><td><input type="text" name="login" value="" size="15" maxlength="15" accesskey="l" /></td></tr>
-<tr><td><label for="passwd">Password:</label></td><td><input type="password" name="passwd" value="" size="15" maxlength="15" accesskey="p" /></td></tr>
+<tr><td><label for="login">Login:</label></td><td><input type="text" id="login" name="login" value="" size="15" maxlength="15" accesskey="l" /></td></tr>
+<tr><td><label for="passwd">Password:</label></td><td><input type="password" id="passwd" name="passwd" value="" size="15" maxlength="255" accesskey="p" /></td></tr>
 <tr><td colspan="2" align="center"><input type="submit" value="Login" /></td></tr>
 </table>
 </form>
