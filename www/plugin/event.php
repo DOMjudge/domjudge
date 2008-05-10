725d745a230e38157883103d4888a1b9b18a0044
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-05-10 16:18:18 +0000

    Beginning of new design event system via HTTP/XML.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2075 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ www/plugin/event.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Output events in XML format.
+ *
+ * $Id$
+ *
+ * Part of the DOMjudge Programming Contest Jury System and licenced
+ * under the GNU GPL. See README and COPYING for details.
+ */
+
+require('init.php');
+
+if ( isset($_REQUEST['fromid']) ) {
+	$fromid = (int) $_REQUEST['fromid'];
+} else {
+	$fromid = -1;
+}
+
+if ( isset($_REQUEST['toid']) ) {
+	$toid = (int) $_REQUEST['toid'];
+} else {
+	$toid = 2147483647;
+}
+
+$now = now();
+
+$cdata = getCurContest(TRUE);
+$cid = $cdata['cid'];
+
+$cstarted = difftime($now, $cdata['starttime'])>0;
+$cended   = difftime($now, $cdata['endtime'])  >0;
+
+function infreeze($time) {
+	if ( ( ! empty($cdata['freezetime']) &&
+		   difftime($time, $cdata['freezetime'])>0 ) &&
+		!( ! empty($cdata['unfreezetime']) &&
+		   difftime($time, $cdata['unfreezetime'])<=0 ) ) return TRUE;
+	return FALSE;
+}
+
+$res = $DB->q('SELECT * FROM event WHERE eventid >= %i AND eventid < %i
+               ORDER BY eventid', $fromid, $toid);
+
+$xml = new DOMDocument('1.0', DJ_CHARACTER_SET);
+
+$root = $xml->createElement('root');
+$xml->appendChild($root);
+
+$events = $xml->createElement('events');
+$root->appendChild($events);
+
+while ( $row = $res->next() ) {
+	
+	$event = $xml->createElement('event');
+	$event->setAttribute('id', $row['eventid']);
+
+	switch ( $row['description'] ) {
+
+	case 'problem submitted':
+		if ( !IS_JURY && infreeze($row['eventtime']) ) continue(2);
+		
+		$data = $DB->q('TUPLE SELECT s.submittime, t.name AS teamname,
+		                             p.name AS probname, l.name AS langname
+		                FROM submission s
+		                LEFT JOIN team     t ON (t.login    = s.teamid)
+		                LEFT JOIN problem  p ON (p.probid   = s.probid)
+		                LEFT JOIN language l ON (l.langid   = s.langid)
+		                WHERE s.submitid = %i', $row['submitid']);
+
+		$elem = $xml->createElement('submission');
+		$elem->setAttribute('id', $row['submitid']);
+
+		$subelem = $xml->createElement('team', htmlspecialchars($data['teamname']));
+		$subelem->setAttribute('id', $row['teamid']);
+		$elem->appendChild($subelem);
+		
+		$subelem = $xml->createElement('problem', htmlspecialchars($data['probname']));
+		$subelem->setAttribute('id', $row['probid']);
+		$elem->appendChild($subelem);
+		
+		$subelem = $xml->createElement('language', htmlspecialchars($data['langname']));
+		$subelem->setAttribute('id', $row['langid']);
+		$elem->appendChild($subelem);
+		break;
+		
+	case 'problem judged':
+		$data = $DB->q('TUPLE SELECT s.submittime, j.result FROM judging j
+		                LEFT JOIN submission s ON (s.submitid = j.submitid)
+		                WHERE j.judgingid = %i', $row['judgingid']);
+
+		if ( !IS_JURY && infreeze($data['submittime']) ) continue(2);
+
+
+		$elem = $xml->createElement('judging', htmlspecialchars($data['result']));
+		$elem->setAttribute('id', $row['judgingid']);
+		
+		break;
+			
+	case 'clarification':
+		$data = $DB->q('TUPLE SELECT * FROM clarification
+		                WHERE clarid = %i', $row['clarid']);
+		
+		$elem = $xml->createElement('clarification', htmlspecialchars($data['body']));
+		$elem->setAttribute('id', $row['clarid']);
+
+		break;
+	}
+	
+	$event->appendChild($elem);
+	
+	$events->appendChild($event);
+}
+
+header('Content-Type: text/xml; charset=' . DJ_CHARACTER_SET);
+
+$xml->formatOutput = true;
+echo $xml->saveXML();
---
+++ www/plugin/init.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Include required files.
+ *
+ * $Id$
+ *
+ * Part of the DOMjudge Programming Contest Jury System and licenced
+ * under the GNU GPL. See README and COPYING for details.
+ */
+
+require_once('../../etc/config.php');
+
+if ( @$_SERVER['REMOTE_USER'] == "jury" ) {
+	define('IS_JURY', 1);
+	require_once(SYSTEM_ROOT . '/lib/use_db_jury.php');
+} else {
+	define('IS_JURY', 0);
+	require_once(SYSTEM_ROOT . '/lib/use_db_team.php');
+}
+
+if( DEBUG ) {
+	include_once (SYSTEM_ROOT . '/lib/lib.timer.php');
+}
+
+require_once(SYSTEM_ROOT . '/lib/lib.error.php');
+require_once(SYSTEM_ROOT . '/lib/lib.misc.php');
+
+require_once('../common.php');
+require_once('../print.php');
+
+$cdata = getCurContest(TRUE);
+$cid = (int)$cdata['cid'];
+
