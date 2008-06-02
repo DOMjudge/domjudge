49c26363fab90c7fb8bc894a60df20c8fd06c4ba
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-06-02 21:57:47 +0000

    Set upload_max_filesize and related PHP settings higher for testcase uploads and do better error checking.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2135 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/testcase.php
+++ www/jury/testcase.php
@@ -19,16 +19,40 @@ requireAdmin();
 $result = '';
 if ( isset($_POST['probid']) ) {
 	foreach(array('input','output') as $inout) {
-		if ( !empty($_FILES['update_'.$inout]['tmp_name']) ) {
+
+		if ( !empty($_FILES['update_'.$inout]['name']) ) {
+
+			// Check for upload errors:
+			switch ( $_FILES['update_'.$inout]['error'] ) {
+			case UPLOAD_ERR_OK: // everything ok!
+				break;
+			case UPLOAD_ERR_INI_SIZE:
+				error('The uploaded file is too large (exceeds the upload_max_filesize directive).');
+			case UPLOAD_ERR_FORM_SIZE:
+				error('The uploaded file is too large (exceeds the MAX_FILE_SIZE directive).');
+			case UPLOAD_ERR_PARTIAL:
+				error('The uploaded file was only partially uploaded.');
+			case UPLOAD_ERR_NO_FILE:
+				error('No file was uploaded.');
+			case 6:	// UPLOAD_ERR_NO_TMP_DIR, constant doesn't exist in our minimal PHP version
+				error('Missing a temporary folder.');
+			case 7: // UPLOAD_ERR_CANT_WRITE
+				error('Failed to write file to disk.');
+			case 8: // UPLOAD_ERR_EXTENSION
+				error('File upload stopped by extension.');
+			default:
+				error('Unknown error while uploading: '. $_FILES['update_'.$inout]['error'] . '.');
+			}
+
 			$content = file_get_contents($_FILES['update_'.$inout]['tmp_name']);
 			if ( $DB->q("VALUE SELECT count(id) FROM testcase WHERE probid = %s", $probid) ) {
 				$DB->q("UPDATE testcase SET md5sum_$inout = %s, $inout = %s
-					WHERE probid = %s",
-					md5($content), $content, $probid);
+				        WHERE probid = %s",
+				       md5($content), $content, $probid);
 			} else {
 				$DB->q("INSERT INTO testcase (probid,md5sum_$inout,$inout)
-					VALUES (%s,%s,%s)",
-					$probid, md5($content), $content);
+				        VALUES (%s,%s,%s)",
+				       $probid, md5($content), $content);
 			}
 			$result .= "<li> Updated $inout from " .
 				htmlspecialchars($_FILES['update_'.$inout]['name']) .
