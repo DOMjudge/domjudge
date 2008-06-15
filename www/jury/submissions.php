adb95fd4a9ace894c972ea454cf049ff61b33d6a
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2008-06-15 08:32:22 +0000

    put show ids into the submit button name; this resolves a XHTML error but also makes the system independent of the exact labeling of the button.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2157 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/submissions.php
+++ www/jury/submissions.php
@@ -13,12 +13,11 @@ if ( isset($_REQUEST['show']) ) {
 	$show = (int)$_REQUEST['show'];
 	if($show < 0 || $show > 3)	$show = 0;
 }
+
 if ( isset($_REQUEST['view']) ) {
-	switch( $_REQUEST['view'] ) {
-		case 'unverified':	$show = 2;	break;
-		case 'all':			$show = 1;	break;
-		case 'newest':
-		default:			$show = 0;	break;
+	// did someone press any of the three view buttons?
+	for ($i=0; $i<=2; ++$i) {
+		if ( isset($_REQUEST['view'][$i]) ) $show = $i;
 	}
 }
 
@@ -37,9 +36,9 @@ require_once(SYSTEM_ROOT . '/lib/www/forms.php');
 
 echo addForm('submissions.php', 'get')
 	. "<p>\n"
-	. addSubmit('newest',     'view', null, ($show != 0))
-	. addSubmit('all',        'view', null, ($show != 1))
-	. addSubmit('unverified', 'view', null, ($show != 2))
+	. addSubmit('newest',     'view[0]', null, ($show != 0))
+	. addSubmit('all',        'view[1]', null, ($show != 1))
+	. addSubmit('unverified', 'view[2]', null, ($show != 2))
 	. "</p>\n"
 	. addEndForm();
 
