5165ad03cdd2a2a45bb0024a7b0375b7c777ac77
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2008-06-22 16:27:24 +0000

    allow testdata to be downloaded next to being uploaded. we don't display it inline because it may be quite large. that could be added of course if there's demand.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2169 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/testcase.php
+++ www/jury/testcase.php
@@ -16,6 +16,32 @@ require('init.php');
 
 requireAdmin();
 
+$INOROUT = array('input','output');
+
+// Download testcase
+if ( isset ($_GET['fetch']) && in_array($_GET['fetch'], $INOROUT)) {
+	$fetch = $_GET['fetch'];
+	$filename = $probid . "." . $fetch;
+
+	$size = $DB->q("MAYBEVALUE SELECT OCTET_LENGTH($fetch)
+		FROM testcase WHERE probid = %s", $probid);
+
+	// sanity check before we start to output headers
+	if ( empty($size) || !is_numeric($size)) error ("Problem while fetching testcase");
+
+	header("Content-Type: application/octet-stream; name=\"$filename\"");
+	header("Content-Disposition: inline; filename=\"$filename\"");
+	header("Content-Length: $size");
+
+	// Todo: add some more metainformation to headers?
+
+	// is this good enough for large testsets? Or do we need to stream
+	// them directly from the database query result (is that even possible?
+	echo $DB->q("VALUE SELECT $fetch FROM testcase WHERE probid = %s", $probid);
+
+	exit(0);
+}
+
 $title = 'Testcase for problem '.htmlspecialchars(@$probid);
 
 require(SYSTEM_ROOT . '/lib/www/header.php');
@@ -27,7 +53,7 @@ echo "<h1>" . $title ."</h1>\n\n";
 
 $result = '';
 if ( isset($_POST['probid']) ) {
-	foreach(array('input','output') as $inout) {
+	foreach($INOROUT as $inout) {
 
 		if ( !empty($_FILES['update_'.$inout]['name']) ) {
 
@@ -63,13 +89,17 @@ $data = $DB->q('MAYBETUPLE SELECT
 echo addForm('', 'post', null, 'multipart/form-data') . 
 	addHidden('probid', $probid);
 
-foreach(array('input','output') as $inout) {
+foreach($INOROUT as $inout) {
 
 	echo "<h2>" . ucfirst($inout) . "</h2>\n\n";
 	if ( isset($data['size_'.$inout]) ) {
 		echo "<table>\n<tr><td>Size:</td><td>" . htmlspecialchars($data['size_' . $inout]) . " bytes</td></tr>\n";
 		echo "<tr><td>MD5 sum:</td><td>" . htmlspecialchars($data['md5sum_' . $inout]) . "</td></tr>\n";
+		echo "<tr><td>Download:</td><td class=\"filename\"><a href=\"./testcase.php?probid=" .
+			urlencode($probid) . "&amp;fetch=" . $inout . "\">" .
+			htmlspecialchars($probid) . "." . $inout . "</a></td>\n";
 		echo "<tr><td>Replace with:</td><td>" . addFileField('update_'.$inout) . "</td></tr>\n";
+
 		echo "</table>\n";
 	} else {
 		echo "<p>No testcase yet.</p>\n";
