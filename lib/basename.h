75b84078d71a702b6a57ab5908b9e21a1996aa61
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-06-23 21:14:45 +0000

    Removed LGPL by relicencing to GPL.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2179 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/basename.h
+++ lib/basename.h
@@ -1,21 +1,12 @@
-/* Return the name-within-directory of a file name.
-   Copyright (C) 1996,97,98,2002 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; see the file COPYING.lgpl. If not,
-   write to the Free Software Foundation, Inc., 51 Franklin Street,
-   Fifth Floor, Boston, MA  02110-1301, USA. */
+/**
+ * basename - return the name-within-directory of a file name.
+ * Inspired by basename.c from the GNU C Library.
+ *
+ * $Id$
+ *
+ * Part of the DOMjudge Programming Contest Jury System and licenced
+ * under the GNU GPL. See README and COPYING for details.
+ */
 
 #include <string.h>
 
