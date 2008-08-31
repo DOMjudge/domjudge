badb0961cbb15de79e6fdb5b8d3ed3354120bf1a
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-08-31 19:24:15 +0000

    Add note on DB initialization.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2331 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/init.php
+++ lib/init.php
@@ -2,6 +2,9 @@
 // $Id$
 if ( !defined('LIBDIR') ) die ("LIBDIR not defined.");
 
-require(LIBDIR . '/use_db.php');
 require(LIBDIR . '/lib.error.php');
 require(LIBDIR . '/lib.misc.php');
+require(LIBDIR . '/use_db.php');
+
+// Here or in script?
+//setup_database_connection('jury');
