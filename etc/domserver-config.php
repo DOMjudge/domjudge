733320d4552f189388c369868b33c4d285f74107
Jeroen van Wolffelaar <jeroen@a-eskwadraat.nl>
2008-08-31 19:23:41 +0000

    Deliver domserver-config.php as-is
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2299 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ etc/domserver-config.php
@@ -0,0 +1,14 @@
+<?
+
+// Default config
+
+define('DEBUG', false);
+define('WEBBASEURI', '/domjudge/');
+
+$DOMJUDGE_ADMINS = array();
+
+// Specify here which of the users in jurymembers.htpasswd should have admin 
+// rights on top of their jury rights
+// $DOMJUDGE_ADMINS[] = 'admin';
+
+
