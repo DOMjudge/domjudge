548e07c6e60d808cc7f8326cf9c05ec9ff611034
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-08-31 19:24:10 +0000

    Include DEBUG config options and fix documentation of htpasswd file.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2325 caf94f87-7bd6-0310-88dc-dee8202e2269

--- etc/domserver-config.php
+++ etc/domserver-config.php
@@ -2,13 +2,24 @@
 
 // Default config
 
-define('DEBUG', false);
+// Set DEBUG as a bitmask of the following settings.
+// Of course never to be used on live systems!
+//
+// Display PHP notice level warnings
+define('DEBUG_PHP_NOTICE', 1);
+//
+// Display timings for loading webpages
+define('DEBUG_TIMINGS', 2);
+//
+// Display SQL queries on webpages
+define('DEBUG_SQL', 4);
+
+define('DEBUG', 1);
 define('WEBBASEURI', '/domjudge/');
 
 $DOMJUDGE_ADMINS = array();
 
-// Specify here which of the users in jurymembers.htpasswd should have admin 
+// Specify here which of the users in htpasswd-jury should have admin 
 // rights on top of their jury rights
 // $DOMJUDGE_ADMINS[] = 'admin';
-
 
