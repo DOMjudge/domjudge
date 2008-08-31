68aa902d2cba65d766b880f2f2b33f3d1c269f31
Jeroen van Wolffelaar <jeroen@a-eskwadraat.nl>
2008-08-31 19:23:28 +0000

    Rewrite use_db logic to read some config file
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2284 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ lib/use_db.php
@@ -0,0 +1,33 @@
+<?php
+/* $Id$ */
+
+require('lib.database.php');
+
+function setup_database_connection($privlevel)
+{
+
+	if (!defined('DBPRIVLEVEL')) {
+		errorbla(Je moet het level aangeven van de privileges;
+	}
+
+	$credentials = file( LIB_PATH . '/database-credentials.csv' );
+
+	global $DB;
+
+	if ($DB) {
+		user_error("DB al geset");
+		exit();
+	}
+
+	for ($credentials as $credential) {
+		list ($priv, $host, $db, $user, $pass) = explode(':', trim($credential)));
+		if ($priv != $privlevel) continue;
+
+		$DB = new db ($db, $host, $user, $pass);
+	}
+
+	if (!$DB) {
+		user_error(sprintf("Privilege level '%s' not supported", $privlevel));
+	}
+}
+
