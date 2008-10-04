7b8a609b3575dbc95f1f26f364b05b0101642a9f
Peter van de Werken <pwerken@a-eskwadraat.nl>
2008-10-04 12:46:31 +0000

    fix casing of problem id
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/branches/nwerc2008@2387 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/team/upload.php
+++ www/team/upload.php
@@ -107,7 +107,7 @@ echo "<table>\n" .
 	"<tr><td>Language:</td><td><i>".htmlspecialchars($lang['name'])."</i></td></tr>\n" .
 	"</table>\n";
 
-submit_solution($login, $ip, $probid, $langext, $_FILES['code']['tmp_name']); 
+submit_solution($login, $ip, $probid['probid'], $langext, $_FILES['code']['tmp_name']); 
 
 echo '<div id="uploadstatus">';
 if (NONINTERACTIVE) echo '<!-- noninteractive-upload-successful -->';
