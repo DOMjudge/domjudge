b0251c78bba62b8cb1add5c65f621335facfec4d
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-10-10 18:28:02 +0000

    Revert r2406 and clarify comment on using file_get_contents in PHP >=5.1.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2430 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/lib.misc.php
+++ lib/lib.misc.php
@@ -18,41 +18,26 @@ define('MYSQL_DATETIME_FORMAT', 'Y-m-d H:i:s');
  * If $sizelimit is true (default), then only limit this to
  * the first 50,000 bytes and attach a note saying so.
  */
-if( !function_exists('version_compare') || version_compare( '5.1.0',PHP_VERSION,'>') ) {
+function getFileContents($filename, $sizelimit = true) {
 
-	function getFileContents($filename, $sizelimit = true) {
-		if ( ! file_exists($filename) ) {
-			return '';
-		}
-		if ( ! is_readable($filename) ) {
-			error("Could not open $filename for reading: not readable");
-		}
-
-		if ( $sizelimit && filesize($filename) > 50000 ) {
-			$fh = fopen($filename,'r');
-			if ( ! $fh ) error("Could not open $filename for reading");
-			$ret = fread($fh, 50000) . "\n[output truncated after 50,000 B]\n";
-			fclose($fh);
-			return $ret;
-		}
-
-		return file_get_contents($filename);
+	if ( ! file_exists($filename) ) {
+		return '';
+	}
+	if ( ! is_readable($filename) ) {
+		error("Could not open $filename for reading: not readable");
 	}
 
-} else {
-
-	function getFileContents($filename, $sizelimit = true) {
-		if ( ! file_exists($filename) ) {
-			return '';
-		}
-		if ( ! is_readable($filename) ) {
-			error("Could not open $filename for reading: not readable");
-		}
-
-		// The maxlen parameter for file_get_contents was introduced in PHP 5.1.0.
-		return file_get_contents($filename, FILE_BINARY, NULL, 0, 50000);
+	if ( $sizelimit && filesize($filename) > 50000 ) {
+		// When our lowest supported PHP version >= 5.1.0, we can just use
+		// file_get_contents() with the maxlen parameter.
+		$fh = fopen($filename,'r');
+		if ( ! $fh ) error("Could not open $filename for reading");
+		$ret = fread($fh, 50000) . "\n[output truncated after 50,000 B]\n";
+		fclose($fh);
+		return $ret;
 	}
 
+	return file_get_contents($filename);
 }
 
 /**
