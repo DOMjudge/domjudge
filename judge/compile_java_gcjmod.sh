fbfb35d1c01453156d2a2ac7f299463b21162447
Peter van de Werken <pwerken@a-eskwadraat.nl>
2008-10-21 14:47:53 +0000

    revert accidental removal of gcjmod script in r2464
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2465 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ judge/compile_java_gcjmod.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Java compile wrapper-script for 'test_solution.sh'.
+# See that script for syntax and more info.
+#
+# This script compiles a statically linked binary with gcj. In
+# addition, it removes some warnings that gcj generates by default
+# with static compilation. These warnings have confused teams in the
+# past.
+
+SOURCE="$1"
+DEST="$2"
+
+TMPFILE=`mktemp /tmp/domjudge_gcj_output.XXXXXX`
+
+# -Wall:	Report all warnings
+# -static:	Static link with all libraries
+gcj -Wall -O2 -static --main=Main -o $DEST $SOURCE > $TMPFILE 2>&1
+exitcode=$?
+grep -vE 'requires at runtime the shared libraries|libgcj\.a' $TMPFILE
+rm -f $TMPFILE
+
+exit $exitcode
