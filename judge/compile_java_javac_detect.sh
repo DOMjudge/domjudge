6b0f3393dffa315ded695179312dc117028374bd
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-11-19 15:32:27 +0000

    Revert r2495: do public class renaming.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/branches/nwerc2008@2507 caf94f87-7bd6-0310-88dc-dee8202e2269

--- judge/compile_java_javac_detect.sh
+++ judge/compile_java_javac_detect.sh
@@ -8,10 +8,24 @@ DEST="$2"
 MEMLIMIT="$3"
 MAINCLASS=""
 
+TMPFILE=`mktemp /tmp/domjudge_gcj_output.XXXXXX`
+
 # Byte-compile:
-javac -d . "$SOURCE"
+javac -d . "$SOURCE" 2> "$TMPFILE"
 EXITCODE=$?
-[ "$EXITCODE" -ne 0 ] && exit $EXITCODE
+if [ "$EXITCODE" -ne 0 ]; then
+	# Let's see if should have named the .java differently
+	PUBLICCLASS=$(sed  -n '/class .* is public, should be declared in a file named /{s/.*named.//;s/.java.*//;p;q}' "$TMPFILE")
+	if [ -z "$PUBLICCLASS" ]; then
+		cat $TMPFILE
+		exit $EXITCODE
+	fi
+	echo "Info: renaming source to '$PUBLICCLASS.java'"
+	mv "$SOURCE" "$PUBLICCLASS.java"
+	javac -d . "$PUBLICCLASS.java"
+	EXITCODE=$?
+	[ "$EXITCODE" -ne 0 ] && exit $EXITCODE
+fi
 
 # Look for class that has the 'main' function:
 for cn in $(find * -type f -regex '^.*\.class$' \
