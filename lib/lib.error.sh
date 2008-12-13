eee8aa48386ba48a3e3bdc347040d40a72eef165
Jaap Eldering <eldering@a-eskwadraat.nl>
2008-12-13 19:15:59 +0000

    Move loglevel definitions to lib.error.sh.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2573 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/lib.error.sh
+++ lib/lib.error.sh
@@ -2,6 +2,16 @@
 #
 # $Id$
 
+# Loglevels (as defined in syslog.h)
+LOG_EMERG=0
+LOG_ALERT=1
+LOG_CRIT=2
+LOG_ERR=3
+LOG_WARNING=4
+LOG_NOTICE=5
+LOG_INFO=6
+LOG_DEBUG=7
+
 # Default verbosity and loglevels:
 VERBOSE=$LOG_NOTICE
 LOGLEVEL=$LOG_DEBUG
