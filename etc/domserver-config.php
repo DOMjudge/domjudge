55b4326a5e2f51cb92f7974329f261667305346d
Jaap Eldering <eldering@a-eskwadraat.nl>
2009-05-05 13:48:30 +0000

    Move ENABLE_{CMD,WEB}SUBMIT_SERVER to static config (from configure).
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2682 caf94f87-7bd6-0310-88dc-dee8202e2269

--- etc/domserver-config.php
+++ etc/domserver-config.php
@@ -27,9 +27,6 @@ define('STRICTIPCHECK', false);
 
 define('LANG_EXTS', 'C,c C++,cpp,cc,c++ Java,java Pascal,pas,p Haskell,hs,lhs Perl,pl Bash,sh');
 
-define('ENABLE_CMDSUBMIT_SERVER', true);
-define('ENABLE_WEBSUBMIT_SERVER', true);
-
 // Specify here which of the users in htpasswd-jury should have admin 
 // rights on top of their jury rights
 $DOMJUDGE_ADMINS = array('domjudge_jury', 'admin');
