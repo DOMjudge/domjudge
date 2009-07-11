7842a8a507c374f69c29012e0e15d1ccce1f9423
Jaap Eldering <eldering@a-eskwadraat.nl>
2009-07-11 10:47:47 +0000

    Update to new config and remove /bin/bash dependency.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@2770 caf94f87-7bd6-0310-88dc-dee8202e2269

--- identify/user_callback.sh
+++ identify/user_callback.sh
@@ -1,31 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # $Id$
 
-# Needs bash because it includes lib.error.sh
+# Script to check for a file on a team account.
+# Usage: $0 <team> <filename>
+#
+# Where <team> is the account of the team and <filename> the file to
+# check for with path relative to the user's homedirectory.
+#
+# This script will depend very much on the setup of your system:
+# what kind of filesystems do you have, how can you access the team
+# accounts, etc...  Rewrite to fit your needs.
 #
 # Part of the DOMjudge Programming Contest Jury System and licenced
 # under the GNU GPL. See README and COPYING for details.
 
-# Global configuration
-. "`dirname $0`/../etc/config.sh"
-
-# Error and logging functions
-. "$SYSTEM_ROOT/lib/lib.error.sh"
-
 PROGNAME="`basename $0`"
 
-[ $# -eq 2 ] || exit 1
+SCP_HOST=localhost
+
+error ()
+{
+	echo "$PROGNAME: error: $@"
+	exit 127
+}
+
+[ $# -eq 2 ] || error  "invalid number of arguments"
 
 team=$1
-fromfile=$2
+file=$2
 
-# TODO: sanity check team and fromfile
+logmsg $LOG_INFO "executing: 'scp -Bq ${team}@${SCP_HOST}:'${file}' /tmp'"
 
-logmsg $LOG_INFO "executing: 'scp -Bq ${team}@${SCP_HOST}:'${fromfile}' $DJTMPDIR'"
-
-output=`scp -Bq "${team}@${SCP_HOST}:'${fromfile}'" "$DJTMPDIR" 2>&1`
+output=`scp -Bq "${team}@${SCP_HOST}:'${file}'" /tmp 2>&1`
 if [ $? -eq 0 -a ${#output} -eq 0 ]; then
-	rm -f "$DJTMPDIR$fromfile"
+	rm -f /tmp/`basename "$file"`
 	exit 0
 fi
 
