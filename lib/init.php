ff2d80338498fca7b5d15b7291ae0076c92c4131
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-02-28 17:00:22 +0000

    Initialize timezone for PHP date() with system default.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3153 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/init.php
+++ lib/init.php
@@ -8,5 +8,9 @@ require(LIBDIR . '/lib.config.php');
 require(LIBDIR . '/lib.dbconfig.php');
 require(LIBDIR . '/use_db.php');
 
+// Initialize default timezone to system default. PHP >= 5.3 generates
+// E_NOTICE warning messages otherwise.
+@date_default_timezone_set(@date_default_timezone_get());
+
 // Here or in script?
 //setup_database_connection('jury');
