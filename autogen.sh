8e4b93f7b8bc13135ccad7780d4cef8d1d749ccb
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-06-12 12:49:07 +0000

    Fix exitcode in autogen.sh script.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3210 caf94f87-7bd6-0310-88dc-dee8202e2269

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 echo "Running aclocal..." ; aclocal -I m4 || exit 1
-echo "Running autoheader..." ; autoheader || exit 
+echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 
 ./configure "$@"
