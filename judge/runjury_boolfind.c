0be15b03fbb599022264c965cdbf911dc7219c7c
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-08-14 11:34:58 +0000

    Some more strict ANSI C fixes.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3323 caf94f87-7bd6-0310-88dc-dee8202e2269

--- judge/runjury_boolfind.c
+++ judge/runjury_boolfind.c
@@ -5,15 +5,15 @@
  * $Id$
  */
 
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
 
-#define write(...) printf(__VA_ARGS__); fflush(NULL)
-
-const struct timespec delay = { 0, 1000000 }; // 1 millisec.
+const struct timespec delay = { 0, 1000000 }; /* 1 millisec. */
 
 #define maxn 1000000
 
@@ -21,7 +21,7 @@ FILE *in, *out;
 
 int run,  nruns;
 
-long long n;
+long n;
 int data[maxn];
 
 void talk()
@@ -29,28 +29,30 @@ void talk()
 	int nqueries = 0;
 	char line[256];
 	int i;
-	long long pos;
+	long pos;
 
-	write("%lld\n",n);
+	printf("%ld\n",n); fflush(NULL);
 
 	do {
 		fgets(line,255,stdin);
 		for(i=strlen(line)-1; i>=0 && line[i]=='\n'; i--) line[i] = 0;
 
 		if ( strncmp(line,"READ ",5)==0 ) {
-			// We should do a more rigorous syntax check in input
-			// here! E.g. check that nothing follows the number read.
-			if ( sscanf(&line[5],"%lld",&pos)!=1 || pos>=n || pos<0 ) {
+			/* We should do a more rigorous syntax check in input
+			 * here! E.g. check that nothing follows the number read.
+			 */
+			if ( sscanf(&line[5],"%ld",&pos)!=1 || pos>=n || pos<0 ) {
 				fprintf(out,"invalid READ query '%s' after %d queries\n",line,nqueries);
 				break;
 			}
-			// Simulate slow query: delay for short while
+			/* Simulate slow query: delay for short while */
 			nanosleep(&delay,NULL);
 			if ( data[pos] ) {
-				write("true\n");
+				printf("true\n");
 			} else {
-				write("false\n");
+				printf("false\n");
 			}
+			fflush(NULL);
 			nqueries++;
 		} else if ( strncmp(line,"OUTPUT ",6)==0 ) {
 			fprintf(out,"%s\n",line);
@@ -65,7 +67,7 @@ void talk()
 
 int main(int argc, char **argv)
 {
-	long long i;
+	long i;
 	size_t nbuf;
 	char buf[256];
 
@@ -74,7 +76,7 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	// Make stdin/stdout unbuffered, just to be sure
+	/* Make stdin/stdout unbuffered, just to be sure */
 	if ( setvbuf(stdin,  NULL, _IONBF, 0)!=0 ||
 	     setvbuf(stdout, NULL, _IONBF, 0)!=0 ) {
 		fprintf(stderr,"error: cannot set unbuffered I/O\n");
@@ -89,19 +91,20 @@ int main(int argc, char **argv)
 	}
 
 	fscanf(in,"%d\n",&nruns);
-	write("%d\n",nruns);
+	printf("%d\n",nruns);
+	fflush(NULL);
 
 	for(run=1; run<=nruns; run++) {
-		fscanf(in,"%lld\n",&n);
+		fscanf(in,"%ld\n",&n);
 		for(i=0; i<n; i++) fscanf(in,"%d\n",&data[i]);
 
 		talk();
 	}
 
-	// We're done, send EOF
+	/* We're done, send EOF */
 	fclose(stdout);
 
-	// Copy any additional data from program
+	/* Copy any additional data from program */
 	while ( (nbuf=fread(buf,1,256,stdin))>0 ) fwrite(buf,1,nbuf,out);
 
 	fprintf(stderr,"jury program exited successfully\n");
