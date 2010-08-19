6af59021395a42d6b343e447374d651d98c3cfbb
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-08-19 19:50:58 +0000

    Merge trunk r3301: fix expected output for reboot test-source.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/branches/3.0@3364 caf94f87-7bd6-0310-88dc-dee8202e2269

--- tests/stress-test-reboot.c
+++ tests/stress-test-reboot.c
@@ -1,9 +1,10 @@
 /* $Id$
  *
  * This will try to reboot the computer by calling 'reboot'. Of course
- * this should not work as this program should not run as root.
+ * this should not work as this program should not run as root. This should
+ * only generate a message to stderr, so result in NO-OUTPUT.
  *
- * @EXPECTED_RESULTS@: RUN-ERROR
+ * @EXPECTED_RESULTS@: NO-OUTPUT
  */
 
 #include <stdlib.h>
