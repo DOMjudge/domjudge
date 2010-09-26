f7fe3d6b92db9bbb8fb7f8443bcdf40e4a7ea04f
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-09-26 15:29:09 +0000

    Add Java solution for boolfind sample problem.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3383 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ tests/boolfind-test-correct.java
@@ -0,0 +1,45 @@
+/* $Id$
+ *
+ * Sample solution in Java for the "boolfind" interactive problem.
+ *
+ * @EXPECTED_RESULTS@: CORRECT
+ */
+
+import java.io.*;
+
+class Main
+{
+	public static BufferedReader in;
+
+	public static void main(String[] args) throws Exception
+	{
+		in = new BufferedReader(new InputStreamReader(System.in));
+
+		int nruns = Integer.parseInt(in.readLine());
+
+		for(int run=1; run<=nruns; run++) {
+
+			long n, lo, hi, mid;
+			String answer;
+
+			n = Integer.parseInt(in.readLine());
+
+			lo = 0;
+			hi = n;
+			while ( lo+1<hi ) {
+				mid = (lo+hi)/2;
+				System.out.println("READ " + mid);
+				answer = in.readLine();
+				if ( answer.equals("true") ) {
+					lo = mid;
+				} else if ( answer.equals("false") ) {
+					hi = mid;
+				} else {
+					throw new Exception("invalid return value '" + answer + "'");
+				}
+			}
+
+			System.out.println("OUTPUT " + lo);
+		}
+	}
+}
