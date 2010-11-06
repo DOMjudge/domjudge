2c802149b1b90b6c791b7fd55942ff99e6af7598
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-11-06 10:58:42 +0000

    Using java packages doesn't work with non-autodetecting compiler script.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3413 caf94f87-7bd6-0310-88dc-dee8202e2269

--- tests/test-package.java
+++ tests/test-package.java
@@ -1,11 +1,15 @@
 /* $Id$
  *
- * This should give CORRECT on the default problem 'hello'.
+ * With the default Java compile script that autodetects the class
+ * name, this should give CORRECT on the default problem 'hello'.
  * If the compiled code is copied into the root of the judging chroot
  * directory however, this will crash the judgedaemon when it tries to
  * create the /bin directory for the static shell.
  *
- * @EXPECTED_RESULTS@: CORRECT
+ * Using the non-autodetecting Java compile script, this fails with
+ * compiler error.
+ *
+ * @EXPECTED_RESULTS@: CORRECT,COMPILER-ERROR
  */
 
 package bin;
