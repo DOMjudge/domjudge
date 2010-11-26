de84483117014ba4653249a3944f677bbe1f46f4
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-11-26 21:14:15 +0000

    Add C++ version of stdout fill test-source, as Pascal is not enabled by default.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3450 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ tests/test-fill-stdout.cc
@@ -0,0 +1,17 @@
+/* $Id$
+ *
+ * Floods stdout and should fail with TIME-LIMIT or RUN-ERROR
+ * depending on whether timelimit or filesize limit is reached first.
+ *
+ * @EXPECTED_RESULTS@: TIME-LIMIT,RUN-ERROR
+ */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	while ( 1 ) std::cout << "Fill stdout with nonsense, to test filesystem stability.\n";
+
+	return 0;
+}
