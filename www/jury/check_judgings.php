5c4b18836b798cae6b6333a0368ed9b14c1a75a8
Jaap Eldering <eldering@a-eskwadraat.nl>
2011-07-09 12:10:49 +0000

    Improvements to judging verifier: - use javascript to collapse unimportant lists - add button to verify multiple outcome judgings - restrict to current contest (others won't display judging info)
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3561 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/check_judgings.php
+++ www/jury/check_judgings.php
@@ -15,99 +15,146 @@ require(LIBWWWDIR . '/header.php');
 
 requireAdmin();
 
-echo "<h1>Judging Verifier</h1>";
+$verify_multiple = isset($_REQUEST['verify_multiple']);
+
+?>
+<script type="text/javascript" language="JavaScript">
+<!--
+function collapse(x){
+  var oTemp=document.getElementById("detail"+x) ;
+  if (oTemp.style.display=="none") {
+    oTemp.style.display="block";
+  } else {
+    oTemp.style.display="none";
+  }
+}
+// -->
+</script>
+
+<h1>Judging Verifier</h1>
+
+<?php
 
 $nchecked = 0;
 $nunchecked = 0;
-$nverified = 0;
-$nmanual = 0;
-$nerrors = 0;
+
+$unexpected = array();
+$multiple = array();
+$verified = array();
+
+$nomatch = array();
+$earlier = array();
 
 $matchstring = '@EXPECTED_RESULTS@: ';
+$verifier = 'auto-verifier';
 
 $res = $DB->q("SELECT s.*, j.judgingid, j.result, j.verified, j.jury_member
                FROM submission s
                LEFT JOIN judging j ON (s.submitid = j.submitid AND j.valid=1)
-	       WHERE j.verified = 0");
+               WHERE s.cid = %i AND j.result IS NOT NULL", $cid);
 
-$unchecked = "";
-$unexpected = "";
-$multiple = "";
-$verified = "";
+$section = 0;
+
+function flushresults($header, $results, $collapse = FALSE)
+{
+	GLOBAL $section;
+
+	$section++;
+
+	echo "<h2><a href=\"javascript:collapse($section)\">$header</a></h2>\n\n";
+	echo "<div class=\"details\" id=\"detail$section\">\n";
+	foreach ($results as $row) {
+		echo "<li>$row</li>\n";
+	}
+	echo "</div>\n\n";
+
+	if ( $collapse ) {
+		echo "<script type=\"text/javascript\" language=\"JavaScript\">
+<!--
+	collapse($section);
+-->
+</script>\n\n";
+	}
+
+	flush();
+}
 
 while( $row = $res->next() ) {
-	if ( $pos = strpos($row['sourcecode'],$matchstring) ) {
+	$sid = $row['submitid'];
+
+	if ( ($pos = strpos($row['sourcecode'],$matchstring)) && $row['verified']==0 ) {
 		$nchecked++;
 
 		$beginpos = $pos + strlen($matchstring);
 		$endpos = strpos($row['sourcecode'],"\n",$beginpos);
 		$results = explode(',',trim(substr($row['sourcecode'],$beginpos,$endpos-$beginpos)));
 
-		$sid = $row['submitid'];
 		$result = strtoupper($row['result']);
 
 		if ( !in_array($result,$results) ) {
-			$unexpected .= "<li><a href=\"submission.php?id=" . $sid
-				. "\">submission $sid</a> has unexpected result '$result', "
-				. "should be one of: "
-				. implode(', ', $results)
-				. "</li>\n";
-			$nerrors++;
+			$unexpected[] = "<a href=\"submission.php?id=" . $sid
+				. "\">s$sid</a> has unexpected result '$result', "
+				. "should be one of: " . implode(', ', $results);
 		} else if ( count($results)>1 ) {
-			$multiple .= "<li><a href=\"submission.php?id=" . $sid
-				. "\">submission $sid</a> is judged as $result, "
-				. "but has multiple possible outcomes ("
-				. implode(', ', $results)
-				. ")</li>\n";
-			$nmanual++;
+			if ( $verify_multiple ) {
+				// Judging result is as expected, set judging to verified:
+				$DB->q('UPDATE judging SET verified = 1, jury_member = %s
+				        WHERE judgingid = %i', $verifier, $row['judgingid']);
+				$multiple[] = "<a href=\"submission.php?id=" . $sid
+				    . "\">s$sid</a> verified as $result, "
+				    . "out of multiple possible outcomes ("
+				    . implode(', ', $results) . ")";
+			} else {
+				$multiple[] = "<a href=\"submission.php?id=" . $sid
+				    . "\">s$sid</a> is judged as $result, "
+				    . "but has multiple possible outcomes ("
+				    . implode(', ', $results) . ")";
+			}
 		} else {
 			// Judging result is as expected, set judging to verified:
-			if ( $row['verified']!=1 ) {
-				$DB->q('UPDATE judging SET verified = 1, jury_member = \'auto-verifier\'
-				        WHERE judgingid = %i', $row['judgingid']);
-			}
-			$verified .= "<li>verified <a href=\"submission.php?id=" . $sid . "\">submission $sid</a> as '$result'</li>\n";
-			$nverified++;
+			$DB->q('UPDATE judging SET verified = 1, jury_member = %s
+			        WHERE judgingid = %i', $verifier, $row['judgingid']);
+			$verified[] = "<a href=\"submission.php?id=" . $sid .
+				"\">s$sid</a> verified as '$result'";
 		}
 	} else {
-		$sid = $row['submitid'];
 		$nunchecked++;
-		$unchecked .= "<li>'$matchstring' not found in <a href=\"submission.php?id=" . $sid
-			. "\">submission $sid</a>, leaving submission unchecked</li>\n";
+
+		if ( $pos===FALSE ) {
+			$nomatch[] = "string '<tt>$matchstring</tt>' not found in " .
+				"<a href=\"submission.php?id=" . $sid .
+				"\">s$sid</a>, leaving submission unchecked";
+		} else {
+			$earlier[] = "<a href=\"submission.php?id=" . $sid .
+			    "\">s$sid</a> already verified earlier";
+		}
 	}
 }
 
-echo "checked $nchecked submissions: " .
-	   "$nverified automatically verified, $nmanual to check manually, " .
-	   "$nerrors unexpected results<br/>\n";
-if ( $nunchecked > 0 ) {
-	echo "not checked $nunchecked submissions<br/>\n";
-}
+echo "$nchecked submissions checked: " .
+	count($unexpected) . " unexpected results, " .
+	count($multiple) . ( $verify_multiple ?
+                         " automatically verified (multiple outcomes), " :
+                         " to check manually, " ).
+	count($verified) . " automatically verified<br/>\n";
+echo "$nunchecked submissions not checked: " .
+	count($earlier) . " verified earlier, " .
+	count($nomatch) . " without magic string<br/>\n";
 
-
-if ( $nerrors > 0 ) {
-	echo "<h2>Unexpected results</h2>";
-	echo "<ul>\n";
-	echo $unexpected;
-	echo "</ul>\n";
+if ( count($unexpected) ) flushresults("Unexpected results", $unexpected);
+if ( count($multiple)   ) {
+	if ( $verify_multiple ) {
+		flushresults("Automatically verified (multiple outcomes)", $multiple, TRUE);
+	} else {
+		flushresults("Check manually", $multiple);
+		echo "<div class=\"details\" id=\"detail$section\">\n" .
+		    addForm($pagename) . "<p>Verify all multiple outcome submissions: " .
+		    addHidden('verify_multiple', '1') . addSubmit('verify') .
+		    addEndForm() . "</p>\n</div>\n\n";
+	}
 }
-if ( $nmanual > 0 ) {
-	echo "<h2>Check manually</h2>";
-	echo "<ul>\n";
-	echo $multiple;
-	echo "</ul>\n";
-}
-if ( $nverified > 0 ) {
-	echo "<h2>Automatically verified</h2>";
-	echo "<ul>\n";
-	echo $verified;
-	echo "</ul>\n";
-}
-if ( $nunchecked > 0 ) {
-	echo "<h2>Unchecked Submissions</h2>";
-	echo "<ul>\n";
-	echo $unchecked;
-	echo "</ul>\n";
-}
+if ( count($verified)   ) flushresults("Automatically verified", $verified, TRUE);
+if ( count($earlier)    ) flushresults("Verified earlier", $earlier, TRUE);
+if ( count($nomatch)    ) flushresults("Without magic string", $nomatch, TRUE);
 
 require(LIBWWWDIR . '/footer.php');
