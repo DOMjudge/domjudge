a76223cc59b50221fa966dd003910c908b88776a
Jaap Eldering <eldering@a-eskwadraat.nl>
2011-07-10 22:10:03 +0000

    Add Makefile rule to create zip bundles for example problems.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3564 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ doc/examples/hello.c
@@ -0,0 +1,11 @@
+// Sample solution for the default "Demo Contest" problem "hello"
+// @EXPECTED_RESULTS@: CORRECT
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello world!\n");
+
+	return 0;
+}
---
+++ doc/examples/hello.java
@@ -0,0 +1,13 @@
+/* Sample solution for the default "Demo Contest" problem "hello"
+ * @EXPECTED_RESULTS@: CORRECT
+ */
+
+import java.io.*;
+
+class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("Hello world!");
+	}
+}
