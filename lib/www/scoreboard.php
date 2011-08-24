b9fbb9e6e71cfe11adae818f81fb81c459eab28c
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2011-08-24 11:21:08 +0000

    fix on fix r3609: a bit too much copy/paste
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3610 caf94f87-7bd6-0310-88dc-dee8202e2269

--- lib/www/scoreboard.php
+++ lib/www/scoreboard.php
@@ -543,7 +543,7 @@ function collapse(x){
 		$affilids = empty($affilids) ? array() : 
 				$DB->q('KEYVALUETABLE SELECT affilid, name FROM team_affiliation
 		                    WHERE affilid IN (%As)', $affilids);
-		$categids = empty($affilids) ? array() :
+		$categids = empty($categids) ? array() :
 				$DB->q('KEYVALUETABLE SELECT categoryid, name FROM team_category
 		                    WHERE categoryid IN (%As)', $categids);
 
