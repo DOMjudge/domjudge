50ed620378043819dec60d9c9dc104ba3596df4e
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2011-08-26 14:10:49 +0000

    use FS-specific separator to deal with konqueror-type browsers, and specifically search for 'fakepath\' to catch Opera.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3623 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/js/domjudge.js
+++ www/js/domjudge.js
@@ -172,8 +172,9 @@ function initFileUploads() {
 			} else {
 				var filename = this.value;
 				// Opera prepends a fake fs path: C:\fakepath\. Strip that.
-				if ( filename.indexOf("\\") >= 0 ) filename = filename.substr(filename.lastIndexOf("\\")+1);
-				if ( filename.indexOf("/") >= 0 ) filename = filename.substr(filename.lastIndexOf("/")+1);
+				if ( filename.indexOf("fakepath\\") >= 0 ) filename = filename.substr(filename.lastIndexOf("fakepath\\")+1);
+				// some other browsers (Konqueror at least) may prepend the full FS path
+				if ( filename.indexOf(File.separator) >= 0 ) filename = filename.substr(filename.lastIndexOf(File.separator)+1);
 				detectProblemLanguage(filename);
 				this.relatedElement.value = filename;
 			}
