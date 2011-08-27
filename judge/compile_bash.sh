fcb72c4d79cf234e6f903831cacb89b71e1f4ea5
Jaap Eldering <eldering@a-eskwadraat.nl>
2011-08-27 10:31:43 +0000

    Add compile script and test source for language 'Bash'.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3629 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ judge/compile_bash.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Bash shell compile wrapper-script for 'compile.sh'.
+# See that script for syntax and more info.
+#
+# This script does not actually "compile" the source, but writes a
+# shell script that will function as the executable: when called, it
+# will execute the source with the correct interpreter syntax, thus
+# allowing this interpreted source to be used transparantly as if it
+# was compiled to a standalone binary.
+#
+# NOTICE: this compiler script cannot be used with the USE_CHROOT
+# configuration option turned on unless proper preconfiguration of
+# the chroot environment has been taken care of.
+# By default the bash executable is located in /bin/bash, which
+# doesn't get included by the shipped chroot-startstop.sh script.
+# Thus, besides installing bash in the chroot environment, it should
+# (for example) be copied to /usr/bin/bash.
+
+SOURCE="$1"
+DEST="$2"
+
+RUNOPTIONS=""
+
+# Check for '#!' interpreter line: don't allow it to prevent teams
+# from passing options to the interpreter.
+if grep '^#!' $SOURCE > /dev/null 2>&1 ; then
+	echo "Error: interpreter statement(s) found:"
+	grep -n '^#!' $SOURCE
+	exit 1
+fi
+
+# Check syntax:
+bash $RUNOPTIONS -n $SOURCE
+EXITCODE=$?
+[ "$EXITCODE" -ne 0 ] && exit $EXITCODE
+
+# Write executing script:
+cat > $DEST <<EOF
+#!/bin/sh
+# Generated shell-script to execute shell interpreter on source.
+
+# Detect dirname and change dir to prevent class not found errors.
+if [ "\${0%/*}" != "\$0" ]; then
+	cd "\${0%/*}"
+fi
+
+exec bash $RUNOPTIONS $SOURCE
+EOF
+
+chmod a+x $DEST
+
+exit 0
---
+++ tests/test-hello.bash
@@ -0,0 +1,9 @@
+# $Id$
+#
+# This should give CORRECT on the default problem 'hello'.
+#
+# @EXPECTED_RESULTS@: CORRECT
+
+echo "Hello world!"
+
+exit 0
