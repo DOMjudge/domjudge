2b89f72133b53688d5e217ab28a381479ebd2d52
Jaap Eldering <eldering@a-eskwadraat.nl>
2011-08-28 19:27:07 +0000

    Fix remote possibility for non-constant time arguments.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3640 caf94f87-7bd6-0310-88dc-dee8202e2269

--- www/jury/contests.php
+++ www/jury/contests.php
@@ -19,8 +19,9 @@ if ( isset($_POST['unfreeze']) ) {
 	if ( $docid != $cid ) {
 		error("Can only unfreeze for current contest");
 	}
+	$now = now();
 	$DB->q('UPDATE contest SET unfreezetime = %s, unfreezetime_string = %s
-	        WHERE cid = %i', now(), now(), $docid);
+	        WHERE cid = %i', $now, $now, $docid);
 }
 
 // Get data. Starttime seems most logical sort criterion.
