6114214a94aa147ec9ad71d05b3e07a3fe109130
Thijs Kinkhorst <kink@a-eskwadraat.nl>
2011-12-16 22:22:59 +0000

    forgot to add in previous commit
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/trunk@3762 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ www/jury/auditlog.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Display the auditlog entries.
+ *
+ * $Id$
+ *
+ * Part of the DOMjudge Programming Contest Jury System and licenced
+ * under the GNU GPL. See README and COPYING for details.
+ */
+
+require('init.php');
+$title = 'Activity log';
+require(LIBWWWDIR . '/header.php');
+
+requireAdmin();
+
+echo "<h1>Activity log</h1>\n\n";
+
+$res = $DB->q('SELECT * FROM auditlog');
+
+if ( $res->count() == 0 ) {
+	echo '<p class="nodata">No entries</p>';
+	require(LIBWWWDIR . '/footer.php');
+	exit;
+}
+
+echo "<table class=\"sortable\">\n" .
+     "<tr><th>id</th><th>when</th><th>cid</th><th>who</th><th colspan=\"3\">what</th><th>extra info</th></tr>\n";
+while ( $logline = $res->next() ) {
+	echo "<tr><td>" .
+	htmlspecialchars($logline['logid']) . "</td>" .
+	"<td title=\"" . htmlspecialchars($logline['logtime']) . "\">" .
+	printtime($logline['logtime']) . "</td><td>" .
+	(empty($logline['cid'])?'':'c'.$logline['cid']) . "</td><td>" .
+	htmlspecialchars($logline['user']) . "</td><td>" .
+	htmlspecialchars($logline['datatype']) . "</td><td>";
+
+	$tables = array ('balloon' => 'b',
+		'clarification' => '',
+		'contest' => 'c',
+		'judgehost' => '',
+		'judging' => 'j',
+		'language' => '',
+		'problem' => '',
+		'submission' => 's',
+		'team' => '',
+		'team_affiliation' => '',
+		'team_category' => '',
+		'testcase' => '');
+
+	if ( !empty($logline['dataid']) ) {
+		echo "<a href=\"" .
+		urlencode($logline['datatype']) . '.php?id=' .
+		urlencode($logline['dataid']) . "\">" .
+		$tables[$logline['datatype']] .
+		htmlspecialchars($logline['dataid']) . "</a>";
+	}
+
+	echo "</td><td>" .
+	htmlspecialchars($logline['action']) . "</td><td>" .
+	htmlspecialchars($logline['extrainfo']) . "</td><td>" .
+	"</td></tr>\n";
+}
+echo "</table>\n\n";
+
+
+require(LIBWWWDIR . '/footer.php');
