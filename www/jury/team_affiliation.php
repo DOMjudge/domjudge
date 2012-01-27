c896b7b84b8999bc2eaaa6c1e7fb616273d13818
Tobias Werth <werth@cs.fau.de>
2012-01-27 15:03:14 +0100

    allow to enter three-letter country codes

--- www/jury/team_affiliation.php
+++ www/jury/team_affiliation.php
@@ -47,7 +47,7 @@ if ( IS_ADMIN && ($cmd == 'add' || $cmd == 'edit') ) {
 <td><?php echo addInput('data[0][name]', @$row['name'], 40, 255)?></td></tr>
 
 <tr><td><label for="data_0__country_">Country:</label></td>
-<td><?php echo addInput('data[0][country]', @$row['country'], 3, 2)?>
+<td><?php echo addInput('data[0][country]', @$row['country'], 4, 3)?>
 <a target="_blank"
 href="http://www.iso.org/iso/country_codes/iso_3166_code_lists/country_names_and_code_elements.htm"><img
 src="../images/b_help.png" class="smallpicto" alt="?" /></a></td></tr>
