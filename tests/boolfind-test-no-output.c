c69cf87edec5a786dec514cfaca754dd514be84f
Jaap Eldering <eldering@a-eskwadraat.nl>
2012-04-08 14:20:14 +0200

    Fix compiler warnings in boolfind example-problem code, add test source.

---
+++ tests/boolfind-test-no-output.c
@@ -0,0 +1,11 @@
+/*
+ * Sample solution in C for the "boolfind" interactive problem.
+ * Exits immediately without any output.
+ *
+ * @EXPECTED_RESULTS@: NO-OUTPUT
+ */
+
+int main()
+{
+	return 0;
+}
