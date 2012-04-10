1749d77f8a5eef1820aae23459ea2875c471ebc6
Rob Franken <rob@rob-franken.nl>
2012-04-10 15:03:56 +0200

    Add support to configure printtime() function with 'time_format' variable.

---
+++ sql/upgrade/upgrade_to-online_judge.sql
@@ -0,0 +1,30 @@
+-- This script upgrades table structure, data, and privileges
+-- from the default DOMjudge database to the online judge version.
+
+--
+-- First execute a check whether this upgrade should apply. The check
+-- below should fail if this upgrade has already been applied, but
+-- keep everything unchanged if not.
+--
+
+-- @UPGRADE-CHECK@
+SELECT 1;
+
+--
+-- Create additional structures
+--
+
+--
+-- Transfer data from old to new structure
+--
+
+--
+-- Add/remove sample/initial contents
+--
+
+INSERT INTO `configuration` (`name`, `value`, `type`, `description`) VALUES ('time_format', 'Y-m-d H:i', 'string', 'The format used to print times. For formatting options see the PHP \'date\' function.');
+
+--
+-- Finally remove obsolete structures after moving data
+--
+
