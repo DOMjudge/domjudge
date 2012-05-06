2944d2b0ce797a0945bf5d64fe809fe121394f4a
Tobias Werth <werth@cs.fau.de>
2012-05-07 00:28:13 +0200

    import submissions; team/language/problem still incorrect

--- import/parseFeed.php
+++ import/parseFeed.php
@@ -3,8 +3,10 @@
 # config section
 $feedURL = 'testfeed.xml';
 $sleeptime = 1;
+$directory = 'submissions';
 
 $knownRuns = array();
+$submittimes = array();
 
 while (1) {
 	$feedXML = file_get_contents($feedURL);
@@ -13,6 +15,7 @@ while (1) {
 
 	foreach ($runs as $run) {
 		if (val($run, 'judged') !== 'True') {
+			$submittimes[val($run, 'id')] = val($run, 'timestamp');
 			continue;
 		}
 
@@ -22,6 +25,40 @@ while (1) {
 		}
 		$knownRuns[] = $id;
 		print "$id\n";
+
+		// wait for submission
+		$zipfile = $directory . '/' . $id . '.zip';
+		$ziptmp = $directory . '/ziptmp';
+		while (1) {
+			sleep(1);
+			if (file_exists($zipfile)) {
+				break;
+			}
+		}
+
+		mkdir($ziptmp);
+		system("unzip -j $zipfile -d $ziptmp > /dev/null");
+		$handle = opendir($ziptmp);
+
+		$files = "";
+		while (false !== ($entry = readdir($handle))) {
+			if ($entry != "." && $entry != "..") {
+				$files .= " '" . $ziptmp . "/" . $entry . "'";
+				$files .= " '" . $entry . "'";
+			}
+		}
+		closedir($handle);
+
+		system("./import.php coolteam fltcmp c " . $submittimes[$id] . " $id " . val($run, 'result') . $files . " 1>2");
+
+		$handle = opendir($ziptmp);
+		while (false !== ($entry = readdir($handle))) {
+			if ($entry != "." && $entry != "..") {
+				unlink($ziptmp . "/" . $entry);
+			}
+		}
+		closedir($handle);
+		rmdir($ziptmp);
 	}
 
 	sleep($sleeptime);
