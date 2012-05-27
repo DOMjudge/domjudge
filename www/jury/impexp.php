f844411805784953eb7e4bd72fadaf50828648b8
Thijs Kinkhorst <thijs@kinkhorst.com>
2012-05-27 20:38:04 +0200

    Basic support for importing groups and teams tsv formats.
    These formats are specified in the ICPC validator standard and have
    been tested as such. It hasn't yet been tested with real data
    coming out of the ICPC registration system.

--- www/jury/impexp.php
+++ www/jury/impexp.php
@@ -17,15 +17,24 @@ requireAdmin();
 <ul>
 <li><a href="import-export-config.php">Contest data (contest.yaml)</a></li>
 <li><a href="problems.php#problem_archive">Problem archive</a></li>
-<li>tsv's:
-	<a href="impexp_tsv.php?fmt=groups">groups.tsv</a>,
-	<a href="impexp_tsv.php?fmt=teams">teams.tsv</a>,
-	<a href="impexp_tsv.php?fmt=scoreboard">scoreboard.tsv</a>
+<li>Tab separated, export:
+	<a href="impexp_tsv.php?act=ex&amp;fmt=groups">groups.tsv</a>,
+	<a href="impexp_tsv.php?act=ex&amp;fmt=teams">teams.tsv</a>,
+	<a href="impexp_tsv.php?act=ex&amp;fmt=scoreboard">scoreboard.tsv</a>
+<li>
+<?php echo addForm('impexp_tsv.php', 'post', null, 'multipart/form-data') .
+	'Tab separated, import: ' .
+	'<label for="fmt">type:</label> ' .
+	addSelect('fmt',array('groups','teams')) .
+        ', <label for="tsv">file:</label>' .
+        addFileField('tsv') .
+        addHidden('act','im') .
+        addSubmit('import') .
+        addEndForm();
+?>
 </li>
 </ul>
 
-
-<p><br /><br /><br /><br /></p>
 
 <?php
 require(LIBWWWDIR . '/footer.php');
