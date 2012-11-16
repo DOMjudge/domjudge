8da719978ad1e55d756d744deb8c9e6d2c8691cd
Tobias Werth <werth@cs.fau.de>
2012-11-16 11:51:21 +0100

    Fix menu for teams.

--- www/team/menu.php
+++ www/team/menu.php
@@ -3,10 +3,11 @@
 echo "<div id=\"menutop\">\n";
 
 echo "<a target=\"_top\" href=\"index.php#submit\">submit</a>\n";
-echo "<a target=\"_top\" href=\"index.php#overview\" accesskey=\"o\">overview</a>\n";
-echo "<a target=\"_top\" href=\"index.php#submissions\">submissions</a>\n";
+echo "<a target=\"_top\" href=\"index.php#stats\" accesskey=\"o\">stats</a>\n";
+echo "<a target=\"_top\" href=\"problems.php#\">problems</a>\n";
+echo "<a target=\"_top\" href=\"ranking.php\">ranking</a>\n";
 echo "<a target=\"_top\" href=\"index.php#clarifications\">clarifications</a>\n";
-echo "<a target=\"_top\" href=\"index.php#clarreq\">clarification requests</a>\n";
+echo "<a target=\"_top\" href=\"index.php#clarreq\">clar. requests</a>\n";
 
 if ( have_printing() ) {
 	echo "<a target=\"_top\" href=\"print.php\" accesskey=\"p\">print</a>\n";
