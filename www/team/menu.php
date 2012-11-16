4a778a5bc6996c20426a5539b291ed2b6f815b18
Tobias Werth <werth@cs.fau.de>
2012-11-16 20:55:30 +0100

    Reduce tabs in team UI.

--- www/team/menu.php
+++ www/team/menu.php
@@ -2,12 +2,9 @@
 
 echo "<div id=\"menutop\">\n";
 
-echo "<a target=\"_top\" href=\"index.php#submit\">submit</a>\n";
-echo "<a target=\"_top\" href=\"index.php#stats\" accesskey=\"o\">stats</a>\n";
+echo "<a target=\"_top\" href=\"index.php\">dashboard</a>\n";
 echo "<a target=\"_top\" href=\"problems.php#\">problems</a>\n";
 echo "<a target=\"_top\" href=\"ranking.php\">ranking</a>\n";
-echo "<a target=\"_top\" href=\"index.php#clarifications\">clarifications</a>\n";
-echo "<a target=\"_top\" href=\"index.php#clarreq\">clar. requests</a>\n";
 
 if ( have_printing() ) {
 	echo "<a target=\"_top\" href=\"print.php\" accesskey=\"p\">print</a>\n";
