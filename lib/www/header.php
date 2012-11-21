edcc3a73c9d9025c8c8d395732918a5d1917d32c
Jaap Eldering <eldering@a-eskwadraat.nl>
2012-11-21 16:02:08 +0100

    Rename configuration option to 'team_select', since the JS implementation is not relevant there; add Changelog.

--- lib/www/header.php
+++ lib/www/header.php
@@ -56,7 +56,7 @@ if ( IS_JURY ) {
 }
 if ( ! IS_PUBLIC ) {
 	echo "<script type=\"text/javascript\" src=\"../js/domjudge.js\"></script>\n";
-} else if ( dbconfig_get('team_select_js', 1) ) {
+} else if ( dbconfig_get('team_select', 1) ) {
 	echo "<script type=\"text/javascript\" src=\"../js/djfav.js\"></script>\n";
 }
 if ( ! empty($extrahead) ) echo $extrahead;
