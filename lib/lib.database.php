e5f02698c1a96013d55c1a87deaddd67ffc4d8a3
Thijs Kinkhorst <thijs@kinkhorst.com>
2013-04-04 07:57:05 +0200

    SQL error not necessarily a syntax error.

--- lib/lib.database.php
+++ lib/lib.database.php
@@ -288,7 +288,7 @@ class db
 			case 2006:	// MySQL server has gone away
 			throw new RuntimeException("MySQL server has gone away");
 			default:
-			throw new RuntimeException("SQL syntax-error, " . $callsite
+			throw new RuntimeException("SQL error, " . $callsite
 			    . "Error#" . mysqli_errno($this->_connection) . ": "
 			    . mysqli_error($this->_connection) . ", query: '$query'");
 		}
