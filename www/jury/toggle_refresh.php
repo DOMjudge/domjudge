a15d5fe96a542297dedca8470e81f2efbccfef7b
Thijs Kinkhorst <thijs@kinkhorst.com>
2013-07-02 10:18:41 +0200

    Apply the z-index to the clock only, otherwise the refresh button stops being clickable.
    Also, check if there's a referrer before redirecting to it, preventing
    an endless loop (when called incorrectly).

--- www/jury/toggle_refresh.php
+++ www/jury/toggle_refresh.php
@@ -1,5 +1,7 @@
 <?php
 
+if ( empty($_SERVER['HTTP_REFERER']) ) die("Missing referrer header.");
+
 setcookie('domjudge_refresh', $_REQUEST['enable']);
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
