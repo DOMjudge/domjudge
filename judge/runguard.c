90287ef99a2a3998d2359f305e05b0934ef247da
Jaap Eldering <eldering@a-eskwadraat.nl>
2013-07-26 22:13:20 +0100

    Change runguard.c copyright clause to default, refering to README and COPYING for details.

--- judge/runguard.c
+++ judge/runguard.c
@@ -1,26 +1,13 @@
 /*
    runguard -- run command with restrictions.
-   Copyright (C) 2004-2013 Jaap Eldering (eldering@a-eskwadraat.nl)
-   Copyright (C) 2013      Keith Johnson
+
+   Part of the DOMjudge Programming Contest Jury System and licenced
+   under the GNU GPL. See README and COPYING for details.
 
    Multiple minor improvements ported from the DOMjudge-ETH tree.
 
    Based on an idea from the timeout program, written by Wietse Venema
    as part of The Coroner's Toolkit.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
    Program specifications:
