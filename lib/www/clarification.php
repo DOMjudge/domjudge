1dc51e8363e695878317b3852e89ae238760cb3e
Jeroen Bransen <J.Bransen@uu.nl>
2013-08-26 15:38:34 +0200

    Fix bug with array_merge and numeric indices

--- lib/www/clarification.php
+++ lib/www/clarification.php
@@ -303,7 +303,7 @@ function confirmClar() {
 			$teams = $DB->q('KEYVALUETABLE SELECT login, CONCAT(login, ": ", name) as name
 			                 FROM team
 			                 ORDER BY categoryid ASC, team.name COLLATE utf8_general_ci ASC');
-			$options = array_merge($options,$teams);
+			$options += $teams;
 		} else {
 			if ( $clar['sender'] ) {
 				$options[$clar['sender']] = $clar['sender'] .': '.
@@ -328,7 +328,7 @@ function confirmClar() {
 	} else {
 		$probs = array();
 	}
-	$options = array_merge(array('general' => 'General issue'), $probs);
+	$options = array('general' => 'General issue') + $probs;
 	echo "<tr><td><b>Subject:</b></td><td>\n" .
 	     addSelect('problem', $options, ($respid ? $clar['probid'] : 'general'), true) .
 	     "</td></tr>\n";
