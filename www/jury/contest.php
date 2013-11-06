44765ee75eff70375328be19fb4040647d0c5e3b
Thijs Kinkhorst <thijs@kinkhorst.com>
2013-11-06 12:04:43 +0100

    Fix cancel button in case of 'add' which does not supply referrer in GET

--- www/jury/contest.php
+++ www/jury/contest.php
@@ -68,7 +68,7 @@ echo addHidden('cmd', $cmd) .
 	addHidden('table','contest') .
 	addHidden('referrer', @$_GET['referrer'] . ( $cmd == 'edit'?(strstr(@$_GET['referrer'],'?') === FALSE?'?edited=1':'&edited=1'):'')) .
 	addSubmit('Save') .
-	addSubmit('Cancel', 'cancel', null, true, 'formnovalidate formaction="' . htmlspecialchars($_GET['referrer']) . '"') .
+	addSubmit('Cancel', 'cancel', null, true, 'formnovalidate' . (isset($_GET['referrer']) ? ' formaction="' . htmlspecialchars($_GET['referrer']) . '"':'')) .
 	addEndForm();
 
 require(LIBWWWDIR . '/footer.php');
