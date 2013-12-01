75750250f7c0f3d6276f5b4e70cf9f2e12aa35da
Jaap Eldering <eldering@a-eskwadraat.nl>
2013-12-01 19:38:25 +0000

    Fix some PHP errors: - check that array is passed to foreach() - typo in function call - suppress PHP notice for undefined variable

--- www/jury/judgehosts.php
+++ www/jury/judgehosts.php
@@ -86,7 +86,7 @@ if( $res->count() == 0 ) {
 			echo "judgehost-nocon";
 			echo "\" title =\"never checked in\">";
 		} else {
-			$reltime = timediff(now(),$row['polltime']);
+			$reltime = difftime(now(),$row['polltime']);
 			if ( $reltime < JUDGEHOST_WARNING ) {
 				echo "judgehost-ok";
 			} else if ( $reltime < JUDGEHOST_CRITICAL ) {
