febc0cdfdb5a202d48e54e21d5649c3603904762
Jaap Eldering <eldering@a-eskwadraat.nl>
2013-12-03 22:07:02 +0000

    Fix buffer overflow found by Coverity scan.
    Also remove redundant check at end of loop, document variables.

--- judge/runpipe.c
+++ judge/runpipe.c
@@ -169,16 +169,17 @@ int main(int argc, char **argv)
 	if ( argc<=optind ) error(0,"no command specified");
 
 	/* Parse commands to be executed */
-	ncmds = 0;
-	newcmd = 1;
+	ncmds = 0; /* Zero-based index to current command in loop,
+	              contains #commands specified after loop */
+	newcmd = 1; /* Is current command newly started? */
 	for(i=optind; i<argc; i++) {
 		/* Check for commands separator */
 		if ( strcmp(argv[i],"=")==0 ) {
 			ncmds++;
 			if ( newcmd ) error(0,"empty command #%d specified", ncmds);
 			newcmd = 1;
-			if ( ncmds>MAX_CMDS ) {
-				error(0,"too many commands specified: %d > %d", ncmds, MAX_CMDS);
+			if ( ncmds+1>MAX_CMDS ) {
+				error(0,"too many commands specified: %d > %d", ncmds+1, MAX_CMDS);
 			}
 			continue;
 		}
@@ -205,9 +206,6 @@ int main(int argc, char **argv)
 	}
 	ncmds++;
 	if ( newcmd ) error(0,"empty command #%d specified", ncmds);
-	if ( ncmds>MAX_CMDS ) {
-		error(0,"too many commands specified: %d > %d", ncmds, MAX_CMDS);
-	}
 	if ( ncmds!=2 ) {
 		error(0,"%d commands specified, 2 required", ncmds);
 	}
