76f227fb309c575830bb85a36d8922b23438d158
Tobias Werth <werth@cs.fau.de>
2014-02-25 12:31:09 +0100

    Add config checks for executables.

--- lib/www/checkers.jury.php
+++ lib/www/checkers.jury.php
@@ -104,8 +104,14 @@ function check_problem($data, $keydata = null)
 
 	if ( !empty($data['special_compare']) && $data['special_compare'] != 'float' ) {
 		global $DB;
-		if ( ! $DB->q('MAYBEVALUE SELECT execid FROM executable WHERE execid = %s', $data['special_compare']) ) {
-			ch_error("Unknown special compare script: " . $data['special_compare']);
+		if ( ! $DB->q('MAYBEVALUE SELECT execid FROM executable WHERE execid = %s AND type = %s', $data['special_compare'], 'compare') ) {
+			ch_error("Unknown special compare script (or wrong type): " . $data['special_compare']);
+		}
+	}
+	if ( !empty($data['special_run']) ) {
+		global $DB;
+		if ( ! $DB->q('MAYBEVALUE SELECT execid FROM executable WHERE execid = %s AND type = %s', $data['special_run'], 'run') ) {
+			ch_error("Unknown special run script (or wrong type): " . $data['special_run']);
 		}
 	}
 
@@ -131,6 +137,14 @@ function check_language($data, $keydata = null)
 	$id = (isset($data['langid']) ? $data['langid'] : $keydata['langid']);
 	if ( ! preg_match ( ID_REGEX, $id ) ) {
 		ch_error("Language ID may only contain characters " . IDENTIFIER_CHARS . ".");
+	}
+	if ( empty($data['compile_script']) ) {
+		ch_error("No compile script specified for language: " . $id);
+	} else {
+		global $DB;
+		if ( ! $DB->q('MAYBEVALUE SELECT execid FROM executable WHERE execid = %s AND type = %s', $data['compile_script'], 'compile') ) {
+			ch_error("Unknown compile script (or wrong type): " . $data['compile_script']);
+		}
 	}
 
 	return $data;
