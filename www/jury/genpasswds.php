9ec3b57a475ce49bbb7ed668801beb199bca0c9b
Tobias Werth <werth@cs.fau.de>
2014-12-16 11:07:18 +0100

    Add support for userdata.tsv as output format.
    See https://clics.ecs.baylor.edu/index.php/Contest_Control_System#userdata.tsv
    for the specification.

--- www/jury/genpasswds.php
+++ www/jury/genpasswds.php
@@ -8,8 +8,99 @@
 
 require('init.php');
 $title = 'Generate Passwords';
-require(LIBWWWDIR . '/header.php');
 requireAdmin();
+
+function genpw($users, $group, $format) {
+	global $DB;
+	$teamroleid = $DB->q('VALUE SELECT roleid FROM role WHERE role = %s', 'team');
+	$juryroleid = $DB->q('VALUE SELECT roleid FROM role WHERE role = %s', 'jury');
+	$adminroleid = $DB->q('VALUE SELECT roleid FROM role WHERE role = %s', 'admin');
+
+	if ( $format == "page" ) {
+		echo "\n\n<pre>";
+	}
+	foreach($users as $user) {
+		// checks if user has a "higher" role
+		// FIXME: integrate in users query
+		if ( $group == 'team' ) {
+			if ( $DB->q('VALUE SELECT COUNT(*) FROM userrole WHERE userid = %i AND (roleid = %i OR roleid = %i)',
+				     $user['userid'], $juryroleid, $adminroleid) > 0 ) {
+				continue;
+			}
+		} else if ( $group == 'judge' ) {
+			if ( $DB->q('VALUE SELECT COUNT(*) FROM userrole WHERE userid = %i AND roleid = %i',
+				     $user['userid'], $adminroleid) > 0 ) {
+				continue;
+			}
+		}
+		$pass = genrandpasswd();
+		// update the user table with a password
+		$DB->q('UPDATE user SET password = %s WHERE username = %s', md5($user['username'].'#'.$pass), $user['username']);
+		auditlog('user', $user['username'], 'set password');
+		$line = implode("\t",
+			array($group, $group == 'team' ? $user['teamid'] : '',
+				str_replace("\t", " ", $user['name']),
+				str_replace("\t", " ", $user['username']),
+				$pass)) . "\n";
+		if ( $format == "page" ) {
+			echo htmlspecialchars($line);
+		} else {
+			echo $line;
+		}
+	}
+	if ( $format == "page" ) {
+		echo "</pre><hr />\n\n<pre>";
+	}
+}
+
+if ( isset($_POST['format']) ) {
+	$format = $_POST['format'];
+
+	if ( $format == "page" ) {
+		require(LIBWWWDIR . '/header.php');
+		echo "<h1>Generated passwords:</h1>\n";
+		// output each password once we're done
+		ob_implicit_flush();
+	} else {
+		$fmt = "userdata";
+		$version = 1;
+		header("Content-Type: text/plain; name=\"" . $fmt . ".tsv\"; charset=" . DJ_CHARACTER_SET);
+		header("Content-Disposition: attachment; filename=\"" . $fmt . ".tsv\"");
+		echo $tsv = "userdata\t1\n";
+	}
+
+	$teamroleid = $DB->q('VALUE SELECT roleid FROM role WHERE role = %s', 'team');
+	$juryroleid = $DB->q('VALUE SELECT roleid FROM role WHERE role = %s', 'jury');
+	$adminroleid = $DB->q('VALUE SELECT roleid FROM role WHERE role = %s', 'admin');
+
+	foreach($_POST['group'] as $group) {
+		switch ( $group ) {
+			case 'team':
+			case 'teamwithoutpw':
+				$users = $DB->q('TABLE SELECT username,name,userid,teamid FROM user' .
+					        ' LEFT JOIN userrole USING (userid)' .
+					        ' WHERE teamid IS NOT NULL AND roleid = %i' .
+						($group == 'teamwithoutpw' ? ' AND password IS NULL' : '') .
+						' GROUP BY userid ORDER BY username', $teamroleid);
+				genpw($users, 'team', $format);
+				break;
+			case 'judge':
+			case 'admin':
+				$users = $DB->q('TABLE SELECT username,name,userid FROM user' .
+					        ' LEFT JOIN userrole USING (userid)' .
+					        ' WHERE roleid = %i' .
+						' GROUP BY userid ORDER BY username',
+						($group == 'judge' ? $juryroleid : $adminroleid));
+				genpw($users, $group, $format);
+				break;
+			default: error('Unknown group: ' . $group);
+		}
+	}
+
+	exit;
+}
+
+require(LIBWWWDIR . '/header.php');
 ?>
 
 <h1>Manage user passwords</h1>
@@ -50,40 +141,17 @@ endswitch;
 
 echo addForm($pagename);
 ?>
-<p>Generate a random password for: <select name="action">
-<option value="doallnull">all users with a team role without a password or IP-address</option>
-<option value="doall">all users with a team role</option>
-</select>
+<p>Generate a random password for:<br/>
+<input type="checkbox" name="group[]" value="team">all teams<br />
+<input type="checkbox" name="group[]" value="teamwithoutpw">teams without password<br />
+<input type="checkbox" name="group[]" value="judge">jury members<br />
+<input type="checkbox" name="group[]" value="admin">admins<br />
 </p>
-
+<p>Output format:<br/>
+<input type="radio" name="format" value="page" checked>on web page<br/>
+<input type="radio" name="format" value="tsv">as userdata.tsv download<br/>
 <?php
 echo addSubmit('generate') . addEndForm();
-
-
-if ( isset($_POST['action']) ) {
-	// output each password once we're done
-	ob_implicit_flush();
-
-	// all users, or optionaly only those with null password
-	$users = $DB->q('TABLE SELECT username,name FROM user WHERE teamid IS NOT NULL' .
-	                ($_POST['action'] == 'doallnull'?' AND password IS NULL':'') .
-	                ' ORDER BY username');
-
-	echo "<hr />\n\n<pre>";
-	foreach($users as $user) {
-		$pass = genrandpasswd();
-
-		// update the user table with a password
-		$DB->q('UPDATE user SET password = %s WHERE username = %s', md5($user['username'].'#'.$pass), $user['username']);
-		auditlog('user', $user['username'], 'set password');
-		echo "Full name: " . htmlspecialchars($user['name']) . "\n" .
-		     "Username:  " . htmlspecialchars($user['username']) . "\n" .
-		     "Password:  $pass\n\n\n\n";
-	}
-	echo "</pre>\n";
-
-	echo "<hr />\n\n<p>Done.</p>\n\n";
-}
 
 require(LIBWWWDIR . '/footer.php');
 
