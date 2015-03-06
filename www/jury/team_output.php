c38c8d43375d4002272b1c54a2db82c927fc959f
Jaap Eldering <eldering@a-eskwadraat.nl>
2015-03-06 23:04:59 +0000

    Match beginning of team output download filename to that of reference testcase filename.

--- www/jury/team_output.php
+++ www/jury/team_output.php
@@ -11,18 +11,21 @@ require('init.php');
 $runid  = (int)$_GET['runid'];
 $cid    = (int)$_GET['cid'];
 
-$row = $DB->q('MAYBETUPLE SELECT OCTET_LENGTH(output_run) as size, rank, cp.shortname
-               FROM judging_run
+$row = $DB->q('MAYBETUPLE SELECT OCTET_LENGTH(r.output_run) as size, t.rank,
+                                 cp.shortname, p.probid, s.teamid
+               FROM judging_run r
                LEFT JOIN testcase t USING (testcaseid)
                LEFT JOIN judging j USING (judgingid)
                LEFT JOIN problem p USING (probid)
                LEFT JOIN contestproblem cp ON (cp.probid=t.probid AND cp.cid=%i)
+               LEFT JOIN submission s USING (submitid)
                WHERE runid=%i', $cid, $runid);
 
 // sanity check before we start to output headers
 if ( $row===NULL || !is_numeric($row['size']) ) error("Problem while fetching team output");
 
-$filename = $row['shortname'] . '.' . $row['rank'] . ".run" . $runid . ".team.out";
+$filename = 'p' . $row['probid'] . '.t' . $row['rank'] . '.' .
+            $row['shortname'] . '.run' . $runid . '.team' . $row['teamid'] . '.out';
 
 header("Content-Type: application/octet-stream; name=\"$filename\"");
 header("Content-Disposition: inline; filename=\"$filename\"");
