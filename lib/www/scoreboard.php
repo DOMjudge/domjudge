DOMjudge/domjudge

commit 3b9fbf0a4bdbfcd20bea4f66c4c7ea3c6c7968eb

Various code layout issues (no functional changes): - remove trailing whitespace - correctly indent/align SQL statements - a few long lines broken to shorter ones

lib/www/scoreboard.php
@@ -567,13 +567,15 @@ function putScoreBoard($cdata, $myteamid = NULL, $static = FALSE, $filter = FALS
 		if ( empty($categids) ) {
 			$affils = array();
 		} else {
-			$affils = $DB->q('KEYTABLE SELECT affilid AS ARRAYKEY, team_affiliation.name, country
-		                      FROM team_affiliation
-		                      LEFT JOIN team USING(affilid)
-		                      INNER JOIN contest ON contest.cid = %i
-		                      LEFT JOIN contestteam ON contestteam.teamid = team.teamid AND contestteam.cid = contest.cid
-		                      WHERE categoryid IN (%As) AND contest.cid = %i AND (contest.public = 1 
-		                      OR contestteam.teamid IS NOT NULL) GROUP BY affilid', 
+			$affils = $DB->q('KEYTABLE SELECT affilid AS ARRAYKEY,
+			                  team_affiliation.name, country
+			                  FROM team_affiliation
+			                  LEFT JOIN team t USING (affilid)
+			                  INNER JOIN contest c ON (contest.cid = %i)
+			                  LEFT JOIN contestteam ct ON (ct.teamid = t.teamid AND ct.cid = c.cid)
+			                  WHERE categoryid IN (%As) AND c.cid = %i AND
+			                  (c.public = 1 OR ct.teamid IS NOT NULL)
+			                  GROUP BY affilid',
 			                 $cdata['cid'], array_keys($categids), $cdata['cid']);
 		}
 

doc/admin/admin-manual.sgml
@@ -274,7 +274,7 @@ On the domserver host:
        sudo a2enconf domjudge &&
        sudo apache2ctl graceful
        </tt>
-       
+
 <item> Check that the web interface works (/team, /public and /jury).
 <item> Change the admin password from its default value ('admin').
 <item> Check that the API (/api) works and create credentials for the judgehosts.
@@ -1122,7 +1122,7 @@ machines when using the commandline submit client) and run:
 When loading teams from the ICPC registration system through the import
 feature in DOMjudge, the certificate from icpc.baylor.edu must similarly
 be accepted by your local installation or if not, added via the procedure
-above. 
+above.
 
 <sect1>NTP time synchronisation
 <p>

doc/examples/boolfind.tex
@@ -55,7 +55,7 @@ sample interactive session.
 
 \end{document}
 
-%%% Local Variables: 
+%%% Local Variables:
 %%% mode: latex
 %%% TeX-master: t
-%%% End: 
+%%% End:

doc/examples/fltcmp.tex
@@ -44,7 +44,7 @@ Sample input and output for this problem:
 
 \end{document}
 
-%%% Local Variables: 
+%%% Local Variables:
 %%% mode: latex
 %%% TeX-master: t
-%%% End: 
+%%% End:

doc/examples/hello.tex
@@ -39,7 +39,7 @@ Sample input and output for this problem:
 
 \end{document}
 
-%%% Local Variables: 
+%%% Local Variables:
 %%% mode: latex
 %%% TeX-master: t
-%%% End: 
+%%% End:

doc/team/team-manual-NL.tex
@@ -361,7 +361,7 @@ ook het Java codevoorbeeld in appendix~\ref{codeexamples}.
 Hieronder staan een aantal voorbeelden van code om de invoer van een
 probleem in te lezen en de uitvoer weg te schrijven.
 
-De code hoort bij de volgende probleembeschrijving: 
+De code hoort bij de volgende probleembeschrijving:
 De invoer bestaat uit \'e\'en regel met daarop het aantal testgevallen.
 Daarna volgt voor elk testgeval een regel met daarop een naam (\'e\'en
 woord). Print voor elke naam de string ``Hello $<$naam$>$!''. Een naam

doc/team/team-manual.tex
@@ -1,4 +1,3 @@
-
 \input{team-manual-preamble}
 
 \usepackage[english]{babel}

judge/judgedaemon.main.php
@@ -215,14 +215,14 @@ function fetch_executable($workdirpath, $execid, $md5sum) {
 				}
 				switch ( $execlang ) {
 				case 'c':
-					$buildscript .= "gcc -Wall -O2 -std=gnu99 '$source' -o $execrunpath -lm\n"; 
+					$buildscript .= "gcc -Wall -O2 -std=gnu99 '$source' -o $execrunpath -lm\n";
 					break;
 				case 'cpp':
-					$buildscript .= "g++ -Wall -O2 -std=c++11 '$source' -o $execrunpath\n"; 
+					$buildscript .= "g++ -Wall -O2 -std=c++11 '$source' -o $execrunpath\n";
 					break;
 				case 'java':
 					$source = basename($source, ".java");
-					$buildscript .= "javac -cp $execpath -d $execpath '$source'.java\n"; 
+					$buildscript .= "javac -cp $execpath -d $execpath '$source'.java\n";
 					$buildscript .= "echo '#!/bin/sh' > run\n";
 					// no main class detection here
 					$buildscript .= "echo 'java -cp $execpath '$source' >> run\n";

commit 0064f9842eb5d4c2b3d7c4406b7fa7580527abb9

Fix SQL bug introduced in layout changes in 3b9fbf0a4bdb.

lib/www/scoreboard.php
@@ -571,7 +571,7 @@ function putScoreBoard($cdata, $myteamid = NULL, $static = FALSE, $filter = FALS
 			                  team_affiliation.name, country
 			                  FROM team_affiliation
 			                  LEFT JOIN team t USING (affilid)
-			                  INNER JOIN contest c ON (contest.cid = %i)
+			                  INNER JOIN contest c ON (c.cid = %i)
 			                  LEFT JOIN contestteam ct ON (ct.teamid = t.teamid AND ct.cid = c.cid)
 			                  WHERE categoryid IN (%As) AND c.cid = %i AND
 			                  (c.public = 1 OR ct.teamid IS NOT NULL)