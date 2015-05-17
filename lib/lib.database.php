5a6568f9a54fce21af750cd5b5c8475fee1d51a2
Thijs Kinkhorst <thijs@kinkhorst.com>
2015-05-17 16:44:47 +0100

    Correct callsite location

--- lib/lib.database.php
+++ lib/lib.database.php
@@ -291,8 +291,8 @@ class db
 
 		if(DEBUG) {
 			$backtrace = debug_backtrace();
-			$callsite = ' file: ' . $backtrace[1]['file'] . ', ' .
-			            ' line: ' . $backtrace[1]['line'] . ', ';
+			$callsite = ' file: ' . $backtrace[2]['file'] . ', ' .
+			            ' line: ' . $backtrace[2]['line'] . ', ';
 		} else {
 			$callsite = '';
 		}
