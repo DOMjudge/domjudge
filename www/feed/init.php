f09d3b62b0e48ec050837edf39e7ab5022c3df80
Thijs Kinkhorst <thijs@kinkhorst.com>
2015-10-27 19:46:26 +0100

    Require full_event_reader role for xml feed.

--- www/feed/init.php
+++ www/feed/init.php
@@ -11,15 +11,27 @@ require_once('../configure.php');
 /* For plugins to have jury access rights to the DB, they should
  * successfully authenticate as user 'jury'.
  */
-define('IS_JURY', (@$_SERVER['REMOTE_USER'] == "jury"));
-define('IS_PUBLIC', false);
-
 require_once(LIBDIR . '/init.php');
+require_once(LIBWWWDIR . '/common.php');
+require_once(LIBWWWDIR . '/print.php');
+require_once(LIBWWWDIR . '/auth.php');
 
 setup_database_connection();
 
-require_once(LIBWWWDIR . '/common.php');
-require_once(LIBWWWDIR . '/print.php');
+if ( ! logged_in() &&
+     isset($_SERVER['PHP_AUTH_USER']) &&
+     isset($_SERVER['PHP_AUTH_PW']) ) {
+	do_login_native($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+	$userdata['roles'] = get_user_roles($userdata['userid']);
+}
+
+if ( !checkrole('full_event_reader') ) {
+	error ("User role full_event_reader required.");
+}
+
+define('IS_JURY', true);
+define('IS_PUBLIC', false);
+
 
 $cdatas = getCurContests(TRUE);
 $cids = array_keys($cdatas);
