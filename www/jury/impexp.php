003816456ec014fe1c181ba8f7e3d45469cd01b0
Jaap Eldering <jaap@jaapeldering.nl>
2015-11-15 21:12:32 -0200

    Fix language in c2339c4f7201d: correct order of im/export and down/upload.

--- www/jury/impexp.php
+++ www/jury/impexp.php
@@ -14,7 +14,7 @@ requireAdmin();
 ?>
 <h1>Import and Export</h1>
 
-<h2>Import / Export via file up/download</h2>
+<h2>Import / Export via file down/upload</h2>
 
 <ul>
 <li><a href="impexp_contestyaml.php">Contest data (contest.yaml)</a></li>
