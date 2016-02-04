e3f0a55cfb1394e16835533290efc0b262374bb4
Nicky Gerritsen <nickygerritsen@me.com>
2016-02-04 19:37:27 +0100

    Add simple public page with menu

--- symfony/src/DOMjudge/PublicBundle/Controller/HomeController.php
+++ symfony/src/DOMjudge/PublicBundle/Controller/HomeController.php
@@ -2,17 +2,18 @@
 
 namespace DOMjudge\PublicBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller
 {
 	/**
 	 * @Route("/", name="public_home")
+	 * @Template()
 	 */
 	public function homeAction()
 	{
-		return new Response("Public home");
+		return array();
 	}
 }
