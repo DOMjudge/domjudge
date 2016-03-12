9b5aa6ee28206b8c250c9dcee775d86653a92ada
Jaap Eldering <jaap@jaapeldering.nl>
2016-03-12 15:20:24 -0300

    Set PHP float precision to 16, fixes json_encode output problem.

--- lib/init.php
+++ lib/init.php
@@ -18,12 +18,14 @@ require_once(LIBDIR . '/use_db.php');
 // use microsecond resolution timestamps. Note that since PHP uses the
 // IEEE 754 double precision format, which can only handle about 15-16
 // digits, we can't go beyond microseconds without reverting to
-// arbitrary precision float formats.
+// arbitrary precision float formats. We set it to exactly 16, since
+// that's the highest IEEE double supports anyways and higher seems to
+// give spurious output with json_encode().
 $precision = ini_get('precision');
 if ( $precision===FALSE || empty($precision) ) {
 	error("Could not read PHP setting 'precision'");
 }
-if ( $precision<17 ) ini_set('precision', '17');
+ini_set('precision', '16');
 
 // Set for using mb_* functions:
 mb_internal_encoding(DJ_CHARACTER_SET);
