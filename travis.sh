d38a69a6f4cf9308560aba54e448827e2d52f179
Jaap Eldering <jaap@jaapeldering.nl>
2016-07-30 23:00:34 -0300

    Fix previous commit: don't exit at successful install.

--- travis.sh
+++ travis.sh
@@ -11,9 +11,7 @@ ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
 if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]; then
     php composer-setup.php --quiet
-    RESULT=$?
     rm composer-setup.php
-    exit $RESULT
 else
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
