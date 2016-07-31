229be3eb534308b1ac65560464cb15c9d85c70af
Jaap Eldering <jaap@jaapeldering.nl>
2016-07-30 23:06:50 -0300

    Next fix: install as 'composer'.

--- travis.sh
+++ travis.sh
@@ -10,7 +10,7 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
 if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]; then
-    php composer-setup.php --quiet
+    php composer-setup.php --filename=composer
     rm composer-setup.php
 else
     >&2 echo 'ERROR: Invalid installer signature'
