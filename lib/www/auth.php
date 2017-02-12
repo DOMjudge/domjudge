0ee09e086d25ad2d8b00bfa0b135f4014f41f9ad
Jaap Eldering <jaap@jaapeldering.nl>
2017-02-12 14:13:54 -0200

    Give PHP session cookie a specific name and bind to this web interface instance only.
    This fixes a security issue where sessions could possibly be
    shared/hijacked between different websites on the same host.
    We also set the httponly flag for the session cookie.
    
    Cherry picked from commit 0ea7a18792ecf0.
    
    Conflicts:
    	ChangeLog

--- lib/www/auth.php
+++ lib/www/auth.php
@@ -12,6 +12,12 @@
 
 $ip = $_SERVER['REMOTE_ADDR'];
 
+session_name('domjudge_session');
+
+session_set_cookie_params(0, preg_replace('/\/(api|jury|public|team)\/?$/', '/',
+                                          dirname($_SERVER['PHP_SELF'])),
+                          null, false, true);
+
 $teamid = NULL;
 $username = NULL;
 $teamdata = NULL;
