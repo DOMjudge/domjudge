bac19c8a87768a9c924a7d163d0c78792396ef46
Jaap Eldering <jaap@jaapeldering.nl>
2017-08-14 00:17:17 +0200

    Bugfix from WF'17: num_correct was renamed num_points in scores matrix returned by genScoreBoard().

--- www/jury/impexp_tsv.php
+++ www/jury/impexp_tsv.php
@@ -367,7 +367,7 @@ function tsv_results_get()
 	$cnt = 0;
 	foreach ($sb['scores'] as $teamid => $srow) {
 		$cnt++;
-		$median = $srow['num_correct'];
+		$median = $srow['num_points'];
 		if ($cnt > $numteams/2) { // XXX: lower or upper median?
 			break;
 		}
@@ -383,7 +383,7 @@ function tsv_results_get()
 		}
 
 		$rank = $srow['rank'];
-		$num_correct = $srow['num_correct'];
+		$num_correct = $srow['num_points'];
 		if ( $rank <= 4 ) {
 			$awardstring = "Gold Medal";
 		} else if ( $rank <= 8 ) {
@@ -409,7 +409,7 @@ function tsv_results_get()
 		}
 
 		$data[] = array(@$sb['teams'][$teamid]['externalid'],
-				$rank, $awardstring, $srow['num_correct'],
+				$rank, $awardstring, $srow['num_points'],
 				$srow['total_time'], $maxtime, $groupwinner);
 	}
 
