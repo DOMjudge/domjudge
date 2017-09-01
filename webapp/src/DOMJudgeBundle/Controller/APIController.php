e9167eb2cbe85184682e2c4931ac2d160cca6acc
Tobias Werth <werth@cs.fau.de>
2017-09-01 16:33:26 +0200

    Add general ability to PATCH start_time.
    Probably it's also a good idea to check more stuff (in the contest
    object) like overlapping times. Or only allow changing the start_time of
    contests with all relative time specifications.
    
    Currently, it's not possible to set the start_time to null and pause
    contest countdown.

--- webapp/src/DOMJudgeBundle/Controller/APIController.php
+++ webapp/src/DOMJudgeBundle/Controller/APIController.php
@@ -12,7 +12,8 @@ use FOS\RestBundle\Controller\Annotations\Patch;
 use FOS\RestBundle\Controller\Annotations\Head;
 use FOS\RestBundle\Controller\Annotations\Delete;
 
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use DOMJudgeBundle\Entity\Contest;
 
@@ -35,6 +36,52 @@ class APIController extends FOSRestController {
 		} else {
 			return $contests[0];
 		}
+	}
+
+	/**
+	 * @Patch("/")
+	 */
+	public function changeStartTime(Request $request) {
+		$contest = $this->getCurrentActiveContest();
+		if ($contest === NULL) {
+			return NULL;
+		}
+		$args = $request->request->all();
+		$response = NULL;
+		if ( !isset($args['id']) ) {
+			$response = new Response('Missing "id" in request.', 400);
+		} else if ( !isset($args['start_time']) ) {
+			$response = new Response('Missing "start_time" in request.', 400);
+		} else if ( $args['id'] != $contest['id'] ) {
+			$response = new Response('Invalid "id" in request.', 400);
+		} else {
+			$em = $this->getDoctrine()->getManager();
+			$contestObject = $em->getRepository(Contest::class)->findOneBy(
+				array(
+					'cid' => $args['id'],
+				)
+			);
+			$date = \DateTime::createFromFormat(\DateTime::ATOM, $args['start_time']);
+			if ( $date === FALSE) {
+				$response = new Response('Invalid "start_time" in request.', 400);
+			} else {
+				$new_start_time = $date->getTimestamp();
+				$now = microtime(TRUE);
+				if ( $new_start_time < $now + 30 ) {
+					$response = new Response('New start_time not far in enough in future.', 403);
+				} else if ( FALSE && $contestObject->getStarttime() != NULL && $contestObject->getStarttime() < $now + 30 ) {
+					$response = new Response('Current contest already started or about to start.', 403);
+				} else {
+					$em->persist($contestObject);
+					$newStartTimeString = date('Y-m-d H:i:s e', $new_start_time);
+					$contestObject->setStarttimeString($newStartTimeString);
+					$contestObject->setStarttime($new_start_time);
+					$response = new Response('Contest start time changed to ' . $newStartTimeString, 200);
+					$em->flush();
+				}
+			}
+		}
+		return $response;
 	}
 
 	/**
