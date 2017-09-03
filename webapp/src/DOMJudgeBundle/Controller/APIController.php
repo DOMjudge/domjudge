11107331df0f0d3de34b5f8abe4229404da3257f
Jaap Eldering <jaap@jaapeldering.nl>
2017-09-03 16:56:32 +0200

    Use DOMjudge version from parameters.yml from global config.

--- webapp/src/DOMJudgeBundle/Controller/APIController.php
+++ webapp/src/DOMJudgeBundle/Controller/APIController.php
@@ -27,8 +27,6 @@ use DOMJudgeBundle\Utils\Utils;
 class APIController extends FOSRestController {
 
 	public $apiVersion = 4;
-	// FIXME: get DOMjudge version from central location
-	public $domjudgeVersion = "5.2.0DEV";
 
 	/**
 	 * @Get("/")
@@ -103,7 +101,7 @@ class APIController extends FOSRestController {
 	public function getInfoAction() {
 		$data = [
 			'api_version' => $this->apiVersion,
-			'domjudge_version' => $this->domjudgeVersion,
+			'domjudge_version' => $this->getParameter('domjudge.version'),
 		];
 		return $data;
 	}
