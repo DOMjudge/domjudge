48d255c5df048b645df8455ecd95465a95fbd89b
Thijs Kinkhorst <thijs@kinkhorst.com>
2017-09-16 16:38:08 +0000

    Update minimum PHP requirement 7.0 also in other places.

--- www/jury/checkconfig.php
+++ www/jury/checkconfig.php
@@ -99,9 +99,9 @@ echo "<table class=\"configcheck\">\n";
 
 // SOFTWARE
 
-if( !function_exists('version_compare') || version_compare( '5.4',PHP_VERSION,'>=') ) {
+if( !function_exists('version_compare') || version_compare( '7.0',PHP_VERSION,'>=') ) {
 	result('software', 'PHP version', 'E',
-		'You have PHP ' . PHP_VERSION . ', but need at least 5.4.0.',
+		'You have PHP ' . PHP_VERSION . ', but need at least 7.0.0.',
 		'See <a href="?phpinfo">phpinfo</a> for details.');
 } else {
 	result('software', 'PHP version', 'O',
