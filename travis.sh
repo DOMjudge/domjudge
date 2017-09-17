485cda264cf0ef6e3a936c907db3fce5b4e6eca3
Thijs Kinkhorst <thijs@kinkhorst.com>
2017-09-17 15:37:29 +0000

    Composer is already available on travis.

--- travis.sh
+++ travis.sh
@@ -5,21 +5,6 @@ export PS4='(${BASH_SOURCE}:${LINENO}): - [$?] $ '
 DIR=$(pwd)
 lsb_release -a
 
-# install composer per the composer docs, see:
-# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
-EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]; then
-    php composer-setup.php --filename=composer
-    rm composer-setup.php
-else
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
-
 # FIXME: This chicken-egg problem is annoying but let us bootstrap for now.
 echo "CREATE DATABASE IF NOT EXISTS \`domjudge\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql
 echo "GRANT SELECT, INSERT, UPDATE, DELETE ON \`domjudge\`.* TO 'domjudge'@'localhost' IDENTIFIED BY 'domjudge';" | mysql
@@ -44,7 +29,7 @@ EOF
 
 # install all php dependencies
 export SYMFONY_ENV="prod"
-./composer install
+composer install
 
 # run phpunit tests
 lib/vendor/bin/phpunit webapp
