9a397e1df1d44369cc2128acba11bb6258c8672f
Nicky Gerritsen <nickygerritsen@me.com>
2017-11-12 13:53:46 +0100

    Add CORS header to API requests. FIxes #312

---
+++ webapp/src/DOMJudgeBundle/EventListener/ApiHeadersListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DOMJudgeBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class ApiHeadersListener
+{
+	public function onKernelResponse(FilterResponseEvent $event)
+	{
+		$request = $event->getRequest();
+		// Check if this is an API request
+		if (strpos($request->getPathInfo(), '/api') === 0) {
+			// It is, so add CORS headers
+			$response = $event->getResponse();
+			$response->headers->set('Access-Control-Allow-Origin', '*');
+		}
+	}
+}
