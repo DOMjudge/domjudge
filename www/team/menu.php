175211ab5349d471efa52d176ee3e0043c46fc0a
Thijs Kinkhorst <thijs@kinkhorst.com>
2018-02-10 09:14:35 +0000

    Fix 'active' class in nav item for the current page.
    This could surely be refactored, but that is left for later.

--- www/team/menu.php
+++ www/team/menu.php
@@ -2,19 +2,18 @@
 $fdata = calcFreezeData($cdata);
 $started = checkrole('jury') || $fdata['started'];
 ?>
-
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <a class="navbar-brand hidden-sm-down" href="./">DOMjudge</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuDefault" aria-controls="menuDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <div class="collapse navbar-collapse" id="menuDefault">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+	  <li class="nav-item<?=($pagename === 'index.php'?' active':'')?>">
             <a class="nav-link" href="./"><span class="octicon octicon-home"></span> Home</a>
           </li>
-          <li class="nav-item">
+	  <li class="nav-item<?=($pagename === 'problems.php'?' active':'')?>">
 <?php if ($started): ?>
             <a class="nav-link" href="problems.php"><span class="octicon octicon-book"></span> Problemset</a>
 <?php else: ?>
@@ -22,11 +21,11 @@ $started = checkrole('jury') || $fdata['started'];
 <?php endif; ?>
           </li>
 <?php if (have_printing()): ?>
-          <li class="nav-item">
+	  <li class="nav-item<?=($pagename === 'print.php'?' active':'')?>">
             <a class="nav-link" href="print.php"><span class="octicon octicon-file-text"></span> Print</a>
 	  </li>
 <?php endif; ?>
-          <li class="nav-item">
+	  <li class="nav-item<?=($pagename === 'scoreboard.php'?' active':'')?>">
             <a class="nav-link" href="scoreboard.php"><span class="octicon octicon-list-ordered"></span> Scoreboard</a>
           </li>
 <?php if ( checkrole('jury') || checkrole('balloon') ): ?>
