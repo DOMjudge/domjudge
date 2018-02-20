2e08f20781b7660b75e884bd7bc52b28c39dccde
Tobias Werth <werth@cs.fau.de>
2018-02-20 23:43:01 +0100

    Minor optimizations to example programs.
    (cherry picked from commit 45c31953a1177750e75352c1614eb20ff06d0f5e)

--- doc/examples/example.py
+++ doc/examples/example.py
@@ -1,14 +1,7 @@
-#!/usr/bin/env python2
-# This shebang line above is not necessary for submission, but makes
-# it easier to run your program locally.
-
 import sys
 
-def main():
-    n = int(input())
-    for i in range(n):
-        name = sys.stdin.readline().rstrip('\n')
-        print('Hello %s!' % (name))
+n = int(input())
+for i in range(n):
+    name = sys.stdin.readline().rstrip('\n')
+    print('Hello %s!' % (name))
 
-if __name__ == '__main__':
-    main()
