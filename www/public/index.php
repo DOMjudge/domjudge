f3f6eba4be05c6de8739c638a182b9af41221879
Tobias Werth <werth@cs.fau.de>
2018-03-09 20:24:26 +0100

    Fix some static scoreboard things.
    - refresh ends up at static page again
    - menu is not displayed on static page
    - banner is still displayed
    
    (cherry picked from commit 137d5f844356d96e07941ef4a4c617565f3f40dd)

--- www/public/index.php
+++ www/public/index.php
@@ -8,20 +8,23 @@
  */
 
 require('init.php');
-$title="Scoreboard";
+$title = "Scoreboard";
+$isstatic = @$_SERVER['argv'][1] == 'static' || isset($_REQUEST['static']);
+
 // set auto refresh
-$refresh=array(
+$refresh = array(
 	"after" => "30",
 	"url" => "./",
 );
+if ( $isstatic ) {
+	$refresh['url'] .= '?static=1';
+}
 
 // This reads and sets a cookie, so must be called before headers are sent.
 $filter = initScorefilter();
 
-$menu = true;
+$menu = !$isstatic;
 require(LIBWWWDIR . '/header.php');
-
-$isstatic = @$_SERVER['argv'][1] == 'static' || isset($_REQUEST['static']);
 
 // call the general putScoreBoard function from scoreboard.php
 putScoreBoard($cdata, null, $isstatic, $filter);
