bdc4d2c9e0284fb2def45be54d6ab903edd99361
Tobias Werth <werth@cs.fau.de>
2018-03-11 15:53:18 +0100

    Include post_max_size value in error message.
    (cherry picked from commit 0aadc2b62037ed0fef5eb54e9059bb67922c5d17)

--- www/jury/init.php
+++ www/jury/init.php
@@ -40,7 +40,7 @@ if (!$allowed) {
 require_once(LIBWWWDIR . '/common.jury.php');
 if ( $_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST) && empty($_FILES)
   && isset($_SERVER['CONTENT_LENGTH']) && $_SERVER['CONTENT_LENGTH'] > 0 ) {
-	error("POST data exceeded php.ini's 'post_max_size' directive.");
+	error("POST data exceeded php.ini's 'post_max_size' directive (currently set to " . ini_get('post_max_size') . ')');
 }
 
 $cdatas = getCurContests(TRUE, null, TRUE);
