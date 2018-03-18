6a2c2ff75e966185873666513d8938e14b8de8da
Tobias Werth <werth@cs.fau.de>
2018-03-18 08:21:15 +0100

    Add basic unit test that the routing works.

---
+++ webapp/tests/DOMJudgeBundle/GeneralAvailabilityTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GeneralAvailabilityTest extends WebTestCase {
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url, $code) {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+	$response = $client->getResponse();
+	$actual = $response->getStatusCode();
+	$this->assertEquals($code, $actual, var_export($response, TRUE));
+    }
+
+    public function urlProvider() {
+	    yield ['/public/problems.php', 200];
+	    yield ['/public/index.php', 200];
+	    yield ['/public/', 200];
+	    yield ['/login', 200];
+
+	    yield ['/api', 301];
+	    yield ['/api/', 200];
+	    yield ['/api/contests', 200];
+	    yield ['/api/contests/demo', 200];
+	    // Note that the individual API endpoints are tested with check-api
+	    // and cannot easily be tested here since phpunit doesn't provide a
+	    // fully featured server environment.
+
+	    yield ['/', 302];
+	    yield ['/team/', 302];
+	    yield ['/jury/', 302];
+	    yield ['/logout', 302];
+
+	    yield ['/public/doesnotexist.php', 404];
+    }
+}
