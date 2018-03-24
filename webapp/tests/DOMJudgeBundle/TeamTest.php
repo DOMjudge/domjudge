ec816b86f19feb32236e0b7a3b80b1773d69742c
Tobias Werth <werth@cs.fau.de>
2018-03-24 14:32:30 +0100

    Add basic team tests.
    Testing clarification requests and submissions is harder since we still
    have an awful lot of global state which is not properly resetted between
    tests.
    
    (cherry picked from commit 433e22be749017b10218538a5528b74b174489e3)

---
+++ webapp/tests/DOMJudgeBundle/TeamTest.php
@@ -0,0 +1,80 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TeamTest extends WebTestCase {
+    public function testTeamRedirectToLogin() {
+        $client = self::createClient();
+        $client->request('GET', '/team/');
+
+	$response = $client->getResponse();
+	$message = var_export($response, TRUE);
+	$this->assertEquals(302, $response->getStatusCode(), $message);
+	$this->assertEquals('http://localhost/login', $response->getTargetUrl(), $message);
+    }
+
+    private function loginHelper($username, $password, $redirectPage) {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/login');
+
+	# load login page
+	$response = $client->getResponse();
+	$message = var_export($response, TRUE);
+	$this->assertEquals(200, $response->getStatusCode(), $message);
+
+	# submit form
+	$button = $crawler->selectButton('Sign in');
+	$form = $button->form(array(
+		'_username' => $username,
+		'_password' => $password,
+	));
+	$crawler = $client->submit($form);
+
+	# check redirect to /
+	$response = $client->getResponse();
+	$message = var_export($response, TRUE);
+	$this->assertEquals(302, $response->getStatusCode(), $message);
+	$this->assertEquals($redirectPage, $response->getTargetUrl(), $message);
+
+	return $client;
+    }
+
+    public function testLogin() {
+	    # test incorrect and correct password
+	    $this->loginHelper('dummy', 'foo', 'http://localhost/login');
+	    $this->loginHelper('dummy', 'dummy', 'http://localhost/');
+    }
+
+    public function testTeamOverviewPage() {
+	    $client = $this->loginHelper('dummy', 'dummy', 'http://localhost/');
+	    $crawler = $client->request('GET', '/team/');
+
+	    $response = $client->getResponse();
+	    $message = var_export($response, TRUE);
+	    $this->assertEquals(200, $response->getStatusCode(), $message);
+
+	    $this->assertEquals(1, $crawler->filter('html:contains("Utrecht University")')->count());
+
+	    $h3s = $crawler->filter('h3')->extract(array('_text'));
+	    $this->assertEquals('Submissions', $h3s[0]);
+	    $this->assertEquals('Clarifications', $h3s[1]);
+	    $this->assertEquals('Clarification Requests', $h3s[2]);
+    }
+
+    public function testClarificationRequest() {
+	    $client = $this->loginHelper('dummy', 'dummy', 'http://localhost/');
+	    $crawler = $client->request('GET', '/team/');
+
+	    $response = $client->getResponse();
+	    $message = var_export($response, TRUE);
+	    $this->assertEquals(200, $response->getStatusCode(), $message);
+
+	    $link = $crawler->selectLink('request clarification')->link();
+	    $message = var_export($link, TRUE);
+	    $this->assertEquals('http://localhost/team/clarification.php', $link->getUri(), $message);
+
+	    # Note that we would like to click the link here but we cannot do
+	    # that since we have too much global state, e.g. define IS_JURY
+	    # constants.
+    }
+}
