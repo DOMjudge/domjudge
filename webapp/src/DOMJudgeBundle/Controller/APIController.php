6bf1d987ebda786ed46978d6984324f5b0a7d9c7
Jaap Eldering <jaap@jaapeldering.nl>
2018-03-25 18:33:11 +0200

    Restrict event-feed access to jury to simplify managing at WFs.
    Note that none of our own tools currently depend on access to
    the event-feed, so we can safely make this restriction.

--- webapp/src/DOMJudgeBundle/Controller/APIController.php
+++ webapp/src/DOMJudgeBundle/Controller/APIController.php
@@ -219,6 +219,7 @@ class APIController extends FOSRestController {
 
 	/**
 	 * @Get("/contests/{externalid}/event-feed")
+	 * @Security("has_role('ROLE_JURY')")
 	 */
 	public function getEventFeed(Request $request, Contest $contest) {
 		// Make sure this script doesn't hit the PHP maximum execution timeout.
