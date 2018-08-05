DOMjudge/domjudge

commit 78923c51809df479fb64a3fa752f809a34eb4db3

Fix indentation and function argument alignment for SQL queries.

www/jury/submission.php
@@ -246,8 +246,7 @@ if (! $submdata) {
     error("Missing submission data");
 }
 
-$jdata = $DB->q(
-    'KEYTABLE SELECT judgingid AS ARRAYKEY, result, j.valid, j.starttime,
+$jdata = $DB->q('KEYTABLE SELECT judgingid AS ARRAYKEY, result, j.valid, j.starttime,
                  j.endtime, j.judgehost, j.verified, j.jury_member, j.verify_comment,
                  r.reason, r.rejudgingid,
                  MAX(jr.runtime) AS max_runtime,
@@ -259,9 +258,7 @@ $jdata = $DB->q(
                  WHERE cid = %i AND submitid = %i
                  GROUP BY (j.judgingid)
                  ORDER BY starttime ASC, judgingid ASC',
-                $submdata['cid'],
-    $id
-);
+                $submdata['cid'], $id);
 
 // When there's no judging selected through the request, we select the
 // valid one.
@@ -464,8 +461,7 @@ if (!isset($jid)) {
             $extra_where .= 'AND s.langid IN (%As) ';
         }
 
-        $submitid = $DB->q(
-            'MAYBEVALUE SELECT s.submitid
+        $submitid = $DB->q('MAYBEVALUE SELECT s.submitid
                             FROM submission s
                             LEFT JOIN language l USING (langid)
                             LEFT JOIN contestproblem cp USING (probid, cid) ' .
@@ -473,11 +469,7 @@ if (!isset($jid)) {
                            'WHERE s.submitid = %i AND s.judgehost IS NULL
                             AND l.allow_judge = 1 AND cp.allow_judge = 1 AND s.valid = 1 ' .
                            $extra_where . 'LIMIT 1',
-                           $id,
-            $contests,
-            $problems,
-            $languages
-        );
+                           $id, $contests, $problems, $languages);
 
         if (isset($submitid)) {
             $can_be_judged = true;
@@ -519,17 +511,11 @@ $jud = $DB->q('TUPLE SELECT j.*, r.valid AS rvalid
 
 // sanity check
 if ($jud['submitid'] != $id) {
-    error(sprintf(
-        "judingid j%d belongs to submitid s%d, not s%d",
-                  $jid,
-        $jud['submitid'],
-        $id
-    ));
+    error(sprintf("judingid j%d belongs to submitid s%d, not s%d", $jid, $jud['submitid'], $id));
 }
 
 // Display testcase runs
-$runs = $DB->q(
-    'TABLE SELECT r.runid, r.judgingid,
+$runs = $DB->q('TABLE SELECT r.runid, r.judgingid,
                 r.testcaseid, r.runresult, r.runtime, ' .
                 truncate_SQL_field('r.output_run')    . ' AS output_run, ' .
                 truncate_SQL_field('r.output_diff')   . ' AS output_diff, ' .
@@ -541,24 +527,18 @@ $runs = $DB->q(
                 LEFT JOIN judging_run r ON ( r.testcaseid = t.testcaseid AND
                                              r.judgingid = %i )
                 WHERE t.probid = %s ORDER BY rank',
-               $jid,
-    $submdata['probid']
-);
+               $jid, $submdata['probid']);
 
 // Use original submission as previous, or try to find a previous
 // submission/judging of the same team/problem.
 if (isset($submdata['origsubmitid'])) {
     $lastsubmitid = $submdata['origsubmitid'];
 } else {
-    $lastsubmitid = $DB->q(
-        'MAYBEVALUE SELECT submitid
+    $lastsubmitid = $DB->q('MAYBEVALUE SELECT submitid
                             FROM submission
                             WHERE teamid = %i AND probid = %i AND submittime < %s
                             ORDER BY submittime DESC LIMIT 1',
-                           $submdata['teamid'],
-        $submdata['probid'],
-                           $submdata['submittime']
-    );
+                           $submdata['teamid'], $submdata['probid'], $submdata['submittime']);
 }
 
 $lastjud = null;
@@ -568,15 +548,12 @@ if ($lastsubmitid !== null) {
                        WHERE submitid = %s AND valid = 1
                        ORDER BY judgingid DESC LIMIT 1', $lastsubmitid);
     if ($lastjud !== null) {
-        $lastruns = $DB->q(
-            'TABLE SELECT r.runtime, r.runresult, rank, description
+        $lastruns = $DB->q('TABLE SELECT r.runtime, r.runresult, rank, description
                             FROM testcase t
                             LEFT JOIN judging_run r ON ( r.testcaseid = t.testcaseid AND
                             r.judgingid = %i )
                             WHERE t.probid = %s ORDER BY rank',
-                           $lastjud['judgingid'],
-            $submdata['probid']
-        );
+                           $lastjud['judgingid'], $submdata['probid']);
     }
 }
 
@@ -728,13 +705,11 @@ if (!empty($jud['result'])) {
          && defined('EXT_CCS_URL')) {
         echo msgbox(
             'results differ',
-                    'This submission was judged as '
-                    . '<a href="' . EXT_CCS_URL
-                    . urlencode($submdata['externalid']) . '" target="extCCS">'
-                    . printresult($submdata['externalresult']) . '</a>'
-                    . ' by the external CCS, but as '
-                    . printresult($jud['result'])
-                    . ' by DOMjudge.'
+            'This submission was judged as ' .
+            '<a href="' . EXT_CCS_URL . urlencode($submdata['externalid']) . '" target="extCCS">'
+            printresult($submdata['externalresult']) . '</a>' .
+            ' by the external CCS, but as ' .
+            printresult($jud['result']) . ' by DOMjudge.'
         );
     }
 } else { // judging does not have a result yet

import/import-RESTfeed.in
@@ -105,11 +105,8 @@ function remap($type, $id)
 function getTeamID($externalid)
 {
     global $DB;
-    return $DB->q(
-        'MAYBEVALUE SELECT teamid FROM team
-                   WHERE teamid = %s AND enabled = 1',
-                  $externalid
-    );
+    return $DB->q('MAYBEVALUE SELECT teamid FROM team
+                   WHERE teamid = %s AND enabled = 1', $externalid);
 }
 
 $contest = dj_json_decode(request('contest'));
@@ -134,12 +131,9 @@ if (difftime($contest['start'], $cdata['starttime'])!=0 ||
 
 $problems = dj_json_decode(request('problems'));
 foreach ($problems as $prob) {
-    $id = $DB->q(
-        'MAYBEVALUE SELECT probid FROM problem INNER JOIN contestproblem USING(probid)
+    $id = $DB->q('MAYBEVALUE SELECT probid FROM problem INNER JOIN contestproblem USING(probid)
                   WHERE probid = %s AND cid = %i AND allow_submit = 1',
-                 remap('problem', $prob['id']),
-        $cid
-    );
+                 remap('problem', $prob['id']), $cid);
 
     if (empty($id)) {
         error("Problem ID '".$prob['id']."' from external CCS not available locally.");
@@ -148,11 +142,9 @@ foreach ($problems as $prob) {
 
 $languages = dj_json_decode(request('languages'));
 foreach ($languages as $lang) {
-    $id = $DB->q(
-        'MAYBEVALUE SELECT langid FROM language
+    $id = $DB->q('MAYBEVALUE SELECT langid FROM language
                   WHERE langid = %s AND allow_submit = 1',
-                 remap('language', $lang['id'])
-    );
+                 remap('language', $lang['id']));
 
     if (empty($id)) {
         error("Language ID '".$lang['id']."' from external CCS not available locally.");
@@ -163,8 +155,8 @@ $teams = dj_json_decode(request('teams'));
 foreach ($teams as $team) {
     $id = getTeamID(remap('team', $team['id']));
 
-    // 	$DB->q('UPDATE team SET name = %s WHERE login = %s',
-    // 	       $team['name'],remap('team',$team['id']));
+    //$DB->q('UPDATE team SET name = %s WHERE login = %s',
+    //       $team['name'],remap('team',$team['id']));
 
     if (empty($id)) {
         error("Team ID '".$team['id']."' from external CCS not available locally.");
@@ -193,12 +185,9 @@ while (true) {
     foreach ($submissions as $subm) {
         $lastsubmitid = max($lastsubmitid, $subm['id']);
 
-        $res = $DB->q(
-            'MAYBETUPLE SELECT submitid, teamid, probid, langid, submittime
+        $res = $DB->q('MAYBETUPLE SELECT submitid, teamid, probid, langid, submittime
                        FROM submission WHERE cid = %i AND externalid = %i',
-                      $cid,
-            $subm['id']
-        );
+                      $cid, $subm['id']);
 
         if (!empty($res)) {
             if ($res['teamid']!=getTeamID(remap('team', $subm['team'])) ||
@@ -267,13 +256,9 @@ while (true) {
     foreach ($judgings as $jud) {
         $lastjudgingid = max($lastjudgingid, $jud['id']);
 
-        $DB->q(
-            'UPDATE submission SET externalresult = %s
+        $DB->q('UPDATE submission SET externalresult = %s
                 WHERE cid = %i AND externalid = %i',
-               remap('result', $jud['outcome']),
-            $cid,
-            $jud['submission']
-        );
+               remap('result', $jud['outcome']), $cid, $jud['submission']);
 
         logmsg(LOG_INFO, "Updated external result for s".$jud['submission'].
                " to '".remap('result', $jud['outcome'])."'");

import/import-XMLfeed.in
@@ -200,12 +200,9 @@ logmsg(LOG_DEBUG, "moved *to* '$feedurl' from '$feedurl.mine.xml'");
 
 $problems = $xpath->query('/contest/problem');
 foreach ($problems as $prob) {
-    $id = $DB->q(
-        'MAYBEVALUE SELECT probid FROM contestproblem
+    $id = $DB->q('MAYBEVALUE SELECT probid FROM contestproblem
                   WHERE probid = %s AND cid = %i AND allow_submit = 1',
-                 remap('problem', val($prob, 'id')),
-        $cid
-    );
+                 remap('problem', val($prob, 'id')), $cid);
 
     if (empty($id)) {
         error("Problem ID '".val($prob, 'id')."' from external CCS not available locally.");
@@ -214,11 +211,9 @@ foreach ($problems as $prob) {
 
 $languages = $xpath->query('/contest/language');
 foreach ($languages as $lang) {
-    $id = $DB->q(
-        'MAYBEVALUE SELECT langid FROM language
+    $id = $DB->q('MAYBEVALUE SELECT langid FROM language
                   WHERE langid = %s AND allow_submit = 1',
-                 remap('language', val($lang, 'id'))
-    );
+                 remap('language', val($lang, 'id')));
 
     if (empty($id)) {
         error("Language ID '".val($lang, 'id')."' from external CCS not available locally.");
@@ -231,11 +226,9 @@ foreach ($languages as $lang) {
 
 $teams = $xpath->query('/contest/team');
 foreach ($teams as $team) {
-    $id = $DB->q(
-        'MAYBEVALUE SELECT teamid FROM team
+    $id = $DB->q('MAYBEVALUE SELECT teamid FROM team
                   WHERE teamid = %s AND enabled = 1',
-                 remap('team', val($team, 'id'))
-    );
+                 remap('team', val($team, 'id')));
 
     if (empty($id)) {
         error("Team ID '".val($team, 'id')."' from external CCS not available locally.");
@@ -319,13 +312,13 @@ while (true) {
 
             $id = submit_solution(
                 remap('team', val($run, 'team')),
-                                  remap('problem', val($run, 'problem')),
-                                  $cid,
-                                  remap('language', val($run, 'language')),
-                                  $files,
+                remap('problem', val($run, 'problem')),
+                $cid,
+                remap('language', val($run, 'language')),
+                $files,
                 $filenames,
                 null,
-                                  $id,
+                $id,
                 val($run, 'timestamp')
             );
 
@@ -339,13 +332,9 @@ while (true) {
         if (val($run, 'judged')==='True') {
             // We're inserting a new judging
 
-            $DB->q(
-                'UPDATE submission SET externalresult = %s
+            $DB->q('UPDATE submission SET externalresult = %s
                     WHERE cid = %i AND externalid = %i',
-                   remap('result', val($run, 'result')),
-                $cid,
-                $id
-            );
+                   remap('result', val($run, 'result')), $cid, $id);
 
             logmsg(LOG_INFO, "Updated external result for s".$id.
                    " to '".remap('result', val($run, 'result'))."'");

lib/lib.impexp.php
@@ -122,16 +122,12 @@ function tsv_teams_set($data)
         // it is legitimate that a team has no affiliation. Do not add it then.
         if (!empty($row['team_affiliation']['shortname'])) {
             // First look up if the affiliation already exists.
-            $affilid = $DB->q(
-                "MAYBEVALUE SELECT affilid FROM team_affiliation
+            $affilid = $DB->q("MAYBEVALUE SELECT affilid FROM team_affiliation
                                WHERE externalid = %s LIMIT 1",
-                              $row['team_affiliation']['externalid']
-            );
+                              $row['team_affiliation']['externalid']);
             if (empty($affilid)) {
-                $affilid = $DB->q(
-                    "RETURNID INSERT INTO team_affiliation SET %S",
-                                  $row['team_affiliation']
-                );
+                $affilid = $DB->q("RETURNID INSERT INTO team_affiliation SET %S",
+                                  $row['team_affiliation']);
 
                 eventlog('team_affiliation', $affilid, 'create');
                 auditlog('team_affiliation', $affilid, 'added', 'imported from tsv');
@@ -227,11 +223,8 @@ function tsv_accounts_set($data)
     $cnt = 0;
     foreach ($data as $row) {
         if (! empty($row['team'])) {
-            $teamid = $DB->q(
-                "MAYBEVALUE SELECT teamid FROM team WHERE name = %s AND categoryid = %i",
-                $row['team']['name'],
-                $row['team']['categoryid']
-            );
+            $teamid = $DB->q("MAYBEVALUE SELECT teamid FROM team WHERE name = %s AND categoryid = %i",
+                             $row['team']['name'], $row['team']['categoryid']);
             if (is_null($teamid)) {
                 $teamid = $DB->q("RETURNID INSERT INTO team SET %S", $row['team']);
             }

lib/lib.misc.php
@@ -138,13 +138,10 @@ function problemVisible($probid)
         return false;
     }
 
-    return $DB->q(
-        'MAYBETUPLE SELECT probid FROM problem
+    return $DB->q('MAYBETUPLE SELECT probid FROM problem
                    INNER JOIN contestproblem USING (probid)
                    WHERE cid = %i AND allow_submit = 1 AND probid = %i',
-                  $cdata['cid'],
-        $probid
-    ) !== null;
+                  $cdata['cid'], $probid) !== null;
 }
 
 /**
@@ -243,8 +240,7 @@ function calcScoreRow($cid, $team, $prob)
     // that these will not count as solved. Correct submissions with
     // submittime after contest end should never happen, unless one
     // resets the contest time after successful judging.
-    $result = $DB->q(
-        'SELECT result, verified, submittime,
+    $result = $DB->q('SELECT result, verified, submittime,
                       (c.freezetime IS NOT NULL && submittime >= c.freezetime) AS afterfreeze
                       FROM submission s
                       LEFT JOIN judging j ON(s.submitid=j.submitid AND j.valid=1)
@@ -254,10 +250,7 @@ function calcScoreRow($cid, $team, $prob)
                        "AND (j.result IS NULL OR j.result != 'compiler-error') ") .
                      'AND submittime < c.endtime
                       ORDER BY submittime',
-                     $team,
-        $prob,
-        $cid
-    );
+                     $team, $prob, $cid);
 
     // reset vars
     $submitted_j = $pending_j = $time_j = $correct_j = 0;
@@ -301,24 +294,14 @@ function calcScoreRow($cid, $team, $prob)
     }
 
     // insert or update the values in the public/team scores table
-    $DB->q(
-        'REPLACE INTO scorecache
+    $DB->q('REPLACE INTO scorecache
             (cid, teamid, probid,
              submissions_restricted, pending_restricted, solvetime_restricted, is_correct_restricted,
              submissions_public, pending_public, solvetime_public, is_correct_public)
             VALUES (%i,%i,%i,%i,%i,%i,%i,%i,%i,%i,%i)',
-           $cid,
-        $team,
-        $prob,
-           $submitted_j,
-        $pending_j,
-        $time_j,
-        $correct_j,
-           $submitted_p,
-        $pending_p,
-        $time_p,
-        $correct_p
-    );
+           $cid, $team, $prob,
+           $submitted_j, $pending_j, $time_j, $correct_j,
+           $submitted_p, $pending_p, $time_p, $correct_p);
 
     if ($DB->q("VALUE SELECT RELEASE_LOCK('$lockstr')") != 1) {
         error("calcScoreRow failed to release lock '$lockstr'");
@@ -379,18 +362,13 @@ function updateRankCache($cid, $team)
     }
 
     // Update the rank cache table
-    $DB->q(
-        "REPLACE INTO rankcache (cid, teamid,
+    $DB->q("REPLACE INTO rankcache (cid, teamid,
             points_restricted, totaltime_restricted,
             points_public, totaltime_public)
             VALUES (%i,%i,%i,%i,%i,%i)",
-           $cid,
-        $team,
-           $num_points['restricted'],
-        $total_time['restricted'],
-           $num_points['public'],
-        $total_time['public']
-    );
+           $cid, $team,
+           $num_points['restricted'], $total_time['restricted'],
+           $num_points['public'],     $total_time['public']);
 
     // Release the lock
     if ($DB->q("VALUE SELECT RELEASE_LOCK('$lockstr')") != 1) {
@@ -422,23 +400,16 @@ function updateBalloons($submitid)
     }
 
     // prevent duplicate balloons in case of multiple correct submissions
-    $numcorrect = $DB->q(
-        'VALUE SELECT count(b.submitid)
+    $numcorrect = $DB->q('VALUE SELECT count(b.submitid)
                           FROM balloon b
                           LEFT JOIN submission s USING(submitid)
                           WHERE valid = 1 AND probid = %i
                           AND teamid = %i AND cid = %i',
-                         $subm['probid'],
-        $subm['teamid'],
-        $subm['cid']
-    );
+                         $subm['probid'], $subm['teamid'], $subm['cid']);
 
     if ($numcorrect == 0) {
-        $balloons_enabled = (bool)$DB->q(
-            'VALUE SELECT process_balloons
-                                          FROM contest WHERE cid = %i',
-                                         $subm['cid']
-        );
+        $balloons_enabled = (bool)$DB->q('VALUE SELECT process_balloons
+                                          FROM contest WHERE cid = %i', $subm['cid']);
         if ($balloons_enabled) {
             $DB->q('INSERT INTO balloon (submitid) VALUES (%i)', $submitid);
         }
@@ -707,12 +678,12 @@ function sig_handler($signal)
     logmsg(LOG_DEBUG, "Signal $signal received");
 
     switch ($signal) {
-    case SIGHUP:
-        $gracefulexitsignalled = true;
-        // no break
-    case SIGINT:   # Ctrl+C
-    case SIGTERM:
-        $exitsignalled = true;
+        case SIGHUP:
+            $gracefulexitsignalled = true;
+            // no break
+        case SIGINT:   # Ctrl+C
+        case SIGTERM:
+            $exitsignalled = true;
     }
 }
 
@@ -749,10 +720,9 @@ function initsignals()
 function daemonize($pidfile = null)
 {
     switch ($pid = pcntl_fork()) {
-    case -1: error("cannot fork daemon");
-    // no break
-    case  0: break; // child process: do nothing here.
-    default: exit;  // parent process: exit.
+        case -1: error("cannot fork daemon");
+        case  0: break; // child process: do nothing here.
+        default: exit;  // parent process: exit.
     }
 
     if (($pid = posix_getpid())===false) {
@@ -776,8 +746,8 @@ function daemonize($pidfile = null)
 
     // Close std{in,out,err} file descriptors
     if (!fclose(STDIN) || !($GLOBALS['STDIN']  = fopen('/dev/null', 'r')) ||
-         !fclose(STDOUT) || !($GLOBALS['STDOUT'] = fopen('/dev/null', 'w')) ||
-         !fclose(STDERR) || !($GLOBALS['STDERR'] = fopen('/dev/null', 'w'))) {
+        !fclose(STDOUT) || !($GLOBALS['STDOUT'] = fopen('/dev/null', 'w')) ||
+        !fclose(STDERR) || !($GLOBALS['STDERR'] = fopen('/dev/null', 'w'))) {
         error("cannot reopen stdio files to /dev/null");
     }
 
@@ -802,11 +772,11 @@ function submit_solution(
     $lang,
     $files,
     $filenames,
-                         $origsubmitid = null,
+    $origsubmitid = null,
     $entry_point = null,
     $extid = null,
     $submittime = null,
-                         $extresult = null
+    $extresult = null
 ) {
     global $DB;
 
@@ -858,7 +828,7 @@ function submit_solution(
 
     // Check 2: valid parameters?
     if (! $langdata = $DB->q('MAYBETUPLE SELECT langid, require_entry_point FROM language
-                            WHERE langid = %s AND allow_submit = 1', $lang)) {
+                              WHERE langid = %s AND allow_submit = 1', $lang)) {
         error("Language '$lang' not found in database or not submittable.");
     }
     $langid = $langdata['langid'];
@@ -870,13 +840,10 @@ function submit_solution(
                            (checkrole('jury') ? '' : ' AND enabled = 1'), $team)) {
         error("Team '$team' not found in database or not enabled.");
     }
-    $probdata = $DB->q(
-        'MAYBETUPLE SELECT probid, points FROM problem
+    $probdata = $DB->q('MAYBETUPLE SELECT probid, points FROM problem
                         INNER JOIN contestproblem USING (probid)
                         WHERE probid = %s AND cid = %i AND allow_submit = 1',
-                       $prob,
-        $contest
-    );
+                       $prob, $contest);
 
     if (empty($probdata)) {
         error("Problem p$prob not found in database or not submittable [c$contest].");
@@ -914,31 +881,17 @@ function submit_solution(
 
     // Insert submission into the database
     $DB->q('START TRANSACTION');
-    $id = $DB->q(
-        'RETURNID INSERT INTO submission
-              (cid, teamid, probid, langid, submittime, origsubmitid, entry_point,
+    $id = $DB->q('RETURNID INSERT INTO submission
+                  (cid, teamid, probid, langid, submittime, origsubmitid, entry_point,
                    externalid, externalresult)
                   VALUES (%i, %i, %i, %s, %s, %i, %s, %s, %s)',
-                 $contest,
-        $teamid,
-        $probid,
-        $langid,
-        $submittime,
-        $origsubmitid,
-        $entry_point,
-                 $extid,
-        $extresult
-    );
+                 $contest, $teamid, $probid, $langid, $submittime,
+                 $origsubmitid, $entry_point, $extid, $extresult);
 
     for ($rank=0; $rank<count($files); $rank++) {
-        $DB->q(
-            'INSERT INTO submission_file
+        $DB->q('INSERT INTO submission_file
                 (submitid, filename, rank, sourcecode) VALUES (%i, %s, %i, %s)',
-               $id,
-            $filenames[$rank],
-            $rank,
-            dj_file_get_contents($files[$rank])
-        );
+               $id, $filenames[$rank], $rank, dj_file_get_contents($files[$rank]));
     }
 
     // Add expected results from source. We only do this for jury
@@ -959,13 +912,15 @@ function submit_solution(
     if (is_writable(SUBMITDIR)) {
         // Copy the submission to SUBMITDIR for safe-keeping
         for ($rank=0; $rank<count($files); $rank++) {
-            $fdata = array('cid' => $contest,
-                           'submitid' => $id,
-                           'teamid' => $teamid,
-                           'probid' => $probid,
-                           'langid' => $langid,
-                           'rank' => $rank,
-                           'filename' => $filenames[$rank]);
+            $fdata = array(
+                'cid' => $contest,
+                'submitid' => $id,
+                'teamid' => $teamid,
+                'probid' => $probid,
+                'langid' => $langid,
+                'rank' => $rank,
+                'filename' => $filenames[$rank]
+            );
             $tofile = SUBMITDIR . '/' . getSourceFilename($fdata);
             if (! @copy($files[$rank], $tofile)) {
                 warning("Could not copy '" . $files[$rank] . "' to '" . $tofile . "'");
@@ -1041,13 +996,9 @@ function rejudge($table, $id, $include_all, $full_rejudge, $reason = null, $user
     }
 
     if ($full_rejudge) {
-        $rejudgingid = $DB->q(
-            'RETURNID INSERT INTO rejudging
+        $rejudgingid = $DB->q('RETURNID INSERT INTO rejudging
                                (userid_start, starttime, reason) VALUES (%i, %s, %s)',
-                              $userid,
-            now(),
-            $reason
-        );
+                              $userid, now(), $reason);
     }
 
     while ($jud = $res->next()) {
@@ -1068,19 +1019,13 @@ function rejudge($table, $id, $include_all, $full_rejudge, $reason = null, $user
         $DB->q('START TRANSACTION');
 
         if (!$full_rejudge) {
-            $DB->q(
-                'UPDATE judging SET valid = 0 WHERE judgingid = %i',
-                   $jud['judgingid']
-            );
+            $DB->q('UPDATE judging SET valid = 0 WHERE judgingid = %i', $jud['judgingid']);
         }
 
-        $DB->q(
-            'UPDATE submission SET judgehost = NULL' .
+        $DB->q('UPDATE submission SET judgehost = NULL' .
                ($full_rejudge ? ', rejudgingid=%i ' : '%_ ') .
                'WHERE submitid = %i AND rejudgingid IS NULL',
-               @$rejudgingid,
-            $jud['submitid']
-        );
+               @$rejudgingid, $jud['submitid']);
 
         // Prioritize single submission rejudgings
         if ($table == 'submission') {
@@ -1178,15 +1123,10 @@ function rejudging_finish($rejudgingid, $request, $userid = null, $show_progress
         }
     }
 
-    $DB->q(
-        'UPDATE rejudging
+    $DB->q('UPDATE rejudging
             SET endtime=%s, userid_finish=%i, valid=%i
             WHERE rejudgingid=%i',
-           now(),
-        $userid,
-        ($request=='apply' ? 1 : 0),
-        $rejudgingid
-    );
+           now(), $userid, ($request=='apply' ? 1 : 0), $rejudgingid);
 
     auditlog('rejudging', $rejudgingid, $request.'ing rejudge', '(end)');
 }
@@ -1261,18 +1201,10 @@ function auditlog(
         $user = $username;
     }
 
-    $DB->q(
-        'INSERT INTO auditlog
+    $DB->q('INSERT INTO auditlog
             (logtime, cid, user, datatype, dataid, action, extrainfo)
             VALUES(%s, %i, %s, %s, %s, %s, %s)',
-           now(),
-        $cid,
-        $user,
-        $datatype,
-        $dataid,
-        $action,
-        $extrainfo
-    );
+           now(), $cid, $user, $datatype, $dataid, $action, $extrainfo);
 }
 
 /* Mapping from REST API endpoints to relevant information:
@@ -1403,12 +1335,9 @@ function rest_extid($endpoint, $intid)
 
     $extkey = explode(',', $ep['extid'])[0];
 
-    $extid = $DB->q(
-        'MAYBEVALUE SELECT `' . $extkey . '`
+    $extid = $DB->q('MAYBEVALUE SELECT `' . $extkey . '`
                      FROM `' . $ep['tables'][0] . '`
-                     WHERE `' . $KEYS[$ep['tables'][0]][0] . '` = %s',
-                    $intid
-    );
+                     WHERE `' . $KEYS[$ep['tables'][0]][0] . '` = %s', $intid);
 
     return $extid;
 }
@@ -1449,14 +1378,11 @@ function rest_intid($endpoint, $extid, $cid = null)
         error("argument 'cid' missing to map to internal ID for $endpoint");
     }
 
-    $intid = $DB->q(
-        'MAYBEVALUE SELECT `' . $KEYS[$ep['tables'][0]][0] . '`
+    $intid = $DB->q('MAYBEVALUE SELECT `' . $KEYS[$ep['tables'][0]][0] . '`
                      FROM `' . $ep['tables'][0] . '`
                      WHERE `' . $extkey . '` = %s' .
                     (isset($cidkey) ? ' AND cid = %i' : ' %_'),
-                    $extid,
-        $cid
-    );
+                    $extid, $cid);
 
     return $intid;
 }
@@ -1657,20 +1583,12 @@ function eventlog($type, $dataids, $action, $cid = null, $json = null, $ids = nu
             } else {
                 $jsonElement = dj_json_encode($json);
             }
-            $eventid = $DB->q(
-                'RETURNID INSERT INTO event
+            $eventid = $DB->q('RETURNID INSERT INTO event
                               (eventtime, cid, endpointtype, endpointid,
                                datatype, dataid, action, content)
                                VALUES (%s, %i, %s, %s, %s, %s, %s, %s)',
-            $now,
-                $cid,
-                $type,
-                $ids[$idx],
-            $table,
-                $dataid,
-                $action,
-                $jsonElement
-            );
+                              $now, $cid, $type, $ids[$idx], $table,
+                              $dataid, $action, $jsonElement);
             $eventids[] = $eventid;
         }
     }

lib/www/auth.php
@@ -71,13 +71,9 @@ function logged_in()
         // Is this the first visit? Record that in the team table.
         if (empty($teamdata['teampage_first_visited'])) {
             $hostname = empty($ip) ? 'PHPUNIT' : gethostbyaddr($ip);
-            $DB->q(
-                'UPDATE team SET teampage_first_visited = %s, hostname = %s
+            $DB->q('UPDATE team SET teampage_first_visited = %s, hostname = %s
                     WHERE teamid = %i',
-                   now(),
-                $hostname,
-                $teamid
-            );
+                   now(), $hostname, $teamid);
         }
     }
 

commit 1afbe22a780ec9089b0d44bb55ecc1f6545eae3b

Fix syntax error introduced in 78923c51809df479fb64a3fa.

www/jury/submission.php
@@ -706,7 +706,7 @@ if (!empty($jud['result'])) {
         echo msgbox(
             'results differ',
             'This submission was judged as ' .
-            '<a href="' . EXT_CCS_URL . urlencode($submdata['externalid']) . '" target="extCCS">'
+            '<a href="' . EXT_CCS_URL . urlencode($submdata['externalid']) . '" target="extCCS">' .
             printresult($submdata['externalresult']) . '</a>' .
             ' by the external CCS, but as ' .
             printresult($jud['result']) . ' by DOMjudge.'