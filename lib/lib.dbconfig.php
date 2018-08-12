DOMjudge/domjudge

commit 6f97d135b985d5f8fd130d0073d763b5f8ae0415

Allow configuration variables to be publicly viewable or not.

lib/lib.dbconfig.php
@@ -15,7 +15,9 @@ function dbconfig_init()
 	global $LIBDBCONFIG, $DB;
 
 	$LIBDBCONFIG = array();
-	$res = $DB->q('SELECT * FROM configuration');
+	$res = $DB->q('SELECT * FROM configuration' .
+	              (IS_JURY || checkrole('jury') || checkrole('judgehost') ?
+	               '' : ' WHERE public = 1'));
 
 	while ( $row = $res->next() ) {
 		$key = $row['name'];
@@ -72,6 +74,8 @@ function dbconfig_init()
 function dbconfig_store()
 {
 	global $LIBDBCONFIG, $DB;
+
+	if ( !checkrole('admin') ) error("must have admin role to store configuration settings");
 
 	foreach ( $LIBDBCONFIG as $key => $row ) {
 

www/api/v4/index.php
@@ -759,8 +759,9 @@ function config($args)
 $doc = 'Get configuration variables.';
 $args = array('name' => 'Search only a single config variable.');
 $exArgs = array(array('name' => 'sourcesize_limit'));
-$roles = array('jury','judgehost');
-$api->provideFunction('GET', 'config', $doc, $args, $exArgs, $roles);
+// Role based (partial) access to configuration variables is handled
+// in lib/lib.dbconfig.php.
+$api->provideFunction('GET', 'config', $doc, $args, $exArgs);
 
 /**
  * Submissions information

sql/mysql_db_defaultdata.sql
@@ -10,49 +10,49 @@
 -- Dumping data for table `configuration`
 --
 
-INSERT INTO `configuration` (`name`, `value`, `type`, `description`) VALUES
-('clar_categories', '{"general":"General issue","tech":"Technical issue"}', 'array_keyval', 'List of additional clarification categories'),
-('script_timelimit', '30', 'int', 'Maximum seconds available for compile/compare scripts. This is a safeguard against malicious code and buggy scripts, so a reasonable but large amount should do.'),
-('script_memory_limit', '2097152', 'int', 'Maximum memory usage (in kB) by compile/compare scripts. This is a safeguard against malicious code and buggy script, so a reasonable but large amount should do.'),
-('script_filesize_limit', '540672', 'int', 'Maximum filesize (in kB) compile/compare scripts may write. Submission will fail with compiler-error when trying to write more, so this should be greater than any *intermediate or final* result written by compilers.'),
-('memory_limit', '524288', 'int', 'Maximum memory usage (in kB) by submissions. This includes the shell which starts the compiled solution and also any interpreter like the Java VM, which takes away approx. 300MB! Can be overridden per problem.'),
-('output_limit', '4096', 'int', 'Maximum output (in kB) submissions may generate. Any excessive output is truncated, so this should be greater than the maximum testdata output.'),
-('process_limit', '64', 'int', 'Maximum number of processes that the submission is allowed to start (including shell and possibly interpreters).'),
-('sourcesize_limit', '256', 'int', 'Maximum source code size (in kB) of a submission. This setting should be kept in sync with that in "etc/submit-config.h.in".'),
-('sourcefiles_limit', '100', 'int', 'Maximum number of source files in one submission. Set to one to disable multiple file submissions.'),
-('timelimit_overshoot', '"1s|10%"', 'string', 'Time that submissions are kept running beyond timelimt before being killed. Specify as "Xs" for X seconds, "Y%" as percentage, or a combination of both separated by one of "+|&" for the sum, maximum, or minimum of both.'),
-('output_storage_limit', '50000', 'int', 'Maximum size of error/system output stored in the database (in bytes); use "-1" to disable any limits.'),
-('output_display_limit', '2000', 'int', 'Maximum size of run/diff/error/system output shown in the jury interface (in bytes); use "-1" to disable any limits.'),
-('verification_required', '0', 'bool', 'Is verification of judgings by jury required before publication?'),
-('score_in_seconds', '0', 'bool', 'Should the scoreboard resolution be measured in seconds instead of minutes?'),
-('show_flags', '1', 'bool', 'Show country flags on the scoreboard?'),
-('show_affiliations', '1', 'bool', 'Show affiliation names on the scoreboard?'),
-('show_affiliation_logos', '0', 'bool', 'Show affiliation logos on the scoreboard?'),
-('show_pending', '0', 'bool', 'Show pending submissions on the scoreboard?'),
-('show_teams_submissions', '1', 'bool', 'Show problem columns with submission information on the public and team scoreboards?'),
-('show_compile', '2', 'int', 'Show compile output in team webinterface? Choices: 0 = never, 1 = only on compilation error(s), 2 = always.'),
-('show_sample_output', '0', 'bool', 'Should teams be able to view a diff of their and the reference output to sample testcases?'),
-('show_balloons_postfreeze', '0', 'bool', 'Give out balloon notifications after the scoreboard has been frozen?'),
-('penalty_time', '20', 'int', 'Penalty time in minutes per wrong submission (if finally solved).'),
-('compile_penalty', '1', 'bool', 'Should submissions with compiler-error incur penalty time (and show on the scoreboard)?'),
-('results_prio', '{"memory-limit":99,"output-limit":99,"run-error":99,"timelimit":99,"wrong-answer":30,"no-output":10,"correct":1}', 'array_keyval', 'Priorities of results for determining final result with multiple testcases. Higher priority is used first as final result. With equal priority, the first occurring result determines the final result.'),
-('results_remap', '{}', 'array_keyval', 'Remap testcase result, e.g. to disable a specific result type such as ''no-output''.'),
-('lazy_eval_results', '1', 'bool', 'Lazy evaluation of results? If enabled, stops judging as soon as a highest priority result is found, otherwise always all testcases will be judged.'),
-('enable_printing', '0', 'bool', 'Enable teams and jury to send source code to a printer via the DOMjudge web interface.'),
-('time_format', '"%H:%M"', 'string', 'The format used to print times. For formatting options see the PHP \'strftime\' function.'),
-('default_compare', '"compare"', 'string', 'The script used to compare outputs if no special compare script specified.'),
-('default_run', '"run"', 'string', 'The script used to run submissions if no special run script specified.'),
-('allow_registration', '0', 'bool', 'Allow users to register themselves with the system?'),
-('allow_openid_auth', '0', 'bool', 'Allow users to log in using OpenID'),
-('openid_autocreate_team', '1', 'bool', 'Create a team for each user that logs in with OpenID'),
-('openid_provider', '"https://accounts.google.com"', 'string', 'OpenID Provider URL'),
-('openid_clientid', '""', 'string', 'OpenID Connect client id'),
-('openid_clientsecret', '""', 'string', 'OpenID Connect client secret'),
-('judgehost_warning', '30', 'int', 'Time in seconds after a judgehost last checked in before showing its status as "warning".'),
-('judgehost_critical', '120', 'int', 'Time in seconds after a judgehost last checked in before showing its status as "critical".'),
-('thumbnail_size', '128', 'int', 'Maximum width/height of a thumbnail for uploaded testcase images.'),
-('diskspace_error', '1048576', 'int', 'Minimum free disk space (in kB) on judgehosts.'),
-('require_entry_point', '0', 'bool', 'Require entry point for submissions.');
+INSERT INTO `configuration` (`name`, `value`, `type`, `public`, `description`) VALUES
+('clar_categories', '{"general":"General issue","tech":"Technical issue"}', 'array_keyval', '1', 'List of additional clarification categories'),
+('script_timelimit', '30', 'int', '0', 'Maximum seconds available for compile/compare scripts. This is a safeguard against malicious code and buggy scripts, so a reasonable but large amount should do.'),
+('script_memory_limit', '2097152', 'int', '0', 'Maximum memory usage (in kB) by compile/compare scripts. This is a safeguard against malicious code and buggy script, so a reasonable but large amount should do.'),
+('script_filesize_limit', '540672', 'int', '0', 'Maximum filesize (in kB) compile/compare scripts may write. Submission will fail with compiler-error when trying to write more, so this should be greater than any *intermediate or final* result written by compilers.'),
+('memory_limit', '524288', 'int', '0', 'Maximum memory usage (in kB) by submissions. This includes the shell which starts the compiled solution and also any interpreter like the Java VM, which takes away approx. 300MB! Can be overridden per problem.'),
+('output_limit', '4096', 'int', '0', 'Maximum output (in kB) submissions may generate. Any excessive output is truncated, so this should be greater than the maximum testdata output.'),
+('process_limit', '64', 'int', '0', 'Maximum number of processes that the submission is allowed to start (including shell and possibly interpreters).'),
+('sourcesize_limit', '256', 'int', '1', 'Maximum source code size (in kB) of a submission. This setting should be kept in sync with that in "etc/submit-config.h.in".'),
+('sourcefiles_limit', '100', 'int', '1', 'Maximum number of source files in one submission. Set to one to disable multiple file submissions.'),
+('timelimit_overshoot', '"1s|10%"', '0', 'string', 'Time that submissions are kept running beyond timelimt before being killed. Specify as "Xs" for X seconds, "Y%" as percentage, or a combination of both separated by one of "+|&" for the sum, maximum, or minimum of both.'),
+('output_storage_limit', '50000', 'int', '0', 'Maximum size of error/system output stored in the database (in bytes); use "-1" to disable any limits.'),
+('output_display_limit', '2000', 'int', '0', 'Maximum size of run/diff/error/system output shown in the jury interface (in bytes); use "-1" to disable any limits.'),
+('verification_required', '0', 'bool', '0', 'Is verification of judgings by jury required before publication?'),
+('score_in_seconds', '0', 'bool', '1', 'Should the scoreboard resolution be measured in seconds instead of minutes?'),
+('show_flags', '1', 'bool', '1', 'Show country flags on the scoreboard?'),
+('show_affiliations', '1', 'bool', '1', 'Show affiliation names on the scoreboard?'),
+('show_affiliation_logos', '0', 'bool', '1', 'Show affiliation logos on the scoreboard?'),
+('show_pending', '0', 'bool', '1', 'Show pending submissions on the scoreboard?'),
+('show_teams_submissions', '1', 'bool', '1', 'Show problem columns with submission information on the public and team scoreboards?'),
+('show_compile', '2', 'int', '1', 'Show compile output in team webinterface? Choices: 0 = never, 1 = only on compilation error(s), 2 = always.'),
+('show_sample_output', '0', 'bool', '1', 'Should teams be able to view a diff of their and the reference output to sample testcases?'),
+('show_balloons_postfreeze', '0', 'bool', '1', 'Give out balloon notifications after the scoreboard has been frozen?'),
+('penalty_time', '20', 'int', '1', 'Penalty time in minutes per wrong submission (if finally solved).'),
+('compile_penalty', '1', 'bool', '1', 'Should submissions with compiler-error incur penalty time (and show on the scoreboard)?'),
+('results_prio', '{"memory-limit":99,"output-limit":99,"run-error":99,"timelimit":99,"wrong-answer":30,"no-output":10,"correct":1}', 'array_keyval', '0', 'Priorities of results for determining final result with multiple testcases. Higher priority is used first as final result. With equal priority, the first occurring result determines the final result.'),
+('results_remap', '{}', 'array_keyval', '0', 'Remap testcase result, e.g. to disable a specific result type such as ''no-output''.'),
+('lazy_eval_results', '1', 'bool', '0', 'Lazy evaluation of results? If enabled, stops judging as soon as a highest priority result is found, otherwise always all testcases will be judged.'),
+('enable_printing', '0', 'bool', '1', 'Enable teams and jury to send source code to a printer via the DOMjudge web interface.'),
+('time_format', '"%H:%M"', 'string', '0', 'The format used to print times. For formatting options see the PHP \'strftime\' function.'),
+('default_compare', '"compare"', 'string', '0', 'The script used to compare outputs if no special compare script specified.'),
+('default_run', '"run"', 'string', '0', 'The script used to run submissions if no special run script specified.'),
+('allow_registration', '0', 'bool', '1', 'Allow users to register themselves with the system?'),
+('allow_openid_auth', '0', 'bool', '1', 'Allow users to log in using OpenID'),
+('openid_autocreate_team', '1', 'bool', '1', 'Create a team for each user that logs in with OpenID'),
+('openid_provider', '"https://accounts.google.com"', 'string', '1', 'OpenID Provider URL'),
+('openid_clientid', '""', 'string',  '0','OpenID Connect client id'),
+('openid_clientsecret', '""', 'string', '0', 'OpenID Connect client secret'),
+('judgehost_warning', '30', 'int', '0', 'Time in seconds after a judgehost last checked in before showing its status as "warning".'),
+('judgehost_critical', '120', 'int', '0', 'Time in seconds after a judgehost last checked in before showing its status as "critical".'),
+('thumbnail_size', '128', 'int', '0', 'Maximum width/height of a thumbnail for uploaded testcase images.'),
+('diskspace_error', '1048576', 'int', '0', 'Minimum free disk space (in kB) on judgehosts.'),
+('require_entry_point', '0', 'bool', '1', 'Require entry point for submissions.');
 
 --
 -- Dumping data for table `executable`

sql/mysql_db_structure.sql
@@ -69,9 +69,11 @@ CREATE TABLE `configuration` (
   `name` varchar(32) NOT NULL COMMENT 'Name of the configuration variable',
   `value` longtext NOT NULL COMMENT 'Content of the configuration variable (JSON encoded)',
   `type` varchar(32) DEFAULT NULL COMMENT 'Type of the value (metatype for use in the webinterface)',
+  `public` tinyint(1) UNSIGNED DEFAULT '0' COMMENT 'Is this variable publicly visible?',
   `description` varchar(255) DEFAULT NULL COMMENT 'Description for in the webinterface',
   PRIMARY KEY (`configid`),
-  UNIQUE KEY `name` (`name`)
+  UNIQUE KEY `name` (`name`),
+  KEY `public` (`public`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Global configuration variables';
 
 --

sql/upgrade/upgrade_5.3.0_6.0.0DEV.sql
@@ -15,6 +15,10 @@ ALTER TABLE `judging_run` DROP COLUMN `endtime`;
 -- Create additional structures
 --
 
+ALTER TABLE `configuration`
+  ADD COLUMN `public` tinyint(1) UNSIGNED DEFAULT '0' COMMENT 'Is this variable publicly visible?' AFTER `type`,
+  ADD KEY `public` (`public`);
+
 ALTER TABLE `contestteam`
   DROP PRIMARY KEY,
   ADD PRIMARY KEY (`cid`,`teamid`);
@@ -142,6 +146,15 @@ UPDATE `language` SET `externalid` = 'prolog'     WHERE `langid` = 'plg';
 UPDATE `language` SET `externalid` = 'python2'    WHERE `langid` = 'py2';
 UPDATE `language` SET `externalid` = 'python3'    WHERE `langid` = 'py3';
 UPDATE `language` SET `externalid` = 'scala'      WHERE `langid` = 'scala';
+
+UPDATE `configuration` SET `public` = '1' WHERE `name` IN (
+  'clar_categories', 'sourcesize_limit', 'sourcefiles_limit',
+  'score_in_seconds', 'show_flags', 'show_affiliations',
+  'show_affiliation_logos', 'show_pending', 'show_teams_submissions',
+  'show_compile', 'show_sample_output', 'show_balloons_postfreeze',
+  'penalty_time', 'compile_penalty', 'enable_printing',
+  'allow_registration', 'allow_openid_auth', 'openid_autocreate_team',
+  'openid_provider', 'require_entry_point');
 
 --
 -- Finally remove obsolete structures after moving data

commit edbcf6621ee51ff94198a532a002bf11625916e3

Implement 'public'-ness of config options differently.
The original approach in 6f97d135b985d5f8fd130d0073d763b5f8ae0415
broke several things in the team and public web interfaces,
because that code does depend on non-public config variables.

lib/lib.dbconfig.php
@@ -15,9 +15,7 @@ function dbconfig_init()
     global $LIBDBCONFIG, $DB;
 
     $LIBDBCONFIG = array();
-    $res = $DB->q('SELECT * FROM configuration' .
-                  (IS_JURY || checkrole('jury') || checkrole('judgehost') ?
-                   '' : ' WHERE public = 1'));
+    $res = $DB->q('SELECT * FROM configuration');
 
     while ($row = $res->next()) {
         $key = $row['name'];
@@ -69,6 +67,7 @@ function dbconfig_init()
 
         $LIBDBCONFIG[$key] = array('value' => $val,
                                    'type' => $row['type'],
+                                   'public' => $row['public'],
                                    'desc' => $row['description']);
     }
 }
@@ -147,8 +146,11 @@ function dbconfig_store()
  * values can be used.
  *
  * When $name is null, then all variables will be returned.
+ *
+ * Set $onlyifpublic to true to only return a value when this is
+ * a variable marked 'public'.
  */
-function dbconfig_get($name, $default = null, $cacheok = true)
+function dbconfig_get($name, $default = null, $cacheok = true, $onlyifpublic = false)
 {
     global $LIBDBCONFIG;
 
@@ -159,12 +161,14 @@ function dbconfig_get($name, $default = null, $cacheok = true)
     if (is_null($name)) {
         $ret = array();
         foreach ($LIBDBCONFIG as $name => $config) {
-            $ret[$name] = $config['value'];
+            if ( !$onlyifpublic || $config['public'] ) {
+                $ret[$name] = $config['value'];
+            }
         }
         return $ret;
     }
 
-    if (isset($LIBDBCONFIG[$name])) {
+    if (isset($LIBDBCONFIG[$name]) && (!$onlyifpublic || $LIBDBCONFIG[$name]['public'])) {
         return $LIBDBCONFIG[$name]['value'];
     }
 

lib/www/common.php
@@ -749,13 +749,14 @@ function putProblemTextList()
     </h3>
     <h4 class="card-subtitle mb-2 text-muted">' . specialchars($row['name']) . '</h4>
 ';
-        if (dbconfig_get('show_limits_on_team_page', false)) {
+        if (dbconfig_get('show_limits_on_team_page')) {
             print '<h5 class="card-subtitle mb-2 text-muted">Limits: ';
             print $row['timelimit'] . ' second' . ($row['timelimit'] > 1 ? 's' : '');
             if ($timeFactorDiffers) {
                 print '<sup>*</sup>';
             }
             print ' / ';
+
             print printsize(($row['memlimit'] ?? dbconfig_get('memory_limit', 0)) * 1024, 1);
             print '</h5>';
         }

www/api/v4/index.php
@@ -868,17 +868,19 @@ if (!isset($api)) {
      */
     function config($args)
     {
+        $onlypublic = !(IS_JURY || checkrole('jury') || checkrole('judgehost'));
+
         if (isset($args['name'])) {
-            return array($args['name'] => dbconfig_get($args['name'], null, false));
+            return array($args['name'] => dbconfig_get($args['name'], null, false, $onlyifpublic));
         }
 
-        return dbconfig_get(null, null, false);
+        return dbconfig_get(null, null, false, $onlypublic);
     }
     $doc = 'Get configuration variables.';
     $args = array('name' => 'Search only a single config variable.');
     $exArgs = array(array('name' => 'sourcesize_limit'));
     // Role based (partial) access to configuration variables is handled
-    // in lib/lib.dbconfig.php.
+    // inside the function.
     $api->provideFunction('GET', 'config', $doc, $args, $exArgs);
 
     /**