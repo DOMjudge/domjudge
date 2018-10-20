9078eaf14375796a1b870a9f48c0f17e531404ae
Jaap Eldering <jaap@jaapeldering.nl>
2018-10-20 18:36:44 +0200

    Only allow filename characters in entry points.
    Teams at UKIEPC triggered a bug by sending various strings
    containing spaces. The POSIX shell code in compile.sh cannot
    handle that, and this is not easy to fix, so let's just
    disallow those. There's also no need since filenames and
    class names (typically) cannot contain spaces anyways.
    
    (cherry picked from commit 8e2d57fea948d491aa0d652eae47fbc005a2836c)

--- lib/lib.misc.php
+++ lib/lib.misc.php
@@ -835,6 +835,9 @@ function submit_solution(
     if ($langdata['require_entry_point'] && empty($entry_point)) {
         error("Entry point required for '$langid' but none given.");
     }
+    if (!empty($entry_point) && !preg_match(FILENAME_REGEX, $entry_point)) {
+        error("Entry point '$entry_point' contains illegal characters.");
+    }
     if (checkrole('jury') && $entry_point == '__auto__') {
         // Fall back to auto detection when we're importing jury submissions.
         $entry_point = NULL;
