50778bc21072f9976dc97d9c20d7a90ef44d0351
Keith Johnson <kj@ubergeek42.com>
2018-10-20 17:20:19 -0400

    Move jury/executables to symfony

---
+++ webapp/src/DOMJudgeBundle/Controller/Jury/ExecutableController.php
@@ -0,0 +1,118 @@
+<?php declare(strict_types=1);
+
+namespace DOMJudgeBundle\Controller\Jury;
+
+use Doctrine\ORM\EntityManagerInterface;
+use DOMJudgeBundle\Service\DOMJudgeService;
+use DOMJudgeBundle\Utils\Utils;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\Asset\Packages;
+
+class ExecutableController extends Controller
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    /**
+     * @var DOMJudgeService
+     */
+    protected $DOMJudgeService;
+
+    /**
+     * GeneralInfoController constructor.
+     * @param EntityManagerInterface $entityManager
+     * @param DOMJudgeService $DOMJudgeService
+     */
+    public function __construct(EntityManagerInterface $entityManager, DOMJudgeService $DOMJudgeService)
+    {
+        $this->entityManager   = $entityManager;
+        $this->DOMJudgeService = $DOMJudgeService;
+    }
+
+    /**
+     * @Route("/jury/executables/", name="jury_executables")
+     * @Security("has_role('ROLE_JURY')")
+     */
+    public function indexAction(Request $request, Packages $assetPackage)
+    {
+        // $res = $DB->q('SELECT execid, description, md5sum, type, OCTET_LENGTH(zipfile) AS size
+        //                FROM executable ORDER BY execid');
+        $em = $this->entityManager;
+        $executables = $em->createQueryBuilder()
+          ->select('e as executable, e.execid as execid, length(e.zipfile) as size')
+          ->from('DOMJudgeBundle:Executable', 'e')
+          ->orderBy('e.execid', 'ASC')
+          ->getQuery()->getResult();
+        $executable_sizes = array_column($executables, 'size', 'execid');
+        $executables = array_column($executables, 'executable', 'execid');
+        dump($executables);
+        $table_fields = [
+          'execid'      => [ 'title' => 'ID',          'sort' => true, ],
+          'type'        => [ 'title' => 'type',        'sort' => true, ],
+          'description' => [ 'title' => 'description', 'sort' => true, 'default_sort' => true ],
+          'size'        => [ 'title' => 'size',        'sort' => true, ],
+          'md5sum'      => [ 'title' => 'md5',         'sort' => true, 'search' => false],
+        ];
+        if ($this->isGranted('ROLE_ADMIN')) {
+          $table_fields = array_merge($table_fields, [
+            'save'        => [ 'title' => '',            'sort' => false,  ],
+            'edit'        => [ 'title' => '',            'sort' => false,  ],
+            'delete'      => [ 'title' => '',            'sort' => false, ],
+          ]);
+        }
+
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+        $executables_table = [];
+        foreach ($executables as $e) {
+          $execdata = [];
+          // Get whatever fields we can from the team object itself
+          foreach($table_fields as $k=>$v) {
+            if ($propertyAccessor->isReadable($e, $k)) {
+              $execdata[$k] = ['value' => $propertyAccessor->getValue($e, $k)];
+            }
+          }
+
+
+          $editvalue = '<img src="' . $assetPackage->getUrl('images/edit.png') . '" alt="edit" title="edit this executable" class="picto">';
+          $editlink = $this->generateUrl('legacy.jury_executable', [
+            'cmd'=>'edit',
+            'id'=>$e->getExecid(),
+            'referrer' => 'executables/'
+          ]);
+          $deletevalue = '<img src="' . $assetPackage->getUrl('images/delete.png') . '" alt="delete" title="delete this executable" class="picto">';
+          $deletelink = $this->generateUrl('legacy.jury_delete', [
+            'table'=>'executable',
+            'execid'=>$e->getExecid(),
+            'referrer' => '',
+            'desc' => $e->getDescription(),
+          ]);
+          $savevalue = '<img src="' . $assetPackage->getUrl('images/b_save.png') . '" alt="edit" title="edit this executable" class="picto">';
+          $savelink = $this->generateUrl('legacy.jury_executable', [
+            'fetch'=>'',
+            'id'=>$e->getExecid(),
+          ]);
+
+          $execdata['md5sum']['cssclass'] = 'text-monospace small';
+          $execdata = array_merge($execdata, [
+            'size' => ['value' => Utils::printsize((int)$executable_sizes[$e->getExecid()])],
+            'save' => ['value' => $savevalue, 'link' => $savelink],
+            'edit' => ['value' => $editvalue, 'link' => $editlink],
+            'delete' => ['value' => $deletevalue, 'link' => $deletelink],
+          ]);
+          $executables_table[] = [
+            'data' => $execdata,
+            'link' => $this->generateUrl('legacy.jury_executable', ['id' => $e->getExecid()]),
+          ];
+        }
+        return $this->render('@DOMJudge/jury/executables.html.twig', [
+          'executables' => $executables_table,
+          'table_fields' => $table_fields,
+        ]);
+    }
+}
