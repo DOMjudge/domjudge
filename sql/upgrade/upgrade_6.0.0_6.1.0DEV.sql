54071caefb9afd6baf266e018ec17f13c82da63f
Tobias Werth <werth@cs.fau.de>
2018-11-06 20:51:52 +0100

    Add config.category to SQL upgrade, should fix demoweb.

--- sql/upgrade/upgrade_6.0.0_6.1.0DEV.sql
+++ sql/upgrade/upgrade_6.0.0_6.1.0DEV.sql
@@ -11,6 +11,15 @@
 INSERT INTO `configuration` (`name`, `value`) VALUES ('data_source', '0');
 DELETE FROM `configuration` WHERE `name` = 'data_source';
 
-INSERT INTO `configuration` (`name`, `value`, `type`, `public`, `description`) VALUES
-('data_source', '0', 'int', '0', 'Source of data. Choices: 0 = all local, 1 = configuration data external, 2 = configuration and live data external'),
-('update_judging_seconds', '0', 'int', '0', 'Post updates to a judging every X seconds. Set to 0 to update after each judging_run.');
+--
+-- Create additional structures
+--
+ALTER TABLE `configuration`
+  ADD COLUMN `category` varchar(32) NOT NULL DEFAULT 'Uncategorized' COMMENT 'Option category of the configuration variable' AFTER `public`;
+
+--
+-- Add/remove sample/initial contents
+--
+INSERT INTO `configuration` (`name`, `value`, `type`, `public`, `category` `description`) VALUES
+('data_source', '0', 'int', '0', 'Misc', 'Source of data. Choices: 0 = all local, 1 = configuration data external, 2 = configuration and live data external'),
+('update_judging_seconds', '0', 'int', '0', 'Judging', 'Post updates to a judging every X seconds. Set to 0 to update after each judging_run.');
