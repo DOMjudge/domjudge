3a4cabb06359a22f46b1aab06aa1969946af97e1
Jaap Eldering <jaap@jaapeldering.nl>
2018-11-06 22:14:52 +0100

    Also set categories of old configuration variables.
    Also fix SQL syntax error and determine whether update is
    required more robustly from table structure than data.

--- sql/upgrade/upgrade_6.0.0_6.1.0DEV.sql
+++ sql/upgrade/upgrade_6.0.0_6.1.0DEV.sql
@@ -8,18 +8,56 @@
 --
 
 -- @UPGRADE-CHECK@
-INSERT INTO `configuration` (`name`, `value`) VALUES ('data_source', '0');
-DELETE FROM `configuration` WHERE `name` = 'data_source';
+ALTER TABLE `configuration` ADD  COLUMN `category` varchar(32);
+ALTER TABLE `configuration` DROP COLUMN `category`;
 
 --
 -- Create additional structures
 --
+
 ALTER TABLE `configuration`
   ADD COLUMN `category` varchar(32) NOT NULL DEFAULT 'Uncategorized' COMMENT 'Option category of the configuration variable' AFTER `public`;
+
+
+--
+-- Transfer data from old to new structure
+--
+
+UPDATE `configuration` SET `category` = 'Scoring' WHERE `name` IN (
+  'verification_required', 'compile_penalty', 'penalty_time',
+  'results_prio', 'results_remap', 'score_in_seconds'
+);
+UPDATE `configuration` SET `category` = 'Judging' WHERE `name` IN (
+  'memory_limit', 'output_limit', 'process_limit', 'sourcesize_limit',
+  'sourcefiles_limit', 'script_timelimit', 'script_memory_limit',
+  'script_filesize_limit', 'timelimit_overshoot', 'output_storage_limit',
+  'output_display_limit', 'lazy_eval_results', 'judgehost_warning',
+  'judgehost_critical', 'diskspace_error', 'default_compare', 'default_run'
+);
+UPDATE `configuration` SET `category` = 'Clarification' WHERE `name` IN (
+  'clar_categories', 'clar_answers', 'clar_queues', 'clar_default_problem_queue'
+);
+UPDATE `configuration` SET `category` = 'Display' WHERE `name` IN (
+  'show_pending', 'show_flags', 'show_affiliations', 'show_affiliation_logos',
+  'show_teams_submissions', 'show_compile', 'show_sample_output',
+  'show_balloons_postfreeze', 'show_relative_time', 'time_format',
+  'thumbnail_size', 'show_limits_on_team_page'
+);
+UPDATE `configuration` SET `category` = 'Misc' WHERE `name` IN (
+  'enable_printing', 'allow_registration', 'allow_openid_auth',
+  'openid_autocreate_team', 'openid_provider', 'openid_clientid',
+  'openid_clientsecret'
+);
 
 --
 -- Add/remove sample/initial contents
 --
-INSERT INTO `configuration` (`name`, `value`, `type`, `public`, `category` `description`) VALUES
+
+INSERT INTO `configuration` (`name`, `value`, `type`, `public`, `category`, `description`) VALUES
 ('data_source', '0', 'int', '0', 'Misc', 'Source of data. Choices: 0 = all local, 1 = configuration data external, 2 = configuration and live data external'),
 ('update_judging_seconds', '0', 'int', '0', 'Judging', 'Post updates to a judging every X seconds. Set to 0 to update after each judging_run.');
+
+--
+-- Finally remove obsolete structures after moving data
+--
+
