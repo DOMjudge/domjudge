ed592c3e1d25fe16d4c8289a7cd07a735dc35195
Nicky Gerritsen <nickygerritsen@me.com>
2018-11-28 21:33:52 +0100

    Disable profiler in debugmode for judgehosts by default, but enable it when DEBUG_JUDGE is set

---
+++ webapp/src/DOMJudgeBundle/EventListener/ProfilerDisableListener.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace DOMJudgeBundle\EventListener;
+
+use DOMJudgeBundle\Service\DOMJudgeService;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
+
+/**
+ * Class ProfilerDisableListener
+ * @package DOMJudgeBundle\EventListener
+ */
+class ProfilerDisableListener
+{
+    /**
+     * @var KernelInterface
+     */
+    protected $kernel;
+
+    /**
+     * @var DOMJudgeService
+     */
+    protected $DOMJudgeService;
+
+    /**
+     * @var Profiler
+     */
+    protected $profiler;
+
+    /**
+     * ProfilerDisableListener constructor.
+     * @param KernelInterface $kernel
+     * @param DOMJudgeService $DOMJudgeService
+     * @param Profiler        $profiler
+     */
+    public function __construct(KernelInterface $kernel, DOMJudgeService $DOMJudgeService, Profiler $profiler)
+    {
+        $this->DOMJudgeService = $DOMJudgeService;
+        $this->profiler        = $profiler;
+        $this->kernel          = $kernel;
+    }
+
+    public function onKernelRequest()
+    {
+        // Disable the profiler for users with the judgehost permission but not the admin one, unless DEBUG contains DEBUG_JUDGE
+        if ($this->DOMJudgeService->checkrole('judgehost') && !$this->DOMJudgeService->checkrole('admin')) {
+            // TODO: use domserver-static.php defines here
+            $dir = realpath(sprintf('%s/../../etc', $this->kernel->getRootDir()));
+            require_once $dir . '/common-config.php';
+
+            if (!(DEBUG & DEBUG_JUDGE)) {
+                $this->profiler->disable();
+            }
+        }
+    }
+}
