06b36080070114cc5a1ad1a4a8612080edd646f9
Jaap Eldering <jaap@jaapeldering.nl>
2018-12-06 23:57:51 +0100

    Use splice with SPLICE_F_NONBLOCK to prevent hanging pipes.
    This makes splice() reproduce the same behavior as read() when the
    pipe was closed by the child exiting, even if there are grandchildren
    still running.

--- judge/runguard.c
+++ judge/runguard.c
@@ -883,7 +883,7 @@ void pump_pipes(fd_set readfds, size_t data_read[], size_t data_passed[])
 				if ( use_splice ) {
 					nread = splice(child_pipefd[i][PIPE_OUT], NULL,
 					               child_redirfd[i], NULL,
-					               to_read, SPLICE_F_MOVE);
+					               to_read, SPLICE_F_MOVE | SPLICE_F_NONBLOCK);
 
 					if ( nread==-1 && errno==EINVAL ) {
 						use_splice = 0;
