a1625bf4b2b3b97093c5d4cd667b355d4126c7ee
Jaap Eldering <jaap@jaapeldering.nl>
2019-02-05 23:53:08 +0100

    Reapply 4df71c8069cf0511 with fix for writing to closed pipe.

--- judge/runpipe.c
+++ judge/runpipe.c
@@ -20,6 +20,7 @@
 
 #include "config.h"
 
+#include <sys/select.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
@@ -37,6 +38,9 @@
 
 #include "lib.error.h"
 #include "lib.misc.h"
+
+/* Use the POSIX minimum for PIPE_BUF. */
+#define BUF_SIZE 512
 
 extern int errno;
 
@@ -56,6 +60,10 @@ pid_t  cmd_pid[MAX_CMDS];
 int    cmd_fds[MAX_CMDS][3];
 int    cmd_exit[MAX_CMDS];
 
+int write_progout;
+char *progoutfilename;
+FILE *progoutfile;
+
 int outputmeta;
 char *metafilename;
 FILE *metafile;
@@ -64,6 +72,7 @@ struct option const long_opts[] = {
 	{"verbose", no_argument,       NULL,         'v'},
 	{"help",    no_argument,       &show_help,    1 },
 	{"version", no_argument,       &show_version, 1 },
+	{"outprog", required_argument, NULL,         'o'},
 	{"outmeta", required_argument, NULL,         'M'},
 	{ NULL,     0,                 NULL,          0 }
 };
@@ -74,6 +83,7 @@ void usage()
 Usage: %s [OPTION]... COMMAND1 [ARGS...] = COMMAND2 [ARGS...]\n\
 Run two commands with stdin/stdout bi-directionally connected.\n\
 \n\
+  -o, --outprog=FILE   write stdout from second program to FILE\n\
   -M, --outmeta=FILE   write metadata (runtime, exitcode, etc.) of first program to FILE\n\
   -v, --verbose        display some extra warnings and information\n\
       --help           display this help and exit\n\
@@ -156,6 +166,73 @@ void set_fd_close_exec(int fd, int value)
 	}
 }
 
+void pump_pipes(int *fd_out, int *fd_in)
+{
+	ssize_t nread, to_write, nwritten;
+	fd_set readfds;
+	char buf[BUF_SIZE+1];
+	int r;
+
+	if ( *fd_out<0 ) return;
+
+	FD_ZERO(&readfds);
+	FD_SET(*fd_out, &readfds);
+
+	r = select(*fd_out+1, &readfds, NULL, NULL, NULL);
+	if ( r==-1 && errno!=EINTR ) error(errno,"waiting for child data");
+
+	if ( FD_ISSET(*fd_out, &readfds) ) {
+		nread = read(*fd_out, buf, BUF_SIZE);
+		verb("read %d bytes from program #2, fd %d", (int)nread, *fd_out);
+		if ( nread>0 ) {
+			buf[nread] = 0;
+			/* First write to file. */
+			errno = 0;
+			nwritten = fwrite(buf, 1, nread, progoutfile);
+			if ( nwritten<nread ) {
+				error(errno,"writing to `%s'", progoutfilename);
+			}
+			if ( fflush(progoutfile)!=0 ) {
+				error(errno,"flushing `%s'", progoutfilename);
+			}
+			/* Then write to the pipe connecting to program #1. */
+			to_write = nread;
+			while ( to_write>0 && *fd_in>=0 ) {
+				nwritten = write(*fd_in, buf+(nread-to_write), to_write);
+				if ( nwritten==-1 ) {
+					if ( errno == EPIPE ) {
+						/* The receiving process already closed the pipe. */
+						if ( close(*fd_in) !=0 ) {
+							error(errno,"closing pipe for fd %d", *fd_in);
+						}
+						*fd_in = -1;
+						break;
+					} else {
+						error(errno,"writing to fd %d", *fd_in);
+					}
+				}
+				verb("wrote %d bytes to fd %d", (int)nwritten, *fd_in);
+				to_write -= nwritten;
+			}
+		}
+		if ( nread==-1 ) {
+			if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK) return;
+			error(errno,"copying data from fd %d", *fd_out);
+		}
+		if ( nread==0 ) {
+			/* EOF detected: close input/output pipe fds. */
+			if ( *fd_out>=0 && close(*fd_out)!=0 ) {
+				error(errno,"closing pipe for fd %d", *fd_out);
+			}
+			if ( *fd_in >=0 && close(*fd_in) !=0 ) {
+				error(errno,"closing pipe for fd %d", *fd_in);
+			}
+			*fd_out = -1;
+			*fd_in  = -1;
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	struct sigaction sigact;
@@ -165,24 +242,32 @@ int main(int argc, char **argv)
 	int   exitcode, myexitcode;
 	int   opt;
 	char *arg;
-	int   i, newcmd, argsize = 0;
+	int   i, r, fd_out, newcmd, argsize = 0;
 	int   pipe_fd[MAX_CMDS][2];
+	int   progout_pipe_fd[2];
 
 	progname = argv[0];
 
 	/* Parse command-line options */
 	be_verbose = show_help = show_version = 0;
 	opterr = 0;
-	while ( (opt = getopt_long(argc,argv,"+M:v",long_opts,(int *) 0))!=-1 ) {
+	while ( (opt = getopt_long(argc,argv,"+o:M:v",long_opts,(int *) 0))!=-1 ) {
 		switch ( opt ) {
 		case 0:   /* long-only option */
 			break;
 		case 'v': /* verbose option */
 			be_verbose = 1;
+			verb("verbose mode enabled");
 			break;
-		case 'M': /* outputmeta option */
+		case 'o': /* outprog option */
+			write_progout = 1;
+			progoutfilename = strdup(optarg);
+			verb("writing program #2 output to '%s'", progoutfilename);
+			break;
+		case 'M': /* outmeta option */
 			outputmeta = 1;
 			metafilename = strdup(optarg);
+			verb("writing metadata to '%s'", metafilename);
 			break;
 		case ':': /* getopt error */
 		case '?':
@@ -262,25 +347,61 @@ int main(int argc, char **argv)
 		set_fd_close_exec(pipe_fd[i][1], 1);
 	}
 
+	/* Setup file and extra pipe for writing program output. */
+	if ( write_progout ) {
+		if ( (progoutfile = fopen(progoutfilename,"w"))==NULL ) {
+			error(errno,"cannot open `%s'",progoutfilename);
+		}
+		if ( pipe(progout_pipe_fd)!=0 ) error(errno,"creating pipes");
+		set_fd_close_exec(progout_pipe_fd[0], 1);
+		set_fd_close_exec(progout_pipe_fd[1], 1);
+		verb("writing program #2 output via pipe %d -> %d",
+		     progout_pipe_fd[1], progout_pipe_fd[0]);
+	}
+
 	/* Execute commands as subprocesses and connect pipes as required. */
 	for(i=0; i<ncmds; i++) {
+		fd_out = ( i==1 && write_progout ? progout_pipe_fd[1] : pipe_fd[1-i][1] );
 		cmd_fds[i][0] = pipe_fd[i][0];
-		cmd_fds[i][1] = pipe_fd[1-i][1];
+		cmd_fds[i][1] = fd_out;
 		cmd_fds[i][2] = FDREDIR_NONE;
+
 		set_fd_close_exec(pipe_fd[i][0], 0);
-		set_fd_close_exec(pipe_fd[1-i][1], 1);
+		set_fd_close_exec(fd_out, 0);
 
 		cmd_exit[i] = -1;
 		cmd_pid[i] = execute(cmd_name[i], (const char **)cmd_args[i],
 		                     cmd_nargs[i], cmd_fds[i], 0);
 		if ( cmd_pid[i]==-1 ) error(errno,"failed to execute command #%d",i+1);
 		verb("started #%d, pid %d: %s",i+1,cmd_pid[i],cmd_name[i]);
+
+		set_fd_close_exec(pipe_fd[i][0], 1);
+		set_fd_close_exec(fd_out, 1);
+	}
+
+	if ( write_progout ) {
+		if ( close(pipe_fd[1][0])!=0 ) error(errno,"closing pipe read end");
+		if ( close(pipe_fd[1][1])!=0 ) error(errno,"closing pipe write end");
+		if ( close(pipe_fd[0][0])!=0 ) error(errno,"closing pipe read end");
+		if ( close(progout_pipe_fd[1])!=0 ) error(errno,"closing pipe write end");
+	} else {
+		for(i=0; i<ncmds; i++) {
+			if ( close(pipe_fd[i][0])!=0 ) error(errno,"closing pipe read end");
+			if ( close(pipe_fd[i][1])!=0 ) error(errno,"closing pipe write end");
+		}
 	}
 
 	/* Wait for running child commands and check exit status. */
-	do {
-		pid = waitpid(-1, &status, WNOHANG);
-		if ( pid==0 ) pid = waitpid(-1, &status, 0);
+	while ( 1 ) {
+
+		if ( write_progout ) {
+			pump_pipes(&progout_pipe_fd[0], &pipe_fd[0][1]);
+
+			pid = waitpid(-1, &status, WNOHANG);
+			if ( pid==0 ) continue;
+		} else {
+			pid = waitpid(-1, &status, 0);
+		}
 
 		if ( pid<0 ) {
 			/* No more child processes, we're done. */
@@ -310,11 +431,20 @@ int main(int argc, char **argv)
 
 		cmd_exit[i] = status;
 		verb("command #%d, pid %d has exited (with status %d)",i+1,pid,status);
+	};
 
-		if ( close(cmd_fds[i][0])!=0 || close(cmd_fds[i][1])!=0 ) {
-			error(errno,"closing command #%d FD's",i+1);
-		}
-	} while ( 1 );
+	/* Reset pipe filedescriptors to use blocking I/O. */
+	if ( write_progout && progout_pipe_fd[0]>=0 ) {
+		r = fcntl(progout_pipe_fd[0], F_GETFL);
+		if (r == -1) error(errno, "fcntl, getting flags");
+
+		r = fcntl(progout_pipe_fd[0], F_SETFL, r ^ O_NONBLOCK);
+		if (r == -1) error(errno, "fcntl, setting flags");
+
+		do {
+			pump_pipes(&progout_pipe_fd[0], &pipe_fd[0][1]);
+		} while ( progout_pipe_fd[0]>=0 );
+	}
 
 	/* Check exit status of commands and report back the exit code of the first. */
 	myexitcode = exitcode = 0;
