a9c31710a88be7c5c6156f4e7ffb9d55072b204d
Jaap Eldering <jaap@jaapeldering.nl>
2019-02-26 00:31:19 +0100

    Revert the 2 previous commits due to bugs to be resolved.
    These are commits 9c5d0a830dd2146d15739f8279e9183dff25c838
    and 4df71c8069cf05110f6e51c0b917f66aeb559e4c.

--- judge/runpipe.c
+++ judge/runpipe.c
@@ -20,13 +20,6 @@
 
 #include "config.h"
 
-/* For Linux specific fcntl F_SETPIPE_SZ command. */
-#if __gnu_linux__
-#define _GNU_SOURCE
-#define PROC_MAX_PIPE_SIZE "/proc/sys/fs/pipe-max-size"
-#endif
-
-#include <sys/select.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
@@ -44,9 +37,6 @@
 
 #include "lib.error.h"
 #include "lib.misc.h"
-
-/* Use the POSIX minimum for PIPE_BUF. */
-#define BUF_SIZE 512
 
 extern int errno;
 
@@ -66,10 +56,6 @@ pid_t  cmd_pid[MAX_CMDS];
 int    cmd_fds[MAX_CMDS][3];
 int    cmd_exit[MAX_CMDS];
 
-int write_progout;
-char *progoutfilename;
-FILE *progoutfile;
-
 int outputmeta;
 char *metafilename;
 FILE *metafile;
@@ -78,7 +64,6 @@ struct option const long_opts[] = {
 	{"verbose", no_argument,       NULL,         'v'},
 	{"help",    no_argument,       &show_help,    1 },
 	{"version", no_argument,       &show_version, 1 },
-	{"outprog", required_argument, NULL,         'o'},
 	{"outmeta", required_argument, NULL,         'M'},
 	{ NULL,     0,                 NULL,          0 }
 };
@@ -89,7 +74,6 @@ void usage()
 Usage: %s [OPTION]... COMMAND1 [ARGS...] = COMMAND2 [ARGS...]\n\
 Run two commands with stdin/stdout bi-directionally connected.\n\
 \n\
-  -o, --outprog=FILE   write stdout from second program to FILE\n\
   -M, --outmeta=FILE   write metadata (runtime, exitcode, etc.) of first program to FILE\n\
   -v, --verbose        display some extra warnings and information\n\
       --help           display this help and exit\n\
@@ -172,97 +156,6 @@ void set_fd_close_exec(int fd, int value)
 	}
 }
 
-/* Try to resize pipes to their maximum size on Linux.
-   We do this to make it as unlikely as possible for either the jury
-   or team program to get blocked writing to the other side, if that
-   side doesn't consume data from the pipe. See also:
-   https://github.com/Kattis/problemtools/issues/113
-
-   max_pipe_size == -1 means uninitialized, and -2 means that we
-   couldn't read from the proc file.
- */
-#ifdef PROC_MAX_PIPE_SIZE
-int max_pipe_size = -1;
-
-void resize_pipe(int fd)
-{
-	FILE *f;
-	int r;
-
-	if ( max_pipe_size<=-2 ) return;
-	if ( max_pipe_size==-1 ) {
-		if ( (f = fopen(PROC_MAX_PIPE_SIZE, "r"))==NULL ) {
-			max_pipe_size = -2;
-			warning(errno, "could not open '%s'", PROC_MAX_PIPE_SIZE);
-			return;
-		}
-		if ( fscanf(f, "%d", &max_pipe_size)!=1 ) {
-			max_pipe_size = -2;
-			warning(errno, "could not read from '%s'", PROC_MAX_PIPE_SIZE);
-			return;
-		}
-	}
-
-	r = fcntl(fd, F_SETPIPE_SZ, max_pipe_size);
-	if ( r==-1 ) {
-		warning(errno, "could not change pipe size");
-	}
-	verb("set pipe fd %d to size %d", fd, r);
-}
-#endif
-
-void pump_pipes(int *fd_out, int *fd_in)
-{
-	ssize_t nread, to_write, nwritten;
-	fd_set readfds;
-	char buf[BUF_SIZE+1];
-	int r;
-
-	if ( *fd_out<0 ) return;
-
-	FD_ZERO(&readfds);
-	FD_SET(*fd_out, &readfds);
-
-	r = select(*fd_out+1, &readfds, NULL, NULL, NULL);
-	if ( r==-1 && errno!=EINTR ) error(errno,"waiting for child data");
-
-	if ( FD_ISSET(*fd_out, &readfds) ) {
-		nread = read(*fd_out, buf, BUF_SIZE);
-		buf[nread] = 0;
-		if ( nread>0 ) {
-			/* First write to file. */
-			errno = 0;
-			nwritten = fwrite(buf, 1, nread, progoutfile);
-			if ( nwritten<nread ) {
-				error(errno,"writing to `%s'", progoutfilename);
-			}
-			if ( fflush(progoutfile)!=0 ) {
-				error(errno,"flushing `%s'", progoutfilename);
-			}
-			/* Then write to the pipe connecting to program #1. */
-			to_write = nread;
-			while ( to_write>0 ) {
-				nwritten = write(*fd_in, buf+(nread-to_write), to_write);
-				if ( nwritten==-1 ) {
-					nread = -1;
-					break;
-				}
-				to_write -= nwritten;
-			}
-		}
-		if ( nread==-1 ) {
-			if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK) return;
-			error(errno,"copying data from fd %d", *fd_out);
-		}
-		if ( nread==0 ) {
-			/* EOF detected: close input/output pipe fds. */
-			if ( close(*fd_out)!=0 ) error(errno,"closing pipe for fd %d", *fd_out);
-			if ( close(*fd_in) !=0 ) error(errno,"closing pipe for fd %d", *fd_in);
-			*fd_out = -1;
-		}
-	}
-}
-
 int main(int argc, char **argv)
 {
 	struct sigaction sigact;
@@ -272,32 +165,24 @@ int main(int argc, char **argv)
 	int   exitcode, myexitcode;
 	int   opt;
 	char *arg;
-	int   i, r, fd_out, newcmd, argsize = 0;
+	int   i, newcmd, argsize = 0;
 	int   pipe_fd[MAX_CMDS][2];
-	int   progout_pipe_fd[2];
 
 	progname = argv[0];
 
 	/* Parse command-line options */
 	be_verbose = show_help = show_version = 0;
 	opterr = 0;
-	while ( (opt = getopt_long(argc,argv,"+o:M:v",long_opts,(int *) 0))!=-1 ) {
+	while ( (opt = getopt_long(argc,argv,"+M:v",long_opts,(int *) 0))!=-1 ) {
 		switch ( opt ) {
 		case 0:   /* long-only option */
 			break;
 		case 'v': /* verbose option */
 			be_verbose = 1;
-			verb("verbose mode enabled");
 			break;
-		case 'o': /* outprog option */
-			write_progout = 1;
-			progoutfilename = strdup(optarg);
-			verb("writing program #2 output to '%s'", progoutfilename);
-			break;
-		case 'M': /* outmeta option */
+		case 'M': /* outputmeta option */
 			outputmeta = 1;
 			metafilename = strdup(optarg);
-			verb("writing metadata to '%s'", metafilename);
 			break;
 		case ':': /* getopt error */
 		case '?':
@@ -375,69 +260,27 @@ int main(int argc, char **argv)
 		if ( pipe(pipe_fd[i])!=0 ) error(errno,"creating pipes");
 		set_fd_close_exec(pipe_fd[i][0], 1);
 		set_fd_close_exec(pipe_fd[i][1], 1);
-#ifdef PROC_MAX_PIPE_SIZE
-		resize_pipe(pipe_fd[i][1]);
-#endif
-	}
-
-	/* Setup file and extra pipe for writing program output. */
-	if ( write_progout ) {
-		if ( (progoutfile = fopen(progoutfilename,"w"))==NULL ) {
-			error(errno,"cannot open `%s'",progoutfilename);
-		}
-		if ( pipe(progout_pipe_fd)!=0 ) error(errno,"creating pipes");
-		set_fd_close_exec(progout_pipe_fd[0], 1);
-		set_fd_close_exec(progout_pipe_fd[1], 1);
-#ifdef PROC_MAX_PIPE_SIZE
-		resize_pipe(progout_pipe_fd[1]);
-#endif
-		verb("writing program #2 output via pipe %d -> %d",
-		     progout_pipe_fd[1], progout_pipe_fd[0]);
 	}
 
 	/* Execute commands as subprocesses and connect pipes as required. */
 	for(i=0; i<ncmds; i++) {
-		fd_out = ( i==1 && write_progout ? progout_pipe_fd[1] : pipe_fd[1-i][1] );
 		cmd_fds[i][0] = pipe_fd[i][0];
-		cmd_fds[i][1] = fd_out;
+		cmd_fds[i][1] = pipe_fd[1-i][1];
 		cmd_fds[i][2] = FDREDIR_NONE;
-
 		set_fd_close_exec(pipe_fd[i][0], 0);
-		set_fd_close_exec(fd_out, 0);
+		set_fd_close_exec(pipe_fd[1-i][1], 1);
 
 		cmd_exit[i] = -1;
 		cmd_pid[i] = execute(cmd_name[i], (const char **)cmd_args[i],
 		                     cmd_nargs[i], cmd_fds[i], 0);
 		if ( cmd_pid[i]==-1 ) error(errno,"failed to execute command #%d",i+1);
 		verb("started #%d, pid %d: %s",i+1,cmd_pid[i],cmd_name[i]);
-
-		set_fd_close_exec(pipe_fd[i][0], 1);
-		set_fd_close_exec(fd_out, 1);
-	}
-
-	if ( write_progout ) {
-		if ( close(pipe_fd[1][0])!=0 ) error(errno,"closing pipe read end");
-		if ( close(pipe_fd[1][1])!=0 ) error(errno,"closing pipe write end");
-		if ( close(pipe_fd[0][0])!=0 ) error(errno,"closing pipe read end");
-		if ( close(progout_pipe_fd[1])!=0 ) error(errno,"closing pipe write end");
-	} else {
-		for(i=0; i<ncmds; i++) {
-			if ( close(pipe_fd[i][0])!=0 ) error(errno,"closing pipe read end");
-			if ( close(pipe_fd[i][1])!=0 ) error(errno,"closing pipe write end");
-		}
 	}
 
 	/* Wait for running child commands and check exit status. */
-	while ( 1 ) {
-
-		if ( write_progout ) {
-			pump_pipes(&progout_pipe_fd[0], &pipe_fd[0][1]);
-
-			pid = waitpid(-1, &status, WNOHANG);
-			if ( pid==0 ) continue;
-		} else {
-			pid = waitpid(-1, &status, 0);
-		}
+	do {
+		pid = waitpid(-1, &status, WNOHANG);
+		if ( pid==0 ) pid = waitpid(-1, &status, 0);
 
 		if ( pid<0 ) {
 			/* No more child processes, we're done. */
@@ -467,20 +310,11 @@ int main(int argc, char **argv)
 
 		cmd_exit[i] = status;
 		verb("command #%d, pid %d has exited (with status %d)",i+1,pid,status);
-	};
 
-	/* Reset pipe filedescriptors to use blocking I/O. */
-	if ( write_progout && progout_pipe_fd[0]>=0 ) {
-		r = fcntl(progout_pipe_fd[0], F_GETFL);
-		if (r == -1) error(errno, "fcntl, getting flags");
-
-		r = fcntl(progout_pipe_fd[0], F_SETFL, r ^ O_NONBLOCK);
-		if (r == -1) error(errno, "fcntl, setting flags");
-
-		do {
-			pump_pipes(&progout_pipe_fd[0], &pipe_fd[0][1]);
-		} while ( progout_pipe_fd[0]>=0 );
-	}
+		if ( close(cmd_fds[i][0])!=0 || close(cmd_fds[i][1])!=0 ) {
+			error(errno,"closing command #%d FD's",i+1);
+		}
+	} while ( 1 );
 
 	/* Check exit status of commands and report back the exit code of the first. */
 	myexitcode = exitcode = 0;
