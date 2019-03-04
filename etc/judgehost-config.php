1ad96075b578dc28d8b7117af4570989f9879bb5
Nicky Gerritsen <nickygerritsen@me.com>
2019-03-04 20:10:34 +0100

    Add support for creating a writable temp directory for submissions

--- etc/judgehost-config.php
+++ etc/judgehost-config.php
@@ -9,3 +9,11 @@ require_once("common-config.php");
 // except when needed for testing or debugging. No security
 // guarantees can be given when it is disabled.
 define('USE_CHROOT', true);
+
+// Normally submissions don't have any directory they can write to,
+// at least when the chroot is enabled. However, some languages might
+// require a temporary (writable) directory to put stuff in (for example
+// R). Setting this to true will create a directory "write_tmp" where
+// the submission can write to. Also the environment variable TMPDIR will
+// be set to this directory
+define('CREATE_WRITABLE_TEMP_DIR', false);
