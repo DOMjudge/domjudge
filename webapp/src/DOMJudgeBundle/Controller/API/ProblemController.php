98b1a8c487d63c04feb56952b28eead84d49ecb6
Tobias Werth <werth@cs.fau.de>
2019-04-01 11:17:37 +0100

    Make it possible to update an existing problem with a problem POST.
    Example command line:
    http -f POST https://localhost/api/contests/test1/problems problem=amazing zip[]@amazing.zip
    
    (cherry picked from commit a8ba7a6f013dc1aaae3f5a471d0126dbad822b89)

--- webapp/src/DOMJudgeBundle/Controller/API/ProblemController.php
+++ webapp/src/DOMJudgeBundle/Controller/API/ProblemController.php
@@ -128,6 +128,12 @@ class ProblemController extends AbstractRestController implements QueryObjectTra
      *     required=true,
      *     description="The problem archives to import"
      * )
+     * @SWG\Parameter(
+     *     name="problem",
+     *     in="formData",
+     *     type="string",
+     *     description="Optional: problem id to update."
+     * )
      * @SWG\Response(
      *     response="200",
      *     description="Returns the IDs of the just imported problems",
@@ -146,6 +152,24 @@ class ProblemController extends AbstractRestController implements QueryObjectTra
         $contest     = $this->entityManager->getRepository(Contest::class)->find($contestId);
         $allMessages = [];
         $probIds     = [];
+
+        $probId = $request->request->get('problem');
+        $problem = null;
+        if (!empty($probId)) {
+            if (sizeof($files) != 1) {
+                throw new BadRequestHttpException('Can only take one problem zip if \'problem\' is set.');
+            }
+            $problem = $this->entityManager->createQueryBuilder()
+                ->from('DOMJudgeBundle:Problem', 'p')
+                ->select('p')
+                ->andWhere(sprintf('%s = :id', $this->getIdField()))
+                ->setParameter(':id', $probId)
+                ->getQuery()
+                ->getOneOrNullResult();
+            if (empty($problem)) {
+                throw new BadRequestHttpException('Specified \'problem\' does not exist.');
+            }
+        }
         /** @var UploadedFile $file */
         foreach ($files as $file) {
             $zip = null;
