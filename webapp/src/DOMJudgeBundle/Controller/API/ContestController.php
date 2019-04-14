e4c88f7c265e24a37e1c21749e25636e1027d5ab
Nicky Gerritsen <nickygerritsen@me.com>
2019-04-14 13:19:30 +0200

    Emit contest update event when changing start time using the API.
    Before this happened in two places:
    * When editing the contest in the UI
    * In the eventdaemon, if not done so already in the UI
    Because we want to get rid of the eventdaemon, we move that one to the API.

--- webapp/src/DOMJudgeBundle/Controller/API/ContestController.php
+++ webapp/src/DOMJudgeBundle/Controller/API/ContestController.php
@@ -5,6 +5,7 @@ namespace DOMJudgeBundle\Controller\API;
 use Doctrine\ORM\QueryBuilder;
 use DOMJudgeBundle\Entity\Contest;
 use DOMJudgeBundle\Entity\Event;
+use DOMJudgeBundle\Service\EventLogService;
 use DOMJudgeBundle\Utils\Utils;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
@@ -116,6 +117,7 @@ class ContestController extends AbstractRestController
         $contest  = $this->getContestWithId($request, $id);
         $response = null;
         $now      = Utils::now();
+        $changed  = false;
         if (!$request->request->has('id')) {
             $response = new JsonResponse('Missing "id" in request.', Response::HTTP_BAD_REQUEST);
         } elseif (!$request->request->has('start_time')) {
@@ -132,6 +134,7 @@ class ContestController extends AbstractRestController
             $contest->setStarttimeEnabled(false);
             $response = new JsonResponse('Contest paused :-/.', Response::HTTP_OK);
             $this->em->flush();
+            $changed = true;
         } else {
             $date = date_create($request->request->get('start_time'));
             if ($date === false) {
@@ -150,9 +153,16 @@ class ContestController extends AbstractRestController
                     $response = new JsonResponse('Contest start time changed to ' . $newStartTimeString,
                                                  Response::HTTP_OK);
                     $this->em->flush();
+                    $changed = true;
                 }
             }
         }
+
+        if ($changed) {
+            $this->eventLogService->log('contests', $contest->getCid(), EventLogService::ACTION_UPDATE,
+                                        $contest->getCid());
+        }
+
         return $response;
     }
 
@@ -281,9 +291,9 @@ class ContestController extends AbstractRestController
         if ($request->query->has('since_id')) {
             $since_id = $request->query->getInt('since_id');
             $event    = $this->em->getRepository(Event::class)->findOneBy([
-                                                                                         'eventid' => $since_id,
-                                                                                         'cid' => $contest->getCid(),
-                                                                                     ]);
+                                                                              'eventid' => $since_id,
+                                                                              'cid' => $contest->getCid(),
+                                                                          ]);
             if ($event === null) {
                 return new Response('Invalid parameter "since_id" requested.', Response::HTTP_BAD_REQUEST);
             }
