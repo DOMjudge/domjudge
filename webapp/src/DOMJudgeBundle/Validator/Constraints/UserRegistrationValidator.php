0b48c317cbaaca5da6a2e9628abff0669a1d5824
Nicky Gerritsen <nickygerritsen@me.com>
2019-04-21 14:14:31 +0200

    Clean up registration form.
    We used to display two error messages relating to the username needing to be only alphanumeric characters, one also allowing - and _.
    This was because we have a default validation on the username property of a user and we added one ourselves as well.
    Also took the time to clean up the registration form code.
    Thanks Mark van Helvoort for reporting the bug!

--- webapp/src/DOMJudgeBundle/Validator/Constraints/UserRegistrationValidator.php
+++ webapp/src/DOMJudgeBundle/Validator/Constraints/UserRegistrationValidator.php
@@ -14,9 +14,6 @@ class UserRegistrationValidator extends ConstraintValidator
     }
     public function validate($value, Constraint $constraint)
     {
-        if (!ctype_alnum($value)) {
-            $this->context->buildViolation("Username must consist of only alphanumeric characters")->addViolation();
-        }
         $user = $this->em->getRepository('DOMJudgeBundle:User')->findOneByUsername($value);
         if ($user) {
             $this->context->buildViolation("User with that username already exists")->addViolation();
