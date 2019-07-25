5ed6e1fdf98e98357fd09ea2f9dcb105733afcfd
Nicky Gerritsen <nickygerritsen@me.com>
2019-07-25 21:19:41 +0200

    Install Doctrine Migrations bundle.
    We will use this for our database migrations as well as to check if our entities match with our current structure.

--- webapp/config/bundles.php
+++ webapp/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
