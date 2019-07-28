65d613e54cd4d414942f60c20caef6b64f3b26e3
Nicky Gerritsen <nickygerritsen@me.com>
2019-07-28 08:59:33 +0200

    Synchronise Judgehost entity and database table.

--- webapp/src/Entity/Judgehost.php
+++ webapp/src/Entity/Judgehost.php
@@ -8,7 +8,10 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * Hostnames of the autojudgers
  * @ORM\Entity()
- * @ORM\Table(name="judgehost", options={"collate"="utf8mb4_unicode_ci", "charset"="utf8mb4"})
+ * @ORM\Table(
+ *     name="judgehost",
+ *     options={"collate"="utf8mb4_unicode_ci", "charset"="utf8mb4", "comment"="Hostnames of the autojudgers"},
+ *     indexes={@ORM\Index(name="restrictionid", columns={"restrictionid"})})
  */
 class Judgehost
 {
@@ -22,20 +25,29 @@ class Judgehost
 
     /**
      * @var boolean
-     * @ORM\Column(type="boolean", name="active", options={"comment"="Should this host take on judgings?"}, nullable=false)
+     * @ORM\Column(type="boolean", name="active",
+     *     options={"comment"="Should this host take on judgings?",
+     *              "unsigned"=true,"default"="1"},
+     *     nullable=false)
      */
     private $active = true;
 
 
     /**
      * @var double
-     * @ORM\Column(type="decimal", precision=32, scale=9, name="polltime", options={"comment"="Time of last poll by autojudger", "unsigned"=true}, nullable=true)
+     * @ORM\Column(type="decimal", precision=32, scale=9, name="polltime",
+     *     options={"comment"="Time of last poll by autojudger",
+     *              "unsigned"=true,"default"="NULL"},
+     *     nullable=true)
      */
     private $polltime;
 
     /**
      * @var int
-     * @ORM\Column(type="integer", name="restrictionid", options={"comment"="Optional set of restrictions for this judgehost"}, nullable=true)
+     * @ORM\Column(type="integer", name="restrictionid",
+     *     options={"comment"="Optional set of restrictions for this judgehost",
+     *              "unsigned"=true,"default"="NULL"},
+     *     nullable=true)
      * @Serializer\Exclude()
      */
     private $restrictionid;
