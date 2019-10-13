ceee309ff8af40999fae7d6d60cc49870a03fc91
Jason Klein <jason.klein@logicforte.com>
2019-10-13 12:37:27 -0500

    Issue 635: Balloon Status page returns 500 (PR v2)
    (cherry picked from commit 5dd70c3dcdec090b92568f661524107e49f581ec)

--- webapp/src/Controller/Jury/BalloonController.php
+++ webapp/src/Controller/Jury/BalloonController.php
@@ -160,7 +160,7 @@ class BalloonController extends AbstractController
             $comments = [];
             if ($AWARD_BALLOONS['contest'][$contest] == $balloonId) {
                 $comments[] = 'first in contest';
-            } elseif (is_array($AWARD_BALLOONS['problem'][$balloonsData['probid']])
+            } elseif (isset($AWARD_BALLOONS['problem'][$balloonsData['probid']])
                    && in_array($balloonId, $AWARD_BALLOONS['problem'][$balloonsData['probid']], true)) {
                 $comments[] = 'first for problem';
             }
