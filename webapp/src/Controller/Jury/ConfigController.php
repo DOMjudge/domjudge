3ef60d78ce9d53c68f784bbe4f1a4264bbf30791
Jaap Eldering <jaap@jaapeldering.nl>
2019-10-28 00:29:50 +0100

    Refactor updating configuration.
    Document special case of bool=false not present because
    of HTML checkbox not sending this.
    
    Also log when a configuration option has unknown type.

--- webapp/src/Controller/Jury/ConfigController.php
+++ webapp/src/Controller/Jury/ConfigController.php
@@ -6,6 +6,7 @@ use App\Entity\Configuration;
 use App\Service\CheckConfigService;
 use App\Service\DOMJudgeService;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +25,11 @@ class ConfigController extends AbstractController
     protected $em;
 
     /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
      * @var DOMJudgeService
      */
     protected $dj;
@@ -36,15 +42,18 @@ class ConfigController extends AbstractController
     /**
      * TeamCategoryController constructor.
      * @param EntityManagerInterface $em
+     * @param LoggerInterface        $logger
      * @param DOMJudgeService        $dj
      * @param CheckConfigService     $checkConfigService
      */
     public function __construct(
         EntityManagerInterface $em,
+        LoggerInterface $logger,
         DOMJudgeService $dj,
         CheckConfigService $checkConfigService
     ) {
         $this->em                 = $em;
+        $this->logger             = $logger;
         $this->dj                 = $dj;
         $this->checkConfigService = $checkConfigService;
     }
@@ -57,42 +66,61 @@ class ConfigController extends AbstractController
         /** @var Configuration[] */
         $options = $this->em->getRepository(Configuration::class)->findAll();
         if ($request->getMethod() == 'POST' && $request->request->has('save')) {
-            $this->addFlash('scoreboard_refresh', 'After changing specific settings, you might need to refresh the scoreboard.');
+            $this->addFlash('scoreboard_refresh', 'After changing specific ' .
+                            'settings, you might need to refresh the scoreboard.');
             foreach ($options as $option) {
-                if ($option->getType() == 'bool') {
-                    $val = $request->request->has('config_' . $option->getName());
-                    $option->setValue($val);
-                    continue;
-                }
                 if (!$request->request->has('config_' . $option->getName())) {
-                    continue;
+                    // Special-case bool, since checkboxes don't return a
+                    // value when unset.
+                    if ( $option->getType() != 'bool' ) continue;
+                    $val = false;
+                } else {
+                    $val = $request->request->get('config_' . $option->getName());
                 }
-                if ($option->getType() == 'int' || $option->getType() == 'string') {
-                    $option->setValue($request->request->get('config_' . $option->getName()));
-                } else if ($option->getType() == 'array_val') {
-                    $vals = $request->request->get('config_' . $option->getName());
-                    $result = array();
-                    foreach ($vals as $data) {
-                        if (!empty($data)) {
-                            $result[] = $data;
+                switch ( $option->getType() ) {
+                    case 'bool':
+                        $option->setValue((bool)$val);
+                        break;
+
+                    case 'int':
+                        $option->setValue((int)$val);
+                        break;
+
+                    case 'string':
+                        $option->setValue($val);
+                        break;
+
+                    case 'array_val':
+                        $result = array();
+                        foreach ($val as $data) {
+                            if (!empty($data)) {
+                                $result[] = $data;
+                            }
                         }
-                    }
-                    $option->setValue($result);
-                } else if ($option->getType() == 'array_keyval') {
-                    $vals = $request->request->get('config_' . $option->getName());
-                    $result = array();
-                    foreach ($vals as $data) {
-                        if (!empty($data['key'])) {
-                            $result[$data['key']] = $data['val'];
+                        $option->setValue($result);
+                        break;
+
+                    case 'array_keyval':
+                        $result = array();
+                        foreach ($val as $data) {
+                            if (!empty($data['key'])) {
+                                $result[$data['key']] = $data['val'];
+                            }
                         }
-                    }
-                    $option->setValue($result);
+                        $option->setValue($result);
+                        break;
+
+                    default:
+                        $msg = sprintf("configation option '%s' has unknown type '%s'",
+                                       $option->getName(), $option->getType());
+                        $this->logger->warn($msg);
                 }
             }
 
             $this->em->flush();
             return $this->redirectToRoute('jury_config');
         }
+
         /** @var Configuration[] */
         $options = $this->em->getRepository(Configuration::class)->findAll();
         $categories = array();
