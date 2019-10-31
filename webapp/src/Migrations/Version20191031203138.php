2db3b863f28db583ee95dff94460290498a8cbba
Nicky Gerritsen <nickygerritsen@me.com>
2019-10-31 21:37:48 +0100

    Also remove .py from Python 2 list.

--- webapp/src/Migrations/Version20191031203138.php
+++ webapp/src/Migrations/Version20191031203138.php
@@ -14,18 +14,20 @@ final class Version20191031203138 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'add .py to Python 3 extension list';
+        return 'add .py to Python 3 extension list and remove it for Python 2';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('UPDATE language SET extensions = \'["py3","py"]\' WHERE langid = \'py3\'');
+        $this->addSql('UPDATE language SET extensions = \'["py2"]\' WHERE langid = \'py2\'');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('UPDATE language SET extensions = \'["py2","py"]\' WHERE langid = \'py2\'');
         $this->addSql('UPDATE language SET extensions = \'["py"]\' WHERE langid = \'py3\'');
     }
 }
