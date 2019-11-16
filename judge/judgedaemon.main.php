242be5ccdbbce42947973312b0748ce4190aba9a
Tobias Werth <werth@cs.fau.de>
2019-11-16 14:21:48 +0100

    Modernize compiler options.

--- judge/judgedaemon.main.php
+++ judge/judgedaemon.main.php
@@ -304,10 +304,10 @@ function fetch_executable(
                 }
                 switch ($execlang) {
                 case 'c':
-                    $buildscript .= "gcc -Wall -O2 -std=gnu99 '$source' -o $execrunpath -lm\n";
+                    $buildscript .= "gcc -Wall -O2 -std=gnu11 '$source' -o $execrunpath -lm\n";
                     break;
                 case 'cpp':
-                    $buildscript .= "g++ -Wall -O2 -std=c++11 '$source' -o $execrunpath\n";
+                    $buildscript .= "g++ -Wall -O2 -std=gnu++17 '$source' -o $execrunpath\n";
                     break;
                 case 'java':
                     $source = basename($source, ".java");
