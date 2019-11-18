c797bb56d6fb099a85f28497e0acccd3a61d57ef
Nicky Gerritsen <nickygerritsen@me.com>
2019-11-18 19:06:36 +0100

    Turn sortorder into integer field. Fixes #659.
    By turning it into an integer field, the maxlength also goes away.

--- webapp/src/Form/Type/TeamCategoryType.php
+++ webapp/src/Form/Type/TeamCategoryType.php
@@ -5,6 +5,7 @@ namespace App\Form\Type;
 use App\Entity\TeamCategory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,7 +16,9 @@ class TeamCategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name');
-        $builder->add('sortorder');
+        $builder->add('sortorder', IntegerType::class, [
+            
+        ]);
         $builder->add('color', TextType::class, [
             'required' => false,
             'attr' => [
