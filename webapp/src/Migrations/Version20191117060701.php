6153c839026e9884147a116d1db4f0b1a6c62478
Jaap Eldering <jaap@jaapeldering.nl>
2019-11-21 23:57:49 +0100

    Also migrate any value for external_ccs_submission_url.
    Migrating down with '[contest]' present will keep that as
    there is no good replacement.

--- webapp/src/Migrations/Version20191117060701.php
+++ webapp/src/Migrations/Version20191117060701.php
@@ -20,7 +20,9 @@ final class Version20191117060701 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $this->addSql(<<<SQL
-UPDATE `configuration` SET `description` = 'URL of a submission detail page on the external CCS. Placeholder [id] will be replaced by submission ID and [contest] by the contest ID. Leave empty to not display links to external CCS'
+UPDATE `configuration` SET
+  `description` = 'URL of a submission detail page on the external CCS. Placeholder [id] will be replaced by submission ID and [contest] by the contest ID. Leave empty to not display links to external CCS',
+  `value` = REPLACE(`value`, ':id:', '[id]')
 WHERE `name` = 'external_ccs_submission_url'
 SQL
         );
@@ -29,7 +31,9 @@ SQL
     public function down(Schema $schema) : void
     {
         $this->addSql(<<<SQL
-UPDATE `configuration` SET `description` = 'URL of a submission detail page on the external CCS. Placeholder :id: will be replaced by submission ID. Leave empty to not display links to external CCS'
+UPDATE `configuration` SET
+  `description` = 'URL of a submission detail page on the external CCS. Placeholder :id: will be replaced by submission ID. Leave empty to not display links to external CCS',
+  `value` = REPLACE(`value`, '[id]', ':id:')
 WHERE `name` = 'external_ccs_submission_url'
 SQL
         );
