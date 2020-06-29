fb4415f13367de4dd47e68532098419407477d95
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-06-29 13:32:08 +0200

    Use ulimit -c 0 instead of try catch.

--- tests/test-memsize.cc
+++ tests/test-memsize.cc
@@ -2,29 +2,24 @@
  * This should fail with RUN-ERROR due to running out of memory, which
  * is restricted.
  *
+ * Note: This may try to create a coredump on exit and time out. This
+ * can be prevented with `ulimit -c 0`.
+ *
  * @EXPECTED_RESULTS@: RUN-ERROR
  */
 
-#include <cstdlib>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
+const size_t inc_mb = 128;
+
 int main() {
 	vector<vector<char>> vs;
 	while(true) {
-		vector<char> v;
-		// Allocate 1MB at a time.
-		try {
-			v.reserve(1ll * 1024 * 1024);
-			vs.push_back(std::move(v));
-		} catch(const std::bad_alloc&) {
-			// Handle the exception and clean-up manually to prevent slow termination of the
-			// program.
-			vs.~vector<vector<char>>();
-			// Make sure to re-raise the exit with SIGABRT.
-			abort();
-		}
+		vs.emplace_back(inc_mb * 1024 * 1024);
+		std::cerr << "Allocated: " << inc_mb * vs.size() << " MB" << std::endl;
 	}
 	return 0;
 }
