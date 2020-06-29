178573e26720096e21487cb7750f932d924e37d3
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-06-29 22:07:03 +0200

    Fix test-memsize.cc (#844)
    (cherry picked from commit e70d0818b58b7e31d75893e983d9a1a0c0ec9fce)

--- tests/test-memsize.cc
+++ tests/test-memsize.cc
@@ -1,8 +1,9 @@
 /*
  * This should fail with RUN-ERROR due to running out of memory, which
- * is restricted. The amount allocated may seem to be half of the
- * available, but that is because (GNU implementation) STL vectors by
- * default allocate double the amount of memory requested.
+ * is restricted.
+ *
+ * Note: This may try to create a coredump on exit and time out. This
+ * can be prevented with `ulimit -c 0`.
  *
  * @EXPECTED_RESULTS@: RUN-ERROR
  */
@@ -12,30 +13,13 @@
 
 using namespace std;
 
-vector<char> a;
+const size_t inc_mb = 128;
 
-int main()
-{
-	int p, i;
-
-	/*
-	  Watch out: resizing of a vector allocates AT LEAST that much memory!
-	  Testing shows, that (glibc 2.2.5) twice the requested amount is
-	  allocated, so e.g. when you have 64 MB memory available, already when
-	  resizing to more than 32 MB, you run out of memory.
-	*/
-	for(p=4; 1; p*=2) {
-		for(i=p/2; i<p; i+=p/4) {
-			cout << "trying to allocate " << i << " MB... ";
-			a.resize(i*1024*1024,0);
-			if ( a.capacity()<i*1024*1024 ) {
-				cout << "resizing failed." << endl;
-				return 0;
-			}
-			for(int j=a.capacity()/2; j<a.capacity(); j += 512) a[j] = j%137;
-			cout << "allocated: " << a.capacity()/1024/1024 << " MB." << endl;
-		}
+int main() {
+	vector<vector<char>> vs;
+	while(true) {
+		vs.emplace_back(inc_mb * 1024 * 1024);
+		std::cerr << "Allocated: " << inc_mb * vs.size() << " MB" << std::endl;
 	}
-
 	return 0;
 }
