4cb4ce6502e887ea64c81aa10501782d9b3a34ec
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2020-10-29 07:47:46 +0100

    Visual regression test
    The current master branch and the PR are fully generated and compared
    pixel by pixel. If there is to much regression the test fails
    
    A small regression is accepted as the time might differ between the
    scraping of the pages and changes of only a pixel will not be noticed.

---
+++ gitlab/visualcompare.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# https://askubuntu.com/questions/209517/does-diff-exist-for-images
+
+set -euxo pipefail
+export PS4='(${BASH_SOURCE}:${LINENO}): - [$?] $ '
+
+failingchanges="failingchanges"
+predictedchanges="predictedchanges"
+
+mkdir -p {$failingchanges,$predictedchanges}
+
+for file in `ls screenshotspr`
+do
+    PR=screenshotspr/$file
+    MR=screenshotsmaster/$file
+    # This fails when there is a change in time between the branches
+    set +e
+    DIFF=`idiff -warn 100 -fail 0.5 $PR $MR -abs -od -scale 10.0`;RET=$?
+    set -e
+    # There is the problem of detecting if a change is wanted or unwanted, currently check all the captured
+    # screenshots against master for changes
+    if [ $RET -ne 0 ]; then
+        REMOVE=".html-ff.png"
+        GITHUB_PR=`cut -d '/' -f1 <<< ${CI_COMMIT_BRANCH##pr-}`
+        ENDPOINT=${file/$REMOVE}
+        WANTED=`python3 gitlab/visualgithubprdiscussion.py $ENDPOINT $GITHUB_PR`
+	if [ $WANTED = "wanted" ]; then
+	    STORDIR=$predictedchanges
+	elif [ $WANTED = "none" ]; then
+	    STORDIR=$failingchanges
+	fi
+	compare $PR $MR -highlight-color blue $STORDIR/$file || true
+    fi
+done
+
+CHANGE=0
+for dir in $failingchanges $predictedchanges
+do
+    if [ $dir == $failingchanges ]; then
+        STATE="failure"
+    else
+        STATE="success"
+    fi
+    MANY=`ls $dir|wc -l`
+    FILE="$dir"
+    if [ $MANY -eq 1 ]; then
+        FILE=$dir/`ls $dir`
+	CHANGE=1
+    elif [ $MANY -gt 1 ]; then
+    CHANGE=1
+    fi
+    if [ $MANY -gt 0 ]; then
+      # Copied from CCS
+      curl https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA \
+        -X POST \
+        -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
+        -H "Accept: application/vnd.github.v3+json" \
+	-d "{\"state\": \"$STATE\", \"target_url\": \"$CI_JOB_URL/artifacts/browse/$FILE\", \"description\":\"UI changes\", \"context\": \"UI diffs ($dir)\"}"
+    fi
+done
+
+if [ $CHANGE -eq 0 ]; then
+    curl https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA \
+        -X POST \
+        -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
+        -H "Accept: application/vnd.github.v3+json" \
+        -d "{\"state\": \"success\", \"target_url\": \"$CI_JOB_URL/artifacts/browse/$FILE\", \"description\":\"No UI changes\", \"context\": \"UI diffs (None)\"}"
+fi
+
---
+++ gitlab/visualpr.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+alias trace_on='set -x'
+alias trace_off='{ set +x; } 2>/dev/null'
+
+function section_start_internal() {
+    echo -e "section_start:`date +%s`:$1[collapsed=true]\r\e[0K$2"
+    trace_on
+}
+
+function section_end_internal() {
+    echo -e "section_end:`date +%s`:$1\r\e[0K"
+    trace_on
+}
+
+alias section_start='trace_off ; section_start_internal '
+alias section_end='trace_off ; section_end_internal '
+
+mkdir screenshots$1
+set -euxo pipefail
+
+section_start setup "Setup and install"
+
+export PS4='(${BASH_SOURCE}:${LINENO}): - [$?] $ '
+
+DIR=$(pwd)
+GITSHA=$(git rev-parse HEAD || true)
+
+# Set up
+"$( dirname "${BASH_SOURCE[0]}" )"/base.sh
+
+# Add jury to dummy user
+echo "INSERT INTO userrole (userid, roleid) VALUES (3, 2);" | mysql domjudge
+
+# Add netrc file for dummy user login
+echo "machine localhost login dummy password dummy" > ~/.netrc
+
+LOGFILE="/opt/domjudge/domserver/webapp/var/log/prod.log"
+
+function log_on_err() {
+    echo -e "\\n\\n=======================================================\\n"
+    echo "Symfony log:"
+    if sudo test -f "$LOGFILE" ; then
+        sudo cat "$LOGFILE"
+    fi
+    # We have access to the host date/time, reset this back
+    # service ntp stop
+    # ntpd -gq
+    # service ntp start
+}
+
+trap log_on_err ERR
+
+cd /opt/domjudge/domserver
+
+# This needs to be done before we do any submission.
+# 8 hours as a helper so we can adjust contest start/endtime
+TIMEHELP=$((8*60*60))
+# Database changes to make the REST API and event feed match better.
+cat <<EOF | mysql domjudge
+DELETE FROM clarification;
+UPDATE contest SET starttime  = UNIX_TIMESTAMP()-$TIMEHELP WHERE cid = 2;
+UPDATE contest SET freezetime = UNIX_TIMESTAMP()+15        WHERE cid = 2;
+UPDATE contest SET endtime    = UNIX_TIMESTAMP()+$TIMEHELP WHERE cid = 2;
+UPDATE team_category SET visible = 1;
+EOF
+
+ADMINPASS=$(cat etc/initial_admin_password.secret)
+
+# configure and restart php-fpm
+sudo cp /opt/domjudge/domserver/etc/domjudge-fpm.conf "/etc/php/7.2/fpm/pool.d/domjudge-fpm.conf"
+sudo /usr/sbin/php-fpm7.2
+
+section_end setup
+
+# Patch some visibility problems
+cp $DIR/gitlab/visualreg.css webapp/public/css/custom/
+webapp/bin/console cache:clear
+
+ADMINPASS=$(cat etc/initial_admin_password.secret)
+export COOKIEJAR
+COOKIEJAR=$(mktemp --tmpdir)
+export CURLOPTS="--fail -sq -m 30 -b $COOKIEJAR"
+
+# First commit I could find, every date should be fine, time should be stable
+# between PR and Master to get semi stable screenshots
+# Breaks so removed for now
+# date -s "18 May 2004 12:05:57"
+
+# Make an initial request which will get us a session id, and grab the csrf token from it
+CSRFTOKEN=$(curl $CURLOPTS -c $COOKIEJAR "http://localhost/domjudge/login" 2>/dev/null | sed -n 's/.*_csrf_token.*value="\(.*\)".*/\1/p')
+# Make a second request with our session + csrf token to actually log in
+curl $CURLOPTS -c $COOKIEJAR -F "_csrf_token=$CSRFTOKEN" -F "_username=admin" -F "_password=$ADMINPASS" "http://localhost/domjudge/login"
+
+cd $DIR
+
+STORAGEDIR=screenshots$1
+mkdir -p $STORAGEDIR
+
+cp $COOKIEJAR cookies.txt
+sed -i 's/#HttpOnly_//g' cookies.txt
+sed -i 's/\t0\t/\t1999999999\t/g' cookies.txt
+for url in public
+do
+    mkdir $url
+    cd $url
+    cp $DIR/cookies.txt ./
+    httrack http://localhost/domjudge/$url --assume html=text/html -*doc* -*/team/* -*/jury/* -*logout*
+    cd $DIR
+    mkdir /var/www/html/$url/
+    cp -r $url/localhost/domjudge/* /var/www/html/$url/
+    cp $DIR/gitlab/default-nginx /etc/nginx/sites-enabled/default
+    service nginx restart
+    for file in `find $url -type f -name "*.html"`
+    do
+        prefix="^$url\/localhost\/domjudge\/"
+        urlpath=$(sed "s/$prefix//g"<<<$file)
+        # Small risk of collision
+        storepath=$(sed "s/\//_s_/g"<<<$urlpath)
+        firefox -screenshot $STORAGEDIR/$storepath-ff.png http://localhost/$url/$urlpath
+    done
+done
