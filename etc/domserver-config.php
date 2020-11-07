e78e5de8f4fcbf8254522c5eff36905bec4a5533
Thijs Kinkhorst <thijs@kinkhorst.com>
2020-11-07 12:37:19 +0000

    Move removed_intervals to a Symfony parameter.
    We think this is obscure enough not to expose it in the general
    Configuration page in the web interface.
    
    In the entity, we keep the code simpler by just executing the
    for loop: if there are no intervals, it terminals immediately.

--- etc/domserver-config.php
+++ etc/domserver-config.php
@@ -1,9 +1,2 @@
 <?php declare(strict_types=1);
 
-require_once("common-config.php");
-
-// Enable this to support removing time intervals from the contest.
-// Although we use this feature at the ICPC World Finals, we strongly
-// discourage using it, and we don't guarantee the code is completely
-// bug-free. This code is rarely tested!
-define('ALLOW_REMOVED_INTERVALS', false);
