19f64af88d2d4081be0d8e4fb54ab0d4d4d1abb1
Tobias Werth <werth@cs.fau.de>
2020-12-30 18:03:27 +0100

    Update polltime in new API endpoint.

--- webapp/src/Controller/API/JudgehostController.php
+++ webapp/src/Controller/API/JudgehostController.php
@@ -1443,6 +1443,15 @@ class JudgehostController extends AbstractFOSRestController
         }
         $hostname = $request->request->get('hostname');
 
+        /** @var Judgehost $judgehost */
+        $judgehost = $this->em->getRepository(Judgehost::class)->find($hostname);
+        if (!$judgehost) {
+            return '';
+        }
+
+        // Update last seen of judgehost
+        $judgehost->setPolltime(Utils::now());
+
         // TODO: Determine a good max batch size here. We may want to do something more elaborate like looking at
         // previous judgements of the same testcase and use median runtime as an indicator.
         $max_batchsize = 5;
