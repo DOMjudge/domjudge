dda4746640f46c1e64760bee5d04adb609d12016
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-01-23 16:21:55 +0100

    Fix executable to work with JuryController
    Use return parts
    
    Replicated code in ECTest removed.

--- webapp/tests/Controller/Jury/ExecutableControllerTest.php
+++ webapp/tests/Controller/Jury/ExecutableControllerTest.php
@@ -2,44 +2,15 @@
 
 namespace App\Tests\Controller\Jury;
 
-use App\Tests\BaseTest;
-use Generator;
+use App\Entity\Executable;
 
-class ExecutableControllerTest extends BaseTest
+class ExecutableControllerTest extends JuryControllerTest
 {
-    protected static $roles = ['admin'];
-
-    /**
-     * @dataProvider provideRoleAccessData
-     */
-    public function testHTTPAccessForRole(string $role, string $url, int $statusCode, string $HTTPMethod) : void
-    {
-        static::$roles = [$role];
-        // Optionally use the setupUser
-        $this->logOut();
-        $this->logIn();
-        $this->verifyPageResponse($HTTPMethod, $url, $statusCode);
-    }
-
-    /**
-     * Data provider used to test role access. Each item contains:
-     * - the base role the user has
-     * - the endpoint to check
-     * - expected statusCode for this role
-     * - the method to try (GET, POST)
-     */
-    public function provideRoleAccessData() : Generator
-    {
-        foreach (['GET', 'POST', 'HEAD'] as $HTTP)
-        {
-            foreach (['admin', 'jury'] as $role) {
-                yield [$role, '/jury/executables', 200, $HTTP];
-            }
-            foreach (['team', 'jury'] as $role) {
-                yield [$role, '/jury/executables/add', 403, $HTTP];
-            }
-            yield ['team', '/jury/executables', 403, $HTTP];
-            yield ['admin', '/jury/executables/add', 200, $HTTP];
-        }
-    }
+    protected static $baseUrl           = '/jury/executables';
+    protected static $shortTag          = 'executable';
+    protected static $getIDFunc         = 'getExecid';
+    protected static $className         = Executable::class;
+    protected static $deleteEntities    = array('description' => ['adb']);
+    protected static $exampleEntries    = ['adb','run','boolfind comparator'];
+    protected static $DOM_elements      = array('h1' => ['Executables']);
 }
