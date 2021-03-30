c14db42a61c87279f64c0d9cbe41d105777180a5
Nicky Gerritsen <nickygerritsen@me.com>
2021-03-30 20:33:04 +0200

    Add tests for the language API.
    Set them up in such a way that we can reuse it for other API tests.

---
+++ webapp/src/DataFixtures/Test/EnableJavaEntrypointFixture.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\DataFixtures\Test;
+
+use App\Entity\Language;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class EnableJavaEntrypointFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $java = $manager->getRepository(Language::class)->find('java');
+        $java->setRequireEntryPoint(true);
+        $manager->flush();
+    }
+}
