a05505174782047ccb738f35239b0a974251ce81
Nicky Gerritsen <nickygerritsen@me.com>
2021-04-06 08:58:55 +0200

    Merge judgetask related migrations

--- webapp/src/Migrations/Version20201219154651.php
+++ webapp/src/Migrations/Version20201219154651.php
@@ -6,25 +6,26 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use ZipArchive;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
 final class Version20201219154651 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
-        return 'Initial DB structure for new judgehost API.';
+        return 'DB structure for new judgehost API and related changes.';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE judgetask (judgetaskid INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'Judgetask ID\', hostname VARCHAR(255) DEFAULT NULL COMMENT \'hostname of the judge which executes the task\', type ENUM(\'judging_run\', \'generic_task\', \'config_check\', \'debug_info\') DEFAULT \'judging_run\' NOT NULL COMMENT \'Type of the judge task.(DC2Type:judge_task_type)\', priority INT NOT NULL COMMENT \'Priority; negative means higher priority\', jobid INT UNSIGNED NOT NULL COMMENT \'All judgetasks with the same jobid belong together.\', submitid INT UNSIGNED DEFAULT NULL COMMENT \'Submission ID being judged\', compile_script_id INT UNSIGNED DEFAULT NULL COMMENT \'Compile script ID\', run_script_id INT UNSIGNED DEFAULT NULL COMMENT \'Run script ID\', compare_script_id INT UNSIGNED DEFAULT NULL COMMENT \'Compare script ID\', testcase_id INT UNSIGNED DEFAULT NULL COMMENT \'Testcase ID\', compile_config LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'The compile config as JSON-blob.\', run_config LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'The run config as JSON-blob.\', compare_config LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'The compare config as JSON-blob.\', PRIMARY KEY(judgetaskid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Individual judge tasks.\' ');
-        $this->addSql('CREATE TABLE executable_file (execfileid INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'Executable file ID\', immutable_execid INT UNSIGNED DEFAULT NULL COMMENT \'ID\', filename VARCHAR(255) NOT NULL COMMENT \'Filename as uploaded\', ranknumber INT UNSIGNED NOT NULL COMMENT \'Order of the executable files, zero-indexed\', file_content LONGBLOB NOT NULL COMMENT \'Full file content(DC2Type:blobtext)\', is_executable TINYINT(1) DEFAULT \'0\' NOT NULL COMMENT \'Whether this file gets an executable bit.\', INDEX immutable_execid (immutable_execid), UNIQUE INDEX rankindex (immutable_execid, ranknumber), UNIQUE INDEX filename (immutable_execid, filename(190)), PRIMARY KEY(execfileid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Files associated to an executable\' ');
-        $this->addSql('CREATE TABLE immutable_executable (immutable_execid INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'ID\', userid INT UNSIGNED DEFAULT NULL COMMENT \'User ID\', INDEX IDX_676B601AF132696E (userid), PRIMARY KEY(immutable_execid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Immutable wrapper for a collection of files for executable bundles.\' ');
+        $this->addSql('CREATE TABLE judgetask (judgetaskid INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'Judgetask ID\', judgehostid INT UNSIGNED DEFAULT NULL COMMENT \'Judgehost ID\', type ENUM(\'judging_run\', \'generic_task\', \'config_check\', \'debug_info\', \'prefetch\') DEFAULT \'judging_run\' NOT NULL COMMENT \'Type of the judge task.(DC2Type:judge_task_type)\', priority INT NOT NULL COMMENT \'Priority; negative means higher priority\', jobid INT UNSIGNED DEFAULT NULL COMMENT \'All judgetasks with the same jobid belong together.\', submitid INT UNSIGNED DEFAULT NULL COMMENT \'Submission ID being judged\', compile_script_id INT UNSIGNED DEFAULT NULL COMMENT \'Compile script ID\', run_script_id INT UNSIGNED DEFAULT NULL COMMENT \'Run script ID\', compare_script_id INT UNSIGNED DEFAULT NULL COMMENT \'Compare script ID\', testcase_id INT UNSIGNED DEFAULT NULL COMMENT \'Testcase ID\', compile_config LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'The compile config as JSON-blob.\', run_config LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'The run config as JSON-blob.\', compare_config LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'The compare config as JSON-blob.\', valid TINYINT(1) DEFAULT \'1\' NOT NULL COMMENT \'Only handed out if still valid.\', starttime NUMERIC(32, 9) UNSIGNED DEFAULT NULL COMMENT \'Time the judgetask was started\', PRIMARY KEY(judgetaskid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Individual judge tasks.\' ');
+        $this->addSql('CREATE TABLE executable_file (execfileid INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'Executable file ID\', immutable_execid INT UNSIGNED DEFAULT NULL COMMENT \'ID\', filename VARCHAR(255) NOT NULL COMMENT \'Filename as uploaded\', ranknumber INT UNSIGNED NOT NULL COMMENT \'Order of the executable files, zero-indexed\', file_content LONGBLOB NOT NULL COMMENT \'Full file content(DC2Type:blobtext)\', hash VARCHAR(32) DEFAULT NULL COMMENT \'hash of the content\', is_executable TINYINT(1) DEFAULT \'0\' NOT NULL COMMENT \'Whether this file gets an executable bit.\', INDEX immutable_execid (immutable_execid), UNIQUE INDEX rankindex (immutable_execid, ranknumber), UNIQUE INDEX filename (immutable_execid, filename(190)), PRIMARY KEY(execfileid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Files associated to an executable\' ');
+        $this->addSql('CREATE TABLE immutable_executable (immutable_execid INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'ID\', userid INT UNSIGNED DEFAULT NULL COMMENT \'User ID\', hash VARCHAR(32) DEFAULT NULL COMMENT \'hash of the files\', INDEX IDX_676B601AF132696E (userid), PRIMARY KEY(immutable_execid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Immutable wrapper for a collection of files for executable bundles.\' ');
         $this->addSql('ALTER TABLE executable_file ADD CONSTRAINT FK_99FA6255979A9F09 FOREIGN KEY (immutable_execid) REFERENCES immutable_executable (immutable_execid) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE immutable_executable ADD CONSTRAINT FK_676B601AF132696E FOREIGN KEY (userid) REFERENCES user (userid) ON DELETE SET NULL');
         $this->addSql('ALTER TABLE judging CHANGE starttime starttime NUMERIC(32, 9) UNSIGNED DEFAULT NULL COMMENT \'Time judging started\'');
@@ -36,28 +37,79 @@ final class Version20201219154651 extends AbstractMigration
         $this->addSql('ALTER TABLE executable ADD immutable_execid INT UNSIGNED DEFAULT NULL COMMENT \'ID\', DROP md5sum');
         $this->addSql('ALTER TABLE executable ADD CONSTRAINT FK_D68EDA01979A9F09 FOREIGN KEY (immutable_execid) REFERENCES immutable_executable (immutable_execid)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D68EDA01979A9F09 ON executable (immutable_execid)');
+        $this->addSql('ALTER TABLE testcase_content ADD CONSTRAINT FK_50A5CCE2D360BB2B FOREIGN KEY (testcaseid) REFERENCES testcase (testcaseid) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX priority ON judgetask (priority)');
+        $this->addSql('CREATE INDEX jobid ON judgetask (jobid)');
+        $this->addSql('CREATE INDEX submitid ON judgetask (submitid)');
+        $this->addSql('CREATE INDEX valid ON judgetask (valid)');
+        $this->addSql('CREATE INDEX judgehostid ON judgetask (judgehostid)');
+        $this->addSql('CREATE INDEX judgehostid_jobid ON judgetask (judgehostid, jobid)');
+        $this->addSql('CREATE INDEX judgehostid_valid_priority ON judgetask (judgehostid, valid, priority)');
+        $this->addSql('CREATE INDEX specific_type ON judgetask (judgehostid, starttime, valid, type, priority, judgetaskid)');
+        $this->addSql('ALTER TABLE submission DROP FOREIGN KEY submission_ibfk_5');
+        $this->addSql('DROP INDEX judgehost_2 ON submission');
+        $this->addSql('DROP INDEX judgehost ON submission');
+        $this->addSql('ALTER TABLE submission DROP judgehost');
+        $this->addSql('ALTER TABLE judging ADD judge_completely TINYINT(1) DEFAULT \'0\' NOT NULL COMMENT \'Explicitly requested to be judged completely.\'');
+        $this->addSql('CREATE UNIQUE INDEX hostname ON judgehost (hostname)');
+        $this->addSql('ALTER TABLE judgehost DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE judgehost ADD judgehostid INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'Judgehost ID\' FIRST, ADD hidden TINYINT(1) DEFAULT \'0\' NOT NULL COMMENT \'Should this host be hidden in the overview?\', ADD PRIMARY KEY (judgehostid)');
+        $this->addSql('ALTER TABLE judgetask ADD CONSTRAINT judgetask_ibfk_1 FOREIGN KEY (judgehostid) REFERENCES judgehost (judgehostid)');
+        $this->addSql('ALTER TABLE judging DROP FOREIGN KEY judging_ibfk_3');
+        $this->addSql('DROP INDEX judgehost ON judging');
+        $this->addSql('ALTER TABLE judging ADD judgehostid INT UNSIGNED DEFAULT NULL COMMENT \'Judgehost ID\' AFTER endtime');
+        $this->addSql('CREATE INDEX judgehostid ON judging (judgehostid)');
+        $this->addSql('ALTER TABLE judging ADD CONSTRAINT judging_ibfk_3 FOREIGN KEY (judgehostid) REFERENCES judgehost (judgehostid)');
+        $this->addSql('UPDATE judging j INNER JOIN judgehost jh ON j.judgehost = jh.hostname SET j.judgehostid = jh.judgehostid');
+        $this->addSql('UPDATE auditlog a INNER JOIN judgehost jh ON jh.hostname = a.dataid AND a.datatype = \'judgehost\' SET a.dataid = jh.judgehostid');
+
+        if ($schema->getTable('executable')->hasColumn('zipfile')) {
+            $oldRows = $this->connection->executeQuery('SELECT execid, zipfile FROM executable')->fetchAll();
+            foreach ($oldRows as $oldRow) {
+                $this->connection->exec('INSERT INTO immutable_executable (`userid`) VALUES (null)');
+                $immutable_execid = $this->connection->lastInsertId();
+
+                $tmpzip = tempnam('/tmp', 'zipfile');
+                file_put_contents($tmpzip, $oldRow['zipfile']);
+                $zip = new ZipArchive();
+                $zip->open($tmpzip, ZIPARCHIVE::CHECKCONS);
+
+                for ($idx = 0; $idx < $zip->numFiles; $idx++) {
+                    $filename = basename($zip->getNameIndex($idx));
+                    $content = $zip->getFromIndex($idx);
+                    $encodedContent = ($content === '' ? '' : ('0x' . strtoupper(bin2hex($content))));
+
+                    // In doubt make files executable, but try to read it from the zip file.
+                    $executableBit = '1';
+                    if ($zip->getExternalAttributesIndex($idx, $opsys, $attr)
+                        && $opsys == ZipArchive::OPSYS_UNIX
+                        && (($attr >> 16) & 0100) === 0) {
+                        $executableBit = '0';
+                    }
+                    $this->connection->exec(
+                        'INSERT INTO executable_file '
+                        . '(`immutable_execid`, `filename`, `ranknumber`, `file_content`, `is_executable`) '
+                        . 'VALUES (' . $immutable_execid . ', "' . $filename . '", '
+                        . $idx . ', ' . $encodedContent . ', '
+                        . $executableBit . ')'
+                    );
+                }
+
+                $this->connection->exec(
+                    'UPDATE executable SET immutable_execid = '
+                    . $immutable_execid . ' WHERE execid = "' . $oldRow['execid'] . '"'
+                );
+            }
+
+            $this->addSql('ALTER TABLE `executable` DROP COLUMN `zipfile`');
+        }
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE judging_run DROP FOREIGN KEY FK_29A6E6E13CBA64F2');
-        $this->addSql('ALTER TABLE executable DROP FOREIGN KEY FK_D68EDA01979A9F09');
-        $this->addSql('ALTER TABLE executable_file DROP FOREIGN KEY FK_99FA6255979A9F09');
-        $this->addSql('DROP TABLE judgetask');
-        $this->addSql('DROP TABLE executable_file');
-        $this->addSql('DROP TABLE immutable_executable');
-        $this->addSql('DROP INDEX UNIQ_D68EDA01979A9F09 ON executable');
-        $this->addSql('ALTER TABLE executable ADD md5sum CHAR(32) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'Md5sum of zip file\', DROP immutable_execid');
-        $this->addSql('ALTER TABLE judging CHANGE starttime starttime NUMERIC(32, 9) UNSIGNED NOT NULL COMMENT \'Time judging started\'');
-        $this->addSql('DROP INDEX IDX_29A6E6E13CBA64F2 ON judging_run');
-        $this->addSql('ALTER TABLE judging_run DROP judgetaskid, CHANGE endtime endtime NUMERIC(32, 9) UNSIGNED NOT NULL COMMENT \'Time run judging ended\'');
-        $this->addSql('ALTER TABLE testcase_content MODIFY tc_contentid INT UNSIGNED NOT NULL COMMENT \'Testcase content ID\'');
-        $this->addSql('DROP INDEX IDX_50A5CCE2D360BB2B ON testcase_content');
-        $this->addSql('ALTER TABLE testcase_content DROP PRIMARY KEY');
-        $this->addSql('ALTER TABLE testcase_content DROP tc_contentid, CHANGE testcaseid testcaseid INT UNSIGNED NOT NULL COMMENT \'Testcase ID\'');
-        $this->addSql('ALTER TABLE testcase_content ADD PRIMARY KEY (testcaseid)');
+        $this->abortIf(
+            true,
+            'Downgrading is not supported'
+        );
     }
 }
