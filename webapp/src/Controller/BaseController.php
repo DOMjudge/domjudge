eddfe87ef8a91770a8467ad7b0d1634502da2533
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-04-25 11:42:16 +0200

    Needed

--- webapp/src/Controller/BaseController.php
+++ webapp/src/Controller/BaseController.php
@@ -195,7 +195,7 @@ abstract class BaseController extends AbstractController
    #}
    #}
 	foreach ($entities as $entity) {
-	    assert($entity == $entities[0]) # No strict equal to check if same interface
+	    assert(get_class($entity) === get_class($entities[0]));
 	}
 
         $isError          = false;
