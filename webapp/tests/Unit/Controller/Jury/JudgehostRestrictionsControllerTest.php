15fb000b7973b8ba3f3421ee9abc6c9fb84b33e6
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-05-01 20:29:48 +0200

    Added a minimal judgehost restriction test
    The example data does not have a restriction, this adds one restriction
    as fixture and deletes this in the test.

--- webapp/tests/Unit/Controller/Jury/JudgehostRestrictionsControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/JudgehostRestrictionsControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Unit\Controller\Jury;
 
+use App\DataFixtures\Test\AddJudgehostRestrictionFixture;
 use App\Entity\JudgehostRestriction;
 
 class JudgehostRestrictionsControllerTest extends JuryControllerTest
@@ -9,9 +10,17 @@ class JudgehostRestrictionsControllerTest extends JuryControllerTest
     protected static $baseUrl        = '/jury/judgehost-restrictions';
     protected static $exampleEntries = ['No'];
     protected static $shortTag       = 'judgehost restriction';
-    protected static $deleteEntities = ['description' => ['adb']];
+    protected static $deleteEntities = ['name' => ['TestRestriction']];
     protected static $getIDFunc      = 'getRestrictionid';
     protected static $className      = JudgehostRestriction::class;
     protected static $DOM_elements   = ['h1' => ['Judgehost restrictions']];
-    protected static $delete         = ''; //TODO: When insert works this can be reset.
+
+    /**
+     * @dataProvider provideDeleteEntity
+     */
+    public function testDeleteEntity(string $identifier, string $entityShortName): void
+    {
+        $this->loadFixture(AddJudgehostRestrictionFixture::class);
+        parent::testDeleteEntity($identifier, $entityShortName);
+    }
 }
