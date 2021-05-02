2249f136cc4a51de3839b1c47d6b1b90ce6be582
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-05-02 22:13:38 +0200

    Test the add action of Jury Controllers
    Not all tests are implemented, interfaces where form fields are only
    added later in the page are not implemented in this commit.
    
    Team is not implemented because of https://github.com/DOMjudge/domjudge/issues/1122.

--- webapp/tests/Unit/Controller/Jury/TeamAffiliationControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/TeamAffiliationControllerTest.php
@@ -6,11 +6,23 @@ use App\Entity\TeamAffiliation;
 
 class TeamAffiliationControllerTest extends JuryControllerTest
 {
-    protected static $baseUrl        = '/jury/affiliations';
-    protected static $exampleEntries = ['UU','Utrecht University',1];
-    protected static $shortTag       = 'affiliation';
-    protected static $deleteEntities = ['shortname' => ['UU']];
-    protected static $getIDFunc      = 'getAffilid';
-    protected static $className      = TeamAffiliation::class;
-    protected static $DOM_elements   = ['h1' => ['Affiliations']];
+    protected static $baseUrl          = '/jury/affiliations';
+    protected static $exampleEntries   = ['UU','Utrecht University',1];
+    protected static $shortTag         = 'affiliation';
+    protected static $deleteEntities   = ['shortname' => ['UU']];
+    protected static $getIDFunc        = 'getAffilid';
+    protected static $className        = TeamAffiliation::class;
+    protected static $DOM_elements     = ['h1' => ['Affiliations']];
+    protected static $addForm          = 'team_affiliation[';
+    protected static $addEntitiesShown = ['shortname','name'];
+    protected static $addEntities      = [['shortname' => 'short',
+                                           'name' => 'New Affil',
+                                           'country' => 'NLD',
+                                           'comments' => 'Lorem ipsum dolor sit amet.'],
+                                          ['shortname' => 'cl',
+                                           'name' => 'Countryless',
+                                           'country' => ''],
+                                          ['shortname' => 'com',
+                                           'name' => 'No comment',
+                                           'comments' => '']];
 }
--- webapp/tests/Unit/Controller/Jury/UserControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/UserControllerTest.php
@@ -6,11 +6,58 @@ use App\Entity\User;
 
 class UserControllerTest extends JuryControllerTest
 {
-    protected static $baseUrl        = '/jury/users';
-    protected static $exampleEntries = ['admin','judgehost','Administrator','team'];
-    protected static $shortTag       = 'user';
-    protected static $deleteEntities = ['username' => ['demo']];
-    protected static $getIDFunc      = 'getUserid';
-    protected static $className      = User::class;
-    protected static $DOM_elements   = ['h1' => ['Users']];
+    protected static $baseUrl          = '/jury/users';
+    protected static $exampleEntries   = ['admin','judgehost','Administrator','team'];
+    protected static $shortTag         = 'user';
+    protected static $deleteEntities   = ['username' => ['demo']];
+    protected static $getIDFunc        = 'getUserid';
+    protected static $className        = User::class;
+    protected static $DOM_elements     = ['h1' => ['Users']];
+    protected static $addForm          = 'user[';
+    protected static $addEntitiesShown = ['name', 'username','email'];
+    protected static $addEntities      = [['username' => 'un',
+                                           'name' => 'Alice',
+                                           'email' => 'alice@example.org',
+                                           'plainPassword' => 'plainpassword',
+                                           'ipAddress' => '10.0.0.0',
+                                           'enabled' => '1',
+                                           'team' => '0',
+                                           'user_roles' => ['0' => '1', '1' =>'2']],
+                                          ['username' => 'ABCabc123_-@', 'name' => 'Allowed username'],
+                                          ['username' => 'nem', 'name' => 'No Email',
+                                           'email' => ''],
+                                          ['username' => 'npw', 'name' => 'No password',
+                                           'plainPassword' => ''],
+                                          ['username' => 'nip', 'name' => 'No IP',
+                                           'ipAddress' => ''],
+                                          ['username' => 'ipv6-1', 'name' => 'IPv6-1',
+                                           'ipAddress' => '2001:db8::1234:5678:5.6.7.8'],
+                                          ['username' => 'ipv6-2', 'name' => 'IPv6-2',
+                                           'ipAddress' => '2001:db8:3333:4444:5555:6666:1.2.3.4'],
+                                          ['username' => 'ipv6-3', 'name' => 'IPv6-3',
+                                           'ipAddress' => '2001:0db8:0001:0000:0000:0ab9:C0A8:0102'],
+                                          ['username' => 'ipv6-4', 'name' => 'IPv6-4',
+                                           'ipAddress' => '2001:db8:3333:4444:5555:6666:7777:8888'],
+                                          ['username' => 'disabled', 'name' => 'Disabled',
+                                           'enabled' => '0'],
+                                          ['username' => 'teamless', 'name' => 'Teamless',
+                                           'team' => ''],
+                                          ['username' => 'singlerole-0', 'name' => 'Single Role-0',
+                                           'user_roles' => ['0' => '1']],
+                                          ['username' => 'singlerole-1', 'name' => 'Single Role-1',
+                                           'user_roles' => ['1' => '2']],
+                                          ['username' => 'singlerole-2', 'name' => 'Single Role-2',
+                                           'user_roles' => ['2' => '3']],
+                                          ['username' => 'singlerole-3', 'name' => 'Single Role-3',
+                                           'user_roles' => ['3' => '4']],
+                                          ['username' => 'singlerole-4', 'name' => 'Single Role-4',
+                                           'user_roles' => ['4' => '5']],
+                                          ['username' => 'singlerole-5', 'name' => 'Single Role-5',
+                                           'user_roles' => ['5' => '6']],
+                                          ['username' => 'singlerole-6', 'name' => 'Single Role-6',
+                                           'user_roles' => ['6' => '7']],
+                                          ['username' => 'singlerole-7', 'name' => 'Single Role-7',
+                                           'user_roles' => ['7' => '8']],
+                                          ['username' => 'singlerole-8', 'name' => 'Single Role-8',
+                                           'user_roles' => ['8' => '9']]];
 }
