61305c3ece9ac9271236fd29b1c4ee7cccc614af
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-05-02 22:13:38 +0200

    Test the add action of Jury Controllers
    Not all tests are implemented, interfaces where form fields are only
    added later in the page are not implemented in this commit.
    
    Teams/User are not implemented because of https://github.com/DOMjudge/domjudge/issues/1122.

--- webapp/tests/Unit/Controller/Jury/LanguagesControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/LanguagesControllerTest.php
@@ -6,11 +6,14 @@ use App\Entity\Language;
 
 class LanguagesControllerTest extends JuryControllerTest
 {
-    protected static $baseUrl        = '/jury/languages';
-    protected static $exampleEntries = ['c','csharp','Haskell','Bash shell',"pas, p",'no','yes','R','r'];
-    protected static $shortTag       = 'language';
-    protected static $deleteEntities = ['name' => ['C++']];
-    protected static $getIDFunc      = 'getLangid';
-    protected static $className      = Language::class;
-    protected static $DOM_elements   = ['h1' => ['Languages']];
+    protected static $baseUrl          = '/jury/languages';
+    protected static $exampleEntries   = ['c','csharp','Haskell','Bash shell',"pas, p",'no','yes','R','r'];
+    protected static $shortTag         = 'language';
+    protected static $deleteEntities   = ['name' => ['C++']];
+    protected static $getIDFunc        = 'getLangid';
+    protected static $className        = Language::class;
+    protected static $DOM_elements     = ['h1' => ['Languages']];
+    protected static $addForm          = 'language[';
+    protected static $addEntitiesShown = ['langid','externalid','name','timefactor'];
+    protected static $addEntities      = [];
 }
--- webapp/tests/Unit/Controller/Jury/ProblemControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/ProblemControllerTest.php
@@ -7,16 +7,32 @@ use App\Entity\Problem;
 
 class ProblemControllerTest extends JuryControllerTest
 {
-    protected static $baseUrl        = '/jury/problems';
-    protected static $exampleEntries = ['Hello World', 'default',5,3,2,1];
-    protected static $shortTag       = 'problem';
-    protected static $deleteEntities = ['name' => ['Hello World']];
-    protected static $getIDFunc      = 'getProbid';
-    protected static $className      = Problem::class;
-    protected static $DOM_elements   = ['h1' => ['Problems'],
+    protected static $baseUrl          = '/jury/problems';
+    protected static $exampleEntries   = ['Hello World', 'default',5,3,2,1];
+    protected static $shortTag         = 'problem';
+    protected static $deleteEntities   = ['name' => ['Hello World']];
+    protected static $getIDFunc        = 'getProbid';
+    protected static $className        = Problem::class;
+    protected static $DOM_elements     = ['h1' => ['Problems'],
                                         'a.btn[title="Import problem"]' => ['admin' => [" Import problem"],'jury'=>[]]];
-    protected static $deleteExtra    = ['pageurl'   => '/jury/problems/3',
-                                        'deleteurl' => '/jury/problems/attachments/1/delete',
-                                        'selector'  => 'interactor',
-                                        'fixture'   => AddProblemAttachmentFixture::class];
+    protected static $deleteExtra      = ['pageurl'   => '/jury/problems/3',
+                                          'deleteurl' => '/jury/problems/attachments/1/delete',
+                                          'selector'  => 'interactor',
+                                          'fixture'   => AddProblemAttachmentFixture::class];
+    protected static $addForm          = 'problem[';
+    protected static $addEntitiesShown = ['name'];
+    protected static $addEntities      = [['name' => 'Problem',
+                                           'timelimit' => '1',
+                                           'memlimit' => '1073741824',
+                                           'outputlimit' => '1073741824',
+                                           'problemtextFile' => '',
+                                           'runExecutable' => 'boolfind_run',
+                                           'compareExecutable' => 'boolfind_cmp',
+                                           'specialCompareArgs' => ''],
+                                          ['name' => 'Long time',
+                                           'timelimit' => '3600'],
+                                          ['name' => 'Default limits',
+                                           'memlimit' => '', 'outputlimit' => ''],
+                                          ['name' => 'Args',
+                                           'specialCompareArgs' => 'args']];
 }
