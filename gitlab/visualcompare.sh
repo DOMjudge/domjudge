66548d9ac6abc0356842ead1b37d399b4246d1f5
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-06-02 08:58:01 +0200

    Extend the test to use all the roles
    The endpoints are split on team versus the rest to speedup the capture
    with the admin role as additional buttons could be given when one has
    either an admin/jury role but the teams pages also already have a lot of pages.
    
    We now also alert when the urls differ (renamed, added or lost) and
    upload the original pictures in the same folder to make comparing easier
    between the differences, and originals.
    
    Keep the original screenshots
    To speedup wget replaces httrack,
    Improve the readability of the logs.
    Replaced some variable names.
    Split the endpoints in seperate jobs to speedup the crawling.
    Fixed the retrieval of the PR number of branches on origin (versus
    forks).
    
    Also some minor indentation fixes, suggestions by shellcheck/bashate.

--- gitlab/visualcompare.sh
+++ gitlab/visualcompare.sh
@@ -9,81 +9,119 @@ predictedchanges="predictedchanges"
 
 mkdir -p {$failingchanges,$predictedchanges}
 
-if grep "^pr-" <<< $CI_COMMIT_BRANCH; then
-    GITHUB_PR=`cut -d '/' -f1 <<< ${CI_COMMIT_BRANCH##pr-}`
+if grep "^pr-" <<< "$CI_COMMIT_BRANCH"; then
+    GITHUB_PR=$(cut -d '/' -f1 <<< "${CI_COMMIT_BRANCH##pr-}")
 else
-    GITHUB_PR=`curl -H "Accept: application/vnd.github.groot-preview+json" https://api.github.com/repos/domjudge/domjudge/commits/$CI_COMMIT_SHA/pulls | awk '/"number": /{print $2}'`
+    URLORG="https://api.github.com/repos/domjudge/domjudge"
+    URL="commits/$CI_COMMIT_SHA/pulls"
+    GITHUB_PR=$(curl -H \
+        "Accept: 'application/vnd.github.groot-preview+json'" \
+        $URLORG/$URL | awk '/"number": /{print $2}')
     GITHUB_PR=${GITHUB_PR%,}
 fi
 
-for file in `ls screenshotspr`
-do
-    PR=screenshotspr/$file
-    MR=screenshotsmaster/$file
-    # This fails when there is a change in time between the branches
-    set +e
-    DIFF=`idiff -warn 100 -fail 0.5 $PR $MR -abs -od -scale 10.0`;RET=$?
-    set -e
-    # There is the problem of detecting if a change is wanted or unwanted, currently check all the captured
-    # screenshots against master for changes
-    if [ $RET -ne 0 ]; then
-        REMOVE=".html-ff.png"
-        if grep "^pr-" <<< $CI_COMMIT_BRANCH; then
-            ENDPOINT=${file/$REMOVE}
-            WANTED=`python3 gitlab/visualgithubprdiscussion.py $ENDPOINT $GITHUB_PR`
-            if [ $WANTED = "wanted" ]; then
-                STORDIR=$predictedchanges
-            elif [ $WANTED = "none" ]; then
-                STORDIR=$failingchanges
+DIR=$(pwd)
+ADDREMLOG="$DIR"/addrem.log
+ADD=0
+DEL=0
+cd screenshotspr
+for URL in ./*; do
+    URL=${URL#.\/}
+    SPR="/screenshotspr/$URL"
+    SMR=${SPR/pr/master}
+    cd "$DIR"/"$SPR"/
+    for ROLE in ./*; do
+        ROLE=${ROLE#.\/}
+        cd "$DIR"/"$SPR"/"$ROLE"
+        for FILE in ./*; do
+            FILE=${FILE#.\/}
+            PR="$DIR"/"$SPR"/"$ROLE"/"$FILE"
+            MR="$DIR"/"$SMR"/"$ROLE"/"$FILE"
+            if test -f "$MR"; then
+                # This fails when there is a change in time between the branches
+                set +e
+                idiff -warn 100 -fail 0.5 "$PR" "$MR" -abs -od -scale 10.0;RET=$?
+                set -e
+                # There is the problem of detecting if a change is wanted or unwanted, currently check all the captured
+                # screenshots against master for changes
+                if [ $RET -ne 0 ]; then
+                    REMOVE=".html-ff.png"
+                    ENDPOINT=${FILE/$REMOVE}
+                    WANTED=$(python3 "$DIR"/gitlab/visualgithubprdiscussion.py "$ENDPOINT" "$GITHUB_PR")
+                    if [ "$WANTED" = "wanted" ]; then
+                        STORDIR="$DIR"/"$predictedchanges"
+                    elif [ "$WANTED" = "none" ]; then
+                        STORDIR="$DIR"/"$failingchanges"
+                    fi
+                    compare "$PR" "$MR" -highlight-color blue "$STORDIR"/"${ROLE}"_"${FILE}" || true
+                    cp "$MR" "$STORDIR"/"${ROLE}"_main_"${FILE}"
+                    cp "$PR" "$STORDIR"/"${ROLE}"_pr_"${FILE}"
+                fi
+            else
+                ADD=$((ADD+1))
+                echo "Add: $FILE" >> "$ADDREMLOG"
             fi
-	else
-	    STORDIR=$predictedchanges
-	fi
-        compare $PR $MR -highlight-color blue $STORDIR/$file || true
-    fi
+        done
+        cd "$DIR"/"$SMR"/"$ROLE"
+        for FILE in ./*; do
+            FILE=${FILE#.\/}
+            PR="$DIR"/"$SPR"/"$ROLE"/"$FILE"
+            MR="$DIR"/"$SMR"/"$ROLE"/"$FILE"
+            if test ! -f "$PR"; then
+                DEL=$((DEL+1))
+                echo "Rem: $FILE" >> "$ADDREMLOG"
+            fi
+        done
+    done
 done
 
 CHANGE=0
-for dir in $failingchanges $predictedchanges
-do
-    if [ $dir == $failingchanges ]; then
+cd "$DIR"
+for DIRCHANGES in "$predictedchanges" "$failingchanges"; do
+    if [ "$DIRCHANGES" == "$failingchanges" ]; then
         STATE="failure"
     else
         STATE="success"
     fi
-    MANY=`ls $dir|wc -l`
-    FILE="browse/$dir"
-    if [ $MANY -eq 1 ]; then
-        FILE="file/$dir/"`ls $dir`
+    MANY=$(ls $DIRCHANGES|wc -l)
+    FILE="browse/$DIRCHANGES"
+    if [ "$MANY" -eq 1 ]; then
+        FILE="file/$DIRCHANGES/"$(ls $DIRCHANGES)
         CHANGE=1
-    elif [ $MANY -gt 1 ]; then
+    elif [ "$MANY" -gt 1 ]; then
         CHANGE=1
     fi
-    if [ $MANY -gt 0 ]; then
-      # Copied from CCS
-      curl https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA \
-        -X POST \
-        -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
-        -H "Accept: application/vnd.github.v3+json" \
-        -d "{\"state\": \"$STATE\", \"target_url\": \"$CI_JOB_URL/artifacts/$FILE\", \"description\":\"UI changes ($dir) \", \"context\": \"UI diffs\"}"
-      if [ $STATE == "failure" ]; then
-        cat <<EOF > $failingchanges/README
+    if [ "$MANY" -gt 0 ]; then
+        # Copied from CCS
+        curl "https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA" \
+            -X POST \
+            -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
+            -H "Accept: application/vnd.github.v3+json" \
+            -d "{\"state\": \"$STATE\", \"target_url\": \"$CI_JOB_URL/artifacts/$FILE\", \"description\":\"UI changes ($DIRCHANGES) \", \"context\": \"UI diffs\"}"
+        if [ $STATE == "failure" ]; then
+            cat <<EOF > $failingchanges/README.txt
 List the changed endpoints in a message on the PR. Regex is allowed.
 
 Changed URLs:
 changed_endpoint(as possible regex)
 another_changed_endpoint(as possible regex)
 EOF
-        exit 1
-      fi
+            exit 1
+        fi
     fi
 done
 
 if [ $CHANGE -eq 0 ]; then
-    curl https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA \
+    curl https://api.github.com/repos/domjudge/domjudge/statuses/"$CI_COMMIT_SHA" \
         -X POST \
         -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
         -H "Accept: application/vnd.github.v3+json" \
         -d "{\"state\": \"success\", \"target_url\": \"$CI_JOB_URL/artifacts/browse/\", \"description\":\"No UI changes\", \"context\": \"UI diffs\"}"
 fi
+
+curl https://api.github.com/repos/domjudge/domjudge/statuses/"$CI_COMMIT_SHA" \
+    -X POST \
+    -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -d "{\"state\": \"success\", \"target_url\": \"$CI_JOB_URL/artifacts/browse/\", \"description\":\"Removed: $DEL, Added: $ADD\", \"context\": \"seen_urls\"}"
 
--- gitlab/visualpr.sh
+++ gitlab/visualpr.sh
@@ -5,19 +5,20 @@ alias trace_on='set -x'
 alias trace_off='{ set +x; } 2>/dev/null'
 
 function section_start_internal() {
-    echo -e "section_start:`date +%s`:$1[collapsed=true]\r\e[0K$2"
+    echo -e "section_start:$(date +%s):$2[collapsed=$1]\r\e[0K$3"
     trace_on
 }
 
 function section_end_internal() {
-    echo -e "section_end:`date +%s`:$1\r\e[0K"
+    echo -e "section_end:$(date +%s):$1\r\e[0K"
     trace_on
 }
 
-alias section_start='trace_off ; section_start_internal '
+alias section_start_collap='trace_off ; section_start_internal true'
+alias section_start='trace_off ; section_start_internal false'
 alias section_end='trace_off ; section_end_internal '
 
-mkdir screenshots$1
+mkdir screenshots"$1"
 set -euxo pipefail
 
 section_start setup "Setup and install"
@@ -25,7 +26,6 @@ section_start setup "Setup and install"
 export PS4='(${BASH_SOURCE}:${LINENO}): - [$?] $ '
 
 DIR=$(pwd)
-GITSHA=$(git rev-parse HEAD || true)
 
 # Set up
 "$( dirname "${BASH_SOURCE[0]}" )"/base.sh
@@ -37,6 +37,7 @@ echo "INSERT INTO userrole (userid, roleid) VALUES (3, 2);" | mysql domjudge
 echo "machine localhost login demo password demo" > ~/.netrc
 
 LOGFILE="/opt/domjudge/domserver/webapp/var/log/prod.log"
+WGETLOGFILE="$DIR"/wget.log
 
 function log_on_err() {
     echo -e "\\n\\n=======================================================\\n"
@@ -66,25 +67,40 @@ UPDATE contest SET endtime    = UNIX_TIMESTAMP()+$TIMEHELP WHERE cid = 2;
 UPDATE team_category SET visible = 1;
 EOF
 
-ADMINPASS=$(cat etc/initial_admin_password.secret)
-# Add team to admin user
-echo "INSERT INTO userrole (userid, roleid) VALUES (1, 3);" | mysql domjudge
-echo "UPDATE user SET teamid = 1 WHERE userid = 1;" | mysql domjudge
-# Add jury to admin user
-echo "INSERT INTO userrole (userid, roleid) VALUES (1, 2);" | mysql domjudge
-# Add balloon to admin user
-echo "INSERT INTO userrole (userid, roleid) VALUES (1, 4);" | mysql domjudge
 # configure and restart php-fpm
-sudo cp /opt/domjudge/domserver/etc/domjudge-fpm.conf "/etc/php/7.4/fpm/pool.d/domjudge-fpm.conf"
+sudo cp /opt/domjudge/domserver/etc/domjudge-fpm.conf \
+    "/etc/php/7.4/fpm/pool.d/domjudge-fpm.conf"
 sudo /usr/sbin/php-fpm7.4
 
 section_end setup
 
+if [ "$ROLE" != "admin" ]; then
+    # Remove admin from admin user
+    echo "DELETE FROM userrole WHERE userid=1;" | mysql domjudge
+fi
+if [ "$ROLE" == "team" ] || [ "$URL" == "team" ]; then
+    # Add team to admin user
+    echo "INSERT INTO userrole (userid, roleid) VALUES (1, 3);" | mysql domjudge
+    echo "UPDATE user SET teamid = 1 WHERE userid = 1;" | mysql domjudge
+fi
+if [ "$ROLE" == "jury" ]; then
+    # Add jury to admin user
+    echo "INSERT INTO userrole (userid, roleid) VALUES (1, 2);" | mysql domjudge
+fi
+if [ "$ROLE" == "balloon" ]; then
+    # Add balloon to admin user
+    echo "INSERT INTO userrole (userid, roleid) VALUES (1, 4);" | mysql domjudge
+fi
+
 # Patch some visibility problems
-cp $DIR/gitlab/visualreg.css webapp/public/css/custom/
+cp "$DIR"/gitlab/visualreg.css webapp/public/css/custom/
 webapp/bin/console cache:clear
 
 ADMINPASS=$(cat etc/initial_admin_password.secret)
+if [ "$ROLE" == "none" ]; then
+    # Stop the login
+    ADMINPASS="NoLogin"
+fi
 export COOKIEJAR
 COOKIEJAR=$(mktemp --tmpdir)
 export CURLOPTS="--fail -sq -m 30 -b $COOKIEJAR"
@@ -97,35 +113,80 @@ export CURLOPTS="--fail -sq -m 30 -b $COOKIEJAR"
 # Make an initial request which will get us a session id, and grab the csrf token from it
 CSRFTOKEN=$(curl $CURLOPTS -c $COOKIEJAR "http://localhost/domjudge/login" 2>/dev/null | sed -n 's/.*_csrf_token.*value="\(.*\)".*/\1/p')
 # Make a second request with our session + csrf token to actually log in
-curl $CURLOPTS -c $COOKIEJAR -F "_csrf_token=$CSRFTOKEN" -F "_username=admin" -F "_password=$ADMINPASS" "http://localhost/domjudge/login"
+curl $CURLOPTS -c $COOKIEJAR \
+    -F "_csrf_token=$CSRFTOKEN" \
+    -F "_username=admin" \
+    -F "_password=$ADMINPASS" "http://localhost/domjudge/login"
 
-cd $DIR
+cd "$DIR"
 
-STORAGEDIR=screenshots$1
-mkdir -p $STORAGEDIR
+STORAGEDIR=$DIR/screenshots"$1"/"$URL"/"$ROLE"
+mkdir -p "$STORAGEDIR"
 
 Xvfb :0 -screen 0  1920x1080x24 &
 export DISPLAY=:0
-cp $COOKIEJAR cookies.txt
+cp "$COOKIEJAR" cookies.txt
 sed -i 's/#HttpOnly_//g' cookies.txt
 sed -i 's/\t0\t/\t1999999999\t/g' cookies.txt
-for url in public
-do
-    mkdir $url
-    cd $url
-    cp $DIR/cookies.txt ./
-    httrack http://localhost/domjudge/$url --assume html=text/html -*doc* -*/team/* -*/jury/* -*logout*
-    cd $DIR
-    mkdir /var/www/html/$url/
-    cp -r $url/localhost/domjudge/* /var/www/html/$url/
-    cp $DIR/gitlab/default-nginx /etc/nginx/sites-enabled/default
-    service nginx restart
-    for file in `find $url -type f -name "*.html"`
-    do
-        prefix="^$url\/localhost\/domjudge\/"
-        urlpath=$(sed "s/$prefix//g"<<<$file)
-        # Small risk of collision
-        storepath=$(sed "s/\//_s_/g"<<<$urlpath)
-	cutycapt --delay=2000 --min-width=1920 --min-height=1080 --url=http://localhost/$url/$urlpath --out=$STORAGEDIR/$storepath-ff.png
-    done
+mkdir -p html/"$URL"/"$ROLE"
+cd html/"$URL"/"$ROLE"
+cp "$DIR"/cookies.txt ./
+
+section_start_collap scrape "Scrape the site with the rebuild admin user"
+if [ "$URL" == "team" ]; then
+    REGEX_TOGGLE=".*\/jury.*|.*\/public.*"
+elif [ "$URL" == "public" ]; then
+    REGEX_TOGGLE=".*\/team.*"
+fi
+REGEX_CONTADD=".*\/contests\/add"
+REGEX_PHP=".*\/phpinfo.*"
+REGEX_DOC=".*\/doc\/.*"
+REGEX_LOGOUT="logout"
+set +e
+wget \
+    --reject-regex "$REGEX_TOGGLE|$REGEX_PHP|$REGEX_CONTADD|$REGEX_DOC|$REGEX_LOGOUT" \
+    --recursive \
+    --waitretry 10 \
+    --tries 20 \
+    --no-clobber \
+    --page-requisites \
+    --html-extension \
+    --convert-links \
+    --restrict-file-names=windows \
+    --domains localhost \
+    --no-parent \
+    --content-on-error \
+    --rejected-log="$WGETLOGFILE" \
+    --load-cookies cookies.txt \
+    "http://localhost/domjudge/$URL"
+RET=$?
+echo $RET
+set -e
+# ‘--content-on-error’
+#https://www.gnu.org/software/wget/manual/html_node/Exit-Status.html
+# Exit code 4 is network error which we can ignore
+# Exit code 8 is error which should be fixed, but in different PR
+if [ $RET -ne 4 ] && [ $RET -ne 8 ] && [ $RET -ne 0 ]; then
+    exit $RET
+fi
+section_end scrape
+
+cd "$DIR"
+mkdir /var/www/html/"$URL"/
+cp -r html/"$URL"/"$ROLE"/localhost/domjudge/* /var/www/html/"$URL"/
+cp "$DIR"/gitlab/default-nginx /etc/nginx/sites-enabled/default
+service nginx restart
+
+section_start_collap capture "Capture the pages in a static webserver"
+SKIPPED="grep -v '\.eot\|\.ttf\|\.woff*\|\.js@*'"
+cd html/"$URL"/"$ROLE"/localhost/domjudge
+for file in $(find ./ -type f|$SKIPPED); do
+    urlpath="${file//.\//}"
+    # Small risk of collision
+    storepath=$(sed "s/\//_s_/g"<<<"$urlpath")
+    cutycapt --delay=2000 --min-width=1920 --min-height=1080 \
+        --url="http://localhost/$URL/$urlpath" \
+        --out="$STORAGEDIR/$storepath-ff.png"
 done
+section_end capture
+
