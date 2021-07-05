2c6ee358318a89683b9724809434aca6723f3662
Nicky Gerritsen <nickygerritsen@me.com>
2021-07-05 11:43:14 +0200

    Add test for scoreboard access, validating we can remove the access check.

---
+++ webapp/src/DataFixtures/Test/ExtendDemoPracticeSessionTimeFixture.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\DataFixtures\Test;
+
+use App\Entity\Contest;
+use DateTime;
+use Doctrine\Persistence\ObjectManager;
+
+class ExtendDemoPracticeSessionTimeFixture extends AbstractTestDataFixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // Make sure the demo practice contest is still running
+        /** @var Contest $demoPracticeContest */
+        $demoPracticeContest = $manager->getRepository(Contest::class)->findOneBy(['shortname' => 'demoprac']);
+        $demoPracticeContest
+            ->setEndtimeString((new DateTime('+1 day'))->format('Y-m-d H:i:s'))
+            ->setDeactivatetimeString((new DateTime('+2 days'))->format('Y-m-d H:i:s'));
+
+        $manager->flush();
+    }
+}
---
+++ webapp/src/DataFixtures/Test/SampleEventsFixture.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace App\DataFixtures\Test;
+
+use App\Entity\Contest;
+use App\Entity\Event;
+use App\Utils\Utils;
+use Doctrine\Persistence\ObjectManager;
+
+class SampleEventsFixture extends AbstractTestDataFixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $contests = $manager->getRepository(Contest::class)->findAll();
+        foreach ($contests as $contest) {
+            $event = (new Event())
+                ->setContest($contest)
+                ->setEndpointid((string)$contest->getCid())
+                ->setEndpointtype('contest')
+                ->setAction('create')
+                ->setEventtime(Utils::now())
+                // Note: we do not care about the actual contents, as long as we have some event
+                ->setContent([]);
+
+            $manager->persist($event);
+        }
+        $manager->flush();
+    }
+}
