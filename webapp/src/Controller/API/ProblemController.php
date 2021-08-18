aae2cb748f5f0859de93ae74fc3ab14ae7813299
Thijs Kinkhorst <thijs@kinkhorst.com>
2021-08-18 19:32:58 +0000

    Fix list problems with ids for non-jury members
    (cherry picked from commit 29085c41bf3d733599698df9f35670d7635e305d)

--- webapp/src/Controller/API/ProblemController.php
+++ webapp/src/Controller/API/ProblemController.php
@@ -100,9 +100,11 @@ class ProblemController extends AbstractRestController implements QueryObjectTra
 
             $objects = [];
             foreach ($ordinalArray->getItems() as $item) {
-                /** @var ContestProblemWrapper $contestProblemWrapper */
-                $contestProblemWrapper = $item->getItem();
-                $contestProblem        = $contestProblemWrapper->getContestProblem();
+                /** @var ContestProblemWrapper|ContestProblem $contestProblem */
+                $contestProblem = $item->getItem();
+                if ($contestProblem instanceof ContestProblemWrapper) {
+                    $contestProblem = $contestProblem->getContestProblem();
+                }
                 $probid                = $this->getIdField() === 'p.probid' ? $contestProblem->getProbid() : $contestProblem->getExternalId();
                 if (in_array($probid, $ids)) {
                     $objects[] = $item;
