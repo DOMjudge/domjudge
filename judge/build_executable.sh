72f1743907f041978c60cc84a149b382e967a94a
Thijs Kinkhorst <thijs@kinkhorst.com>
2021-08-22 18:12:43 +0000

    Attempt to fix build

--- judge/build_executable.sh
+++ judge/build_executable.sh
@@ -36,7 +36,7 @@ cleanexit ()
 
 # Error and logging functions
 # shellcheck disable=SC1090
-. "$DJ_LIBDIR/lib.error.sh"
+. "$DJ_LIBJUDGEDIR/lib.error.sh"
 
 LOGLEVEL=$LOG_DEBUG
 PROGNAME="$(basename "$0")"
