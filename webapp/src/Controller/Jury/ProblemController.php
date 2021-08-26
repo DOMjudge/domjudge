7d61bba9b8e9a668988a95aa4a7b9cb2efeec900
Tobias Werth <werth@cs.fau.de>
2021-08-26 10:34:37 +0200

    Remove unused imports.

--- webapp/src/Controller/Jury/ProblemController.php
+++ webapp/src/Controller/Jury/ProblemController.php
@@ -32,7 +32,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 use Symfony\Component\HttpKernel\KernelInterface;
