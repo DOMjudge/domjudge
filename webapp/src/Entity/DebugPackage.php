5629cad680d8812caa81fb93d283a3d26409946c
Tobias Werth <werth@cs.fau.de>
2021-09-01 21:47:51 +0200

    Add new entity for the debug package.
    (cherry picked from commit e11e67e104f89ad4cf6166644e928c3a7fcf1e49)

---
+++ webapp/src/Entity/DebugPackage.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * A debug package from a specific judgehost/judging combination.
+ *
+ * @ORM\Entity()
+ * @ORM\Table(
+ *     name="debug_package",
+ *     indexes={
+ *         @ORM\Index(name="judgingid", columns={"judgingid"}),
+ *     },
+ *     options={"collate"="utf8mb4_unicode_ci", "charset"="utf8mb4", "comment"="Debug packages."}
+ *     )
+ */
+class DebugPackage
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer", name="debug_package_id", length=4,
+     *     options={"comment"="Debug Package ID","unsigned"=true},
+     *     nullable=false)
+     */
+    private $debug_package_id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Judging", inversedBy="debug_packages")
+     * @ORM\JoinColumn(name="judgingid", referencedColumnName="judgingid", onDelete="CASCADE")
+     */
+    private $judging;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", name="filename", length=255,
+     *     options={"comment"="Name of the file where we stored the debug package."},
+     *     nullable=false)
+     */
+    private $filename;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Judgehost")
+     * @ORM\JoinColumn(name="judgehostid", referencedColumnName="judgehostid")
+     */
+    private $judgehost;
+
+    public function getDebugPackageId(): int
+    {
+        return $this->debug_package_id;
+    }
+
+    public function getJudging(): Judging
+    {
+        return $this->judging;
+    }
+
+    public function setJudging(Judging $judging): DebugPackage
+    {
+        $this->judging = $judging;
+        return $this;
+    }
+
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): DebugPackage
+    {
+        $this->filename = $filename;
+        return $this;
+    }
+
+    public function getJudgehost(): Judgehost
+    {
+        return $this->judgehost;
+    }
+
+    public function setJudgehost(Judgehost $judgehost): DebugPackage
+    {
+        $this->judgehost = $judgehost;
+        return $this;
+    }
+}
---
+++ webapp/src/Migrations/Version20210829113200.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210829113200 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'adding debug packages';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE debug_package (debug_package_id INT UNSIGNED AUTO_INCREMENT NOT NULL COMMENT \'Debug Package ID\', judgingid INT UNSIGNED DEFAULT NULL COMMENT \'Judging ID\', judgehostid INT UNSIGNED DEFAULT NULL COMMENT \'Judgehost ID\', filename VARCHAR(255) NOT NULL COMMENT \'Name of the file where we stored the debug package.\', INDEX IDX_9E17399BE0E4FC3E (judgehostid), INDEX judgingid (judgingid), PRIMARY KEY(debug_package_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Debug packages.\' ');
+        $this->addSql('ALTER TABLE debug_package ADD CONSTRAINT FK_9E17399B5D5FEA72 FOREIGN KEY (judgingid) REFERENCES judging (judgingid) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE debug_package ADD CONSTRAINT FK_9E17399BE0E4FC3E FOREIGN KEY (judgehostid) REFERENCES judgehost (judgehostid)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE debug_package');
+    }
+}
