299c6b14eb34f291ff682034cf4eefb2a5334313
Tobias Werth <werth@cs.fau.de>
2021-09-09 19:38:01 +0200

    More fixes for the wf branch / data_source = external.

--- webapp/tests/Unit/Controller/API/ContestControllerTest.php
+++ webapp/tests/Unit/Controller/API/ContestControllerTest.php
@@ -7,20 +7,20 @@ class ContestControllerTest extends BaseTest
     protected $apiEndpoint = 'contests';
 
     protected $expectedObjects = [
-        '2' => [
+        'demo' => [
             'formal_name'                => 'Demo contest',
             'penalty_time'               => 20,
             'start_time'                 => '2020-01-01T11:00:00+00:00',
             'end_time'                   => '2023-01-01T16:00:00+00:00',
             'duration'                   => '26309:00:00.000',
             'scoreboard_freeze_duration' => '1:00:00.000',
-            'id'                         => '2',
+            'id'                         => 'demo',
             'external_id'                => 'demo',
             'name'                       => 'Demo contest',
             'shortname'                  => 'demo',
             'banner'           => [
                 [
-                    'href'   => 'contests/2/banner.png',
+                    'href'   => 'contests/demo/banner.png',
                     'mime'   => 'image/png',
                     'width'  => 181,
                     'height' => 101
