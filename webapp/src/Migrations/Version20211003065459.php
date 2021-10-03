f877ff3560260627579b51810f48df97432049f9
Tobias Werth <werth@cs.fau.de>
2021-10-03 09:44:30 +0200

    Add missing file.

---
+++ webapp/src/Migrations/Version20211003065459.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211003065459 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE contest ADD b2 SMALLINT UNSIGNED DEFAULT 0 COMMENT \'Last team receiving high honors\', ADD b3 SMALLINT UNSIGNED DEFAULT 0 COMMENT \'Last team receiving honors\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE contest DROP b2, DROP b3');
+    }
+}
