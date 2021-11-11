843fdd7191c92d948075b9eb066c60a7496d8dd4
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-11-11 23:11:33 +0100

    Simplify GHA by removing duplication
    Alternative is composite action:
    https://docs.github.com/en/actions/creating-actions/creating-a-composite-action
    
    Also scrape the API in script to reuse for other (future) tests

---
+++ .github/workflowscripts/baseinstall.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -eux
+
+sudo apt update
+sudo apt install -y acl zip unzip nginx php php-fpm php-gd \
+                    php-cli php-intl php-mbstring php-mysql php-curl php-json \
+                    php-xml php-zip composer ntp make sudo debootstrap \
+                    libcgroup-dev lsof php-cli php-curl php-json php-xml \
+                    php-zip procps gcc g++ default-jre-headless \
+                    default-jdk-headless ghc fp-compiler autoconf automake bats \
+                    python3-sphinx python3-sphinx-rtd-theme rst2pdf fontconfig \
+                    python3-yaml latexmk
+
+composer install --no-scripts
+composer run-script package-versions-dump
+
+DIR=$(pwd)
+make configure
+./configure --with-baseurl='https://localhost/domjudge/' --enable-doc-build=no --prefix=$HOME/domjudge
+
+make domserver
+sudo make install-domserver
+
+sudo /etc/init.d/mysql start
+
+$HOME/domjudge/domserver/bin/dj_setup_database -uroot -proot install
+
+sudo cp $HOME/domjudge/domserver/etc/domjudge-fpm.conf /etc/php/7.4/fpm/pool.d/domjudge.conf
+sudo systemctl restart php7.4-fpm
+sudo systemctl status php7.4-fpm
+
+sudo rm -f /etc/nginx/sites-enabled/*
+sudo cp $HOME/domjudge/domserver/etc/nginx-conf /etc/nginx/sites-enabled/domjudge
+
+openssl req -nodes -new -x509 -keyout /tmp/server.key -out /tmp/server.crt -subj "/C=NL/ST=Noord-Holland/L=Amsterdam/O=TestingForPR/CN=localhost"
+cat $(pwd)/.github/workflowscripts/nginx_extra | sudo tee -a /etc/nginx/sites-enabled/domjudge
+
+sudo systemctl restart nginx
