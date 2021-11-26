0bcaabe75825cd894fe8b0e35edb7cd063b2f72f
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-11-26 17:43:13 +0100

    Revert "Revert "Update docker_visualpr.sh""
    This reverts commit 611ba7f3ef77122f1ea0772d7510e62888a7d77d.

--- gitlab/docker_visualpr.sh
+++ gitlab/docker_visualpr.sh
@@ -116,6 +116,8 @@ export CURLOPTS="--fail -sq -m 30 -b $COOKIEJAR"
 
 cat /etc/nginx/sites-enabled/*
 
+service php7.4-fpm status
+
 exit 0
 # Make an initial request which will get us a session id, and grab the csrf token from it
 CSRFTOKEN=$(curl $CURLOPTS -c $COOKIEJAR "http://localhost/domjudge/login" 2>/dev/null | sed -n 's/.*_csrf_token.*value="\(.*\)".*/\1/p')
