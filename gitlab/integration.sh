4d96877666bd48c6fb587d46b04ed959a9a96bb3
Tobias Werth <werth@cs.fau.de>
2021-12-01 18:53:57 +0100

    Check for CRITICAL and ERROR in symfony log.
    This should inform us how much work needs to be done for #1379.

--- gitlab/integration.sh
+++ gitlab/integration.sh
@@ -247,6 +247,8 @@ curl $CURLOPTS -X POST -d 'contest=2&donow[freeze]=freeze now' http://localhost/
 curl $CURLOPTS -X POST -d 'contest=2&donow[end]=end now' http://localhost/domjudge/jury/contests || true
 curl $CURLOPTS -X POST -d 'finalize_contest[b]=0&finalize_contest[finalizecomment]=gitlab&finalize_contest[finalize]=' http://localhost/domjudge/jury/contests/2/finalize
 
+cat /opt/domjudge/domserver/webapp/var/log/prod.log | egrep '(CRITICAL|ERROR):' || true
+
 # Check the Contest API:
 $CHECK_API -n -C -e -a 'strict=1' http://admin:$ADMINPASS@localhost/domjudge/api
 section_end api_check |& tee "$gitlabartifacts/check_api.log"
