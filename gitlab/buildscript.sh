998a617a9070d543c6d879756ee650c5bb353f0c
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-12-03 21:37:59 +0100

    Use own repo

--- gitlab/buildscript.sh
+++ gitlab/buildscript.sh
@@ -11,28 +11,28 @@ docker run -d --net=sqlnetwork --name mariadb -e MYSQL_ROOT_PASSWORD=${MYSQL_ROO
 docker run -d --net=sqlnetwork --name mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} mysql:latest --default-authentication-plugin=mysql_native_password
 # Do the actual building of the domserver
 cd ..
-docker build -t $CI_REGISTRY/domjudge/domjudge/domserver:$CI_COMMIT_SHA --build-arg DJ_ETC=/opt/domjudge/domserver/etc --build-arg DJ_SRC=/tmp/domjudge -f domjudge/gitlab/buildimage/Dockerfile .
+docker build -t $CI_REGISTRY/mvasseur/domjudge/domserver:$CI_COMMIT_SHA --build-arg DJ_ETC=/opt/domjudge/domserver/etc --build-arg DJ_SRC=/tmp/domjudge -f domjudge/gitlab/buildimage/Dockerfile .
 # Install SQL databases
-docker run --net=sqlnetwork --name domserver -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} $CI_REGISTRY/domjudge/domjudge/domserver:$CI_COMMIT_SHA /scripts/configure_sql.sh
+docker run --net=sqlnetwork --name domserver -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} $CI_REGISTRY/mvasseur/domjudge/domserver:$CI_COMMIT_SHA /scripts/configure_sql.sh
 # Store the images for the next jobs
 docker ps -a
-docker run --net=sqlnetwork $CI_REGISTRY/domjudge/domjudge/domserver:$CI_COMMIT_SHA /scripts/dbs.sh
+docker run --net=sqlnetwork $CI_REGISTRY/mvasseur/domjudge/domserver:$CI_COMMIT_SHA /scripts/dbs.sh
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 # The databases have the correct value and can be stored
 for SQL in mariadb mysql; do
-    docker commit "$SQL" "$CI_REGISTRY/domjudge/domjudge/$SQL:$CI_COMMIT_SHA"
+    docker commit "$SQL" "$CI_REGISTRY/mvasseur/domjudge/$SQL:$CI_COMMIT_SHA"
 done
 for CONTAINER in mariadb mysql; do
-    URL=$CI_REGISTRY/domjudge/domjudge/$CONTAINER:$CI_COMMIT_SHA
+    URL=$CI_REGISTRY/mvasseur/domjudge/$CONTAINER:$CI_COMMIT_SHA
     echo $URL
     docker tag $URL $URL
     docker push $URL
 done
-docker run -it --net=sqlnetwork --name mariadbcommited -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} $CI_REGISTRY/domjudge/domjudge/$CONTAINER:$CI_COMMIT_SHA
-docker run --net=sqlnetwork $CI_REGISTRY/domjudge/domjudge/domserver:$CI_COMMIT_SHA /scripts/dbs2.sh
+docker run -it --net=sqlnetwork --name mariadbcommited -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} $CI_REGISTRY/mvasseur/domjudge/$CONTAINER:$CI_COMMIT_SHA
+docker run --net=sqlnetwork $CI_REGISTRY/mvasseur/domjudge/domserver:$CI_COMMIT_SHA /scripts/dbs2.sh
 #- >
 #  for CONTAINER in mariadb mysql domserver; do
-#    URL=$CI_REGISTRY/domjudge/domjudge/$CONTAINER:$CI_COMMIT_SHA
+#    URL=$CI_REGISTRY/mvasseur/domjudge/$CONTAINER:$CI_COMMIT_SHA
 #    echo $URL
 #    docker push $URL
 #  done
