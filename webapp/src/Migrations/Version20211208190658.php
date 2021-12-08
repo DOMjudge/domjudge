0711b9e116687dd1aeeef8162dc8d472aefd7b7c
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-12-08 20:10:16 +0100

    Missing migration for auditlog fix

---
+++ webapp/src/Migrations/Version20211208190658.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211208190658 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Allow larger auditlog entries';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE auditlog CHANGE action action VARCHAR(128) DEFAULT NULL COMMENT \'Description of action performed\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE auditlog CHANGE action action VARCHAR(64) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'Description of action performed\'');
+    }
+}
