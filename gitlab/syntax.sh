c3fae86ca7b4d4eec65599533592b5ac85c07876
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2022-01-05 20:43:58 +0100

    Extra document the syntax check
    This is not the problematic script, its only to demonstrate teh
    possibilities.

--- gitlab/syntax.sh
+++ gitlab/syntax.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-tests/syntax
+. gitlab/dind.profile
 
+section_start syntax_check "Check unbuild files"
+tests/syntax
+section_end syntax_check
+
+section_start configure "Build the docs"
 make configure
 ./configure --with-baseurl='http://localhost/domjudge/'
 make install-docs
 make clean
+section_end configure
 
+section_start sphinx "Sphinx build"
 cd doc/manual/
 make version.py
 ./gen_conf_ref.py
 sphinx-build -b html . build
+section_end sphinx
