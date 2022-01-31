db5ebe27eaaa9ae8c595788e78f2d9b898116f6a
Nicky Gerritsen <nickygerritsen@me.com>
2022-01-31 19:18:09 +0100

    Do not overwrite ZIP file when import problem.

--- webapp/src/Service/ImportProblemService.php
+++ webapp/src/Service/ImportProblemService.php
@@ -360,7 +360,7 @@ class ImportProblemService
                             }
                             file_put_contents($tempzipFile, $outputValidatorZip);
                             $zipArchive = new ZipArchive();
-                            $zipArchive->open($tempzipFile, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+                            $zipArchive->open($tempzipFile, ZipArchive::CREATE);
 
                             $executable         = new Executable();
                             $executable
