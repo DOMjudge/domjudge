c82f01ee807b1b5dbd203bf0a1fb74b85c578373
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2022-02-05 12:39:24 +0100

    Ignore specific files with no doctype
    One is for upstream code the other for a feature which might get removed.

--- gitlab/webstandard.sh
+++ gitlab/webstandard.sh
@@ -77,7 +77,12 @@ if [ "$TEST" = "w3cval" ]; then
     rm -rf localhost/domjudge/bundles/nelmioapidoc*
     rm -f localhost/domjudge/css/bootstrap.min.css*
     rm -f localhost/domjudge/css/select2-bootstrap.min.css*
+    rm -f localhost/domjudge/jury/config/check/phpinfo*
     section_end upstream_problems
+
+    section_start_collap upstream_icpc "Remove icpc only features"
+    rm -rf localhost/domjudge/jury/import-export/export/results-icpc.html*
+    section_end upstream_icpc
 
     section_start_collap test_suite "Install testsuite"
     cd $DIR
