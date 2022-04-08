5787e8c8778f5805b25d951aa40aa2c5c1227f64
Nicky Gerritsen <nickygerritsen@me.com>
2022-04-08 09:12:23 +0200

    Fix typo in team external ID column.

---
+++ webapp/migrations/Version20220408071147.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220408071147 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Fix comment for team external ID.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE team CHANGE externalid externalid VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'Team ID in an external system\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE team CHANGE externalid externalid VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'Team affiliation ID in an external system\'');
+    }
+}
