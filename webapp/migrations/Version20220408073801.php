db8ea2365905aacd73a0b80624d19842fcf5b6dd
Nicky Gerritsen <nickygerritsen@me.com>
2022-04-09 13:51:56 +0200

    Fix migration: add externalid in correct spot and set initial external ID's.

--- webapp/migrations/Version20220408073801.php
+++ webapp/migrations/Version20220408073801.php
@@ -20,8 +20,9 @@ final class Version20220408073801 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD externalid VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'User ID in an external system\'');
+        $this->addSql('ALTER TABLE user ADD externalid VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_bin` COMMENT \'User ID in an external system\' AFTER `userid`');
         $this->addSql('CREATE UNIQUE INDEX externalid ON user (externalid(190))');
+        $this->addSql('UPDATE user SET externalid = username');
     }
 
     public function down(Schema $schema): void
