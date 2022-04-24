4b605bb58cbf0c580f7f2c159633948f8462af23
Tobias Werth <werth@cs.fau.de>
2022-04-24 15:38:13 +0200

    Cleanup CI scripts and configurations

--- gitlab/runpipe-tests.sh
+++ gitlab/runpipe-tests.sh
@@ -2,22 +2,12 @@
 
 . gitlab/ci_settings.sh
 
-set -euxo pipefail
-
-gitlabartifacts="$(pwd)/gitlabartifacts"
-mkdir -p "$gitlabartifacts"
-
-DIR=$(pwd)
-lsb_release -a
-
-GITSHA=$(git rev-parse HEAD || true)
-
 section_start compile "Compile runpipe"
 # Configure and make the runpipe binaries.
 make configure
-./configure --with-baseurl='http://localhost/domjudge/' --with-domjudge-user=domjudge --with-judgehost_chrootdir=${DIR}/chroot/domjudge |& tee "$gitlabartifacts/configure.log"
-make judgehost |& tee "$gitlabartifacts/make.log"
+./configure --with-baseurl='http://localhost/domjudge/' --with-domjudge-user=domjudge --with-judgehost_chrootdir=${DIR}/chroot/domjudge |& tee "$GITLABARTIFACTS/configure.log"
+make judgehost |& tee "$GITLABARTIFACTS/make.log"
 section_end compile
 
 cd judge/test
-make test |& tee "$gitlabartifacts/test.log"
+make test |& tee "$GITLABARTIFACTS/test.log"
