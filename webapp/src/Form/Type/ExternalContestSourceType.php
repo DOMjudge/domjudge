81bd2296fe05f820988e50008b827272fc012a2b
Nicky Gerritsen <nickygerritsen@me.com>
2022-04-30 14:11:46 +0200

    Hide password for external contest source by default.

--- webapp/src/Form/Type/ExternalContestSourceType.php
+++ webapp/src/Form/Type/ExternalContestSourceType.php
@@ -7,6 +7,7 @@ use App\Entity\ExternalContestSource;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -46,9 +47,7 @@ class ExternalContestSourceType extends AbstractType
         $builder->add('username', TextType::class, [
             'required' => false,
         ]);
-        $builder->add('password', TextType::class, [
-            'required' => false,
-        ]);
+        $builder->add('password', TextType::class);
         $builder->add('save', SubmitType::class);
     }
 
