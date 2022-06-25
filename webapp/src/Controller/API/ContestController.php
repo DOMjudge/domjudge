33834b69dd6cb92bea27d03b15549dcaacb4723a
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-06-25 14:22:24 +0200

    When the URL is malformed the server returns 400 instead of 404
    In the future we should just append these to all the routes.

--- webapp/src/Controller/API/ContestController.php
+++ webapp/src/Controller/API/ContestController.php
@@ -38,6 +38,7 @@ use Symfony\Component\Yaml\Yaml;
 /**
  * @Rest\Route("/contests")
  * @OA\Tag(name="Contests")
+ * @OA\Response(response="400", ref="#/components/responses/InvalidResponse")
  * @OA\Response(response="404", ref="#/components/responses/NotFound")
  * @OA\Response(response="401", ref="#/components/responses/Unauthorized")
  */
