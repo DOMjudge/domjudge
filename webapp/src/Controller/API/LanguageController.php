e1f11ce0eab1984e83912407d188efe3868ba6e6
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-06-26 11:38:31 +0200

    Fix sorting of HTTP codes in annotation

--- webapp/src/Controller/API/LanguageController.php
+++ webapp/src/Controller/API/LanguageController.php
@@ -15,9 +15,9 @@ use Symfony\Component\HttpFoundation\Response;
  * @Rest\Route("/contests/{cid}/languages")
  * @OA\Tag(name="Languages")
  * @OA\Parameter(ref="#/components/parameters/cid")
- * @OA\Response(response="404", ref="#/components/responses/NotFound")
  * @OA\Response(response="400", ref="#/components/responses/InvalidResponse")
  * @OA\Response(response="401", ref="#/components/responses/Unauthenticated")
+ * @OA\Response(response="404", ref="#/components/responses/NotFound")
  */
 class LanguageController extends AbstractRestController
 {
