aecb965576b31415ba7072f3dbc42ef8ed609065
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-07-08 08:23:10 +0200

    Break down the list of ids for 2nd posting

--- webapp/src/Controller/BaseController.php
+++ webapp/src/Controller/BaseController.php
@@ -415,6 +415,7 @@ abstract class BaseController extends AbstractController
             'showModalSubmit' => !$isError,
             'modalUrl' => $request->getRequestUri(),
             'redirectUrl' => $redirectUrl,
+            'primaryKeyItems' => $primaryKeyData,
         ];
         if ($request->isXmlHttpRequest()) {
             return $this->render('jury/delete_modal.html.twig', $data);
