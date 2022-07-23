df5132e272b58bf0f6d1a76642dbe7a66cff2870
Nicky Gerritsen <nickygerritsen@me.com>
2022-07-23 17:09:30 +0200

    Add button to download static scoreboard ZIP. Fixes #1595.

--- webapp/src/Controller/PublicController.php
+++ webapp/src/Controller/PublicController.php
@@ -5,20 +5,26 @@ namespace App\Controller;
 use App\Entity\Contest;
 use App\Entity\ContestProblem;
 use App\Entity\Team;
+use App\Helpers\RequestBasePathModifier;
 use App\Service\ConfigurationService;
 use App\Service\DOMJudgeService;
 use App\Service\ScoreboardService;
 use App\Service\StatisticsService;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\NonUniqueResultException;
+use ReflectionClass;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment;
+use ZipArchive;
 
 /**
  * Class PublicController
@@ -64,37 +70,10 @@ class PublicController extends BaseController
             $refreshParams = [
                 'static' => 1,
             ];
-            // For static scoreboards, allow to pass a contest= param.
-            if ($contestId = $request->query->get('contest')) {
-                if ($contestId === 'auto') {
-                    // Automatically detect the contest that is activated the latest.
-                    $contest      = null;
-                    $activateTime = null;
-                    foreach ($this->dj->getCurrentContests(-1) as $possibleContest) {
-                        if (!$possibleContest->getPublic() || !$possibleContest->getEnabled()) {
-                            continue;
-                        }
-                        if ($activateTime === null || $activateTime < $possibleContest->getActivatetime()) {
-                            $activateTime = $possibleContest->getActivatetime();
-                            $contest      = $possibleContest;
-                        }
-                    }
-                } else {
-                    // Find the contest with the given ID.
-                    $contest = null;
-                    foreach ($this->dj->getCurrentContests(-1) as $possibleContest) {
-                        if ($possibleContest->getCid() === $contestId || $possibleContest->getExternalid() === $contestId) {
-                            $contest = $possibleContest;
-                            break;
-                        }
-                    }
 
-                    if ($contest) {
-                        $refreshParams['contest'] = $contest->getCid();
-                    } else {
-                        throw new NotFoundHttpException('Specified contest not found');
-                    }
-                }
+            if ($requestedContest = $this->getContestFromRequest($request)) {
+                $contest                  = $requestedContest;
+                $refreshParams['contest'] = $contest->getCid();
             }
 
             $refreshUrl = sprintf('?%s', http_build_query($refreshParams));
@@ -114,6 +93,114 @@ class PublicController extends BaseController
             return $this->render('partials/scoreboard.html.twig', $data, $response);
         }
         return $this->render('public/scoreboard.html.twig', $data, $response);
+    }
+
+    /**
+     * @Route("/scoreboard-data.zip", name="public_scoreboard_data_zip")
+     */
+    public function scoreboardDataZipAction(RequestStack $requestStack, string $projectDir, Request $request): Response
+    {
+        $contest = $this->getContestFromRequest($request) ?? $this->dj->getCurrentContest(-1);
+        $data    = $this->scoreboardService->getScoreboardTwigData(
+                $request, null, '', false, true, true, $contest
+            ) + ['hide_menu' => true, 'current_contest' => $contest];
+
+        $request = $requestStack->pop();
+        // Use reflection to change the basepath property of the request, so we can detect
+        // all requested and assets
+        $requestReflection = new ReflectionClass($request);
+        $basePathProperty  = $requestReflection->getProperty('basePath');
+        $basePathProperty->setValue($request, '/CHANGE_ME');
+        $requestStack->push($request);
+
+        $contestPage = $this->renderView('public/scoreboard.html.twig', $data);
+
+        // Now get all assets that are used
+        $assetRegex = '|/CHANGE_ME/([/a-z0-9_\-\.]*)(\??[/a-z0-9_\-\.=]*)|i';
+        preg_match_all($assetRegex, $contestPage, $assetMatches);
+        $contestPage = preg_replace($assetRegex, '$1$2', $contestPage);
+
+        $zip = new ZipArchive();
+        if (!($tempFilename = tempnam($this->dj->getDomjudgeTmpDir(), "contest-"))) {
+            throw new ServiceUnavailableHttpException(null, 'Could not create temporary file.');
+        }
+
+        $res = $zip->open($tempFilename, ZipArchive::OVERWRITE);
+        if ($res !== true) {
+            throw new ServiceUnavailableHttpException(null, 'Could not create temporary zip file.');
+        }
+        $zip->addFromString('index.html', $contestPage);
+
+        $publicPath = realpath(sprintf('%s/public/', $projectDir));
+        foreach ($assetMatches[1] as $file) {
+            $zip->addFile($publicPath . '/' . $file, $file);
+        }
+
+        // Also copy in the webfonts
+        $webfontsPath = $publicPath . '/webfonts/';
+        foreach (glob($webfontsPath . '*') as $fontFile) {
+            $fontName = basename($fontFile);
+            $zip->addFile($fontFile, 'webfonts/' . $fontName);
+        }
+
+        $zip->close();
+
+        $zipFilename = 'contest.zip';
+
+        $response = new StreamedResponse();
+        $response->setCallback(function () use ($tempFilename) {
+            $fp = fopen($tempFilename, 'rb');
+            fpassthru($fp);
+            unlink($tempFilename);
+        });
+        $response->headers->set('Content-Type', 'application/zip');
+        $response->headers->set('Content-Disposition', 'attachment; filename="' . $zipFilename . '"');
+        $response->headers->set('Content-Length', filesize($tempFilename));
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Connection', 'Keep-Alive');
+        $response->headers->set('Accept-Ranges', 'bytes');
+
+        return $response;
+    }
+
+    /**
+     * Get the contest from the request, if any
+     */
+    protected function getContestFromRequest(Request $request): ?Contest
+    {
+        $contest = null;
+        // For static scoreboards, allow to pass a contest= param.
+        if ($contestId = $request->query->get('contest')) {
+            if ($contestId === 'auto') {
+                // Automatically detect the contest that is activated the latest.
+                $contest      = null;
+                $activateTime = null;
+                foreach ($this->dj->getCurrentContests(-1) as $possibleContest) {
+                    if (!$possibleContest->getPublic() || !$possibleContest->getEnabled()) {
+                        continue;
+                    }
+                    if ($activateTime === null || $activateTime < $possibleContest->getActivatetime()) {
+                        $activateTime = $possibleContest->getActivatetime();
+                        $contest      = $possibleContest;
+                    }
+                }
+            } else {
+                // Find the contest with the given ID.
+                $contest = null;
+                foreach ($this->dj->getCurrentContests(-1) as $possibleContest) {
+                    if ($possibleContest->getCid() == $contestId || $possibleContest->getExternalid() == $contestId) {
+                        $contest = $possibleContest;
+                        break;
+                    }
+                }
+
+                if (!$contest) {
+                    throw new NotFoundHttpException('Specified contest not found');
+                }
+            }
+        }
+
+        return $contest;
     }
 
     /**
