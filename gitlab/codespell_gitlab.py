c7db7c5a88590a5e40ff706ee9090fc5015ebd64
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-08-18 22:10:56 +0200

    Find first usage of codespell in github action for gitlab CI step

--- gitlab/codespell_gitlab.py
+++ gitlab/codespell_gitlab.py
@@ -9,8 +9,13 @@ with open(".github/workflows/codespell.yml", "r") as stream:
   try:
     settings = yaml.safe_load(stream)
     for githubkey,codespellkey in github_to_codespell.items():
-      value = settings['jobs']['codespell']['steps'][2]['with'][githubkey]
-      command += f" -{codespellkey} {value}"
+      for github_step in settings['jobs']['codespell']['steps']:
+        try:
+          value = github_step['with'][githubkey]
+          command += f" -{codespellkey} {value}"
+          break
+        except KeyError:
+          continue
   except yaml.YAMLError as exc:
     print(exc)
 with open("gitlab/codespellignorefiles.txt", "r") as f:
