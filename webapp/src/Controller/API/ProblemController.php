89ca4ec26cc6422c11b2f173d4ed98cc4f394190
root <root@gamification.id>
2022-08-18 07:17:28 +0700

    Update v1.3 Commit

--- webapp/src/Controller/API/ProblemController.php
+++ webapp/src/Controller/API/ProblemController.php
@@ -181,11 +181,6 @@ class ProblemController extends AbstractRestController implements QueryObjectTra
      *                 property="problem",
      *                 description="Optional: problem id to update.",
      *                 type="string"
-     *             ),
-     *             @OA\Property(
-     *                 property="delete_old_data",
-     *                 description="Optional: whether to delete old (existing) data before importing into an existing problem.",
-     *                 type="boolean"
      *             )
      *         )
      *     )
@@ -376,6 +371,36 @@ class ProblemController extends AbstractRestController implements QueryObjectTra
         return $this->renderData($request, $object);
     }
 
+    /**
+     * GetPDFProblem
+     * @throws NonUniqueResultException
+     * @Rest\Get("/{id}/pdf")
+     * @OA\Response(
+     *     response="200",
+     *     description="Returns the given problem for this contest",
+     *     @OA\JsonContent(ref="#/components/schemas/ContestProblem")
+     * )
+     * @OA\Parameter(ref="#/components/parameters/id")
+     * @OA\Parameter(ref="#/components/parameters/strict")
+     */
+    public function getPDFProblem(int $probId): StreamedResponse
+    {
+        return $this->getBinaryFile($probId, function (
+            int $probId,
+            Contest $contest,
+            ContestProblem $contestProblem
+        ) {
+            $problem = $contestProblem->getProblem();
+
+            try {
+                return $problem->getProblemTextStreamedResponse();
+            } catch (BadRequestHttpException $e) {
+                $this->addFlash('danger', $e->getMessage());
+                return $this->redirectToRoute('public_problems');
+            }
+        });
+    }
+
     protected function getQueryBuilder(Request $request): QueryBuilder
     {
         $contestId = $this->getContestId($request);
@@ -421,35 +446,5 @@ class ProblemController extends AbstractRestController implements QueryObjectTra
         } else {
             return $problem;
         }
-    }
-
-    /**
-     * GetPDFProblem
-     * @throws NonUniqueResultException
-     * @Rest\Get("/{id}/pdf")
-     * @OA\Response(
-     *     response="200",
-     *     description="Returns the given problem for this contest",
-     *     @OA\JsonContent(ref="#/components/schemas/ContestProblem")
-     * )
-     * @OA\Parameter(ref="#/components/parameters/id")
-     * @OA\Parameter(ref="#/components/parameters/strict")
-     */
-    public function getPDFProblem(int $probId): StreamedResponse
-    {
-        return $this->getBinaryFile($probId, function (
-            int $probId,
-            Contest $contest,
-            ContestProblem $contestProblem
-        ) {
-            $problem = $contestProblem->getProblem();
-
-            try {
-                return $problem->getProblemTextStreamedResponse();
-            } catch (BadRequestHttpException $e) {
-                $this->addFlash('danger', $e->getMessage());
-                return $this->redirectToRoute('public_problems');
-            }
-        });
     }
 }
