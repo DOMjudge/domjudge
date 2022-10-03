db019c83c7dd17c2551f86c5010d891dea022818
Seokju Hong <seokju@vessl.ai>
2022-10-03 21:09:12 +0900

    fix: handle transient network errors with exponential backoff
    Signed-off-by: Seokju Hong <seokju@vessl.ai>

--- lib/lib.wrappers.php
+++ lib/lib.wrappers.php
@@ -15,6 +15,15 @@ function dj_json_decode(string $str)
 }
 
 /**
+ * Try decode a JSON string with our preferred settings.
+ * Does not throw error, but errors can be obtained via json_last_error().
+ */
+function dj_json_try_decode(string $str)
+{
+    return json_decode($str, true, 512);
+}
+
+/**
  * Encode data to JSON with our preferred settings.
  */
 function dj_json_encode($data) : string
