43e1595e4cabd64d60143165494b556177f56059
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-10-14 00:08:01 +0200

    Realign cases for readability

--- webapp/src/Utils/Utils.php
+++ webapp/src/Utils/Utils.php
@@ -811,10 +811,17 @@ class Utils
     public static function phpiniToBytes(string $size_str): int
     {
         switch (substr($size_str, -1)) {
-            case 'M': case 'm': return (int)$size_str * 1048576;
-            case 'K': case 'k': return (int)$size_str * 1024;
-            case 'G': case 'g': return (int)$size_str * 1073741824;
-            default: return (int)$size_str;
+            case 'M':
+            case 'm':
+                return (int)$size_str * 1048576;
+            case 'K':
+            case 'k':
+                return (int)$size_str * 1024;
+            case 'G':
+            case 'g':
+                return (int)$size_str * 1073741824;
+            default:
+                return (int)$size_str;
         }
     }
 
