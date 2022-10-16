aeab4bf288e537c43be1f0d9d798b7cc937db594
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-10-16 16:05:25 +0200

    Fix mayhem4api sed command

--- .github/workflowscripts/getapi.sh
+++ .github/workflowscripts/getapi.sh
@@ -3,7 +3,7 @@
 set -eux 
 
 # Ignore the CLICS API strict mode
-sed -i "s/'strict'/'ignore-clics'/g" /opt/domjudge/domserver/webapp/src/Controller/API/AbstractRestController.php
+sudo sed -i "s/'strict'/'ignore-clics'/g" /opt/domjudge/domserver/webapp/src/Controller/API/AbstractRestController.php
 
 curl --cacert /tmp/server.crt https://localhost/domjudge/api/doc.json > ./openapi.json
 python3 -m json.tool < ./openapi.json
