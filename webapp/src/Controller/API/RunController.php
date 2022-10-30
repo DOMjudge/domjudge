939b85d4fc9e89449002d509b10a90118bc3e91c
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-10-30 23:14:45 +0100

    Use forgotten include
    Fixes: https://github.com/DOMjudge/domjudge/security/code-scanning/283

--- webapp/src/Controller/API/RunController.php
+++ webapp/src/Controller/API/RunController.php
@@ -16,6 +16,7 @@ use OpenApi\Annotations as OA;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * @Rest\Route("/contests/{cid}/runs")
