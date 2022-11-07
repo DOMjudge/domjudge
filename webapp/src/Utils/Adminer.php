300b45a41f1c4e8d49e9a758dc4d52a3604bc430
Keith Johnson <kj@ubergeek42.com>
2022-11-07 18:19:29 -0500

    Add Adminer.php (for easy db fiddling)
    # Conflicts:
    #	webapp/config/bundles.php

---
+++ webapp/src/Utils/Adminer.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace App\Utils;
+
+class Adminer extends \Adminer
+{
+    public function name(): string
+    {
+        return "DOMjudge DB Admin";
+    }
+
+    public function database(): string
+    {
+        return 'domjudge';
+    }
+
+    public function databases($flush = true): array
+    {
+        return ['domjudge'];
+    }
+
+    function credentials(): array
+    {
+        // Load credentials from <etcDir>/dbpasswords.secret
+        $dbsecretsfile = $GLOBALS['etcDir'] . '/dbpasswords.secret';
+        $db_credentials = file($dbsecretsfile);
+        foreach ($db_credentials as $line) {
+            if ($line[0] == '#') {
+                continue;
+            }
+            list($_, $host, $db, $user, $pass, $port) = array_pad(explode(':', trim($line)), 6, null);
+            break;
+        }
+
+        return [$host, $user, $pass];
+    }
+
+    function login($login, $password): bool
+    {
+        return true;
+    }
+
+    function tableName($tableStatus): array|string
+    {
+        return h($tableStatus['Name']);
+    }
+
+    function permanentLogin($create = false): string
+    {
+        return 'domjudge';
+    }
+
+    function loginForm()
+    {
+        echo "<input type='hidden' value='server' name='auth[driver]'/>";
+        echo "<input type='hidden' value='domjudge' name='auth[db]'/>";
+        echo "<p><input type='submit' value='" . lang('Click to Login') . "'>\n";
+    }
+}
