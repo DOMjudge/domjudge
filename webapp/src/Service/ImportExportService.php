ea69d1fd9285f5ddbaf08c91da966c5c12b00757
DOMjudge team <team@domjudge.org>
2022-11-09 08:50:00 +0600

    Add B2/B3 to UI and fix results for honors/honorable mention split.

--- webapp/src/Service/ImportExportService.php
+++ webapp/src/Service/ImportExportService.php
@@ -375,9 +375,11 @@ class ImportExportService
         $honorsCount = empty($contest->getB3()) ? $numberOfTeams / 2 : $contest->getB3();
         foreach ($scoreboard->getScores() as $teamScore) {
             $count++;
-            $honorsPoints = $teamScore->numPoints;
             if ($count <= $highHonorsCount) {
                 $highHonorsPoints = $teamScore->numPoints;
+            }
+            if ($count <= $honorsCount) {
+                $honorsPoints = $teamScore->numPoints;
             }
             if ($count > $honorsCount) {
                 break;
