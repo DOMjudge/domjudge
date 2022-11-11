fb6e7b738ec81cab9c94af4aae5b43746c2c4eaa
Nicky Gerritsen <nickygerritsen@me.com>
2022-11-11 23:47:25 +0400

    We hide the hidden teams in the prometheus metrics, so fix the test.

--- webapp/tests/Unit/Controller/API/MetricsControllerTest.php
+++ webapp/tests/Unit/Controller/API/MetricsControllerTest.php
@@ -32,28 +32,28 @@ domjudge_submissions_correct{contest="demo"} 0
 domjudge_submissions_ignored{contest="demo"} 0
 # HELP domjudge_submissions_perteam Number of teams that have a queued submission
 # TYPE domjudge_submissions_perteam gauge
-domjudge_submissions_perteam{contest="demo"} 2
+domjudge_submissions_perteam{contest="demo"} 1
 # HELP domjudge_submissions_queued Number of queued submissions
 # TYPE domjudge_submissions_queued gauge
-domjudge_submissions_queued{contest="demo"} 2
+domjudge_submissions_queued{contest="demo"} 1
 # HELP domjudge_submissions_total Total number of all submissions
 # TYPE domjudge_submissions_total gauge
-domjudge_submissions_total{contest="demo"} 2
+domjudge_submissions_total{contest="demo"} 1
 # HELP domjudge_submissions_unverified Number of unverified submissions
 # TYPE domjudge_submissions_unverified gauge
 domjudge_submissions_unverified{contest="demo"} 0
 # HELP domjudge_teams Total number of teams
 # TYPE domjudge_teams gauge
-domjudge_teams{contest="demo"} 2
+domjudge_teams{contest="demo"} 1
 # HELP domjudge_teams_correct Number of teams that have solved at least one problem
 # TYPE domjudge_teams_correct gauge
 domjudge_teams_correct{contest="demo"} 0
 # HELP domjudge_teams_logged_in Number of teams logged in
 # TYPE domjudge_teams_logged_in gauge
-domjudge_teams_logged_in{contest="demo"} 1
+domjudge_teams_logged_in{contest="demo"} 0
 # HELP domjudge_teams_submitted Number of teams that have submitted at least once
 # TYPE domjudge_teams_submitted gauge
-domjudge_teams_submitted{contest="demo"} 2
+domjudge_teams_submitted{contest="demo"} 1
 
 EOF;
 
