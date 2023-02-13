009df2a5e4006567b4d0e8935929a1ee396198db
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2023-02-13 21:00:57 +0100

    Correctly replace installed code for testing
    Earlier version did not replace the tested code. This change is to make
    sure that mayhem4api will receive items in the stream and not a timeout
    as the connection is never closed.

--- .github/workflowscripts/getapi.sh
+++ .github/workflowscripts/getapi.sh
@@ -5,7 +5,7 @@ set -eux
 # Ignore the CLICS API strict mode
 sudo sed -i "s/'strict'/'ignore-clics'/g" /opt/domjudge/domserver/webapp/src/Controller/API/AbstractRestController.php
 # Stop the event-feed from timing out
-sudo sed -i "s/\$request->query->getBoolean('stream', true);/false;/g" webapp/src/Controller/API/ContestController.php
+sudo sed -i "s/\$request->query->getBoolean('stream', true);/false;/g" /opt/domjudge/domserver/webapp/src/Controller/API/ContestController.php
 
 curl --cacert /tmp/server.crt https://localhost/domjudge/api/doc.json > ./openapi.json
 python3 -m json.tool < ./openapi.json
