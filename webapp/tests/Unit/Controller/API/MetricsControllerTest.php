f607bc58ffb306eff8166c526a87105bc7c7b705
Nicky Gerritsen <nickygerritsen@me.com>
2023-02-17 11:36:51 +0100

    Fix metrics controller test.

--- webapp/tests/Unit/Controller/API/MetricsControllerTest.php
+++ webapp/tests/Unit/Controller/API/MetricsControllerTest.php
@@ -24,6 +24,15 @@ class MetricsControllerTest extends BaseTest
     public function testPrometheusMetrics(): void
     {
         $expected =<<<EOF
+# HELP domjudge_balloons_longest_waitingtime Current longest waiting time for a balloon
+# TYPE domjudge_balloons_longest_waitingtime gauge
+domjudge_balloons_longest_waitingtime{contest="demo"} 0
+# HELP domjudge_balloons_waiting Balloons left todo
+# TYPE domjudge_balloons_waiting gauge
+domjudge_balloons_waiting{contest="demo"} 0
+# HELP domjudge_queuetasks Number of queued tasks for judgehosts
+# TYPE domjudge_queuetasks gauge
+domjudge_queuetasks 0
 # HELP domjudge_submissions_correct Number of correct submissions
 # TYPE domjudge_submissions_correct gauge
 domjudge_submissions_correct{contest="demo"} 0
@@ -54,9 +63,21 @@ domjudge_teams_correct{contest="demo"} 0
 # HELP domjudge_teams_logged_in Number of teams logged in
 # TYPE domjudge_teams_logged_in gauge
 domjudge_teams_logged_in{contest="demo"} 0
+# HELP domjudge_teams_logged_in_api Number of teams logged in via API
+# TYPE domjudge_teams_logged_in_api gauge
+domjudge_teams_logged_in_api{contest="demo"} 0
+# HELP domjudge_teams_logged_in_ui Number of teams logged in via UI
+# TYPE domjudge_teams_logged_in_ui gauge
+domjudge_teams_logged_in_ui{contest="demo"} 0
 # HELP domjudge_teams_submitted Number of teams that have submitted at least once
 # TYPE domjudge_teams_submitted gauge
 domjudge_teams_submitted{contest="demo"} 1
+# HELP domjudge_teams_users Total number of users in teams
+# TYPE domjudge_teams_users gauge
+domjudge_teams_users{contest="demo"} 1
+# HELP domjudge_teams_users_logged_in Number of users of teams logged in
+# TYPE domjudge_teams_users_logged_in gauge
+domjudge_teams_users_logged_in{contest="demo"} 0
 
 EOF;
 
