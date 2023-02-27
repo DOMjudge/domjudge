fe364c77a51c0756afac64b8259483f364531add
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-02-27 20:53:14 +0100

    Minimal version to get unit tests to run

--- .github/jobs/unit-tests.sh
+++ .github/jobs/unit-tests.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-. gitlab/ci_settings.sh
+. .github/jobs/data/gha_ci_bashrc
+
+section_start chown_checkout
+git config --global --add safe.directory /__w/domjudge/domjudge
+section_end chown_checkout
+
+section_start install_needed_sqlclient_tools
+if [ $3 = "mysql" ]; then
+    apt-get purge mariadb-client -y
+    apt-get update
+    apt-get install mysql-client -y
+fi
+section_end install_needed_sqlclient_tools
 
 export version=$1
 unittest=$2
@@ -8,8 +20,7 @@ unittest=$2
 
 show_phpinfo $version
 
-# Set up
-"$( dirname "${BASH_SOURCE[0]}" )"/base.sh
+.github/jobs/base.sh
 
 # Add team to admin user
 echo "INSERT INTO userrole (userid, roleid) VALUES (1, 3);" | mysql domjudge
@@ -31,50 +42,33 @@ pcov=""
 phpcov=""
 if [ "$CODECOVERAGE" -eq 1 ]; then
     phpcov="-dpcov.enabled=1 -dpcov.directory=webapp/src"
-    pcov="--coverage-html=${CI_PROJECT_DIR}/coverage-html --coverage-clover coverage.xml"
+    pcov="--coverage-html=${PWD}/coverage-html --coverage-clover coverage.xml"
 fi
 set +e
-php $phpcov lib/vendor/bin/phpunit -c webapp/phpunit.xml.dist webapp/tests/$unittest --log-junit ${CI_PROJECT_DIR}/unit-tests.xml --colors=never $pcov > "$GITLABARTIFACTS"/phpunit.out
+php $phpcov lib/vendor/bin/phpunit -c webapp/phpunit.xml.dist webapp/tests/$unittest --log-junit ${DIR}/unit-tests.xml --colors=never $pcov > "$ARTIFACTS"/phpunit.out
 UNITSUCCESS=$?
 set -e
 CNT=0
 if [ $CODECOVERAGE -eq 1 ]; then
-    CNT=$(sed -n '/Generating code coverage report/,$p' "$GITLABARTIFACTS"/phpunit.out | grep -v DoctrineTestBundle | grep -cv ^$)
+    CNT=$(sed -n '/Generating code coverage report/,$p' "$ARTIFACTS"/phpunit.out | grep -v DoctrineTestBundle | grep -cv ^$)
     FILE=deprecation.txt
-    sed -n '/Generating code coverage report/,$p' "$GITLABARTIFACTS"/phpunit.out > ${CI_PROJECT_DIR}/$FILE
+    sed -n '/Generating code coverage report/,$p' "$ARTIFACTS"/phpunit.out > "$DIR/$FILE"
     if [ $CNT -le 12 ]; then
         STATE=success
     else
         STATE=failure
     fi
-    ORIGINAL="gitlab.com/DOMjudge"
-    REPLACETO="domjudge.gitlab.io/-"
-    # Copied from CCS
-    curl https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA \
-      -X POST \
-      -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
-      -H "Accept: application/vnd.github.v3+json" \
-      -d "{\"state\": \"$STATE\", \"target_url\": \"${CI_JOB_URL/$ORIGINAL/$REPLACETO}/artifacts/$FILE\", \"description\":\"Symfony deprecations\", \"context\": \"Symfony deprecation\"}"
 fi
-if [ $UNITSUCCESS -eq 0 ]; then
-    STATE=success
-else
-    STATE=failure
-fi
-curl https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA \
-    -X POST \
-    -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
-    -H "Accept: application/vnd.github.v3+json" \
-    -d "{\"state\": \"$STATE\", \"target_url\": \"${CI_PIPELINE_URL}/test_report\", \"description\":\"Unit tests\", \"context\": \"unit_tests ($version)\"}"
 if [ $UNITSUCCESS -ne 0 ]; then
-    exit 1
+    exit $UNITSUCCESS
 fi
 
 if [ $CODECOVERAGE -eq 1 ]; then
-    section_start_collap uploadcoverage "Upload code coverage"
+    section_start uploadcoverage "Upload code coverage"
     # Only upload when we got working unit-tests.
     set +u # Uses some variables which are not set
     # shellcheck disable=SC1090
-    . $DIR/.github/jobs/uploadcodecov.sh 1>/dev/zero 2>/dev/zero
+    . $DIR/.github/jobs/uploadcodecov.sh &>/dev/zero
+    set -u # Undo set dance
     section_end uploadcoverage
 fi
