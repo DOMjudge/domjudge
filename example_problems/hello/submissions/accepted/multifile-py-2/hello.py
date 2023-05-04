59b8fc4961c77fca73d65533a4804eab1378c3bc
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-05-04 09:18:16 +0200

    Add extra python example submissions to test the same as for Java/C++

---
+++ example_problems/hello/submissions/accepted/multifile-py-2/hello.py
@@ -0,0 +1,12 @@
+'''
+This is a multi-file submission which produces two public classes.
+Note that to successfully compile, the source file names must be
+preserved to match the public class names.
+
+@EXPECTED_RESULTS@: CORRECT
+'''
+
+import message
+
+foo = message.Message();
+foo.myPrint();
