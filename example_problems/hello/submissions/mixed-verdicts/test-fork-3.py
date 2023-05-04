99ac6ad92d09b8154604e13d40c9a59d16a30005
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-05-04 09:59:18 +0200

    Add some extra test files
    Not everything is implemented with pypy3

---
+++ example_problems/hello/submissions/mixed-verdicts/test-fork-3.py
@@ -0,0 +1,24 @@
+'''
+This code tries to fork as many processes as possible. The limit on
+the number of processes will limit that and the program will
+timeout.
+
+The result should be a TIMELIMIT and the running forked programs
+killed by testcase_run. When using pypy3 this will be RUN-ERROR as the
+pool is not implemented
+
+@EXPECTED_RESULTS@: TIMELIMIT,RUN-ERROR
+'''
+
+from multiprocessing import Pool
+
+def thread_function(name):
+	a = 0
+	while True:
+		a += 1
+		a = a%10
+
+# Directly make all processes
+numb_processes = 1000*60*10
+with Pool(numb_processes) as p:
+	p.map(thread_function, range(numb_processes))
---
+++ example_problems/hello/submissions/time_limit_exceeded/test-fork.py
@@ -0,0 +1,23 @@
+'''
+This code tries to fork as many processes as possible. The limit on
+the number of processes will limit that and the program will
+timeout.
+
+The result should be a TIMELIMIT and the running forked programs
+killed by testcase_run.
+
+@EXPECTED_RESULTS@: TIMELIMIT
+'''
+
+import threading
+
+def thread_function(name):
+	a = 0
+	while True:
+		a += 1
+		a = a%10
+
+while True:
+	for b in [True, False]:
+		x = threading.Thread(target=thread_function, args=(1,), daemon=b)
+		x.start()
