a261eace74caaf46eb117405fe057ed3d64262e4
Daniel Brinkers <daniel@brinky.net>
2023-05-06 19:21:38 +0200

    migration

---
+++ webapp/migrations/Version20230506141723.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230506141723 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE judging_run_output ADD team_message LONGBLOB DEFAULT NULL COMMENT \'Judge message to team(DC2Type:blobtext)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE judging_run_output DROP team_message');
+    }
+
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+}
