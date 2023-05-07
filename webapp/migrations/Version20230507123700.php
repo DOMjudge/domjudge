40fc4ef6b7a85286030391c7eb401bfe2b258686
Tobias Werth <werth@cs.fau.de>
2023-05-07 19:36:15 +0200

    desc

--- webapp/migrations/Version20230507123700.php
+++ webapp/migrations/Version20230507123700.php
@@ -14,7 +14,7 @@ final class Version20230507123700 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add compiler and runner versions information.';
     }
 
     public function up(Schema $schema): void
