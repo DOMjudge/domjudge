79a37c85feb6d5d8793faad1e67c2cd42f6d1de2
Tobias Werth <werth@cs.fau.de>
2023-05-08 18:27:34 +0200

    make it more clear in the migration what we are doing

--- webapp/migrations/Version20230508135106.php
+++ webapp/migrations/Version20230508135106.php
@@ -20,19 +20,15 @@ final class Version20230508135106 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX jobid ON queuetask');
-        $this->addSql('ALTER TABLE queuetask ADD judgingid INT UNSIGNED DEFAULT NULL COMMENT \'Judging ID\', DROP jobid');
+        $this->addSql('ALTER TABLE queuetask RENAME COLUMN jobid TO judgingid');
         $this->addSql('ALTER TABLE queuetask ADD CONSTRAINT FK_45E85FF85D5FEA72 FOREIGN KEY (judgingid) REFERENCES judging (judgingid) ON DELETE CASCADE');
-        $this->addSql('CREATE INDEX judgingid ON queuetask (judgingid)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE queuetask DROP FOREIGN KEY FK_45E85FF85D5FEA72');
-        $this->addSql('DROP INDEX judgingid ON queuetask');
-        $this->addSql('ALTER TABLE queuetask ADD jobid INT UNSIGNED DEFAULT NULL COMMENT \'All queuetasks with the same jobid belong together.\', DROP judgingid');
-        $this->addSql('CREATE INDEX jobid ON queuetask (jobid)');
+        $this->addSql('ALTER TABLE queuetask RENAME COLUMN judgingid TO jobid');
     }
 
     public function isTransactional(): bool
