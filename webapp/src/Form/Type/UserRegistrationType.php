7257a756763ecafaf7ea6899c831ab30d048953a
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-06-20 13:48:32 +0200

    Add new options to configuration

--- webapp/src/Form/Type/UserRegistrationType.php
+++ webapp/src/Form/Type/UserRegistrationType.php
@@ -11,10 +11,12 @@ use App\Service\ConfigurationService;
 use App\Service\DOMJudgeService;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -50,37 +52,39 @@ class UserRegistrationType extends AbstractType
             ])
             ->add('name', TextType::class, [
                 'label' => false,
-                'required' => false,
                 'attr' => [
-                    'placeholder' => 'Full name (optional)',
+                    'placeholder' => 'Full name',
                     'autocomplete' => 'name',
                 ],
             ])
             ->add('email', EmailType::class, [
                 'label' => false,
-                'required' => false,
                 'attr' => [
-                    'placeholder' => 'Email address (optional)',
+                    'placeholder' => 'Email address',
                     'autocomplete' => 'email',
                 ],
                 'constraints' => new Email(),
-            ])
-            ->add('teamName', TextType::class, [
-                'label' => false,
-                'attr' => [
-                    'placeholder' => 'Team name',
-                ],
-                'constraints' => [
-                    new NotBlank(),
-                    new Callback(function ($teamName, ExecutionContext $context) {
-                        if ($this->em->getRepository(Team::class)->findOneBy(['name' => $teamName])) {
-                            $context->buildViolation('This team name is already in use.')
-                                ->addViolation();
-                        }
-                    }),
-                ],
-                'mapped' => false,
             ]);
+
+        if ($this->config->get('allow_custom_team_name_registration')) {
+            $builder
+                ->add('teamName', TextType::class, [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Team name',
+                    ],
+                    'constraints' => [
+                        new NotBlank(),
+                        new Callback(function ($teamName, ExecutionContext $context) {
+                            if ($this->em->getRepository(Team::class)->findOneBy(['name' => $teamName])) {
+                                $context->buildViolation('This team name is already in use.')
+                                    ->addViolation();
+                            }
+                        }),
+                    ],
+                    'mapped' => false,
+                ]);
+        }
 
         $selfRegistrationCategoriesCount = $this->em->getRepository(TeamCategory::class)->count(['allow_self_registration' => 1]);
         if ($selfRegistrationCategoriesCount > 1) {
@@ -111,13 +115,21 @@ class UserRegistrationType extends AbstractType
                 $countries["$name ($alpha3)"] = $alpha3;
             }
 
+            $affiliationChoices = [
+                'Use existing school' => 'existing'
+            ];
+
+            if ($this->config->get('allow_create_affiliation_during_registration')) {
+                $affiliationChoices['Add new school'] = 'new';
+            }
+
+            if ($this->config->get('allow_registration_without_affiliation')) {
+                $affiliationChoices['No school'] = 'none';
+            }
+
             $builder
                 ->add('affiliation', ChoiceType::class, [
-                    'choices' => [
-                        'No affiliation' => 'none',
-                        'Add new affiliation' => 'new',
-                        'Use existing affiliation' => 'existing',
-                    ],
+                    'choices' => $affiliationChoices,
                     'expanded' => true,
                     'mapped' => false,
                     'label' => false,
@@ -126,17 +138,14 @@ class UserRegistrationType extends AbstractType
                     'label' => false,
                     'required' => false,
                     'attr' => [
-                        'placeholder' => 'Affiliation name',
+                        'placeholder' => 'School name',
                     ],
                     'mapped' => false,
                 ])
-                ->add('affiliationShortName', TextType::class, [
+                ->add('affiliationShortName', HiddenType::class, [
                     'label' => false,
                     'required' => false,
-                    'attr' => [
-                        'placeholder' => 'Affiliation shortname',
-                        'maxlength' => '32',
-                    ],
+                    'empty_data' => Uuid::uuid4()->toString(),
                     'mapped' => false,
                 ]);
             if ($this->config->get('show_flags')) {
@@ -154,9 +163,9 @@ class UserRegistrationType extends AbstractType
                     'required' => false,
                     'mapped' => false,
                     'choice_label' => 'name',
-                    'placeholder' => '-- Select affiliation --',
+                    'placeholder' => '-- Select school --',
                     'attr' => [
-                        'placeholder' => 'Affiliation',
+                        'placeholder' => 'School',
                     ],
                 ]);
         }
