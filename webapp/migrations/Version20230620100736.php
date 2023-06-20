332f4cb8b98077ad3ac0034fdadc200490fb1d53
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-06-20 13:46:50 +0200

    Increase the configuration name length to 64

---
+++ webapp/migrations/Version20230620100736.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230620100736 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Expands configuration name to allow key names up to 64 characters';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE configuration MODIFY `name` varchar(64)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE configuration MODIFY `name` varchar(32)');
+    }
+
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+}
