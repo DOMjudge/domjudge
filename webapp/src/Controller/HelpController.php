34f6840585a15fff2887932746616f5ab8db19e2
Lubos Zapotocny <zapotocnylubos@gmail.com>
2023-06-22 14:53:58 +0200

    Use older @Route annotation

--- webapp/src/Controller/HelpController.php
+++ webapp/src/Controller/HelpController.php
@@ -2,21 +2,15 @@
 
 namespace App\Controller;
 
-use App\Service\DOMJudgeService;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Twig\Extra\Markdown\MarkdownRuntime;
-
-#[Route(path: '/public/help')]
+/**
+ * @Route("/public/help")
+ */
 class HelpController extends BaseController
 {
-    #[Route(path: '', name: 'public_help')]
+    /**
+     * @Route("", name="public_help")
+     */
     public function helpAction(): Response
     {
         return $this->render('public/help.html.twig');
