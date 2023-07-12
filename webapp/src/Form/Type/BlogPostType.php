0791c2328fb7685d5eb4e219e85b54397a9b1790
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-07-12 11:55:06 +0200

    Create the blog post form type

---
+++ webapp/src/Form/Type/BlogPostType.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use App\Entity\BlogPost;
+use App\Entity\Executable;
+use App\Entity\Problem;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('title', TextType::class);
+        $builder->add('subtitle', TextType::class);
+        $builder->add('author', TextType::class, [
+            'required' => false
+        ]);
+        $builder->add('thumbnail_file_name', FileType::class, ['label' => 'Thumbnail']);
+        $builder->add('body', HiddenType::class);
+
+        $builder->add('send', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => BlogPost::class]);
+    }
+}
