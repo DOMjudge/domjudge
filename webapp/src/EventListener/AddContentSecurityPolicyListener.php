62c3efcdc609d2a3a14260d8d5c54fae6386fa52
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-08-21 16:12:38 +0200

    Refactor the CSP listener

--- webapp/src/EventListener/AddContentSecurityPolicyListener.php
+++ webapp/src/EventListener/AddContentSecurityPolicyListener.php
@@ -9,15 +9,45 @@ use Symfony\Component\HttpKernel\Profiler\Profiler;
 #[AsEventListener]
 class AddContentSecurityPolicyListener
 {
-    public function __construct(protected readonly ?Profiler $profiler) {}
+    public function __construct(
+        protected readonly ?Profiler $profiler,
+        protected readonly array $cspConfig
+    ) {}
 
     public function __invoke(ResponseEvent $event): void
     {
+        $response = $event->getResponse();
+
+        $csp = implode('; ', [
+            $this->getDefaultSrcCsp(),
+            $this->getStyleSrcCsp(),
+            $this->getScriptSrcCsp(),
+            $this->getImageSrcCsp(),
+        ]);
+
+        $response->headers->set('Content-Security-Policy', $csp);
+    }
+
+    private function getDefaultSrcCsp(): string
+    {
+        return "default-src " . $this->cspConfig['defaultSrc'];
+    }
+
+    private function getStyleSrcCsp(): string
+    {
+        return "style-src " . $this->cspConfig['styleSrc'];
+    }
+
+    private function getScriptSrcCsp(): string
+    {
         // Set the correct CSP based on whether the profiler is enabled, since
         // the profiler requires 'unsafe-eval' for script-src 'self'.
-        $response = $event->getResponse();
-        $cspExtra = $this->profiler ? "'unsafe-eval'" : "";
-        $csp = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' $cspExtra; img-src 'self' data:";
-        $response->headers->set('Content-Security-Policy', $csp);
+        $unsafeEvalCsp = $this->profiler ? " 'unsafe-eval'" : "";
+        return "script-src " . $this->cspConfig['scriptSrc'] . $unsafeEvalCsp;
+    }
+
+    private function getImageSrcCsp(): string
+    {
+        return "img-src " . $this->cspConfig['imgSrc'];
     }
 }
