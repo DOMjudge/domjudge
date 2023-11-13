2705f81fd12edbe83948b0960a0436cbc40df09a
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-11-13 09:52:00 +0100

    We need this as bootstrap uses it
    The validator can't handle this
    (https://github.com/w3c/css-validator/issues/111) and the feature is
    only a draft (https://www.w3.org/TR/css-variables-1/) but widely
    supported.

--- gitlab/webstandard.sh
+++ gitlab/webstandard.sh
@@ -82,7 +82,7 @@ if [ "$TEST" = "w3cval" ]; then
     rm -f localhost/domjudge/jury/config/check/phpinfo*
     # We need any value for bs-gutter-x to calculate with in CSS
     for f in $(find ./ -name "style_domjudge.css*"); do
-        sed -i 's/:root {/:root {--bs-gutter-x: 0em;/g' $f
+        sed -i 's/var(--bs-gutter-x)/1em/g' $f
     done
     section_end upstream_problems
 
