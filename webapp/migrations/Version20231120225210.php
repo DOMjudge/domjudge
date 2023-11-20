0cddbb00c7fe7cf10d04896029daaf8021ef142a
ItsNiklas <nikbau2000@gmail.com>
2023-11-21 00:31:13 +0100

    Assert extensions don't start with a dot
    Including migration, fix invalid-feedback divs

---
+++ webapp/migrations/Version20231120225210.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231120225210 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Remove leading dots from extensions in the `language` table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $languages = $this->connection->fetchAllAssociative('SELECT langid, extensions FROM language');
+
+        foreach ($languages as $language) {
+            $extensions = json_decode($language['extensions'], true);
+
+            $updated = false;
+            foreach ($extensions as &$extension) {
+                if (strpos($extension, '.') === 0) {
+                    $extension = ltrim($extension, '.');
+                    $updated = true;
+                }
+            }
+
+            if ($updated) {
+                $newExtensionsJson = json_encode($extensions);
+                $this->connection->executeQuery('UPDATE language SET extensions = :extensions WHERE langid = :langid', [
+                    'extensions' => $newExtensionsJson,
+                    'langid' => $language['langid'],
+                ]);
+            }
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        // This migration is not reversible
+    }
+
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+}
