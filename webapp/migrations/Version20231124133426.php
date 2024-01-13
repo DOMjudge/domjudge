cc7cca3797c705f3a2c3e4eaebf91ce6a1e4c55b
Nicky Gerritsen <nickygerritsen@me.com>
2024-01-13 10:31:58 +0100

    Only add problemtexts to new table if they are not null.

--- webapp/migrations/Version20231124133426.php
+++ webapp/migrations/Version20231124133426.php
@@ -21,7 +21,7 @@ final class Version20231124133426 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE problem_text_content (probid INT UNSIGNED NOT NULL COMMENT \'Problem ID\', content LONGBLOB NOT NULL COMMENT \'Text content(DC2Type:blobtext)\', PRIMARY KEY(probid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'Stores contents of problem texts\' ');
-        $this->addSql('INSERT INTO problem_text_content (probid, content) SELECT probid, problemtext FROM problem');
+        $this->addSql('INSERT INTO problem_text_content (probid, content) SELECT probid, problemtext FROM problem WHERE problemtext IS NOT NULL');
         $this->addSql('ALTER TABLE problem_text_content ADD CONSTRAINT FK_21B6AD6BEF049279 FOREIGN KEY (probid) REFERENCES problem (probid) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE problem DROP problemtext');
     }
