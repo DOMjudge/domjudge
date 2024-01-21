6e0a9b29e986db5701e07aedbe2dc31e79b65dae
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2024-01-21 16:22:45 +0100

    Keep the owner:group of the one downloading the tarball
    In the scenario:
    ./configure --prefix=<something_which_needs_su/sudo>
    make domserver <as unpriviliged user which downloaded the tarball>
    sudo make-installdomserver <as root>
    
    We would copy the Nelmiodoc files as root, so the user can't run a make
    {dist}clean without errors as we can't remove the folder as its owned by
    root.
    
    We now also install nelmiodoc together with composer in the base image
    for CI:
    - together with the needed dependencies
    - make sure we get an autoload.php

--- .github/jobs/configure-checks/setup_configure_image.sh
+++ .github/jobs/configure-checks/setup_configure_image.sh
@@ -10,11 +10,21 @@ case $distro_id in
         dnf install pkg-config make bats autoconf automake util-linux -y ;;
     *)
         apt-get update; apt-get full-upgrade -y
-        apt-get install pkg-config make bats autoconf -y ;;
+        apt-get install pkg-config make bats autoconf -y
+        apt-get install composer php php-cli php-curl php-fpm php-gd \
+                        php-intl php-json php-mbstring php-mysql php-xml php-zip -y
+	;;
 esac
 
 # Build the configure file
 make configure
+
+case $distro_id in
+    "ID=fedora")
+        true ;;
+    *)
+        composer install --no-scripts ;;
+esac
 
 # Install extra assert statements for bots
 cp submit/assert.bash .github/jobs/configure-checks/
