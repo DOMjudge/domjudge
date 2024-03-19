2714317603aae3b65e0526c554eea99d81c5a9eb
Nicky Gerritsen <nickygerritsen@me.com>
2024-03-19 16:51:20 +0100

    Use PUT endpoint for updating / setting users to be in sync with CLICS.

---
+++ webapp/src/DataTransferObject/UpdateUser.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\DataTransferObject;
+
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(required: ['id', 'username', 'name', 'roles'])]
+class UpdateUser extends AddUser
+{
+    public function __construct(
+        public readonly string $id,
+        string $username,
+        string $name,
+        ?string $email,
+        ?string $ip,
+        ?string $password,
+        ?bool $enabled,
+        ?string $teamId,
+        array $roles
+    ) {
+        parent::__construct($username, $name, $email, $ip, $password, $enabled, $teamId, $roles);
+    }
+}
