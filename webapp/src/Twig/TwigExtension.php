094f9538c84f44727f423cbab203a62613410b03
Tobias Werth <werth@cs.fau.de>
2024-03-22 16:51:46 +0100

    Fix problem badge for "no contest".
    (cherry picked from commit b26ebfb622011abb433c03b91ae9443a3b6cc04e)

--- webapp/src/Twig/TwigExtension.php
+++ webapp/src/Twig/TwigExtension.php
@@ -1108,7 +1108,7 @@ EOF;
         );
     }
 
-    public function problemBadgeForProblemAndContest(Problem $problem, Contest $contest): string
+    public function problemBadgeForProblemAndContest(Problem $problem, ?Contest $contest): string
     {
         foreach ($problem->getContestProblems() as $contestProblem) {
             if ($contestProblem->getContest() === $contest) {
