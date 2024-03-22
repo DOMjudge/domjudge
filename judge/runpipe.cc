3ad3847f171b27aff706a69813ec04c40d4970f6
Tobias Werth <werth@cs.fau.de>
2024-03-22 09:40:16 +0100

    Fix compiler warning.

--- judge/runpipe.cc
+++ judge/runpipe.cc
@@ -777,7 +777,7 @@ struct state_t {
         int time_millis = time % 1000;
         char direction = from.index == 0 ? ']' : '[';
         char eofbuf[128];
-        sprintf(eofbuf, "[%3d.%03ds/%ld]%c", time_sec, time_millis, 0LL, direction);
+        sprintf(eofbuf, "[%3d.%03ds/%ld]%c", time_sec, time_millis, 0L, direction);
         write_all(output_file.output_file, eofbuf, strlen(eofbuf));
 
         warning(0, "EOF from process #%ld", from.index);
