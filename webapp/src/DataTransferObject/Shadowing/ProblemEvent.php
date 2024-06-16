9596e0dd1dc3625bb8145c0efe7c31f455dd0a59
Dup4 <lyuzhi.pan@gmail.com>
2024-06-16 14:27:08 +0800

    fix: time_limit type in ProblemEvent Object
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- webapp/src/DataTransferObject/Shadowing/ProblemEvent.php
+++ webapp/src/DataTransferObject/Shadowing/ProblemEvent.php
@@ -7,7 +7,7 @@ class ProblemEvent implements EventData
     public function __construct(
         public readonly string $id,
         public readonly string $name,
-        public readonly int $timeLimit,
+        public readonly float $timeLimit,
         public readonly ?string $label,
         public readonly ?string $rgb,
     ) {}
