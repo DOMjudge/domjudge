f81ec56e96b7a2562ea648e514225931c62a71c9
Soha Jin <soha@jin.sh>
2024-06-17 20:10:24 +0800

    Add `getContestProblem` to Contest for getting an associated ContestProblem
    (cherry picked from commit 56104f63024f11599719b1e4553d732cf8e6cbce)

--- webapp/src/Entity/Contest.php
+++ webapp/src/Entity/Contest.php
@@ -862,6 +862,16 @@ class Contest extends BaseApiEntity implements AssetEntityInterface
         return $this->problems;
     }
 
+    public function getContestProblem(Problem $problem): ?ContestProblem
+    {
+        foreach ($this->getProblems() as $contestProblem) {
+            if ($contestProblem->getProblem() === $problem) {
+                return $contestProblem;
+            }
+        }
+        return null;
+    }
+
     public function addClarification(Clarification $clarification): Contest
     {
         $this->clarifications[] = $clarification;
