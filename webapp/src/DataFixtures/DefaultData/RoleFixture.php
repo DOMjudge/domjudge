33a415021c39eb0a22597a4e8126f58fa2149045
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2024-07-01 22:06:04 +0200

    Add the new role
    The new API roles are bundled with another role for readability, no
    intent at this point to make this something which we can assign to
    users.

--- webapp/src/DataFixtures/DefaultData/RoleFixture.php
+++ webapp/src/DataFixtures/DefaultData/RoleFixture.php
@@ -29,7 +29,8 @@ class RoleFixture extends AbstractDefaultDataFixture
             'api_writer'         => 'API writer',
             'api_source_reader'  => 'Source code reader',
             'clarification_rw'   => 'Clarification handler',
-            'api_problem_change' => 'API Problem Changer'
+            'api_problem_change' => 'API Problem Changer',
+            'api_contest_change' => 'API Contest Changer'
         ];
         foreach ($roles as $roleName => $description) {
             if (!($role = $manager->getRepository(Role::class)->findOneBy(['dj_role' => $roleName]))) {
