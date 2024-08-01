63206f5e314b0053623f503c643b13dac265f094
Lubos Zapotocny <zapotocnylubos@gmail.com>
2024-08-01 20:50:56 +0000

    Fix problem text in team editor (partially)

--- webapp/src/Entity/Problem.php
+++ webapp/src/Entity/Problem.php
@@ -165,7 +165,6 @@ class Problem extends BaseApiEntity
         cascade: ['persist'],
         orphanRemoval: true
     )]
-    #[ORM\OrderBy(['type' => 'ASC'])]
     #[Serializer\Exclude]
     private Collection $problemStatementContent;
 
