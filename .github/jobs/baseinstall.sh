a7eed4d417bc59f940996f7cc149de1002f1a2f6
Tobias Werth <werth@cs.fau.de>
2024-08-04 16:30:25 +0200

    debug

--- .github/jobs/baseinstall.sh
+++ .github/jobs/baseinstall.sh
@@ -101,6 +101,7 @@ for service in nginx php${PHPVERSION}-fpm; do
     service "$service" restart
     service "$service" status
 done
+echo | openssl s_client -servername localhost -connect localhost:443 2>/dev/null | openssl x509 -noout -text
 section_end
 
 if [ "${db}" = "install" ]; then
