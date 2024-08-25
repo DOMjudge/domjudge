ec379b7002ef89de9cf40a2ffab4128817a2a2f8
Kevin Jilissen <info@kevinjilissen.nl>
2024-08-25 21:06:42 +0200

    Fix syntax-check errors
    As the CI job was not correctly run for some time, a lot of small errors
    were in the repository.

--- webapp/config/preload.php
+++ webapp/config/preload.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 if (file_exists(dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php';
