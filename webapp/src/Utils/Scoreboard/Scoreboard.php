cd2fd1b1cf15cfd7a1be89433b0293303758e023
DOMjudge team <team@domjudge.org>
2024-09-16 18:02:47 +0500

    Fix highlighting of in-freeze submissions by introducing separate metadata.

--- webapp/src/Utils/Scoreboard/Scoreboard.php
+++ webapp/src/Utils/Scoreboard/Scoreboard.php
@@ -152,14 +152,14 @@ class Scoreboard
             );
 
             $this->matrix[$teamId][$probId] = new ScoreboardMatrixItem(
-                $scoreRow->getIsCorrect($this->restricted),
-                $scoreRow->getIsCorrect($this->restricted) && $scoreRow->getIsFirstToSolve(),
-                // When public scoreboard is frozen, also show "x + y tries" for jury
-                $scoreRow->getSubmissions($this->freezeData->showFrozen() ? false : $this->restricted),
-                $scoreRow->getPending($this->freezeData->showFrozen() ? false : $this->restricted),
-                $scoreRow->getSolveTime($this->restricted),
-                $penalty,
-                $scoreRow->getRuntime($this->restricted)
+                isCorrect: $scoreRow->getIsCorrect($this->restricted),
+                isFirst: $scoreRow->getIsCorrect($this->restricted) && $scoreRow->getIsFirstToSolve(),
+                numSubmissions: $scoreRow->getSubmissions($this->restricted),
+                numSubmissionsPending: $scoreRow->getPending($this->restricted),
+                time: $scoreRow->getSolveTime($this->restricted),
+                penaltyTime: $penalty,
+                runtime: $scoreRow->getRuntime($this->restricted),
+                numSubmissionsInFreeze: $scoreRow->getPending(false),
             );
 
             if ($scoreRow->getIsCorrect($this->restricted)) {
@@ -217,7 +217,14 @@ class Scoreboard
                 $problemId = $contestProblem->getProbid();
                 // Provide default scores when nothing submitted for this team + problem yet
                 if (!isset($this->matrix[$teamId][$problemId])) {
-                    $this->matrix[$teamId][$problemId] = new ScoreboardMatrixItem(false, false, 0, 0, 0, 0, 0);
+                    $this->matrix[$teamId][$problemId] = new ScoreboardMatrixItem(
+                        isCorrect: false,
+                        isFirst: false,
+                        numSubmissions: 0,
+                        numSubmissionsPending: 0,
+                        time: 0,
+                        penaltyTime: 0,
+                        runtime: 0);
                 }
 
                 $problemMatrixItem = $this->matrix[$teamId][$problemId];
