DOMjudge/domjudge

commit 881d4663bebc35454a61caf1967cc71c460c4eff

Make chmod only for script directory explicit

judge/version_check.sh
@@ -66,8 +66,8 @@ touch version_check.out version_check.meta
 if [ -e "$WORKDIR/version_check-script" ]; then
     mv "$WORKDIR/version_check-script" "$WORKDIR/version_check-script-old"
 fi
-# shellcheck disable=SC2174
-mkdir -m 0777 -p "$WORKDIR/version_check-script"
+mkdir -p "$WORKDIR/version_check-script"
+chmod 0777 -p "$WORKDIR/version_check-script"
 cp -a "$VERSION_CHECK_SCRIPT" "$PWD/version_check-script/"
 
 cd "$WORKDIR/version_check"

judge/compile.sh
@@ -122,8 +122,8 @@ chmod a+rwx "$WORKDIR/compile"
 touch compile.out compile.meta
 
 # Copy compile script into chroot
-# shellcheck disable=SC2174
-mkdir -m 0777 -p "$WORKDIR/compile-script"
+mkdir -p "$WORKDIR/compile-script"
+chmod 0777 "$WORKDIR/compile-script"
 cp -a "$(dirname "$COMPILE_SCRIPT")"/* "$WORKDIR/compile-script/"
 
 cd "$WORKDIR/compile"

commit 082efdb059a1c52faae3e9ebbd776b12177ad857

Fix typo introduced in 881d4663bebc35454a61caf1967cc71c460c4eff.

judge/version_check.sh
@@ -67,7 +67,7 @@ if [ -e "$WORKDIR/version_check-script" ]; then
     mv "$WORKDIR/version_check-script" "$WORKDIR/version_check-script-old"
 fi
 mkdir -p "$WORKDIR/version_check-script"
-chmod 0777 -p "$WORKDIR/version_check-script"
+chmod 0777 "$WORKDIR/version_check-script"
 cp -a "$VERSION_CHECK_SCRIPT" "$PWD/version_check-script/"
 
 cd "$WORKDIR/version_check"