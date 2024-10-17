1edc428f3e0acb2fbd2f858a478f7dacb1785889
Michael Vasseur <michael.vasseur@vu.nl>
2024-10-17 22:34:56 +0200

    Fix the visualizer script

--- example_problems/boolfind/output_visualizer/boolfind_visual/visualize.py
+++ example_problems/boolfind/output_visualizer/boolfind_visual/visualize.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 #
 # Invoke as:
-# <output_visualizer_program> input answer_file feedback_dir [additional_arguments] < team_output [ > team_input ]
+# <output_visualizer_program> answer_file feedback_file
 import matplotlib.pyplot as plt
 import sys
 
 my_name  = sys.argv[0]
 my_input = sys.argv[1]
-#real_answer = sys.argv[2]
 my_feedback = sys.argv[2]
 
 with open(my_input, 'r') as f:
@@ -16,7 +15,6 @@ with open(my_input, 'r') as f:
   for line in lines:
     if 'READ' in line:
       vals.append(int(line.split(' ')[-1]))
-      print(line)
   plt.plot([0,1])
   plt.ylabel('Guesses')
-  plt.savefig(f"{my_feedback}")
+  plt.savefig(f"{my_feedback}", format='png')
