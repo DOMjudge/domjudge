51eb0a98a971c2ff6d84e74cd5076a7aa29b65e4
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2024-10-29 08:12:27 +0100

    Extending the images is unneeded without the test

--- .github/jobs/configure-checks/setup_configure_image.sh
+++ .github/jobs/configure-checks/setup_configure_image.sh
@@ -10,24 +10,11 @@ case $distro_id in
         dnf install pkg-config make bats autoconf automake util-linux -y ;;
     *)
         apt-get update; apt-get full-upgrade -y
-        apt-get install pkg-config make bats autoconf -y
-        apt-get install composer php php-cli php-curl php-fpm php-gd \
-                        php-intl php-json php-mbstring php-mysql php-xml php-zip \
-                        acl zip unzip mariadb-server python3-sphinx \
-                        python3-sphinx-rtd-theme rst2pdf fontconfig python3-yaml \
-                        latexmk texlive-latex-recommended texlive-latex-extra \
-                        tex-gyre -y ;;
+        apt-get install pkg-config make bats autoconf ;;
 esac
 
 # Build the configure file
 make configure
-
-case $distro_id in
-    "ID=fedora")
-        true ;;
-    *)
-        (cd webapp; composer install --no-scripts ) ;;
-esac
 
 # Install extra assert statements for bots
 cp submit/assert.bash .github/jobs/configure-checks/
