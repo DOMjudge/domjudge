cae71c41a450a89fb73cef9aa1e95fdaa4295aab
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2024-12-14 15:24:41 +0100

    Fix unit test setup

--- .github/jobs/unit-tests.sh
+++ .github/jobs/unit-tests.sh
@@ -19,7 +19,7 @@ echo "UPDATE user SET teamid = 1 WHERE userid = 1;" | mysql domjudge_test
 cp webapp/.env.test /opt/domjudge/domserver/webapp/
 
 # We also need the composer.json for PHPunit to detect the correct directory.
-cp webapp/composer.json /opt/domjudge/domserver/webapp/
+cp composer.json /opt/domjudge/domserver/webapp/
 
 cd /opt/domjudge/domserver
 
