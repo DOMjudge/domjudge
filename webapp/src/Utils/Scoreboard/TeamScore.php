6613b28ba7eebfdb300222361252d2e731399c99
Tobias Werth <werth@cs.fau.de>
2025-03-09 20:03:21 +0100

    Introduce the concept of sort keys.
    Progress towards https://github.com/DOMjudge/domjudge/issues/2525.
    
    The basic idea is to replace a lot of scattered scoring logic (which
    might become more complex when we introduce more scoring options such as
    optimization problems) by encoding sufficient information within the
    existing rank cache.
    
    The information is encoded into a sort key that is designed to be sorted
    descendingly. The sort key is a tuple (serialized as string into the
    database) of fixed precision decimals. Each tuple uses 9 decimals and is
    left padded with `0`s, enabling sorting via standard database query
    operations. For ascendingly sorted tuple entries (e.g. penalty time),
    values are subtracted from a very large constant - this ensures that
    that the key can be sorted as a whole.
    
    For example, with ICPC scoring, the top 3 teams from NWERC 2024 receive
    these sort keys:
    ```
    00000000000000000000013.000000000,99999999999999999998725.000000000,99999999999999999999711.000000000
    00000000000000000000011.000000000,99999999999999999998918.000000000,99999999999999999999701.000000000
    00000000000000000000011.000000000,99999999999999999998916.000000000,99999999999999999999706.000000000
    ```
    
    This mechanism should facilitate the implementation of other planned
    scoring methods, particularly partial scoring and optimization problems,
    with reasonable complexity in the business logic.
    
    We do use `bcmath` with fixed precision to avoid numerical precision
    issues caused by the order of floating point operations.  While not
    critical currently, this will be essential when handling non-integers,
    such as those in optimization problems.
    
    The new mechanism also caches some computations and thus improves
    scoreboard computation efficiency.

--- webapp/src/Utils/Scoreboard/TeamScore.php
+++ webapp/src/Utils/Scoreboard/TeamScore.php
@@ -2,20 +2,39 @@
 
 namespace App\Utils\Scoreboard;
 
+use App\Entity\RankCache;
 use App\Entity\Team;
 
 class TeamScore
 {
     public int $numPoints = 0;
 
-    /** @var float[] */
-    public array $solveTimes = [];
     public int $rank = 0;
     public int $totalTime;
     public int $totalRuntime = 0;
 
-    public function __construct(public Team $team)
+    public function __construct(public Team $team, public ?RankCache $rankCache, bool $restricted)
     {
         $this->totalTime = $team->getPenalty();
+        if ($this->rankCache) {
+            if ($restricted) {
+                $this->numPoints = $rankCache->getPointsRestricted();
+                $this->totalTime += $rankCache->getTotaltimeRestricted();
+                $this->totalRuntime = $rankCache->getTotalruntimeRestricted();
+            } else {
+                $this->numPoints = $rankCache->getPointsPublic();
+                $this->totalTime += $rankCache->getTotaltimePublic();
+                $this->totalRuntime = $rankCache->getTotalruntimePublic();
+            }
+        }
+    }
+
+    public function getSortKey(bool $restricted): string
+    {
+        if ($this->rankCache === null) {
+            // Sorts teams without a rank last.
+            return '.';
+        }
+        return $restricted ? $this->rankCache->getSortKeyRestricted() : $this->rankCache->getSortKeyPublic();
     }
 }
