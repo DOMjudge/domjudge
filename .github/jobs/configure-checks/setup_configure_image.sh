5f873f866dcd3228476db37065ab03633afe5f9d
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-03-09 18:51:49 +0100

    Test with other linux distros with new tests
    We'll need to start with multiple clean images in future tests so we now
    skip certain tests to keep a "clean" image for certain tests and test in
    other matrix jobs.
    
    The intent was to both extend the tests and test with different
    distrobutions:
    - Alpine fails on running bats,
    - Fedora has issues with building `runpipe` so fails on `make judgehost`
    - Arch needs another package manager to install certain libraries (so is
      left for another PR).
    - OpenSUSE lacks some libraries.
    
    The pre-work is already done to start using those images.
    
    make clean should remove what was created by make all
    make distclean should remove what was created by configure (plus what
    make clean does)

--- .github/jobs/configure-checks/setup_configure_image.sh
+++ .github/jobs/configure-checks/setup_configure_image.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+. .github/jobs/ci_settings.sh
+
 set -euxo pipefail
 
-distro_id=$(grep "^ID=" /etc/os-release)
+ghtest="$1"
+export ghtest
+
+section_start "Install all tools"
+distro_id="$(grep "^ID=" /etc/os-release)"
 
 # Install everything for configure and testing
 case $distro_id in
+    "ID=arch")
+        pacman -Sy --noconfirm bats ;;
+    "ID=alpine")
+        apk add bash make bats tar ;;
     "ID=fedora")
         dnf install make bats util-linux -y ;;
+    'ID="opensuse-leap"')
+        zypper install -y make bats ;;
     *)
         apt-get update; apt-get full-upgrade -y
         apt-get install make bats -y ;;
@@ -15,6 +27,14 @@ esac
 
 # Install extra assert statements for bots
 cp submit/assert.bash .github/jobs/configure-checks/
+section_end
 
 # Run the configure tests for this usecase
-test_path="/__w/domjudge/domjudge/release" bats .github/jobs/configure-checks/all.bats
+test_path="/__w/domjudge/domjudge/release"
+export test_path
+
+if [ "${ghtest:?}" = "distclean" ]; then
+    .github/jobs/configure-checks/distclean.bats
+else
+    .github/jobs/configure-checks/all.bats
+fi
