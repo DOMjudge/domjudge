32bc15bf708c4b42529f6e82a802fd668c3da1cc
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-03-09 16:43:52 +0100

    Build example test release for configure/make tests
    Bats testing now works from another directory

--- .github/jobs/configure-checks/setup_configure_image.sh
+++ .github/jobs/configure-checks/setup_configure_image.sh
@@ -7,17 +7,14 @@ distro_id=$(grep "^ID=" /etc/os-release)
 # Install everything for configure and testing
 case $distro_id in
     "ID=fedora")
-        dnf install pkg-config make bats autoconf automake util-linux -y ;;
+        dnf install make bats util-linux -y ;;
     *)
         apt-get update; apt-get full-upgrade -y
-        apt-get install pkg-config make bats autoconf -y ;;
+        apt-get install make bats -y ;;
 esac
-
-# Build the configure file
-make configure
 
 # Install extra assert statements for bots
 cp submit/assert.bash .github/jobs/configure-checks/
 
 # Run the configure tests for this usecase
-test_path="/__w/domjudge/domjudge" bats .github/jobs/configure-checks/all.bats
+test_path="/__w/domjudge/domjudge/release" bats .github/jobs/configure-checks/all.bats
