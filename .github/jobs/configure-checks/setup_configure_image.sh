5c09805589ea68337a9fa8e976aecc4092424e85
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2025-03-10 07:45:56 +0100

    Update setup_configure_image.sh

--- .github/jobs/configure-checks/setup_configure_image.sh
+++ .github/jobs/configure-checks/setup_configure_image.sh
@@ -7,18 +7,15 @@ distro_id=$(grep "^ID=" /etc/os-release)
 # Install everything for configure and testing
 case $distro_id in
     "ID=arch")
-        pacman -Sy --noconfirm make bats autoconf automake tar ;;
+        pacman -Sy --noconfirm make bats tar ;;
     "ID=fedora")
-        dnf install make autoconf automake bats -y ;;
+        dnf install make bats -y ;;
     'ID="opensuse-leap"')
-        zypper install -y  make bats autoconf automake ;;
+        zypper install -y make bats ;;
     *)
         apt-get update; apt-get full-upgrade -y
-        apt-get install make bats autoconf -y ;;
+        apt-get install make bats -y ;;
 esac
-
-# Build the configure file
-make configure
 
 # Install extra assert statements for bots
 cp submit/assert.bash .github/jobs/configure-checks/
