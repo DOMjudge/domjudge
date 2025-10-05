250e05142c6f45ac38438549bc32c9cbfcca29db
Nicky Gerritsen <github@accounts.nickygerritsen.nl>
2025-10-05 20:36:01 +0200

    Set types correctly during migration

--- webapp/migrations/Version20250801124024.php
+++ webapp/migrations/Version20250801124024.php
@@ -21,20 +21,24 @@ final class Version20250801124024 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<SQL
-            ALTER TABLE team_category 
+            ALTER TABLE team_category
             ADD types INT NOT NULL DEFAULT 1 COMMENT 'Bitmask of category types, default is scoring.' AFTER name,
             CHANGE sortorder sortorder TINYINT UNSIGNED DEFAULT NULL COMMENT 'Where to sort this category on the scoreboard',
             ADD css_class VARCHAR(255) DEFAULT NULL COMMENT 'CSS class to apply to scoreboard rows (only for TYPE_CSS_CLASS)' AFTER allow_self_registration
             SQL);
+
+        // Now update the types for existing categories based on whether the color is set
+        $this->addSql('UPDATE team_category SET types = 7 WHERE color IS NOT NULL');
+        $this->addSql('UPDATE team_category SET types = 5 WHERE color IS NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<SQL
-            ALTER TABLE team_category 
-            DROP types, 
-            DROP css_class, 
+            ALTER TABLE team_category
+            DROP types,
+            DROP css_class,
             CHANGE sortorder sortorder TINYINT UNSIGNED DEFAULT 0 NOT NULL COMMENT 'Where to sort this category on the scoreboard'
             SQL);
     }
