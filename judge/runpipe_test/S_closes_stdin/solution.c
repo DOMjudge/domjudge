8d395f0abf79ef8f75263624f1368d06e9d7d730
Tobias Werth <werth@cs.fau.de>
2025-10-12 16:56:24 +0200

    Fix compile error in one of the runpipe tests with some gcc versions.

--- judge/runpipe_test/S_closes_stdin/solution.c
+++ judge/runpipe_test/S_closes_stdin/solution.c
@@ -1,7 +1,14 @@
+#define _XOPEN_SOURCE 700
+
 #include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 
 int main() {
   fclose(stdin);
   printf("42\n");
-  usleep(800000);
+
+  struct timespec req = {0};
+  req.tv_nsec = 800000000L; // 0.8 seconds
+  nanosleep(&req, NULL);
 }
