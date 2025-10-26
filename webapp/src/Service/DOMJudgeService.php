962a3502ecb7947cf96b6654896051db988e68ff
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-10-26 22:50:07 +0100

    Handle the case with missing file for symlink

--- webapp/src/Service/DOMJudgeService.php
+++ webapp/src/Service/DOMJudgeService.php
@@ -1568,6 +1568,9 @@ class DOMJudgeService
         $publicPath = realpath(sprintf('%s/public/', $this->projectDir));
         foreach ($assetMatches[1] as $file) {
             $filepath = realpath($publicPath . '/' . $file);
+            if ($filepath === false) {
+                throw new BadRequestHttpException("Could not find (symlinked) file: " . $file);
+            }
             if (!str_starts_with($filepath, $publicPath) &&
                 !str_starts_with($filepath, $this->vendorDir)
             ) {
