808748ce262463b6332dd3a55386e29ef7443127
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-10-26 19:53:07 +0100

    Manage JS frontend libraries with yarn
    Either Composer managed those before,
    or those were checked into git.
    
    Now we track those with `yarn`:
    - Coloris
    - JQuery debounce
    - Monaco editor
    - Bootstrap-toggle
    - File-saver
    - d3
    - nvd3
    - Select2
    
    D3 has a newer release but we can't upgrade because of the nvd3 which needs
    this specific Major version.

--- .github/jobs/baseinstall.sh
+++ .github/jobs/baseinstall.sh
@@ -28,6 +28,13 @@ composer install --no-scripts |tee "$ARTIFACTS"/composer_out.txt
 cd ..
 section_end
 
+section_start "Install JS frontend dependencies"
+cd webapp
+apt-get update; apt-get install -y yarnpkg
+yarnpkg install
+cd ..
+section_end
+
 section_start "Set simple admin password"
 echo "password" > ./etc/initial_admin_password.secret
 echo "default login admin password password" > ~/.netrc
