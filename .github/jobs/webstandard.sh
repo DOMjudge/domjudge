b6891fdcc2a45c9628dc382e6a90be3c9fa7fc45
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2025-11-05 10:52:03 +0100

    Update webstandard.sh

--- .github/jobs/webstandard.sh
+++ .github/jobs/webstandard.sh
@@ -117,7 +117,7 @@ if [ "$TEST" = "w3cval" ]; then
 
     section_start "Install testsuite"
     cd "$DIR"
-    wget https://github.com/validator/validator/archive/refs/tags/latest.zip
+    wget https://github.com/validator/validator/archive/refs/tags/latest.zip -O vnu.linux.zip
     unzip -q vnu.linux.zip
     # Remove a warning by creating an empty config.
     touch vnu.properties
