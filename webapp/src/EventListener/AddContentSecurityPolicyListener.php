0bb2923b57a2cd875b8aec8b39005f27123e8994
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-11-09 09:58:06 +0100

    Fix Monaco-editor font rendering
    Fixes a CSP violation as monaco-editor loads a font over data:.

--- webapp/src/EventListener/AddContentSecurityPolicyListener.php
+++ webapp/src/EventListener/AddContentSecurityPolicyListener.php
@@ -17,7 +17,7 @@ class AddContentSecurityPolicyListener
         // the profiler requires 'unsafe-eval' for script-src 'self'.
         $response = $event->getResponse();
         $cspExtra = $this->profiler ? "'unsafe-eval'" : "";
-        $csp = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' $cspExtra; img-src 'self' data:; worker-src 'self' blob:";
+        $csp = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' $cspExtra; img-src 'self' data:; worker-src 'self' blob:; font-src 'self' data:;";
         $response->headers->set('Content-Security-Policy', $csp);
     }
 }
