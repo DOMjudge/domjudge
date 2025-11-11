e178902d0b17cbd3b2dd243b42939177e8ffa370
Nicky Gerritsen <github@accounts.nickygerritsen.nl>
2025-11-11 22:39:49 +0100

    Change jury team affiliation and category pages to use external IDs instead of internal IDs
    Part of #3024

--- webapp/src/Entity/TeamCategory.php
+++ webapp/src/Entity/TeamCategory.php
@@ -2,6 +2,7 @@
 namespace App\Entity;
 
 use App\Controller\API\AbstractRestController as ARC;
+use App\Repository\TeamCategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -15,7 +16,7 @@ use Symfony\Component\Validator\Context\ExecutionContextInterface;
 /**
  * Categories for teams (e.g.: participants, observers, ...).
  */
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: TeamCategoryRepository::class)]
 #[ORM\Table(options: [
     'collation' => 'utf8mb4_unicode_ci',
     'charset' => 'utf8mb4',
