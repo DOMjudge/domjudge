4e6644141dbc6d68799471fe81760872e2676b23
Tobias Werth <werth@cs.fau.de>
2025-11-30 09:45:43 +0100

    Upload bats logs

--- .github/jobs/configure-checks/setup_configure_image.sh
+++ .github/jobs/configure-checks/setup_configure_image.sh
@@ -22,4 +22,5 @@ make configure
 cp submit/assert.bash .github/jobs/configure-checks/
 
 # Run the configure tests for this usecase
-test_path="/__w/domjudge/domjudge" bats .github/jobs/configure-checks/all.bats
+mkdir /tmp/bats_logs
+test_path="/__w/domjudge/domjudge" bats --print-output-on-failure --gather-test-outputs-in /tmp/bats_logs .github/jobs/configure-checks/all.bats
