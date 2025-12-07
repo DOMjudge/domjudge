eeb53d605c136419124a70fb33c13f1d3aa93796
Tobias Werth <werth@cs.fau.de>
2025-12-07 16:24:28 +0100

    Fold `testcase_run.sh` logic into judgedaemon.
    The primary motivation is to significantly reduce the per-testcase
    overhead of judging a submission.
    
    Historically, with fewer test cases, the separation was carrying its
    weight. However, with modern problems requiring many test cases or
    passes, the overhead from dozens of forked simple programs (`cp`, `mv`,
    `chmod`, `grep` etc.) within the shell script has become a major
    performance bottleneck. Moving them into PHP effectively replaces them
    with simple system calls.
    
    This change results in a substantial performance improvement:
    * Roughly a 50% speed-up in total judging overhead per submission
    * Example: A simple C++ solution to a multi-pass problem (NWERC 2025
      practice) was reduced from ~70s to ~33s end-to-end.

--- example_problems/hello/submissions/time_limit_exceeded/stress-test-fork-setsid.c
+++ example_problems/hello/submissions/time_limit_exceeded/stress-test-fork-setsid.c
@@ -3,7 +3,7 @@
  *
  * Without cgroups however, this will crash the judging daemon: it
  * forks processes and places these in a new session, such that
- * testcase_run cannot retrace and kill these. They are left running
+ * the judgedaemon cannot retrace and kill these. They are left running
  * and should be killed before restarting the judging daemon. The
  * cgroups code can detect this because the processes will belong to the
  * same cgroup.
