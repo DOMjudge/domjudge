63025a0b5e958c27233417c6bf075fd416d72edb
Nicky Gerritsen <github@accounts.nickygerritsen.nl>
2026-01-10 14:11:20 +0100

    Import PHP reflection class

--- webapp/tests/Unit/BaseTestCase.php
+++ webapp/tests/Unit/BaseTestCase.php
@@ -13,6 +13,7 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\Loader;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use ReflectionClass;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DomCrawler\Crawler;
@@ -69,7 +70,7 @@ abstract class BaseTestCase extends WebTestCase
         // Use the Symfony fixture loader so the fixtures can be autowired as a service
         $loader = static::getContainer()->get('doctrine.fixtures.loader');
         // Use reflection on the base class to clear the enabled fixtures, so we can only add what we want
-        $loaderReflection = new \ReflectionClass(Loader::class);
+        $loaderReflection = new ReflectionClass(Loader::class);
         $loaderReflection->getProperty('fixtures')->setValue($loader, []);
         foreach ($fixtures as $fixture) {
             if (!is_subclass_of($fixture, FixtureInterface::class)) {
