af0c08180e04b48afd3e000dfc3a14c66de0f054
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2026-02-18 22:31:43 +0100

    Try to set all array parameters

--- judge/judgedaemon.main.php
+++ judge/judgedaemon.main.php
@@ -2404,6 +2404,13 @@ class JudgeDaemon
 
     /**
      * @param JudgeTask $judgeTask
+     * @param array{
+     *          runresult: string, start_time: string, end_time: string,
+     *          runtime: string, output_run: string, output_error: string,
+     *          output_system: string, metadata: string, output_diff: string,
+     *          hostname: string, testcasedir: string, compare_metadata: string,
+     *          team_message?: string, score?: int
+     *        } $new_judging_run
      */
     private function reportJudgingRun(array $judgeTask, array $new_judging_run, bool $asynchronous): ?string
     {
