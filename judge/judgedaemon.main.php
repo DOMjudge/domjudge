69e990eb240a3dc07d2350cced1933ee452c67cc
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2026-02-20 16:45:19 +0100

    Steer PHPstan
    Initializing those is not that slow and takes as much work as doing the PHPstan annotation.

--- judge/judgedaemon.main.php
+++ judge/judgedaemon.main.php
@@ -2264,6 +2264,19 @@ class JudgeDaemon
         $input = $tcfile['input'];
         $output = $tcfile['output'];
         $passLimit = $run_config['pass_limit'] ?? 1;
+        // All of those are to help PHPStan, it seems to not see that they are defined in the loop
+        // and the loop always runs as tpassLimit >= $passCnt.
+        $score = "";
+        $nextPass = false;
+        $result = Verdict::INTERNAL_ERROR;
+        $runtime = null;
+        $keys = [
+            'runresult', 'start_time', 'end_time', 'runtime', 'output_run',
+            'output_error', 'output_system', 'metadata', 'output_diff',
+            'hostname', 'testcasedir', 'compare_metadata'
+        ];
+        $new_judging_run = array_fill_keys($keys, '');
+
         for ($passCnt = 1; $passCnt <= $passLimit; $passCnt++) {
             $nextPass = false;
             if ($passLimit > 1) {
