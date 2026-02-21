5fcecf17e41db76ee0b77a4e4eba118151c21f4b
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2026-02-21 16:12:18 +0100

    This is always a string

--- judge/judgedaemon.main.php
+++ judge/judgedaemon.main.php
@@ -2480,7 +2480,7 @@ class JudgeDaemon
      *          runtime: string, output_run: string, output_error: string,
      *          output_system: string, metadata: string, output_diff: string,
      *          hostname: string, testcasedir: string, compare_metadata: string,
-     *          team_message?: string, score?: int
+     *          team_message?: string, score?: string
      *        } $new_judging_run
      */
     private function reportJudgingRun(array $judgeTask, array $new_judging_run, bool $asynchronous): ?string
