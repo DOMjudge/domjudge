DOMjudge/domjudge

commit 8171b16a91717193e6bd2e779b1d5331327d3f97

Fix various PHPstan issues
PHPStan complained that the sig_handler() function didn't have the $siginfo type specified.
See: https://www.php.net/manual/en/function.pcntl-signal.php for why this has to be mixed.

Binary operation "*" between string and 1000 results in an error.
------ -------------------------------------------------------------------
 Line   lib/lib.error.php
------ -------------------------------------------------------------------
 51     Binary operation "*" between string and 1000 results in an error.
        🪪  binaryOp.invalid
------ -------------------------------------------------------------------

A php constant and JSON encoding & decoding can accept any valid object
Which could be as simple as a basetype and as complicated as an array.

preg_match returns array|null so PHPstan complained about the shape
------ ----------------------------------------------------------------------------------------------------------------------
 Line   lib/lib.misc.php
------ ----------------------------------------------------------------------------------------------------------------------
 19     PHPDoc tag @var with type array<string> is not subtype of native type list<array{string, int<0, max>}|string>|false.
        🪪  varTag.nativeType

lib/lib.error.php
@@ -48,7 +48,7 @@ function logmsg(int $msglevel, string $string): void
     // Trim $string to reasonable length
     $string = substr($string, 0, 10000);
 
-    $msec = sprintf("%03d", (int)(explode(' ', microtime())[0]*1000));
+    $msec = sprintf("%03d", (int)(explode(' ', microtime()))[0]*1000);
     $stamp = "[" . date('M d H:i:s') . ".$msec] " . SCRIPT_ID .
         (function_exists('posix_getpid') ? "[" . posix_getpid() . "]" : "") .
         ": ";

etc/judgehost-config.php
@@ -15,7 +15,7 @@ define('CREATE_WRITABLE_TEMP_DIR', getenv('DOMJUDGE_CREATE_WRITABLE_TEMP_DIR') ?
 // If any transient network error occurs on the nth trial,
 // the judgehost retries the HTTP request after pow(factor, trial - 1) + rand(0, jitter) sec.
 
-function define_backoff_params_from_env(string $var_name, $default_value)
+function define_backoff_params_from_env(string $var_name, mixed $default_value): void
 {
     if (defined($var_name)) {
         return;

lib/lib.misc.php
@@ -15,7 +15,6 @@ require_once('lib.wrappers.php');
  */
 function overshoot_time(float $timelimit, string $overshoot_cfg) : float
 {
-    /** @var string[] $tokens */
     $tokens = preg_split('/([+&|])/', $overshoot_cfg, -1, PREG_SPLIT_DELIM_CAPTURE);
     if ($tokens === false) {
         error("failed parsing/splitting overshoot string '$overshoot_cfg'");
@@ -77,7 +76,7 @@ function alert(string $msgtype, string $description = ''): void
  * Functions to support (graceful) shutdown of daemons upon receiving a
  * signal.
  */
-function sig_handler(int $signal, $siginfo = null): void
+function sig_handler(int $signal, mixed $siginfo = null): void
 {
     global $exitsignalled, $gracefulexitsignalled;
 

lib/lib.wrappers.php
@@ -26,7 +26,7 @@ function dj_json_try_decode(string $str): mixed
 /**
  * Encode data to JSON with our preferred settings.
  */
-function dj_json_encode($data) : string
+function dj_json_encode(mixed $data) : string
 {
     return json_encode($data, JSON_PRESERVE_ZERO_FRACTION | JSON_UNESCAPED_SLASHES | JSON_THROW_ON_ERROR);
 }

commit 4043d9ca6236ac20bbb6f879656f607297421229

Fix judgedaemon time logging.
Commit 8171b16a9 broke things, after which times were logged at second
granularity, e.g.

```
$ judge/judgedaemon -n 0
[Mar 14 21:35:55.000] judgedaemon[1276234]: Judge started on goo-0 [DOMjudge/10.0.0DEV/df498521d]
[Mar 14 21:35:55.000] judgedaemon[1276234]: 🔏 Executing chroot script: 'chroot-startstop.sh check'
[Mar 14 21:35:55.000] judgedaemon[1276234]: Registering judgehost on endpoint default: http://localhost/domjudge/api
[Mar 14 21:35:56.000] judgedaemon[1276234]: ⇝ Received 7 'judging_run' judge tasks (endpoint default)
[Mar 14 21:35:56.000] judgedaemon[1276234]:   Working directory: /home/sitowert/domjudge/output/judgings/goo-0/endpoint-default/1/1
[Mar 14 21:35:56.000] judgedaemon[1276234]:   🔒 Executing chroot script: 'chroot-startstop.sh start'
[Mar 14 21:35:56.000] judgedaemon[1276234]:   📋 Verifying versions.
[Mar 14 21:35:56.000] judgedaemon[1276234]:   💾 Fetching new executable 'compile/20' with hash '4e301e4bc46ab73673e209ee3437707d'.
[Mar 14 21:35:57.000] judgedaemon[1276234]:   💻 Compilation: (addone-medium-only.py) 'correct'
[Mar 14 21:35:57.000] judgedaemon[1276234]:   🏃 Running testcase 1...
...
```

After this fix, we are back to normal:
```
judge/judgedaemon -n 0
[Mar 14 21:38:19.244] judgedaemon[1291523]: Judge started on goo-0 [DOMjudge/10.0.0DEV/df498521d]
[Mar 14 21:38:19.244] judgedaemon[1291523]: 🔏 Executing chroot script: 'chroot-startstop.sh check'
[Mar 14 21:38:19.259] judgedaemon[1291523]: Registering judgehost on endpoint default: http://localhost/domjudge/api
[Mar 14 21:38:19.487] judgedaemon[1291523]: No submissions in queue (for endpoint default), waiting...
```

lib/lib.error.php
@@ -48,7 +48,7 @@ function logmsg(int $msglevel, string $string): void
     // Trim $string to reasonable length
     $string = substr($string, 0, 10000);
 
-    $msec = sprintf("%03d", (int)(explode(' ', microtime()))[0]*1000);
+    $msec = sprintf("%03d", (float)(explode(' ', microtime()))[0]*1000);
     $stamp = "[" . date('M d H:i:s') . ".$msec] " . SCRIPT_ID .
         (function_exists('posix_getpid') ? "[" . posix_getpid() . "]" : "") .
         ": ";