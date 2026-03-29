33ae72a6273e2d9392d811e23eb581fa6b34c595
Hiroki Sato <hsugar.310@gmail.com>
2026-03-29 18:10:43 +0900

    Make mitration Version20250309122806 work with MySQL
    In MySQL, a default value can be assigned only if the value is written
    as an expression, even if the expression value is a literal.
    
    (cherry picked from commit 61c0f166115937fb87fa6fd01e6c30451bcd44b8)

--- webapp/migrations/Version20250309122806.php
+++ webapp/migrations/Version20250309122806.php
@@ -20,7 +20,7 @@ final class Version20250309122806 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE rankcache ADD sort_key_public TEXT DEFAULT \'\' NOT NULL COMMENT \'Opaque sort key for public audience.\', ADD sort_key_restricted TEXT DEFAULT \'\' NOT NULL COMMENT \'Opaque sort key for restricted audience.\'');
+        $this->addSql('ALTER TABLE rankcache ADD sort_key_public TEXT DEFAULT (\'\') NOT NULL COMMENT \'Opaque sort key for public audience.\', ADD sort_key_restricted TEXT DEFAULT (\'\') NOT NULL COMMENT \'Opaque sort key for restricted audience.\'');
         $this->addSql('CREATE INDEX sortKeyPublic ON rankcache (sort_key_public(768))');
         $this->addSql('CREATE INDEX sortKeyRestricted ON rankcache (sort_key_restricted(768))');
     }
