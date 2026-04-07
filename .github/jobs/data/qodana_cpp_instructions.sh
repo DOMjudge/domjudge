557f676f3d9d5a0a0c1a5107a1ccf60d66a2aa6a
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2026-04-07 20:22:36 +0200

    Try with different C++ instructions

--- .github/jobs/data/qodana_cpp_instructions.sh
+++ .github/jobs/data/qodana_cpp_instructions.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+apt-get update -y
+apt-get install -y --no-install-recommends bear libc++-19-dev libc++abi-19-dev
+autoreconf -fi
+#bear -- make || true
+
 make configure
 CPP="/usr/bin/clang-19 -E"
 CC=/usr/bin/clang-19
-CXX=/usr/bin/clang++-19
+CXX="/usr/bin/clang++-19 -stdlib=libc++"
 export CC
 export CPP
 export CXX
