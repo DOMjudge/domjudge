e806e5bde7e19196b9afb9b9cf2a5f97ff302d95
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2026-06-07 10:38:04 +0200

    Use testdata.yml output_validator options in example problems

--- example_problems/hangman/output_validator/validator.py
+++ example_problems/hangman/output_validator/validator.py
@@ -3,11 +3,34 @@
 # <output_validator_program> input_file answer_file feedback_dir [additional_arguments] < team_output [ > team_input ]
 
 import _io
+import argparse
 import os
 import random
 import string
 import sys
 import tempfile
+
+
+def write_message(message: str, singular = False) -> None:
+    with open(os.path.join(feedback_dir, 'judgemessage.txt'), 'a') as judgemessage:
+        print("Team" + message, file=judgemessage)
+    if not arguments.secret:
+        teammessage_file = os.path.join(feedback_dir, 'teammessage.txt')
+        # Explicit overwrite the earlier hint
+        with open(teammessage_file, 'w') as teammessage:
+            print(("You" if singular else "Your") + message, file=teammessage)
+
+
+def write_generic_message(message: str, feedback_file: str) -> None:
+    message_file_path = os.path.join(feedback_dir, feedback_file)
+    try:
+        with open(message_file_path, 'r+') as message_file:
+            if message not in message_file.read():
+                print(message, file=message_file)
+    except FileNotFoundError:
+        # Overwrite in case the file got created in between
+        with open(message_file_path, 'w') as message_file:
+            print(message, file=message_file)
 
 
 def strip_newline(raw: _io.TextIOWrapper) -> list[str]:
@@ -43,6 +66,16 @@ def check_writable(path: str) -> bool:
         return False
 
 
+parser = argparse.ArgumentParser(
+    prog='Hangman output validator',
+    description='Example output validator for DOMjudge Hangmang multi-pass problem',
+    epilog='This output validator is crafted in such a way that multiple facets can be tested, most of the options are not needed for a normal problem')
+parser.add_argument('--judgemessage')
+parser.add_argument('--teammessage')
+parser.add_argument('--secret', action='store_true')
+
+arguments = parser.parse_args(sys.argv[4:])
+
 input_file = sys.argv[1]
 answer_file = sys.argv[2]
 feedback_dir = sys.argv[3]
@@ -62,19 +95,16 @@ INPUT_FILE_ERROR = 54
 DIRECTORY_ERROR = 55
 
 if len(team_output) != 1:
-    with open(os.path.join(feedback_dir, 'judgemessage.txt'), 'a') as judgemessage:
-        print("Team output must only contain 1 line.", file=judgemessage)
+    write_message(" output must only contain 1 line.")
     exit(WA)
 else:
     team_output = team_output[0]
 
 if len(team_output) != 1:
-    with open(os.path.join(feedback_dir, 'judgemessage.txt'), 'a') as judgemessage:
-        print("Team output must only contain 1 symbol.", file=judgemessage)
+    write_message(" output must only contain 1 symbol.")
     exit(WA)
 if team_output[0] not in string.ascii_lowercase:
-    with open(os.path.join(feedback_dir, 'judgemessage.txt'), 'a') as judgemessage:
-        print("Team output must only contain 1 lowercase letter.", file=judgemessage)
+    write_message(" output must only contain 1 lowercase letter ([a-z]{1}).")
     exit(WA)
 
 inp = check_readable(input_file, 0)
@@ -105,8 +135,7 @@ guessed_total = inp[0]
 guessed_progress = inp[1]
 
 if team_output in guessed_total:
-    with open(os.path.join(feedback_dir, 'judgemessage.txt'), 'a') as judgemessage:
-        print("Team guessed the same letter before.", file=judgemessage)
+    write_message(" guessed the same letter before.", singular=True)
     exit(WA)
 
 new = ''
@@ -127,4 +156,9 @@ guessed_total = ''.join(guessed_total)
 with open(os.path.join(feedback_dir, 'nextpass.in'), 'w') as nextpass:
     print(guessed_total, file=nextpass)
     print(new, file=nextpass)
+
+if arguments.judgemessage:
+    write_generic_message(arguments.judgemessage, 'judgemessage.txt')
+if arguments.teammessage and not arguments.secret:
+    write_generic_message(arguments.teammessage, 'teammessage.txt')
 exit(AC)
